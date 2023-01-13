5833933c787fd8b732e28af3d99834c5ab866292
man0s <95379755+losman0s@users.noreply.github.com>
2023-01-13 20:56:14 +0100

    tailwind prod fix 2

--- apps/marginfi-v2-ui/tailwind.config.js
+++ apps/marginfi-v2-ui/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/context/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       backgroundImage: {
