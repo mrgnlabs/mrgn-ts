ffa6f63c60ee8e838e91160fcbc4d86375acb215
man0s <95379755+losman0s@users.noreply.github.com>
2023-01-13 20:54:22 +0100

    tailwind prod fix

--- apps/marginfi-v2-ui/tailwind.config.js
+++ apps/marginfi-v2-ui/tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      } 
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [],
-}
+};
