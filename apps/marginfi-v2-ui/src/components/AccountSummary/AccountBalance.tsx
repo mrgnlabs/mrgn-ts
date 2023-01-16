6ced5dbaa006292980db4b76eec56a35bed7dfd3
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-01-16 12:52:04 -0500

    fixing account summary look a bit

--- apps/marginfi-v2-ui/src/components/AccountSummary/AccountBalance.tsx
+++ apps/marginfi-v2-ui/src/components/AccountSummary/AccountBalance.tsx
@@ -11,7 +11,9 @@ const AccountBalance: FC<AccountBalanceProps> = ({
   isConnected,
 }) => {
   return (
-    <div className="min-w-[39%] bg-[#131619] rounded-xl h-full flex flex-col justify-evenly items-start px-[4%] py-1 h-[112px]">
+    <div
+      className="min-w-[39%] bg-[#131619] rounded-xl h-full flex flex-col justify-evenly items-start px-[4%] pl-1 py-1 h-[112px]"
+    >
       <div className="text-lg text-[#868E95]">Account balance</div>
       <div className="text-5xl">
         {isConnected ? usdFormatter.format(accountBalance) : "-"}
