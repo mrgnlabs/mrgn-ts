be1d0cf1348688022dfd5a8a04df873ee485bc8e
man0s <95379755+losman0s@users.noreply.github.com>
2023-01-28 22:27:54 +0800

    fix(mfi-fe): user position headers showing up alone when position amount is dust

--- apps/marginfi-v2-ui/src/components/UserPositions/UserPositions.tsx
+++ apps/marginfi-v2-ui/src/components/UserPositions/UserPositions.tsx
@@ -6,39 +6,41 @@ import { roundToDecimalPlace } from "~/utils";
 
 const UserPositions: FC = () => {
   const { accountSummary, selectedAccount, refreshData } = useBorrowLendState();
-  const { isLending, isBorrowing } = useMemo(
+  const { lendPositions, borrowPositions } = useMemo(
     () => ({
-      isLending: accountSummary.lendingAmount > 0,
-      isBorrowing: accountSummary.borrowingAmount > 0,
+      lendPositions: accountSummary.positions.filter(
+        (p) => p.isLending && roundToDecimalPlace(p.amount, p.bank.mintDecimals) > 0
+      ),
+      borrowPositions: accountSummary.positions.filter(
+        (p) => !p.isLending && roundToDecimalPlace(p.amount, p.bank.mintDecimals) > 0
+      ),
     }),
     [accountSummary]
   );
 
   return (
     <>
-      {isLending && selectedAccount && (
+      {lendPositions.length > 0 && selectedAccount && (
         <Card elevation={0} className="bg-transparent w-full p-0 grid">
           <div className="font-aeonik font-normal text-2xl my-8 text-white pl-1">Lending</div>
           <TableContainer>
             <Table className="w-full table-fixed">
               <TableBody className="w-full flex flex-col gap-4">
-                {accountSummary.positions
-                  .filter((p) => p.isLending && roundToDecimalPlace(p.amount, p.bank.mintDecimals) > 0)
-                  .map((position, index) => (
-                    <UserPositionRow
-                      key={index}
-                      position={position}
-                      marginfiAccount={selectedAccount}
-                      refreshBorrowLendState={refreshData}
-                    />
-                  ))}
+                {lendPositions.map((position, index) => (
+                  <UserPositionRow
+                    key={index}
+                    position={position}
+                    marginfiAccount={selectedAccount}
+                    refreshBorrowLendState={refreshData}
+                  />
+                ))}
               </TableBody>
             </Table>
           </TableContainer>
         </Card>
       )}
       <div>
-        {isBorrowing && selectedAccount && (
+        {borrowPositions.length > 0 && selectedAccount && (
           <Card elevation={0} className="bg-transparent w-full p-0 grid">
             <div className="text-2xl my-8 text-white pl-1" style={{ fontFamily: "Aeonik Pro", fontWeight: 400 }}>
               Borrowing
@@ -46,16 +48,14 @@ const UserPositions: FC = () => {
             <TableContainer>
               <Table className="table-fixed">
                 <TableBody>
-                  {accountSummary.positions
-                    .filter((p) => !p.isLending && roundToDecimalPlace(p.amount, p.bank.mintDecimals) > 0)
-                    .map((position, index) => (
-                      <UserPositionRow
-                        key={index}
-                        position={position}
-                        marginfiAccount={selectedAccount}
-                        refreshBorrowLendState={refreshData}
-                      />
-                    ))}
+                  {borrowPositions.map((position, index) => (
+                    <UserPositionRow
+                      key={index}
+                      position={position}
+                      marginfiAccount={selectedAccount}
+                      refreshBorrowLendState={refreshData}
+                    />
+                  ))}
                 </TableBody>
               </Table>
             </TableContainer>
