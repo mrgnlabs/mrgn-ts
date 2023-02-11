301711aeb54d25e43727f77f78c512cdf1b9b585
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-02-10 19:06:26 -0500

    updated

--- packages/lip-client/src/archive/accounts/index.ts
+++
@@ -1,4 +0,0 @@
-export { Campaign } from "./Campaign"
-export type { CampaignFields, CampaignJSON } from "./Campaign"
-export { Deposit } from "./Deposit"
-export type { DepositFields, DepositJSON } from "./Deposit"
