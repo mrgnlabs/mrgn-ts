31c86af309e98d6664736d2eb6db29c4dfb8656c
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-02-19 18:07:15 -0500

    hero

--- apps/marginfi-landing-page/src/pages/index.tsx
+++ apps/marginfi-landing-page/src/pages/index.tsx
@@ -1,37 +1,44 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { Button } from "@mui/material";
 
 const Home = () => {
 
   return (
-    <div
-      className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl"
-      // style={{
-      //   border: 'solid red 1px',
-      // }}
-    >
-      <div className="mb-6 max-w-7xl">
-        Connecting liquidity
+    <>
+      <div
+        className="fixed top-[-10vw] right-[-25vw] md:top-[-25vw] md:right-[-25vw] w-[100vw] h-[67vw]"
+        // style={{
+        //   border: 'solid red 1px',
+        // }}
+      >
+        <Image src="/hero.jpg" alt="marginfi logo" fill />
       </div>
-      <div className="mb-6 max-w-7xl">
-        across DeFi
+      
+      <div className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
+        <div className="mb-6 max-w-7xl">
+          Connecting liquidity
+        </div>
+        <div className="mb-6 max-w-7xl">
+          across DeFi
+        </div>
+        <Link href={"https://app.marginfi.com"}>
+          <Button
+            className="h-full w-[200px] min-w-fit text-xl flex justify-center items-center font-light normal-case rounded-[100px] h-12"
+            variant="text"
+            style={{
+              backgroundColor: "#DCE85D",
+              color: "#000",
+              fontFamily: "Aeonik Pro",
+              fontWeight: 700,
+            }}
+          >
+            Launch App
+          </Button>
+        </Link>
       </div>
-      <Link href={"https://app.marginfi.com"}>
-        <Button
-          className="h-full w-[200px] min-w-fit text-xl flex justify-center items-center font-light normal-case rounded-[100px] h-12"
-          variant="text"
-          style={{
-            backgroundColor: "#DCE85D",
-            color: "#000",
-            fontFamily: "Aeonik Pro",
-            fontWeight: 700,
-          }}
-        >
-          Launch App
-        </Button>
-      </Link>
-    </div>
+      </>
   );
 };
 
