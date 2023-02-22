d8657c64cba528078d2b313fd74b4e891caeee12
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-02-21 23:47:12 -0500

    more reasonable

--- apps/marginfi-v2-ui/src/pages/index.tsx
+++ apps/marginfi-v2-ui/src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <>
       <PageHeader />
-      <div className="w-full h-full flex flex-col justify-start content-start py-[64px] grid w-[95%] sm:w-4/5 max-w-7xl gap-4 grid-cols-1 xl:grid-cols-2">
+      <div className="w-full h-full flex flex-col justify-start content-start py-[64px] grid w-4/5 max-w-7xl gap-4 grid-cols-1 xl:grid-cols-2">
         {wallet.connected && userAccounts.length > 1 && <MultipleAccountsFoundWarning />}
         <AccountSummary />
         <AssetsList />
