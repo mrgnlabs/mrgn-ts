9c4b31938b1d06f143a706d648b727ee73509561
Connor Lee <connor@Connors-MacBook-Pro.local>
2023-03-14 07:17:41 -0700

    landing revisions on correct branch

--- apps/marginfi-landing-page/src/components/Footer/Footer.tsx
+++ apps/marginfi-landing-page/src/components/Footer/Footer.tsx
@@ -1,48 +1,55 @@
 import { FC } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { NavbarCenterItem } from "./NavbarCenterItem";
+import { Button } from "@mui/material";
+import styles from "./Footer.module.css";
 
-const Footer: FC = () => (
-  <header>
-    <nav className="fixed w-full bottom-0 h-[64px] z-10">
-      <div
-        className="w-full top-0 flex justify-between items-center h-[64px] text-2xl z-10"
-        style={{
-          padding: "0 15px",
-        }}
-      >
-        <div className="h-full relative flex justify-start items-center z-10">
-          <Link href={"https://app.marginfi.com/"}>
-            <NavbarCenterItem text="Docs" disabled />
-          </Link>
-          <Link href={"https://github.com/mrgnlabs"}>
-            <NavbarCenterItem text="Github" />
-          </Link>
-        </div>
-        <div className="absolute fixed left-0 right-0 flex justify-center items-center w-full h-full invisible lg:visible">
-          <div className="h-full w-[28%] flex min-w-fit max-w-[600px] justify-center items-center">
+// @todo implement second pretty navbar row
+const Footer: FC = () => {
+
+  return (
+    <header>
+      <nav className="fixed w-full bottom-0 h-[64px] z-10">
+        <div
+          className="w-full top-0 flex justify-between items-center h-[64px] text-2xl z-10"
+          style={{
+            padding: "0 15px",
+          }}
+        >
+          <div className="h-full relative flex justify-start items-center z-10">
+            <Link href={"https://docs.marginfi.com"}>
+              <NavbarCenterItem text="Docs"/>
+            </Link>
+            <Link href={"https://analytics.marginfi.com"}>
+              <NavbarCenterItem text="Analytics" />
+            </Link>
+            <Link href={"https://github.com/mrgnlabs"}>
+              <NavbarCenterItem text="Github" />
+            </Link>
+          </div>
+          <div className="absolute fixed left-0 right-0 flex justify-center items-center w-full h-full invisible lg:visible">
+            <div className="h-full w-[28%] flex min-w-fit max-w-[600px]">
             <Link href={"https://app.marginfi.com/"}>
               <NavbarCenterItem text="marginfi is a decentralized lending and borrowing protocol on Solana." />
             </Link>
+            </div>
+          </div>
+          <div className="h-full relative flex justify-start items-center z-10">
+            <Link href={"https://twitter.com/marginfi"}>
+              <NavbarCenterItem text="Twitter" />
+            </Link>
+            <Link href={"https://t.me/mrgncommunity"}>
+              <NavbarCenterItem text="Telegram" />
+            </Link>
+            <Link href={"https://app.marginfi.com/"}>
+              <NavbarCenterItem text="© 2023 MRGN, Inc." disabled/>
+            </Link>
           </div>
         </div>
-        <div className="h-full relative flex justify-start items-center z-10">
-          <Link href={"https://t.me/mrgncommunity"}>
-            <NavbarCenterItem text="Telegram" />
-          </Link>
-          <Link href={"https://twitter.com/marginfi"}>
-            <NavbarCenterItem text="Twitter" />
-          </Link>
-          <Link href={"https://discord.gg/wbKERVhQcs"}>
-            <NavbarCenterItem text="Discord" />
-          </Link>
-          <Link href={"https://app.marginfi.com/"}>
-            <NavbarCenterItem text="© 2023 MRGN, Inc." disabled />
-          </Link>
-        </div>
-      </div>
-    </nav>
-  </header>
-);
+      </nav>
+    </header>
+  );
+};
 
 export { Footer };
