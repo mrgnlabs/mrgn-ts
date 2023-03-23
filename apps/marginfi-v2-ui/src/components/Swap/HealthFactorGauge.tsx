0878b2aa6ab7f3907aaa92d7cedb510e7bd68a7f
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-03-23 01:54:41 -0400

    updating gauge

--- apps/marginfi-v2-ui/src/components/Swap/HealthFactorGauge.tsx
+++ apps/marginfi-v2-ui/src/components/Swap/HealthFactorGauge.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import Needle from './Needle';
+import GaugeChart from './GaugeChart';
 
 interface HealthFactorGaugeProps {
   healthFactor: number;
@@ -9,24 +7,19 @@ interface HealthFactorGaugeProps {
 
 const HealthFactorGauge: React.FC<HealthFactorGaugeProps> = ({ healthFactor }) => {
   const gaugeColor = `rgb(${255 * (1 - healthFactor / 100) + 100}, ${255 * healthFactor / 100 + 100}, 100)`;
-  // const rotation = (healthFactor / 100) * 180;
-  const rotation = 130;
 
   return (
-    <div className="z-0 w-[200px] h-[200px] relative">
-      <CircularProgressbar
-        value={healthFactor}
-        maxValue={100}
-        strokeWidth={10}
-        styles={buildStyles({
-          strokeLinecap: 'butt',
-          textSize: '14px',
-          pathColor: gaugeColor,
-          trailColor: gaugeColor,
-        })}
-        counterClockwise
+    <div className="z-0 w-[250px] h-[250px] relative">
+      <GaugeChart
+        id="gauge-chart3" 
+        nrOfLevels={1} 
+        colors={[gaugeColor]} 
+        arcWidth={0.25} 
+        percent={healthFactor} 
+        needleColor="#DCE85D"
+        needleBaseColor="#DCE85D"
+        hideText
       />
-      <Needle rotation={rotation} />
     </div>
   );
 };
