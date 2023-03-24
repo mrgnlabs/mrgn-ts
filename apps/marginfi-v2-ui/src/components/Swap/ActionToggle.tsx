048eef7b733f15e2b5da6a8244db7ae32f6fda9e
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-03-24 15:14:21 -0400

    icon support

--- apps/marginfi-v2-ui/src/components/Swap/ActionToggle.tsx
+++ apps/marginfi-v2-ui/src/components/Swap/ActionToggle.tsx
@@ -2,12 +2,17 @@ import React, { MouseEvent, useState } from 'react';
 import { ToggleButtonGroup } from '@mui/material';
 import CustomToggleButton from './CustomToggleButton';
 
+type ProductType = {
+  name: string;
+}
+
 interface ActionToggleProps {
+  products: ProductType[];
   selectedAction: string;
   handleActionChange: (event: MouseEvent<HTMLElement>, newAction: string | null) => void;
 }
 
-const ActionToggle: React.FC<ActionToggleProps> = ({ selectedAction, handleActionChange }) => {
+const ActionToggle: React.FC<ActionToggleProps> = ({ products, selectedAction, handleActionChange }) => {
   const [indicatorPosition, setIndicatorPosition] = useState(0);
 
   const handleButtonClick = (
@@ -19,7 +24,7 @@ const ActionToggle: React.FC<ActionToggleProps> = ({ selectedAction, handleActio
     setIndicatorPosition(position);
   };
 
-  const options = ['Lend', 'Borrow', 'Lock', '⚡️stake'];
+  // const options = ['Lend', 'Borrow', 'Lock', '⚡️stake'];
 
   return (
     <div className="relative w-[340px] mx-auto mb-[111px]">
@@ -28,16 +33,16 @@ const ActionToggle: React.FC<ActionToggleProps> = ({ selectedAction, handleActio
         exclusive
         className="w-full flex justify-between border-[#1C2125] rounded-[30px] bg-[#0D1011] mb-2"
       >
-        {options.map((option, index, arr) => (
+        {products.map((option, index, arr) => (
           <CustomToggleButton
-            key={option}
-            value={option}
+            key={option.name}
+            value={option.name}
             selectedAction={selectedAction}
-            onClick={(event) => handleButtonClick(event, option, index)}
+            onClick={(event) => handleButtonClick(event, option.name, index)}
             index={index}
             arr={arr}
           >
-            <span className="relative z-10">{option}</span>
+            <span className="relative z-10">{option.name}</span>
           </CustomToggleButton>
         ))}
       </ToggleButtonGroup>
