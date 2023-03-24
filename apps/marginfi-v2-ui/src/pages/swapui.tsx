b990242d9badb14c484ad644d0b4796dcd636cc0
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-03-24 16:59:40 -0400

    cleaning

--- apps/marginfi-v2-ui/src/pages/swapui.tsx
+++ apps/marginfi-v2-ui/src/pages/swapui.tsx
@@ -7,65 +7,10 @@ import { InputBox } from '~/components/Swap/InputBox';
 import { useBanks, useProgram, useUserAccounts } from "~/context";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { AssetRowAction } from '~/components/Swap/ActionButton';
+import { HalfCircularGauge } from '~/components/Swap/Gauge';
 import { ProductType } from '~/types';
 import config from '~/config';
 
-// const products = [
-//   {
-//     name: 'Lend',
-//     icon: <YardIcon className="h-[60%] w-[60%]" style={{ color: '#1C2125' }}/>,
-//   },
-//   {
-//     name: 'Borrow',
-//     icon: <HailIcon className="h-[60%] w-[60%]" style={{ color: '#1C2125' }}/>,
-//   },
-//   {
-//     name: 'Lock',
-//     icon: <LockIcon className="h-[60%] w-[60%]" style={{ color: '#1C2125' }}/>,
-//   },
-//   {
-//     name: '⚡️stake',
-//     icon: <BoltIcon className="h-[60%] w-[60%]" style={{ color: '#DCE85D' }}/>,
-//   },
-// ]
-
-const HalfCircularGauge = ({ percentage }: { percentage: number }) => {
-  const viewBox = "0 0 100 50";
-  const radius = 45;
-  const cx = 50;
-  const cy = 50 + 10;
-  const startAngle = -167;
-  const endAngle = -13;
-  const needleWidth = 2;
-  const gaugeColor = `rgb(${255 * (1 - percentage / 100) + 100}, ${255 * percentage / 100 + 100}, 100)`;
-  const needleColor = "#DCE85D";
-
-  // calculate the angle of the needle based on the percentage
-  const angle = startAngle + (endAngle - startAngle) * (percentage / 100);
-
-  // calculate the coordinates of the needle tip
-  const x = cx + radius * Math.cos(angle * Math.PI / 180);
-  const y = cy + radius * Math.sin(angle * Math.PI / 180);
-
-  return (
-    <svg
-      viewBox={viewBox} className="absolute h-[100px] w-[200px] left-[-68px] bottom-[32px] z-[-1]"
-    >
-      <path
-        d={`M ${cx - radius}, ${cy} A ${radius}, ${radius} 0 0 1 ${cx + radius}, ${cy}`}
-        stroke={gaugeColor}
-        strokeWidth={10}
-        fill="none"
-      />
-      <path
-        d={`M ${cx}, ${cy - 10} L ${x}, ${y}`}
-        stroke={needleColor}
-        strokeWidth={needleWidth}
-        strokeLinecap="round"
-      />
-    </svg>
-  );
-};
 
 interface SwapUIProps {
   healthFactor: number;
@@ -101,7 +46,6 @@ const SwapUI: FC<SwapUIProps> = ({ healthFactor }) => {
   return (
     <div className="p-4 mt-2 w-full relative">
       <ActionToggle
-        // products={config.productsConfig}
         selectedAction={selectedAction}
         handleActionChange={handleActionChange}
       />
