b1d1e230cd34ac728c6c8e1e1aaeb442382311e4
edgar <edgarpavlovsky@users.noreply.github.com>
2023-03-28 14:03:41 -0600

    alpha-liquidator and marginfi-landing-page formatting (#93)

--- apps/alpha-liquidator/src/config.ts
+++ apps/alpha-liquidator/src/config.ts
@@ -13,18 +13,35 @@ dotenv.config();
 
 /*eslint sort-keys: "error"*/
 let envSchema = z.object({
-  IS_DEV: z.string().optional().default("false").transform((s) => s === "true" || s === "1"),
+  IS_DEV: z
+    .string()
+    .optional()
+    .default("false")
+    .transform((s) => s === "true" || s === "1"),
   LIQUIDATOR_PK: z.string().transform((pkStr) => new PublicKey(pkStr)),
-  MARGINFI_ACCOUNT_BLACKLIST: z.string().transform((pkArrayStr) => {
-    return pkArrayStr.split(",").map((pkStr) => new PublicKey(pkStr));
-  }).optional(),
-  MARGINFI_ACCOUNT_WHITELIST: z.string().transform((pkArrayStr) => {
-    return pkArrayStr.split(",").map((pkStr) => new PublicKey(pkStr));
-  }).optional(),
+  MARGINFI_ACCOUNT_BLACKLIST: z
+    .string()
+    .transform((pkArrayStr) => {
+      return pkArrayStr.split(",").map((pkStr) => new PublicKey(pkStr));
+    })
+    .optional(),
+  MARGINFI_ACCOUNT_WHITELIST: z
+    .string()
+    .transform((pkArrayStr) => {
+      return pkArrayStr.split(",").map((pkStr) => new PublicKey(pkStr));
+    })
+    .optional(),
   MIN_SOL_BALANCE: z.coerce.number().default(0.5),
-  MRGN_ENV: z.enum(["production", "alpha", "staging", "dev", "mainnet-test-1", "dev.1"]).default("production").transform((s) => s as Environment),
+  MRGN_ENV: z
+    .enum(["production", "alpha", "staging", "dev", "mainnet-test-1", "dev.1"])
+    .default("production")
+    .transform((s) => s as Environment),
   RPC_ENDPOINT: z.string().url(),
-  SENTRY: z.string().optional().default("false").transform((s) => s === "true" || s === "1"),
+  SENTRY: z
+    .string()
+    .optional()
+    .default("false")
+    .transform((s) => s === "true" || s === "1"),
   SENTRY_DSN: z.string().optional(),
   SLEEP_INTERVAL: z.number().default(5_000),
   WALLET_KEYPAIR: z.string().transform((keypairStr) => {
--- apps/alpha-liquidator/src/setup.ts
+++ apps/alpha-liquidator/src/setup.ts
@@ -27,10 +27,9 @@ const USDC_MINT = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
     process.exit(0);
   }
 
-
   const balance = new BigNumber(
     (await connection.getTokenAccountBalance(getAssociatedTokenAddressSync(USDC_MINT, client.wallet.publicKey))).value
-      .uiAmount ?? 0,
+      .uiAmount ?? 0
   );
   if (balance.gt(0)) {
     console.log("Fund liquidator account with %s USDC? [y/N]", balance);
