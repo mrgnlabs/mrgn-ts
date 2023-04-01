d318aa39f1121d95f994cb43cb2603cc0c73f7eb
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-01 12:42:13 -0400

    removing unused screens

--- apps/marginfi-v2-ui/src/components/Swap/ProductScreens/index.ts
+++ apps/marginfi-v2-ui/src/components/Swap/ProductScreens/index.ts
@@ -1,5 +1,3 @@
-import { ProductScreensLend } from "./ProductScreensLend";
-import { ProductScreensBorrow } from "./ProductScreensBorrow";
 import { ProductScreensSuperstake } from "./ProductScreensSuperstake";
 
 export { ProductScreensLend, ProductScreensBorrow, ProductScreensSuperstake };
