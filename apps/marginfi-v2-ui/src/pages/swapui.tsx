c8ec341a554d8ddcc12d4838d22b11764536c270
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-01 13:11:26 -0400

    small formatting

--- apps/marginfi-v2-ui/src/pages/swapui.tsx
+++ apps/marginfi-v2-ui/src/pages/swapui.tsx
@@ -6,12 +6,12 @@ const SwapUI: FC = () => {
   const { selectedAccount } = useUserAccounts();
 
   return (
-    <div className="p-4 mt-2 w-full relative">
+    <div className="mt-[40px] sm:mt-0 p-4 mt-2 w-full relative">
       <div className="h-[320px] w-[400px] flex flex-col items-center justify-between mx-auto rounded-2xl px-10 py-8 bg-[#0E1113] border-2 border-[#1C2125] gap-2">
         <div className="absolute top-[140px]">          
         </div>
         <div className="h-full gap-1">
-          <ProductScreensSuperstake />
+          {/* <ProductScreensSuperstake /> */}
         </div>
       </div>
     </div>
