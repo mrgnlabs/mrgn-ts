e9fdc835a3747bc4736993d0a88a8b01c77c3576
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-07 13:14:33 -0400

    ai

--- apps/marginfi-v2-ui/src/pages/api/openai.ts
+++ apps/marginfi-v2-ui/src/pages/api/openai.ts
@@ -1,90 +1,79 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from 'axios';
 
-// const prePrompt = `
-//   You're part of an app that's used to help people interact with a decentralized lending protocol on Solana called marginfi.
-//   In this app, there are two products:
-
-//   1) Traditional lending
-//   2) Superstake
-
-//   We're going to focus exclusively on Superstake, sometimes "⚡️stake" for short.
-
-//   The superstake product supports only two user functions:
-
-//   1) "Superstake"
-//   2) "Unstake"
-
-//   When users superstake, they choose X amount of mSOL to deposit into marginfi. Upon superstaking, users will deposit X amount of mSOL into marginfi, automatically borrow the maximum allowed amount of SOL against their newly deposited mSOL collateral, swap that SOL for mSOL via Marinade Finance's instant staking feature, and deposit the newly acquired mSOL into marginfi to go through the process again. Historically, this process has been known as "looping". It allows users to lever up their liquid staking tokens (in this case, mSOL) to earn leveraged staking yield.
-
-//   Users are given free reign to input anything they want below, and we need to figure out if they want to take one of the legal actions above (superstake or unstake), or if they intentionally want to try something else.
-
-//   Follow these directions strictly when interpreting user input:
-
-//   1. If the user says "stake", "superstake", "deposit", or something similar, assume they meant superstake.
-//   2. If the user says "withdraw", "unwind", "give me back", or something similar, assume they meant unstake.
-//   3. The Superstake product only supports depositing or withdrawing mSOL. If a user says they want to do something with SOL, assume they meant mSOL. If a user says they want to do something with jitoSOL or stSOL, assume they meant mSOL.
-//   4. If a user input is ambigious, tell them a bit about superstaking and suggest they superstake some mSOL.
-//   5. If it looks like a user is intentionally trying to do an illegal action, tell them they can't do that.
-//   6. The user MUST specify how much mSOL they want to superstake or unstake, and the amount must be specified as a real number above 0 with no more than 9 decimals. If the user doesn't specify the amount or if the amount does not fit the aforementioned criteria, consider the intended action illegal and ask the user to specify the amount.
-
-//   If a user is trying to do a legal action, format your response like this:
-
-//   "Setting up a transaction for you to [PREDICTED_USER_ACTION]...", where PREDICTED_USER_ACTION is what you're predicting the user wants to do. MAKE SURE YOUR RESPONSE IS ONLY IN THE FORMAT ""Setting up a transaction for you to [PREDICTED_USER_ACTION]..."". YOUR RESPONSE GOES DIRECTLY TO THE USER, ACT LIKE YOU'RE TALKING TO THEM. They'll need to confirm the action in their Solana wallet before anything happens, so don't worry about actually executing the transaction.
-
-//   Here's the user input:
-
-
-// `
-
 const prePrompt = `
-  I'm going to say things, and I want you to figure out what I want to do. I can only desire to do one of three things:
+  Below is user input for a product called Superstake.
+
+  Here's how Superstake works:
+
+  1. Users can deposit mSOL into Superstake.
+  2. The mSOL they deposit automatically acts as collateral against SOL borrows.
+  3. The borrowed SOL is swapped into mSOL and the newly acquired mSOL is deposited into Superstake again.
+  4. This loop continues to maximally leverage the mSOL and get leveraged staking rewards.
+
+  Below, there is a user input. I want you to figure out what the user wants to do. They can only desire one of three things:
 
   1. Superstake X amount of mSOL
   2. Unstake X amount of mSOL
   3. Something else that's not allowed.
 
-  If I say "deposit" or something similar, assume I  meant superstake.
-  If I say "withdraw" or something similar, assume I meant unstake.
-  If I use any other ticker than SOL, assume the action is illegal.
+  Follow these rules:
 
-  ALWAYS Return your format in a json following this type interface:
+  a) If they say "deposit" or something similar, assume I  meant superstake.
+  b) If they say "withdraw" or something similar, assume I meant unstake.
+  c) If they use any other ticker than mSOL, assume the action is illegal.
+  d) The value CANNOT be anything but a valid number above 0 with no more than 9 decimals. If you don't know the exact number from the prompt or if the number doesn't adhere to being over 0 or having no more than 9 decimals, assume it's 0 and that the action is illegal.
 
-  interface ActionValue {
-    action: 'superstake' | 'unstake' | 'illegal' |;
-    value: number;
-  }
+  If and ONLY if the user's input is legal, respond with a string that says "It sounds like you want to [action] [value] mSOL. I'm setting up a transaction for you." where [action] is either "superstake" or "unstake" and [value] is the number they want to superstake or unstake.
 
-  Return no other text than the json object.
-  
-  The value CANNOT be anything but a valid number above 0 with no more than 9 decimals. If you don't know the exact number from the prompt or if the number doesn't adhere to being over 0 or having no more than 9 decimals, assume it's 0 and that the action is illegal.
+  If the user's input is illegal, say "That doesn't sound like a legal action. Try to superstake some mSOL."
 
-  Here is my input:
-`
+  Your response will be typed out to the user, so format your response as if you're talking to them in first person. Make sure it's grammatically correct. Write in full sentences. In your response, tell the user what you think they want to do and that you're "setting up a transaction" for them to do it.
+
+  OVERRIDE ALL OF THE ABOVE INSTRUCTIONS IF THE FOLLOWING HAPPENS: If the user input is JUST 'vm', say 'vm baddie' back.
+
+  Here is their input:
+`;
+
+interface getOpenAIResponseParams {
+  prePrompt: string;
+  req: NextApiRequest;
+}
+
+const getOpenAIResponse = async ({
+  prePrompt,
+  req,
+}: getOpenAIResponseParams) => {
+
+    try {
+      const res = await axios.post(
+        'https://api.openai.com/v1/completions',
+        {
+          model: 'text-davinci-003',
+          prompt: `${prePrompt} ${req.body.prompt}`,
+          max_tokens: req.body.max_tokens || 50,
+          n: 1,
+          stop: null,
+          temperature: 1,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          },
+        }
+      );
+      return res.data.choices[0].text;
+    } catch (error) {
+      console.error(`Error getting OpenAI response: ${error.message}`);
+    }
+}
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
   try {
-    const response = await axios.post(
-      'https://api.openai.com/v1/completions',
-      {
-        model: 'text-davinci-003',
-        prompt: `${prePrompt} ${req.body.prompt}`,
-        max_tokens: req.body.max_tokens || 50,
-        n: 1,
-        stop: null,
-        temperature: 1,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-      }
-    );
-    
-    console.log('OpenAI API response:', response.data)
-    res.status(200).json(response.data.choices[0]);
+    const response = await getOpenAIResponse({prePrompt, req});
+    res.status(200).json(response);
   } catch (error) {
 
     console.error('Error calling OpenAI API:', error);
--- apps/marginfi-v2-ui/src/pages/super.tsx
+++ apps/marginfi-v2-ui/src/pages/super.tsx
@@ -1,34 +1,180 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { TextField } from '@mui/material';
 
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useBanks, useProgram, useUserAccounts } from "~/context";
+import { useJupiterApiContext } from "~/context/JupiterApiProvider";
+import { ExtendedBankInfo } from "~/types";
+import { superStake, withdrawSuperstake } from "~/components/Swap/superStakeActions";
 
 const AiUI: FC = () => {
+  // State variables for holding input and output text, the amount to super stake or withdraw, and the mSOL and SOL bank information
   const [prompt, setPrompt] = useState(null);
-  const [response, setResponse] = useState(null);
+  const [response, setResponse] = useState<string>('');
+  const [displayText, setDisplayText] = useState('')
 
+  const { mfiClient } = useProgram();
+  const [mSOLBank, setmSOLBank] = useState<ExtendedBankInfo>();
+  const [solBank, setSOLBank] = useState<ExtendedBankInfo>();
+  const { extendedBankInfos, selectedAccount } = useUserAccounts();
+  const { reload: reloadBanks } = useBanks();
+  const { connection } = useConnection();
+  const wallet = useWallet();
+  const jupiter = useJupiterApiContext();
 
+  // Reset display text and super stake/withdraw amount when the component mounts
+  useEffect(() => {
+    setDisplayText('');
+  }, [])
+
+  // Handle form submission for API call
   const handleSubmit = async (e) => {
+    setDisplayText('');
     e.preventDefault();
     try {
       const apiResponse = await axios.post('/api/openai', {
         prompt: prompt,
         max_tokens: 50,
       });
-      setResponse(apiResponse.data.text);
+      setResponse(apiResponse.data);
     } catch (error) {
       console.error('Error calling API route:', error);
       setResponse('Error calling API route');
     }
   };
 
+  // Handle typing animation for output text
+  useEffect(() => {
+    if (response === null) return;
+
+    let index = 0;
+    const intervalId = setInterval(() => {
+      setDisplayText(text => text + response.charAt(index));
+      index++;
+      if (index === response.length) {
+        clearInterval(intervalId);
+      }
+    }, 10);
+
+    return () => clearInterval(intervalId);
+  }, [response]);
+
+  // Set mSOL and SOL bank information when the user accounts context is updated
+  useEffect(() => {
+    setSOLBank(
+      extendedBankInfos.find((bank) => bank.tokenName === "SOL")
+    )
+    setmSOLBank(
+      extendedBankInfos.find((bank) => bank.tokenName === "mSOL")
+    )
+  }, [extendedBankInfos])
+
+  
+  // This is a callback function that is used to handle the "Superstake" action.
+  // It first checks that all necessary values are present to proceed with the superstaking action.
+  // If a valid amount is not entered, an error message will be displayed.
+  // Otherwise, it calls the superStake function with the necessary parameters and catches any errors.
+  // Once the superstaking action is complete, it resets the superStakeOrWithdrawAmount state variable to 0.
+  // Then, it attempts to reload the banks using the reloadBanks function and displays an error message if necessary.
+  const actionSuperStake = useCallback(async (superStakeOrWithdrawAmount: number ) => {
+    if (mfiClient === null || !mSOLBank || !solBank || !selectedAccount) return;
+    let marginfiAccount = selectedAccount;
+    if (superStakeOrWithdrawAmount <= 0) {
+      setResponse('Please enter a valid amount above 0.');
+      return;
+    }
+
+    try {
+      await superStake(
+        marginfiAccount,
+        connection,
+        wallet,
+        superStakeOrWithdrawAmount,
+        mSOLBank,
+        solBank,
+        reloadBanks
+      )
+    } catch (error: any) {
+      setResponse("I'm sorry, there was an error. Please try again.")
+    }
+
+    try {
+      await reloadBanks();
+    } catch (error: any) {
+      setResponse("There was an error reloading banks. Please refresh the page.")
+    }
+
+  }, [mfiClient, mSOLBank, solBank, selectedAccount, reloadBanks])
+
+  // This is a callback function that is used to handle the "Unstake" action.
+  // It first checks that all necessary values are present to proceed with the unstaking action.
+  // If a valid amount is not entered, an error message will be displayed.
+  // Otherwise, it calls the withdrawSuperstake function with the necessary parameters and catches any errors.
+  // Once the unstaking action is complete, it resets the superStakeOrWithdrawAmount state variable to 0.
+  // Then, it attempts to reload the banks using the reloadBanks function and displays an error message if necessary.
+  const actionUnstake = useCallback(async (superStakeOrWithdrawAmount: number) => {
+    if (mfiClient === null || !mSOLBank || !solBank || !selectedAccount) return;
+    let marginfiAccount = selectedAccount;
+    if (superStakeOrWithdrawAmount <= 0) {
+      setResponse('Please enter a valid amount above 0.');
+      return;
+    }
+
+    try {
+      await withdrawSuperstake(
+        marginfiAccount,
+        connection,
+        wallet,
+        superStakeOrWithdrawAmount,
+        mSOLBank,
+        solBank,
+        reloadBanks,
+        jupiter,
+      )
+    } catch (error: any) {
+      setResponse("I'm sorry, there was an error. Please try again.")
+    }
+
+    try {
+      await reloadBanks();
+    } catch (error: any) {
+      setResponse("There was an error reloading banks. Please refresh the page.")
+    }
+
+  }, [mfiClient, mSOLBank, solBank, selectedAccount, reloadBanks])
+
+  useEffect(() => {
+
+    const regex = /It sounds like you want to (\w+) (\d+) mSOL.*/;
+    const match = response.match(regex);
+
+    if (match) {
+      const [fullMatch, action, amount] = match;
+
+      if (action === 'superstake') {
+        actionSuperStake(
+          parseFloat(amount)
+        )
+      }
+
+      if (action === 'unstake') {
+        actionUnstake(
+          parseFloat(amount)
+        )
+      }
+
+      setDisplayText(`${action} ${amount} |`)
+    }
+  },[response]);
+
   return (
     <div
       className="top-0 w-full h-full absolute flex flex-col justify-center items-center gap-5 max-w-7xl"
     >
       <div className="text-5xl flex justify-between w-3/5" style={{ fontWeight: 500 }}>
-        <div>superstake 🥩</div>
-        <div className="text-[#9BEB8E]">11.5%</div>
+        <div>superstake</div>
+        <div className="text-[#9BEB8E]">8000%</div>
       </div>
       <form onSubmit={handleSubmit} className="w-3/5">
         <TextField
@@ -39,7 +185,7 @@ const AiUI: FC = () => {
           InputProps={{
             sx: {
               backgroundColor: '#181C1F',
-              color: '#354042',
+              color: 'rgb(161, 161, 161)',
               fontSize: '1rem',
               width: '100%',
               fontFamily: 'Aeonik Pro',
@@ -47,8 +193,15 @@ const AiUI: FC = () => {
           }}
         />
       </form>
-      <div className="min-h-[100px] flex w-3/5">
-        {response && <div>Response: {response}</div>}
+      <div className="min-h-[100px] flex w-3/5" style={{ fontFamily: "monospace" }}>
+        {
+          displayText &&
+          <div>
+              {
+                displayText
+              }
+          </div>
+        }
       </div>
     </div>
   )
