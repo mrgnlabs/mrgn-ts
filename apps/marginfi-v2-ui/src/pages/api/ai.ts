f21a529739844b947154387ce52bf17b6fd827fd
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-08 21:49:22 -0400

    cleaning

--- apps/marginfi-v2-ui/src/pages/api/ai.ts
+++ apps/marginfi-v2-ui/src/pages/api/ai.ts
@@ -1,13 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-      res.status(200).json("OK");
-    } catch (error) {
+  try {
+
+    //
   
-      console.error('Error calling OpenAI API:', error);
-      res.status(500).json({ error: 'Error calling OpenAI API' });
-  
-    }
+    res.status(200).json("OK");
+  } catch (error) {
+
+    console.error('Error calling OpenAI API:', error);
+    res.status(500).json({ error: 'Error calling OpenAI API' });
+
   }
-  
+}
