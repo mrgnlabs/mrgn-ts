d74460ed672026d20db393b8539fcd901480092f
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-10 13:05:09 -0400

    pretty blur

--- apps/marginfi-v2-ui/src/pages/super.tsx
+++ apps/marginfi-v2-ui/src/pages/super.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState, useEffect, useCallback, useMemo } from 'react';
+import Image from 'next/image';
 import axios from 'axios';
 import { TextField } from '@mui/material';
 
@@ -168,27 +169,33 @@ const AiUI: FC = () => {
     <div
       className="top-0 w-full h-full absolute flex flex-col justify-center items-center gap-5 max-w-7xl"
     >
-      <div className="text-5xl flex justify-between w-3/5" style={{ fontWeight: 500 }}>
-        <div>superstake</div>
-        <div className="text-[#9BEB8E]">+11.5%</div>
+      <div className="text-5xl flex justify-between w-3/5 ml-24" style={{ fontWeight: 500 }}>
+        <div>marginfi</div>
       </div>
-      <form onSubmit={handleSubmit} className="w-3/5">
-        <TextField
-          fullWidth
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder='Enter a prompt, like "I want to superstake 10 mSOL."'
-          InputProps={{
-            sx: {
-              backgroundColor: '#181C1F',
-              color: 'rgb(227, 227, 227)',
-              fontSize: '1rem',
-              width: '100%',
-              fontFamily: 'Aeonik Pro',
-            },
-          }}
-        />
-      </form>
+      <div className="w-3/5 flex items-center gap-5">
+        <div className="relative w-[28.02px] h-[24.81px]">
+          <div className="absolute w-[1px] h-[1px] top-[13.51px] left-[11.905px] z-[-1]" style={{ boxShadow: '0 0 40px 15px yellow' }}></div>
+          {/* <div className="absolute w-full h-full" style={{ boxShadow: '0 0 20px yellow' }}></div> */}
+          <Image src="/marginfi_logo.png" alt="marginfi logo" fill className="z-10"/>
+        </div>
+        <form onSubmit={handleSubmit} className="w-full">
+          <TextField
+            fullWidth
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder='Enter a prompt, like "I want to superstake 10 mSOL."'
+            InputProps={{
+              sx: {
+                backgroundColor: '#181C1F',
+                color: 'rgb(227, 227, 227)',
+                fontSize: '1rem',
+                width: '100%',
+                fontFamily: 'Aeonik Pro',
+              },
+            }}
+          />
+        </form>
+      </div>
       <div className="min-h-[50px] flex w-3/5 font-[rgb(227, 227, 227)]" style={{ fontFamily: "monospace" }}>
         {
           response &&
