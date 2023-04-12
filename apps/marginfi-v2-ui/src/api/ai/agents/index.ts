b74058fb91175042e515805a60350a70420c125b
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-12 14:46:26 -0400

    one agent wip

--- apps/marginfi-v2-ui/src/api/ai/agents/index.ts
+++ apps/marginfi-v2-ui/src/api/ai/agents/index.ts
@@ -1,9 +1,34 @@
-import { getActionAgent } from './action'
-import { getInformationAgent } from './information';
-import { getManagerAgent } from './manager';
+import { OpenAI } from "langchain";
+import { ChatOpenAI } from "langchain/chat_models";
+import { initializeAgentExecutor } from "langchain/agents";
+import { AccountsTool, BanksTool, TokenInfoTool, getOmniQaTool } from '../tools';
 
-export { 
-    getActionAgent,
-    getInformationAgent,
-    getManagerAgent
+const getGeneralAgent = async ({ walletPublicKey }: { walletPublicKey: string; }) => {
+
+  // Get base OpenAI model
+  const model = new OpenAI({ 
+      modelName: "text-davinci-003",
+      openAIApiKey: process.env.OPENAI_API_KEY, 
+      maxTokens: 1000,
+      temperature: 0.5,
+      verbose: true,
+  });
+
+  const tools = [
+    new BanksTool(), 
+    new TokenInfoTool(),
+    new AccountsTool(walletPublicKey),
+    await getOmniQaTool(),
+  ];
+
+  const executor = await initializeAgentExecutor(
+    tools,
+    model,
+    "zero-shot-react-description"
+  );
+
+  return executor;
 }
+
+
+export { getGeneralAgent }
--- apps/marginfi-v2-ui/src/api/ai/index.ts
+++ apps/marginfi-v2-ui/src/api/ai/index.ts
@@ -1,39 +1,4 @@
-import { getManagerAgent, getInformationAgent, getActionAgent } from "./agents";
-import { PromptTemplate } from "langchain/prompts";
-import { ChatOpenAI } from "langchain/chat_models";
-
-const PREFIX = `
-  Determine which agent should be used for the user input below. 
-  
-  You have two choices: information or action. You must choose one of these two agents.
-
-  Here is context on the two agents and what they can do:
-
-  1. "information": Useful if the user is trying to answer a question or seeking information about something.
-  2. "action": Useful if the user is trying to DO something. Examples: deposit, withdraw, borrow, repay, stake, unstake.
-`
-
-export const NAIVE_FIX_TEMPLATE = (instructions: string, completion: string, error: string) => `
-Instructions:
---------------
-${instructions}
---------------
-Completion:
---------------
-${completion}
---------------
-
-Above, the Completion did not satisfy the constraints given in the Instructions.
-Error:
---------------
-${error}
---------------
-
-Please try again. Please only respond with an answer that satisfies the constraints laid out in the Instructions:`;
-
-const formatInstructions = `
-  The output should be a string literal that is either "information" or "action". Be very strict with this output. Do not respond with any other text.
-`
+import { getGeneralAgent } from "./agents";
 
 const callAI = async ({
   input,
@@ -43,58 +8,7 @@ const callAI = async ({
   walletPublicKey: string;
 }) => {
 
-  const manager = await getManagerAgent();  
-
-  const prompt = new PromptTemplate({
-    template: 
-      [
-        PREFIX,
-        "\n{format_instructions}",
-        "Here is the user input:",
-        "\n{prompt}"
-      ].join('\n\n'),
-    inputVariables: ["prompt"],
-    partialVariables: { format_instructions: formatInstructions },
-  });
-  const formattedInput = await prompt.format({ prompt: input });
-
-  let response;
-  let choice;
-  const regex = /[^a-zA-Z0-9\s]/g;
-
-  try {
-    console.log('1. trying manager')
-    response = await manager.call({ input: formattedInput });
-    console.log({ response: JSON.stringify(response) })
-
-    choice = response.output.trim().toLowerCase().replace(regex, '');
-
-    console.log({ choice })
-    if (choice !== "information" && choice !== "action") {
-      throw new Error("The Completion did not satisfy the constraints given in the Instructions.");
-    }
-  } catch (error: any) {
-    console.log('2. error during first manager try. trying again.')
-    const fixinput = NAIVE_FIX_TEMPLATE([
-        PREFIX,
-        formatInstructions
-      ].join("\n\n"), 
-      response?.output || 'The output was undefined.', 
-      error.message
-    );
-
-    console.log({ fixinput })
-    response = await manager.call({ input: fixinput });
-    console.log({ response: JSON.stringify(response) })
-    choice = response.output.trim().toLowerCase().replace(regex, '');
-  }
-
-  let executor;
-  if (choice === "action") {
-    executor = await getActionAgent({ walletPublicKey });
-  } else {
-    executor = await getInformationAgent({ walletPublicKey });
-  }
+  const executor = await getGeneralAgent({ walletPublicKey });
 
   const output = await executor.call({ input });
 
