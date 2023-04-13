a83977ff3e587f972f84c7e97876413b4a6bf917
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-13 18:35:11 -0400

    updating tools

--- apps/omni/src/api/ai/tools/accounts.ts
+++ apps/omni/src/api/ai/tools/accounts.ts
@@ -12,29 +12,9 @@ const getAccounts = async ({ walletPublicKey }: AccountsProps) => {
   const accounts = await client.getMarginfiAccountsForAuthority(
     new PublicKey(walletPublicKey)
   );
-  const account = accounts[0]
+  const marginfiAccount = accounts[0]
 
-  const componentsEquity = account.getHealthComponents(MarginRequirementType.Equity);
-  const componentMaintenance = account.getHealthComponents(MarginRequirementType.Maint);
-
-  // User metrics
-  const equityValueUsd = componentsEquity.assets.minus(componentsEquity.liabilities).toNumber();
-  const totalLentValueUsd = componentsEquity.assets.toNumber();
-  const totalBorrowedValueUsd = componentsEquity.liabilities.toNumber();
-  const healthMaintenancePercent = componentMaintenance.assets
-    .minus(componentMaintenance.liabilities)
-    .div(componentMaintenance.assets)
-    .times(100)
-    .toNumber();
-
-  const resJson = {
-    userAccountBalanceInUSD: equityValueUsd,
-    totalLentByUserInUSD: totalLentValueUsd,
-    totalBorrowedBYUserInUSD: totalBorrowedValueUsd,
-    userHealthRatio: healthMaintenancePercent,
-  }
-
-  return JSON.stringify(resJson)
+  return marginfiAccount.describe();
 }
 
 class AccountsTool extends Tool {
@@ -42,7 +22,7 @@ class AccountsTool extends Tool {
   walletPublicKey: string;
 
   description =
-    "A tool to get information about a user's marginfi account. Useful when you need to answer questions about a user's portfolio, specific positions, health ratio, etc. The wallet public key is initialized in the constructor.";
+    "A tool to get information about the state of a user's marginfi account. Useful when you need to answer questions about A user's balance, total deposits, liabilities, equity, or account health. The user's wallet public key is intialized in the constructor. Input should be null."
 
   constructor(walletPublicKey: string) {
     super();
--- apps/omni/src/api/ai/tools/banks.ts
+++ apps/omni/src/api/ai/tools/banks.ts
@@ -5,40 +5,19 @@ const getBanks = async () => {
   const client = await getClient();
   const banks = client.group.banks;
 
-  const resJson = [...banks.values()].map(
-    (bank) => (
-      {
-        overview: {
-          token: bank.label,
-          tokenMintPubkey: bank.mint.toBase58(),
-        },
-        depositAndBorrowingConfigs: {
-          assetWeightInitial: bank.config.assetWeightInit.toNumber(),
-          assetWeightMaintenance: bank.config.assetWeightMaint.toNumber(),
-          liabilityWeightInitial: bank.config.liabilityWeightInit.toNumber(),
-          liabilityWeightMaintenance: bank.config.liabilityWeightMaint.toNumber(),
-        },
-        interestRateFeeConfigs: {
-          insuranceFeeFixedApr: bank.config.interestRateConfig.insuranceFeeFixedApr.toNumber(),
-          maxInterestRate: bank.config.interestRateConfig.maxInterestRate.toNumber(),
-          insuranceIrFee: bank.config.interestRateConfig.insuranceIrFee.toNumber(),
-          optimalUtilizationRate: bank.config.interestRateConfig.optimalUtilizationRate.toNumber(),
-          plateauInterestRate: bank.config.interestRateConfig.plateauInterestRate.toNumber(),
-          protocolFixedFeeApr: bank.config.interestRateConfig.protocolFixedFeeApr.toNumber(),
-          protocolIrFee: bank.config.interestRateConfig.protocolIrFee.toNumber(),
-        }
-      }
-    )
-  )
+  const allBanksInformation = [...banks.values()].map(
+    (bank) => bank.describe()
+  );
 
-  return JSON.stringify(resJson);
+  return JSON.stringify(allBanksInformation);
 }
 
 class BanksTool extends Tool {
     name = "bank-tool";
   
     description =
-      "A tool to get information about marginfi token pools, which are internally called Banks. useful when you need to answer questions about the whole protocol. input should be null."
+      "A tool to get information about marginfi token pools, which are internally called Banks. Useful when you need to answer questions about the state of liquidity pools on the marginfi protocol. Input should be null."
+      
   
     constructor() {
       super();
