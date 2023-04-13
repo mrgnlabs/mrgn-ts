65851b2577c8de7376afd7937295c9bc856cc05c
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-13 18:05:25 -0400

    removing unnecessary files

--- apps/omni/src/components/index.tsx
+++ apps/omni/src/components/index.tsx
@@ -1,7 +1,3 @@
 import { Navbar } from "./Navbar";
-import { AccountSummary } from "./AccountSummary";
-import { AssetsList } from "./AssetsList";
-import { UserPositions } from "./UserPositions";
-import { MultipleAccountsFoundWarning } from "./MultipleAccountsFoundWarning";
 
-export { Navbar, AccountSummary, AssetsList, UserPositions, MultipleAccountsFoundWarning };
+export { Navbar };
