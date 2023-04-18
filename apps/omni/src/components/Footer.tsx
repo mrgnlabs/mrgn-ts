7a8a7b62081c1ff9fd99d6ef0d98b7c378062cb2
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-18 08:56:24 -0400

    formatting

--- apps/omni/src/components/Footer.tsx
+++ apps/omni/src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer: FC = () => {
 
   return (
     <div>
-      <nav className="fixed w-full bottom-0 h-[32px] z-10 backdrop-blur-md bg-[#C0B3A5] text-center text-xs text-[rgb(84,84,84)]">
+      <nav className="fixed w-full bottom-0 h-[36px] sm:h-[32px] z-10 backdrop-blur-md text-center text-xs text-[rgb(84,84,84)]">
         Omni is highly experimental software. Use with caution. Inaccurate results may be produced.
       </nav>
     </div>
