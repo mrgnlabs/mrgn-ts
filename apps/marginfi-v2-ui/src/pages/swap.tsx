15f68f81d80e7ab74e152524373de60d359c3942
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-04-20 21:34:36 -0400

    quick formatting change

--- apps/marginfi-v2-ui/src/pages/swap.tsx
+++ apps/marginfi-v2-ui/src/pages/swap.tsx
@@ -30,12 +30,11 @@ const JupiterPage = () => {
     return (
       <>
         <PageHeaderSwap />
-        <div className='w-full h-full flex flex-col justify-start items-center content-start py-[96px] sm:py-[32px] gap-8 w-4/5 max-w-[350px]'
-        >
+        <div className='w-full h-full flex flex-col justify-start items-center content-start py-[96px] sm:py-[32px] gap-8 w-4/5 max-w-7xl'>
           <div>
             <div className="text-[#fff] text-3xl min-w-[600px] text-center">Zero fees. <span className="text-[#DCE85D]">Always.</span></div>
           </div>
-          <div id="integrated-terminal"></div>
+          <div style={{ width: 420, maxWidth: '80%' }} id="integrated-terminal"></div>
         </div>
       </>
       
