2fa364adcc79d899389f3087ec4863638d9bf503
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-05-01 14:29:32 -0400

    cleaning

--- apps/marginfi-v2-ui/src/components/AssetsList/AssetRow/AssetRow.tsx
+++ apps/marginfi-v2-ui/src/components/AssetsList/AssetRow/AssetRow.tsx
@@ -256,8 +256,10 @@ const AssetRow: FC<{
       <TableCell className="text-white border-none font-aeonik px-2 hidden md:table-cell" align="right" style={{ fontWeight: 300 }}>
         {
           isInLendingMode ?            
+            // (bankInfo.bank.config.assetWeightMaint.toNumber() * 100).toFixed(0) + '%'
+            bankInfo.bank.config.assetWeightMaint.toNumber() > 0 ?
             (bankInfo.bank.config.assetWeightMaint.toNumber() * 100).toFixed(0) + '%'
-            // bankInfo.bank.config.assetWeightMaint.toNumber() > 0 ? '✅' : '❌'
+            : '-'
           :
             (1/bankInfo.bank.config.liabilityWeightInit.toNumber() * 100).toFixed(0) + '%'
         }
