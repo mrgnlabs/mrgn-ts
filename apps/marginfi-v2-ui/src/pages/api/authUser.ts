2d299d9d620f091b8ca92c6c337fd77bcf099f4b
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2023-06-23 16:59:56 -0400

    removing database url

--- apps/marginfi-v2-ui/src/pages/api/authUser.ts
+++ apps/marginfi-v2-ui/src/pages/api/authUser.ts
@@ -9,7 +9,6 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   });
 }
 
