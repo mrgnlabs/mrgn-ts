859357061af50c6fcd590a878dafc8f86bf9c0e0
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-07-18 17:53:16 +0530

    allow modifiers

--- apps/marginfi-v2-ui/src/components/Navbar/Navbar.tsx
+++ apps/marginfi-v2-ui/src/components/Navbar/Navbar.tsx
@@ -94,7 +94,7 @@ const Navbar: FC = () => {
           break;
       }
     },
-    { preventDefault: true, enableOnFormTags: true, ignoreModifiers: true }
+    { preventDefault: true, enableOnFormTags: true }
   );
 
   useHotkeys(
