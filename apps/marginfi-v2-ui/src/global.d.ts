44838663bfddf79242da40aa36ebee6fff34dec6
man0s <95379755+losman0s@users.noreply.github.com>
2023-08-14 19:32:54 +0800

    feat(mfi-v2-ui): partial port of deleted commit

---
+++ apps/marginfi-v2-ui/src/global.d.ts
@@ -0,0 +1,14 @@
+interface Window {
+  MayanSwap: {
+    init: (id: string, config: MayanWidgetConfigType) => void;
+    updateSolanaWallet: (newData: SolanaWalletData) => void;
+    updateConfig: (newConfig: MayanWidgetConfigType | MayanWidgetSolanaConfigType) => void;
+    setSwapInitiateListener(callback: (swap: MayanSwapInfo) => void): void;
+    removeSwapInitiateListener(): void;
+    setSwapCompleteListener(callback: (swap: MayanSwapInfo) => void): void;
+    removeSwapCompleteListener(): void;
+    setSwapRefundListener(callback: (swap: MayanSwapInfo) => void): void;
+    removeSwapRefundListener(): void;
+  };
+  Jupiter: any;
+}
