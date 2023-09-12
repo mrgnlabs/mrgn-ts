d1dd5290a9836fa922878dc6d0e41502a8c60563
Kobe Leenders <leenderskobe@gmail.com>
2023-09-12 08:53:36 +0200

    feat(mfi-v2-ui): LST staking mobile design

--- apps/marginfi-v2-ui/src/pages/stake.tsx
+++ apps/marginfi-v2-ui/src/pages/stake.tsx
@@ -7,7 +7,7 @@ const StakePage = () => {
   return (
     <>
       <PageHeader text={"$LST"} />
-      <div className="flex flex-col h-full justify-center content-center pt-[64px] sm:pt-[16px] gap-4">
+      <div className="flex flex-col h-full justify-center content-center pt-[64px] sm:pt-[16px] gap-4 mx-4">
         <StakingStats tvl={1250} projectedApy={0.5215} />
         <StakingCard />
       </div>
