c8be4eeacf346068155fb0ab050bfb049f4fb643
Kobe Leenders <leenderskobe@gmail.com>
2023-09-12 12:53:52 +0200

    fix(mfi-v2-ui): fixed divers console error
    - Rehydration error is fixed with ready usestate
    - div nested in p & wrong table structure fixes

--- apps/marginfi-v2-ui/src/components/desktop/UserPositions/UserPositions.tsx
+++ apps/marginfi-v2-ui/src/components/desktop/UserPositions/UserPositions.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useMemo } from "react";
-import { Card, Table, TableBody, TableContainer, TableHead, TableCell, Typography } from "@mui/material";
+import { Card, Table, TableBody, TableContainer, TableHead, TableCell, Typography, TableRow } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -45,113 +45,7 @@ const UserPositions: FC = () => {
               }}
             >
               <TableHead>
-                <TableCell className="border-none"></TableCell>
-                <TableCell
-                  className="hidden md:table-cell border-none"
-                  style={{ fontFamily: "Aeonik Pro", fontWeight: 300 }}
-                  align="right"
-                >
-                  <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1">Lending</Typography>
-                </TableCell>
-                <TableCell
-                  className="hidden md:table-cell border-none"
-                  style={{ fontFamily: "Aeonik Pro", fontWeight: 300 }}
-                  align="right"
-                >
-                  <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1">
-                    Wtd
-                    <div className="self-center">
-                      <MrgnTooltip
-                        title={
-                          <React.Fragment>
-                            <Typography color="inherit" style={{ fontFamily: "Aeonik Pro" }}>
-                              Weighted values
-                            </Typography>
-                            <div className="flex flex-col gap-2 pb-2">
-                              For risk purposes, token values are adjusted by oracle price bias and risk weights.
-                            </div>
-                            <div className="flex flex-col gap-2 pb-2">
-                              Weighted prices used in risk calculations and are relevant to your health factor.
-                            </div>
-                            <Link href="https://t.me/mrgncommunity">
-                              <u>Learn more here.</u>
-                            </Link>
-                          </React.Fragment>
-                        }
-                        placement="top"
-                      >
-                        <Image src="/info_icon.png" alt="info" height={16} width={16} />
-                      </MrgnTooltip>
-                    </div>
-                  </Typography>
-                </TableCell>
-                <TableCell
-                  className="hidden md:table-cell border-none"
-                  style={{ fontFamily: "Aeonik Pro", fontWeight: 300 }}
-                  align="right"
-                >
-                  <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1">
-                    USD
-                    <div className="self-center">
-                      <MrgnTooltip
-                        title={
-                          <React.Fragment>
-                            <Typography color="inherit" style={{ fontFamily: "Aeonik Pro" }}>
-                              USD Values
-                            </Typography>
-                            <div className="flex flex-col gap-2 pb-2">
-                              Unadjusted USD values are based on oracle mid prices - or the best estimate the oracle has
-                              of average token price.
-                            </div>
-                            <div className="flex flex-col gap-2 pb-2">
-                              Unadjusted USD values are not used in risk calculations. Values with price bias are used
-                              because they are more conservative, and can control for oracle inaccuracies.
-                            </div>
-                            <Link href="https://t.me/mrgncommunity">
-                              <u>Learn more here.</u>
-                            </Link>
-                          </React.Fragment>
-                        }
-                        placement="top"
-                      >
-                        <Image src="/info_icon.png" alt="info" height={16} width={16} />
-                      </MrgnTooltip>
-                    </div>
-                  </Typography>
-                </TableCell>
-                <TableCell className="border-none"></TableCell>
-                <TableCell className="border-none"></TableCell>
-              </TableHead>
-              <TableBody>
-                {lendPositions.map((bankInfo, index) => (
-                  <UserPositionRow
-                    key={index}
-                    activeBankInfo={bankInfo}
-                    marginfiAccount={selectedAccount}
-                    reloadPositions={async () => {
-                      setIsRefreshingStore(true);
-                      fetchMrgnlendState();
-                    }}
-                  />
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Card>
-      )}
-      <div>
-        {borrowPositions.length > 0 && selectedAccount && (
-          <Card elevation={0} className="bg-transparent w-full p-0 grid">
-            <div className="font-aeonik font-normal text-2xl my-0 lg:mt-2 mb-[-20px] text-white">Borrowing</div>
-            <TableContainer>
-              <Table
-                className="table-fixed"
-                style={{
-                  borderCollapse: "separate",
-                  borderSpacing: "0px 8px",
-                }}
-              >
-                <TableHead>
+                <TableRow>
                   <TableCell className="border-none"></TableCell>
                   <TableCell
                     className="hidden md:table-cell border-none"
@@ -159,7 +53,7 @@ const UserPositions: FC = () => {
                     align="right"
                   >
                     <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1">
-                      Borrowing
+                      Lending
                     </Typography>
                   </TableCell>
                   <TableCell
@@ -167,7 +61,7 @@ const UserPositions: FC = () => {
                     style={{ fontFamily: "Aeonik Pro", fontWeight: 300 }}
                     align="right"
                   >
-                    <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1">
+                    <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1" component="div">
                       Wtd
                       <div className="self-center">
                         <MrgnTooltip
@@ -199,7 +93,7 @@ const UserPositions: FC = () => {
                     style={{ fontFamily: "Aeonik Pro", fontWeight: 300 }}
                     align="right"
                   >
-                    <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1">
+                    <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1" component="div">
                       USD
                       <div className="self-center">
                         <MrgnTooltip
@@ -230,6 +124,118 @@ const UserPositions: FC = () => {
                   </TableCell>
                   <TableCell className="border-none"></TableCell>
                   <TableCell className="border-none"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {lendPositions.map((bankInfo, index) => (
+                  <UserPositionRow
+                    key={index}
+                    activeBankInfo={bankInfo}
+                    marginfiAccount={selectedAccount}
+                    reloadPositions={async () => {
+                      setIsRefreshingStore(true);
+                      fetchMrgnlendState();
+                    }}
+                  />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Card>
+      )}
+      <div>
+        {borrowPositions.length > 0 && selectedAccount && (
+          <Card elevation={0} className="bg-transparent w-full p-0 grid">
+            <div className="font-aeonik font-normal text-2xl my-0 lg:mt-2 mb-[-20px] text-white">Borrowing</div>
+            <TableContainer>
+              <Table
+                className="table-fixed"
+                style={{
+                  borderCollapse: "separate",
+                  borderSpacing: "0px 8px",
+                }}
+              >
+                <TableHead>
+                  <TableRow>
+                    <TableCell className="border-none"></TableCell>
+                    <TableCell
+                      className="hidden md:table-cell border-none"
+                      style={{ fontFamily: "Aeonik Pro", fontWeight: 300 }}
+                      align="right"
+                    >
+                      <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1">
+                        Borrowing
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      className="hidden md:table-cell border-none"
+                      style={{ fontFamily: "Aeonik Pro", fontWeight: 300 }}
+                      align="right"
+                    >
+                      <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1" component="div">
+                        Wtd
+                        <div className="self-center">
+                          <MrgnTooltip
+                            title={
+                              <React.Fragment>
+                                <Typography color="inherit" style={{ fontFamily: "Aeonik Pro" }}>
+                                  Weighted values
+                                </Typography>
+                                <div className="flex flex-col gap-2 pb-2">
+                                  For risk purposes, token values are adjusted by oracle price bias and risk weights.
+                                </div>
+                                <div className="flex flex-col gap-2 pb-2">
+                                  Weighted prices used in risk calculations and are relevant to your health factor.
+                                </div>
+                                <Link href="https://t.me/mrgncommunity">
+                                  <u>Learn more here.</u>
+                                </Link>
+                              </React.Fragment>
+                            }
+                            placement="top"
+                          >
+                            <Image src="/info_icon.png" alt="info" height={16} width={16} />
+                          </MrgnTooltip>
+                        </div>
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      className="hidden md:table-cell border-none"
+                      style={{ fontFamily: "Aeonik Pro", fontWeight: 300 }}
+                      align="right"
+                    >
+                      <Typography className="text-[#A1A1A1] font-aeonik font-[300] text-sm flex gap-1" component="div">
+                        USD
+                        <div className="self-center">
+                          <MrgnTooltip
+                            title={
+                              <React.Fragment>
+                                <Typography color="inherit" style={{ fontFamily: "Aeonik Pro" }}>
+                                  USD Values
+                                </Typography>
+                                <div className="flex flex-col gap-2 pb-2">
+                                  Unadjusted USD values are based on oracle mid prices - or the best estimate the oracle
+                                  has of average token price.
+                                </div>
+                                <div className="flex flex-col gap-2 pb-2">
+                                  Unadjusted USD values are not used in risk calculations. Values with price bias are
+                                  used because they are more conservative, and can control for oracle inaccuracies.
+                                </div>
+                                <Link href="https://t.me/mrgncommunity">
+                                  <u>Learn more here.</u>
+                                </Link>
+                              </React.Fragment>
+                            }
+                            placement="top"
+                          >
+                            <Image src="/info_icon.png" alt="info" height={16} width={16} />
+                          </MrgnTooltip>
+                        </div>
+                      </Typography>
+                    </TableCell>
+                    <TableCell className="border-none"></TableCell>
+                    <TableCell className="border-none"></TableCell>
+                  </TableRow>
                 </TableHead>
                 <TableBody>
                   {borrowPositions.map((bankInfo, index) => (
