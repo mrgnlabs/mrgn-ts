d1f8ca05c792a067deaae42f323e073799c23b76
man0s <95379755+losman0s@users.noreply.github.com>
2023-09-13 16:45:59 +0800

    checkpoint

--- apps/marginfi-v2-ui/src/components/Staking/StakingCard/StakingModal.tsx
+++ apps/marginfi-v2-ui/src/components/Staking/StakingCard/StakingModal.tsx
@@ -1,18 +1,18 @@
-import { Typography, Skeleton, Input, InputBase, Modal, Box } from "@mui/material";
+import { Typography, Modal } from "@mui/material";
+import { Dispatch, FC, SetStateAction } from "react";
+import { Close } from "@mui/icons-material";
+import { TokenData } from "./StakingCard";
 import Image from "next/image";
-import Link from "next/link";
-import { FC } from "react";
-import { MrgnTooltip } from "~/components/Tooltip";
-
-import { numeralFormatter, percentFormatterDyn } from "@mrgnlabs/mrgn-common";
-import { ArrowDropDown, Close } from "@mui/icons-material";
+import { PublicKey } from "@solana/web3.js";
 
 interface StakingModalProps {
   isOpen: boolean;
   handleClose: () => void;
+  setSelectedMint: Dispatch<SetStateAction<PublicKey>>;
+  supportedTokens: TokenData[];
 }
 
-export const StakingModal: FC<StakingModalProps> = ({ isOpen, handleClose }) => {
+export const StakingModal: FC<StakingModalProps> = ({ isOpen, handleClose, supportedTokens, setSelectedMint }) => {
   return (
     <Modal
       open={isOpen}
@@ -21,12 +21,35 @@ export const StakingModal: FC<StakingModalProps> = ({ isOpen, handleClose }) => 
       aria-describedby="parent-modal-description"
       className="border-none"
     >
-      <div className="mx-auto mt-40 rounded-xl bg-[#1C2023] w-[400px] h-[500px] p-4">
+      <div className="mx-auto mt-40 rounded-xl bg-[#1C2023] w-[400px] max-w-[90%] h-[500px] p-4">
         <div className="flex flex-row justify-between">
           <Typography className="font-aeonik font-[700] text-2xl inline">Select token</Typography>
           <div className="cursor-pointer" onClick={handleClose}>
             <Close />
           </div>
+        </div>
+        <div className="flex flex-col gap-1 mt-3">
+          {supportedTokens.map((token) => {
+            return (
+              <a
+                  key={token.mint.toBase58()}
+                  onClick={() => {
+                  setSelectedMint(token.mint);
+                  handleClose();
+                }}
+              >
+                <div
+                  className="flex flex-row w-full gap-4 p-2 rounded-lg hover:bg-gray-700 cursor-pointer"
+                >
+                  <Image src={token.iconUrl} alt="token logo" height={35} width={35} />
+
+                  <Typography className="flex h-full font-aeonik font-[500] text-2xl items-center">
+                    {token.symbol}
+                  </Typography>
+                </div>
+              </a>
+            );
+          })}
         </div>
       </div>
     </Modal>
--- apps/marginfi-v2-ui/src/pages/stake.tsx
+++ apps/marginfi-v2-ui/src/pages/stake.tsx
@@ -1,3 +1,5 @@
+import { JupiterProvider } from "@jup-ag/react-hook";
+import { createJupiterStore } from "@mrgnlabs/marginfi-v2-ui-state";
 import { useConnection } from "@solana/wallet-adapter-react";
 import { useEffect } from "react";
 import { PageHeader } from "~/components/PageHeader";
@@ -7,25 +9,28 @@ import { useWalletContext } from "~/components/useWalletContext";
 import { createLstStore } from "~/store/lstStore";
 
 export const useLstStore = createLstStore();
+export const useJupiterStore = createJupiterStore();
 
 const StakePage = () => {
   const { wallet } = useWalletContext();
   const { connection } = useConnection();
-  
-  const [
-  fetchLstState,
-  setIsRefreshingStore,
-] = useLstStore((state) => [
-  state.fetchLstState,
-  state.setIsRefreshingStore,
-  state.userDataFetched,
-]);
+
+  const [fetchLstState, setIsRefreshingStore] = useLstStore((state) => [
+    state.fetchLstState,
+    state.setIsRefreshingStore,
+    state.userDataFetched,
+  ]);
+
+  const [fetchJupiterState] = useJupiterStore((state) => [state.fetchJupiterState]);
+
   useEffect(() => {
     setIsRefreshingStore(true);
     fetchLstState({ connection, wallet }).catch(console.error);
+    fetchJupiterState({ connection, wallet }).catch(console.error);
     const id = setInterval(() => {
       setIsRefreshingStore(true);
       fetchLstState().catch(console.error);
+      fetchJupiterState().catch(console.error);
     }, 30_000);
     return () => clearInterval(id);
   }, [wallet]); // eslint-disable-line react-hooks/exhaustive-deps
@@ -33,13 +38,13 @@ const StakePage = () => {
   // TODO: fix...
 
   return (
-    <>
+    <JupiterProvider connection={connection}>
       <PageHeader text={"$LST"} />
       <div className="flex flex-col h-full justify-center content-center pt-[64px] sm:pt-[16px] gap-4 mx-4">
         <StakingStats />
         <StakingCard />
       </div>
-    </>
+    </JupiterProvider>
   );
 };
 
