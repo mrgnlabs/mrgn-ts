d753a76b37d69ca9cbdc5931a359e0bd49ba3d46
man0s <95379755+losman0s@users.noreply.github.com>
2023-09-22 16:16:34 +0800

    feat(mfi-v2-ui): use env var for Sentry DSN

--- apps/marginfi-v2-ui/sentry.edge.config.ts
+++ apps/marginfi-v2-ui/sentry.edge.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://4e45fad1924b45c8de06994f5e49a23d@o1279738.ingest.sentry.io/4505855634636800",
+  dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1,
   debug: false,
 });
