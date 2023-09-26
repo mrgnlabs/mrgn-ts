f792d27b887a5887762b9e99fba5a7cb252887d9
man0s <95379755+losman0s@users.noreply.github.com>
2023-09-26 15:10:56 +0800

    feat(mfi-v2-ui): some final touches

--- apps/marginfi-v2-ui/src/pages/api/user/utils.ts
+++ apps/marginfi-v2-ui/src/pages/api/user/utils.ts
@@ -111,31 +111,3 @@ export async function createFirebaseUser(walletAddress: string, referralCode?: s
     referralCode: uuidv4(),
   });
 }
-
-// ------- Next helpers
-
-export declare type Env = {
-  [key: string]: string | undefined;
-};
-
-export interface NextApiRequest<T> extends IncomingMessage {
-  /**
-   * Object of `query` values from url
-   */
-  query: Partial<{
-    [key: string]: string | string[];
-  }>;
-  /**
-   * Object of `cookies` from header
-   */
-  cookies: Partial<{
-    [key: string]: string;
-  }>;
-  body: T;
-  env: Env;
-  preview?: boolean;
-  /**
-   * Preview data set on the request, if any
-   * */
-  previewData?: PreviewData;
-}
