5991431494e03a76299643787df4e419ab3a0264
Adam Chambers <adammchambers@gmail.com>
2023-10-16 17:30:42 -0400

    fix: increase sns throttle to 100ms

--- packages/marginfi-v2-ui-state/src/lib/points.ts
+++ packages/marginfi-v2-ui-state/src/lib/points.ts
@@ -79,7 +79,7 @@ async function fetchLeaderboardData({
 
   const leaderboardFinalSliceWithDomains: LeaderboardRow[] = await Promise.all(
     leaderboardFinalSlice.map(async (value) => {
-      sleep(50);
+      sleep(100);
       const newValue = { ...value, shortAddress: shortAddress(value.id) };
       // attempt to get favorite domain
       try {
