565585df18a3f1bf61682d0bccfd5c2a2b2c69c6
Adam Chambers <adammchambers@gmail.com>
2023-10-16 21:02:56 -0400

    fix: comment out sns logic to fetch all domains, default to favorite

--- packages/marginfi-v2-ui-state/src/lib/points.ts
+++ packages/marginfi-v2-ui-state/src/lib/points.ts
@@ -90,22 +90,24 @@ async function fetchLeaderboardData({
         };
       } catch (e) {
         // attempt to get all domains
-        try {
-          await sleep(100);
-          const domains = await getAllDomains(connection, new PublicKey(value.id));
-          if (domains.length > 0) {
-            const reverse = await reverseLookup(connection, domains[0]);
-            return {
-              ...newValue,
-              domain: `${reverse}.sol`,
-            };
-          }
-        } catch (e) {
-          return newValue;
-        }
+        // try {
+        //   await sleep(100);
+        //   const domains = await getAllDomains(connection, new PublicKey(value.id));
+        //   if (domains.length > 0) {
+        //     await sleep(100);
+        //     const reverse = await reverseLookup(connection, domains[0]);
+        //     return {
+        //       ...newValue,
+        //       domain: `${reverse}.sol`,
+        //     };
+        //   }
+        // } catch (e) {
+        //   return newValue;
+        // }
+        return newValue;
       }
 
-      return newValue;
+      // return newValue;
     })
   );
 
