946caffc9f99115b5623f502b96c01a6f0ee6866
Adam Chambers <adammchambers@gmail.com>
2023-10-18 09:07:48 -0400

    chore: explore moongate customization

--- apps/marginfi-v2-ui/src/components/common/Navbar/WalletButton.tsx
+++ apps/marginfi-v2-ui/src/components/common/Navbar/WalletButton.tsx
@@ -1,36 +1,23 @@
-import dynamic from "next/dynamic";
 import { FC } from "react";
 import { useWalletContext } from "~/hooks/useWalletContext";
 import { AuthModal } from "@moongate/wallet-wrapper-sdk";
-import {
-  useWalletModal,
-} from "@solana/wallet-adapter-react-ui";
+import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 import { useWallet } from "@solana/wallet-adapter-react";
 
-const WalletMultiButtonDynamic = dynamic(
-  async () => (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
-  { ssr: false }
-);
-
 const WalletButton: FC = () => {
-  const { connected, walletContextState, openWalletSelector } = useWalletContext();
+  const { connected } = useWalletContext();
   const { select } = useWallet();
-  const { visible, setVisible } = useWalletModal();
+  const { setVisible } = useWalletModal();
 
   return (
     <div>
-      <AuthModal select={select} connected={connected} setVisible={setVisible}>
-        {walletContextState.connecting ? (
-          <a
-            onClick={walletContextState.connecting ? openWalletSelector : undefined}
-            className={"font-aeonik font-[500]"}
-          >
-            CONNECTING
-          </a>
-        ) : (
-          !connected && <div className={"font-aeonik font-[500]"}>CONNECT</div>
-        )}
-      </AuthModal>
+      <AuthModal
+        select={select}
+        connected={connected}
+        setVisible={setVisible}
+        btnStyles="bg-white rounded-md px-4 text-sm font-aeonik font-[500] text-black"
+        modalStyles="bg-[#171C1F] text-white rouded-lg"
+      />
     </div>
   );
 };
