37c313b961e6a2bce9fa4d8f42cedce2c081a588
Adam Chambers <adammchambers@gmail.com>
2023-11-03 14:30:34 -0400

    feat: store tutorial acknowledgement in cookie

--- apps/marginfi-v2-ui/src/components/common/icons/Alert.tsx
+++ apps/marginfi-v2-ui/src/components/common/icons/Alert.tsx
@@ -4,11 +4,11 @@ const Alert: FC<SVGProps<SVGSVGElement>> = ({ ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    stroke-width="2"
+    strokeWidth="2"
     stroke="currentColor"
     fill="none"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
