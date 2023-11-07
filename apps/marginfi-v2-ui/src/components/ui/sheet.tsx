f817d36beb9354dc427cb49c34198e443a3c9e7e
Adam Chambers <adammchambers@gmail.com>
2023-11-07 09:42:17 -0500

    feat: switch logout / private key from text links to outline buttons on small screens

--- apps/marginfi-v2-ui/src/components/ui/sheet.tsx
+++ apps/marginfi-v2-ui/src/components/ui/sheet.tsx
@@ -68,12 +68,12 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
 SheetContent.displayName = SheetPrimitive.Content.displayName;
 
 const SheetHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("flex flex-col space-y-2 text-center sm:text-left", className)} {...props} />
+  <div className={cn("flex flex-col space-y-2 text-center md:text-left", className)} {...props} />
 );
 SheetHeader.displayName = "SheetHeader";
 
 const SheetFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2", className)} {...props} />
+  <div className={cn("flex flex-col-reverse md:flex-row md:justify-center md:space-x-2", className)} {...props} />
 );
 SheetFooter.displayName = "SheetFooter";
 
