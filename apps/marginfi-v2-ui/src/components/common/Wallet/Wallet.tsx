293771a15d7b4e36379d87c2c11de681d8c82b9b
Adam Chambers <adammchambers@gmail.com>
2023-11-07 18:40:51 -0500

    chore: abstract pk dialog to separate sub component

--- apps/marginfi-v2-ui/src/components/common/Wallet/Wallet.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/Wallet.tsx
@@ -8,9 +8,8 @@ import { useConnection } from "~/hooks/useConnection";
 import { useWalletContext } from "~/hooks/useWalletContext";
 import { useWeb3AuthWallet } from "~/hooks/useWeb3AuthWallet";
 import { useIsMobile } from "~/hooks/useIsMobile";
-import { WalletAvatar, WalletTokens, Token, WalletOnramp } from "~/components/common/Wallet";
+import { WalletAvatar, WalletTokens, Token, WalletOnramp, WalletPkDialog } from "~/components/common/Wallet";
 import { Sheet, SheetContent, SheetTrigger, SheetFooter } from "~/components/ui/sheet";
-import { Dialog, DialogContent } from "~/components/ui/dialog";
 import { Button } from "~/components/ui/button";
 import { IconCheck, IconChevronDown, IconCopy } from "~/components/ui/icons";
 import { MrgnTooltip } from "../MrgnTooltip";
@@ -266,51 +265,7 @@ export const Wallet = () => {
           )}
         </SheetContent>
       </Sheet>
-      <Dialog
-        open={Boolean(pk)}
-        onOpenChange={(open) => {
-          if (!open) {
-            resetPk();
-          }
-        }}
-      >
-        <DialogContent className="md:max-w-[640px]">
-          <div className="flex flex-col items-center space-y-4">
-            <h2 className="font-medium text-2xl">Your private key</h2>
-            <p>
-              Your private key grants access to your wallet. Keep it safe at all time. marginfi does not store your
-              private key and cannot help you recover your wallet.
-            </p>
-          </div>
-          <div className="space-y-2">
-            <CopyToClipboard
-              text={pk}
-              onCopy={() => {
-                setIsPrivateKeyCopied(true);
-                setTimeout(() => {
-                  setIsPrivateKeyCopied(false);
-                }, 2000);
-              }}
-            >
-              <MrgnTooltip title="Click to copy private key">
-                <button className="font-medium flex items-center gap-1 cursor-pointer">
-                  {isPrivateKeyCopied && (
-                    <>
-                      <IconCheck size={14} /> copied!
-                    </>
-                  )}
-                  {!isPrivateKeyCopied && (
-                    <>
-                      <IconCopy size={14} /> Copy to clipboard
-                    </>
-                  )}
-                </button>
-              </MrgnTooltip>
-            </CopyToClipboard>
-            <div className="break-words font-mono text-xs p-2 border rounded-md max-w-[540px]">{pk}</div>
-          </div>
-        </DialogContent>
-      </Dialog>
+      <WalletPkDialog pk={pk} resetPk={resetPk} />
     </>
   );
 };
