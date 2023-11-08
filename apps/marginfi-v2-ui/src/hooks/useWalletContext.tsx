e851054e05ec897dbac9fe8d453f29bbb42bb06e
Adam Chambers <adammchambers@gmail.com>
2023-11-08 11:44:22 -0500

    feat: create wallet context override for web3auth wallets, fix jupiter terminal embed

--- apps/marginfi-v2-ui/src/hooks/useWalletContext.tsx
+++ apps/marginfi-v2-ui/src/hooks/useWalletContext.tsx
@@ -1,12 +1,52 @@
 import { Wallet } from "@mrgnlabs/mrgn-common";
-import { useAnchorWallet, useWallet } from "@solana/wallet-adapter-react";
+import { useAnchorWallet, useWallet, WalletContextState } from "@solana/wallet-adapter-react";
 import { PublicKey, Transaction, VersionedTransaction } from "@solana/web3.js";
 import { useRouter } from "next/router";
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useWeb3AuthWallet } from "./useWeb3AuthWallet";
 
+type WalletContextOveride = {
+  connected: boolean;
+  connecting: boolean;
+  icon: string;
+  disconnect: () => void;
+  select: () => void;
+  publicKey: PublicKey | undefined;
+  signTransaction: <T extends Transaction | VersionedTransaction>(transactions: T) => Promise<T>;
+  signAllTransactions: <T extends Transaction | VersionedTransaction>(transactions: T[]) => Promise<T[]>;
+  signMessage?: (message: Uint8Array) => Promise<Uint8Array>;
+};
+
+type WalletContextStateOveride = {
+  wallet: {
+    adapter: WalletContextOveride;
+  };
+} & WalletContextOveride;
+
+const makeWeb3AuthWalletContextState = (wallet: Wallet): WalletContextStateOveride => {
+  const walletProps: WalletContextOveride = {
+    connected: true,
+    connecting: false,
+    icon: "https://app.marginfi.com/mrgn-white.svg",
+    disconnect: () => {},
+    select: () => {},
+    publicKey: wallet?.publicKey,
+    signTransaction: wallet?.signTransaction,
+    signAllTransactions: wallet?.signAllTransactions,
+    signMessage: wallet?.signMessage,
+  };
+  return {
+    ...walletProps,
+    wallet: {
+      adapter: {
+        ...walletProps,
+      },
+    },
+  };
+};
+
 const useWalletContext = () => {
-  const walletContextState = useWallet();
+  let walletContextStateDefault = useWallet();
   const anchorWallet = useAnchorWallet();
   const {
     walletData: web3AuthWalletData,
@@ -15,15 +55,20 @@ const useWalletContext = () => {
     logout: web3AuthLogout,
   } = useWeb3AuthWallet();
   const { query } = useRouter();
+  const [walletContextState, setWalletContextState] = useState<WalletContextState | WalletContextStateOveride>(
+    walletContextStateDefault
+  );
 
   const { wallet, isOverride }: { wallet: Wallet | undefined; isOverride: boolean } = useMemo(() => {
     const override = query?.wallet as string;
     if (web3AuthWalletData && web3AuthConnected) {
+      setWalletContextState(makeWeb3AuthWalletContextState(web3AuthWalletData));
       return {
         wallet: web3AuthWalletData,
         isOverride: false,
       };
     } else if (anchorWallet && override) {
+      setWalletContextState(walletContextStateDefault);
       return {
         wallet: {
           ...anchorWallet,
@@ -39,6 +84,7 @@ const useWalletContext = () => {
         isOverride: true,
       };
     }
+    setWalletContextState(walletContextStateDefault);
     return {
       wallet: {
         ...anchorWallet,
@@ -53,7 +99,7 @@ const useWalletContext = () => {
       },
       isOverride: false,
     };
-  }, [anchorWallet, web3AuthWalletData, query]);
+  }, [anchorWallet, web3AuthWalletData, query, walletContextStateDefault, web3AuthConnected]);
 
   const logout = useCallback(() => {
     if (web3AuthConnected) {
