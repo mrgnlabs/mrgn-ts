cc8bc76b3897d939fab575267c64856d303a7e69
Adam Chambers <adammchambers@gmail.com>
2023-11-09 09:07:01 -0500

    chore: switch LSTDialog to new dialog component

--- apps/marginfi-v2-ui/src/components/common/AssetList/LSTDialog.tsx
+++ apps/marginfi-v2-ui/src/components/common/AssetList/LSTDialog.tsx
@@ -1,8 +1,9 @@
-import { useMemo } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button, Dialog, DialogContent } from "@mui/material";
 import { useMrgnlendStore } from "~/store";
+import { Dialog, DialogContent } from "~/components/ui/dialog";
+import { Button } from "~/components/ui/button";
 
 export enum LSTDialogVariants {
   SOL = "SOL",
@@ -18,7 +19,7 @@ type LSTDialogProps = {
 const LSTDialog = ({ variant, open, onClose }: LSTDialogProps) => {
   const [sortedBanks] = useMrgnlendStore((state) => [state.extendedBankInfos]);
 
-  const tokenImage = useMemo(() => {
+  const tokenImage = React.useMemo(() => {
     const bank = sortedBanks.find((bank) => bank.meta.tokenSymbol === variant);
     if (!bank) return null;
 
@@ -26,75 +27,60 @@ const LSTDialog = ({ variant, open, onClose }: LSTDialogProps) => {
   }, [variant, sortedBanks]);
 
   return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      maxWidth="xl"
-      PaperProps={{
-        style: {
-          backgroundColor: "transparent",
-          boxShadow: "none",
-        },
-      }}
-    >
-      <DialogContent className="bg-[#171C1F] w-full rounded-lg text-white items-center justify-center text-center py-16">
-        <div className="w-full lg:min-w-[900px]">
-          <div className="max-w-[540px] mx-auto flex flex-col gap-6">
-            {tokenImage && (
-              <div className="flex items-center justify-center">
-                <Image
-                  src={tokenImage}
-                  alt={tokenImage}
-                  height={80}
-                  width={80}
-                  className="rounded-full translate-x-4 drop-shadow-lg"
-                />
-                <Image
-                  src="https://storage.googleapis.com/static-marginfi/lst.png"
-                  alt="lst"
-                  height={80}
-                  width={80}
-                  className="rounded-full -translate-x-4 drop-shadow-lg"
-                />
-              </div>
-            )}
-            {variant === LSTDialogVariants.stSOL && (
-              <>
-                <h2 className="text-2xl font-bold">Swap stSOL for LST?</h2>
-                <div className="space-y-4">
-                  <p>Lido is sunsetting stSOL. Swap your stSOL for LST now and earn higher yield</p>
-                  <div className="text-[#DCE85D] space-y-2 font-bold">
-                    <p>LST is the only 8% yielding LST.</p>
-                    <p>LST is the only 0% commission LST.</p>
-                    <p>LST is the only 0 fee LST.</p>
-                  </div>
-                </div>
-              </>
-            )}
-            {variant === LSTDialogVariants.SOL && (
-              <>
-                <h2 className="text-2xl font-bold">Stake for LST?</h2>
-                <div className="space-y-4">
-                  <p>The highest natural yield available from any LST on Solana. By a lot.</p>
-                  <div className="text-[#DCE85D] space-y-2 font-bold">
-                    <p>LST is the only 8% yielding LST.</p>
-                    <p>LST is the only 0% commission LST.</p>
-                    <p>LST is the only 0 fee LST.</p>
-                  </div>
-                </div>
-              </>
-            )}
-            <div className="flex flex-col space-y-4 mt-3">
-              <Link href={variant === LSTDialogVariants.stSOL ? "/stake?deposit=stSOL" : "/stake"}>
-                <Button className="bg-white text-[#020815] normal-case mx-auto px-4 hover:bg-white/80">Mint LST</Button>
-              </Link>
-              <button
-                className=" border-white/50 text-white/50 transition hover:border-white hover:text-white mx-auto text-sm"
-                onClick={() => onClose()}
-              >
-                No thanks, continue
-              </button>
+    <Dialog open={open} onOpenChange={() => onClose()}>
+      <DialogContent className="md:max-w-4xl">
+        <div className="flex flex-col items-center text-center gap-6">
+          {tokenImage && (
+            <div className="flex items-center justify-center">
+              <Image
+                src="https://storage.googleapis.com/static-marginfi/lst.png"
+                alt="lst"
+                height={80}
+                width={80}
+                className="rounded-full translate-x-4 drop-shadow-lg"
+              />
+              <Image
+                src={tokenImage}
+                alt={tokenImage}
+                height={80}
+                width={80}
+                className="rounded-full -translate-x-4 drop-shadow-lg"
+              />
             </div>
+          )}
+          {variant === LSTDialogVariants.stSOL && (
+            <>
+              <h2 className="text-2xl font-bold">Swap stSOL for LST?</h2>
+              <div className="space-y-4">
+                <p>Lido is sunsetting stSOL. Swap your stSOL for LST now and earn higher yield</p>
+                <div className="text-[#DCE85D] space-y-2 font-bold">
+                  <p>LST is the only 8% yielding LST.</p>
+                  <p>LST is the only 0% commission LST.</p>
+                  <p>LST is the only 0 fee LST.</p>
+                </div>
+              </div>
+            </>
+          )}
+          {variant === LSTDialogVariants.SOL && (
+            <>
+              <h2 className="text-2xl font-bold">Stake for LST?</h2>
+              <div className="space-y-4">
+                <p>The highest natural yield available from any LST on Solana. By a lot.</p>
+                <div className="text-[#DCE85D] space-y-2 font-bold">
+                  <p>LST is the only 8% yielding LST.</p>
+                  <p>LST is the only 0% commission LST.</p>
+                  <p>LST is the only 0 fee LST.</p>
+                </div>
+              </div>
+            </>
+          )}
+          <div className="flex flex-col space-y-4 mt-3">
+            <Link href={variant === LSTDialogVariants.stSOL ? "/stake?deposit=stSOL" : "/stake"}>
+              <Button className="bg-white text-[#020815] normal-case mx-auto px-4 hover:bg-white/80">Mint LST</Button>
+            </Link>
+            <Button variant="link" onClick={() => onClose()} className="text-muted-foreground">
+              No thanks, continue
+            </Button>
           </div>
         </div>
       </DialogContent>
