016d46029a302aa38eb25e0800bb9a3f2ad7222b
Adam Chambers <adammchambers@gmail.com>
2023-11-09 13:46:06 -0500

    feat: switch pwa notice to header banner with local storage integration

--- apps/marginfi-v2-ui/tailwind.config.js
+++ apps/marginfi-v2-ui/tailwind.config.js
@@ -44,6 +44,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        chartreuse: "#DCE85D",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
