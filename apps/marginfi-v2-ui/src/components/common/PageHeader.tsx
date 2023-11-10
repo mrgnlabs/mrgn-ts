a66d4fcb3b11e2854886f5e9e681f23935c25bc4
Adam Chambers <adammchambers@gmail.com>
2023-11-10 11:41:51 -0500

    feat: improve skeleton loading in asset grid

--- apps/marginfi-v2-ui/src/components/common/PageHeader.tsx
+++ apps/marginfi-v2-ui/src/components/common/PageHeader.tsx
@@ -13,7 +13,7 @@ const PageHeader: FC<PageHeaderProps> = ({ children }) => {
 
   return (
     <div className="flex w-full h-[90px] sm:h-[60px] justify-center items-center border-y bg-[url('/WaveBG3.png')]">
-      <div className="w-full px-4 sm:w-4/5 sm:max-w-7xl flex flex-row justify-between items-center border-solid font-aeonik font-normal text-2xl sm:text-3xl">
+      <div className="w-full px-4 sm:max-w-7xl flex flex-row justify-between items-center border-solid font-aeonik font-normal text-2xl sm:text-3xl">
         <div className="flex gap-4 justify-center items-center">
           <Mobile>
             <div className="ml-[4px]">
