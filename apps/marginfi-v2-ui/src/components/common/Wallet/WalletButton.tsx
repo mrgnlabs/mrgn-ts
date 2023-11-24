6162125c1ad8b5480d9131b43789d661655e8d76
Kobe Leenders <leenderskobe@gmail.com>
2023-11-24 15:40:29 +0100

    feat: store login provider

--- apps/marginfi-v2-ui/src/components/common/Wallet/WalletButton.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/WalletButton.tsx
@@ -1,21 +1,78 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
+import Image from "next/image";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 import { useUiStore } from "~/store";
-import { useWalletContext } from "~/hooks/useWalletContext";
+import { WalletInfo, Web3AuthProvider, useWalletContext } from "~/hooks/useWalletContext";
 
 import { Wallet } from "~/components/common/Wallet";
-
+import { IconChevronDown, IconBrandGoogle, IconBrandX, IconBrandApple, IconMrgn } from "~/components/ui/icons";
 import { Button } from "~/components/ui/button";
 
+const web3AuthIconMap: { [key in Web3AuthProvider]: { icon: JSX.Element } } = {
+  google: {
+    icon: <IconBrandGoogle />,
+  },
+  twitter: {
+    icon: <IconBrandX />,
+  },
+  apple: {
+    icon: <IconBrandApple />,
+  },
+  email_passwordless: {
+    icon: <IconMrgn />,
+  },
+};
+
 export const WalletButton = () => {
-  const { connected } = useWalletContext();
+  const { select, wallets } = useWallet();
+  const { connected, loginWeb3Auth } = useWalletContext();
   const [setIsWalletAuthDialogOpen] = useUiStore((state) => [state.setIsWalletAuthDialogOpen]);
+
+  const walletInfo = useMemo(() => JSON.parse(localStorage.getItem("walletInfo")) as WalletInfo | null, [connected]);
+
+  const WalletIcon = useMemo(() => {
+    if (walletInfo?.icon) {
+      return () => <Image src={walletInfo.icon} alt="wallet_icon" width={24} height={24} />;
+    } else {
+      return () => web3AuthIconMap[walletInfo?.name]?.icon ?? <IconMrgn />;
+    }
+  }, [walletInfo]);
+
+  const handleWalletConnect = useCallback(() => {
+    try {
+      if (!walletInfo) throw new Error("No local storage");
+      if (walletInfo.web3Auth) {
+        if (walletInfo.name === "email_passwordless") {
+          loginWeb3Auth("email_passwordless", { login_hint: walletInfo.email });
+        } else {
+          loginWeb3Auth(walletInfo.name);
+        }
+      } else {
+        select(walletInfo.name as any);
+      }
+    } catch (error) {
+      setIsWalletAuthDialogOpen(true);
+    }
+  }, [walletInfo]);
 
   return (
     <>
       {!connected ? (
-        <Button onClick={() => setIsWalletAuthDialogOpen(true)} className="gap-1.5">
-          Sign In
+        <Button className="gap-1.5 py-0">
+          <div className="flex flex-row relative h-full relative gap-4">
+            <div onClick={() => handleWalletConnect()} className="inline-flex items-center gap-2">
+              Sign In with {walletInfo && <WalletIcon />}
+            </div>
+            {walletInfo && (
+              <div
+                onClick={() => setIsWalletAuthDialogOpen(true)}
+                className="pl-1 -mr-2 border-l-2 border-[#0f1010] inline-flex items-center"
+              >
+                <IconChevronDown />
+              </div>
+            )}
+          </div>
         </Button>
       ) : (
         <Wallet />
