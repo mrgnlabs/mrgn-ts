02244935e17ff069f1c710ef4f02b814389b5890
Adam Chambers <adammchambers@gmail.com>
2023-11-24 21:57:30 -0500

    feat: mobile inline positions

--- apps/marginfi-v2-ui/src/components/mobile/MobileAssetsList/AssetCard/AssetCardPosition.tsx
+++ apps/marginfi-v2-ui/src/components/mobile/MobileAssetsList/AssetCard/AssetCardPosition.tsx
@@ -1,42 +1,31 @@
-import React, { FC, useState } from "react";
+import React from "react";
 
-import { usdFormatter } from "@mrgnlabs/mrgn-common";
+import { usdFormatter, numeralFormatter } from "@mrgnlabs/mrgn-common";
 import { ActiveBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
 
-import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { cn } from "~/utils";
 
-export const AssetCardPosition: FC<{
-  activeBank: ActiveBankInfo;
-}> = ({ activeBank }) => {
-  const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
-
+export const AssetCardPosition = ({ activeBank }: { activeBank: ActiveBankInfo }) => {
   return (
-    <div className="flex flex-col bg-[#23272B] p-[12px] rounded-lg">
-      <div className="flex flex-row justify-between" onClick={() => setIsCollapsed(!isCollapsed)}>
-        <div className="my-auto">Your position details</div>
-        <span className="cursor-pointer">{isCollapsed ? <ExpandMoreIcon /> : <ExpandLessIcon />}</span>
-      </div>
-      {!isCollapsed && (
-        <div className="flex flex-col gap-[8px] pt-[8px]">
-          <div className="flex flex-row justify-between">
-            <div className="text-[#A1A1A1] my-auto text-sm">
-              {(activeBank as ActiveBankInfo).position.isLending ? "Lending" : "Borrowing"}
-            </div>
-            <div className="text-primary text-sm my-auto">
-              {(activeBank as ActiveBankInfo).position.amount.toFixed(activeBank.info.state.mintDecimals) +
-                " " +
-                activeBank.meta.tokenSymbol}
-            </div>
-          </div>
-          <div className="flex flex-row justify-between">
-            <div className="text-[#A1A1A1] text-sm my-auto">USD value</div>
-            <div className="text-sm my-auto">
-              {usdFormatter.format((activeBank as ActiveBankInfo).position.usdValue)}
-            </div>
-          </div>
-        </div>
-      )}
+    <div className="bg-accent p-3.5 rounded-lg">
+      <h3>Your position details</h3>
+      <dl className="grid grid-cols-2 text-accent-foreground mt-2 text-sm w-full space-y-1">
+        <dt>{activeBank.position.isLending ? "Lending" : "Borrowing"}</dt>
+        <dd className="text-white font-medium text-right">
+          {activeBank.position.amount < 0.01 && "< "}
+          {numeralFormatter(activeBank.position.amount) + " " + activeBank.meta.tokenSymbol}
+        </dd>
+        <dt>USD Value</dt>
+        <dd className="text-white font-medium text-right">{usdFormatter.format(activeBank.position.usdValue)}</dd>
+        {activeBank.position.liquidationPrice && activeBank.position.liquidationPrice > 0 && (
+          <>
+            <dt>Liq price</dt>
+            <dd className="text-white font-medium text-right">
+              {usdFormatter.format(activeBank.position.liquidationPrice)}
+            </dd>
+          </>
+        )}
+      </dl>
     </div>
   );
 };
