6a4780b9789c66a77066e966bdbfa8759f9ada50
Kobe Leenders <leenderskobe@gmail.com>
2023-12-01 17:41:10 +0100

    feat: new portfolio comp WIP

---
+++ apps/marginfi-v2-ui/src/components/common/Portfolio/UserStats/UserStats.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+
+interface props {
+  supplied: string;
+  borrowed: string;
+  netValue: string;
+  interest: string;
+}
+
+export const UserStats: FC<props> = ({ supplied, borrowed, netValue, interest }) => {
+  return (
+    <div className="flex justify-between my-5">
+      <Stat label="Supplied" value={supplied} />
+      <Stat label="Borrowed" value={borrowed} />
+      <Stat label="Net value" value={netValue} />
+      <Stat label="Total interest" value={interest} />
+    </div>
+  );
+};
+
+const Stat = ({ label, value }: { label: string; value: string }) => (
+  <dl className="w-60">
+    <dt className="text-sm">{label}</dt>
+    <dd className="text-xl font-medium text-white">{value}</dd>
+  </dl>
+);
