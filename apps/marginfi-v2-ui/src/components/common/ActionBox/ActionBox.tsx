a2b282f4c78b34734ee7488636db96c363690e87
Adam Chambers <adammchambers@gmail.com>
2023-11-30 20:34:12 -0500

    feat: actionbox token combobox

--- apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBox.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBox.tsx
@@ -1,50 +1,139 @@
 import React from "react";
 
+import Image from "next/image";
+
 import { cn } from "~/utils";
+import { useMrgnlendStore, useUiStore } from "~/store";
 
 import { MrgnLabeledSwitch } from "~/components/common/MrgnLabeledSwitch";
 
 import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "~/components/ui/command";
+import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { IconChevronDown } from "~/components/ui/icons";
 
+import { LendingModes } from "~/types";
+
+import type { ExtendedBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
+
 export const ActionBox = () => {
+  const [extendedBankInfos] = useMrgnlendStore((state) => [state.extendedBankInfos]);
+  const [lendingMode, setLendingMode] = useUiStore((state) => [state.lendingMode, state.setLendingMode]);
   const [isTokenPopoverOpen, setIsTokenPopoverOpen] = React.useState(false);
+  const [currentToken, setCurrentToken] = React.useState<ExtendedBankInfo | null>(null);
+  const amountRef = React.useRef<HTMLInputElement>(null);
+
   return (
     <div className="bg-background p-4 flex flex-col items-center gap-4">
       <div className="space-y-6 text-center w-full flex flex-col items-center">
         <div className="flex w-[150px] h-[42px]">
-          <MrgnLabeledSwitch labelLeft="Lend" labelRight="Borrow" checked={true} onClick={() => {}} />
+          <MrgnLabeledSwitch
+            labelLeft="Lend"
+            labelRight="Borrow"
+            checked={lendingMode === LendingModes.BORROW}
+            onClick={() => {
+              setLendingMode(lendingMode === LendingModes.LEND ? LendingModes.BORROW : LendingModes.LEND);
+            }}
+          />
         </div>
         <p>Supply. Earn interest. Borrow. Repeat.</p>
       </div>
       <div className="p-6 bg-background-gray text-white w-full max-w-[480px] rounded-xl">
-        <p className="text-lg mb-3">You supply</p>
+        <p className="text-lg mb-3">You {lendingMode === LendingModes.LEND ? "supply" : "borrow"}</p>
         <div className="bg-background text-3xl rounded-lg flex justify-between items-center p-4 font-medium mb-5">
           <Popover open={isTokenPopoverOpen} onOpenChange={(open) => setIsTokenPopoverOpen(open)}>
             <PopoverTrigger asChild>
               <Button
                 className={cn(
-                  "bg-background-gray text-white text-xl p-6 pr-4 gap-4 transition-colors hover:bg-background-gray-light",
+                  "bg-background-gray text-white text-xl p-6 pr-5 gap-2.5 transition-colors hover:bg-background-gray-light",
                   isTokenPopoverOpen && "bg-background-gray-light"
                 )}
               >
-                Select token <IconChevronDown />
+                {currentToken && (
+                  <>
+                    <Image
+                      src={currentToken.meta.tokenLogoUri!}
+                      alt={currentToken.meta.tokenName}
+                      width={28}
+                      height={28}
+                      className="rounded-full"
+                    />
+                    <span>{currentToken.meta.tokenSymbol}</span>
+                  </>
+                )}
+                {!currentToken && <>Select token</>}
+                <IconChevronDown className="shrink-0 ml-2" />
               </Button>
             </PopoverTrigger>
-            <PopoverContent className="w-80" align="start" sideOffset={10}>
-              <ul className="flex flex-col gap-4">
-                <li>Token item 1</li>
-                <li>Token item 2</li>
-                <li>Token item 3</li>
-              </ul>
+            <PopoverContent className="p-2 w-[300px] max-h-96" align="start" side="bottom" sideOffset={10}>
+              <Command>
+                <CommandInput placeholder="Search token..." className="h-9 text-lg" />
+                <CommandEmpty>No framework found.</CommandEmpty>
+                <CommandGroup heading="In your wallet">
+                  {extendedBankInfos.slice(0, 3).map((bankInfo, index) => (
+                    <CommandItem
+                      key={index}
+                      value={bankInfo?.address?.toString().toLowerCase()}
+                      onSelect={(currentValue) => {
+                        setCurrentToken(
+                          extendedBankInfos.find(
+                            (bankInfo) => bankInfo.address.toString().toLowerCase() === currentValue
+                          ) ?? null
+                        );
+
+                        setIsTokenPopoverOpen(false);
+                      }}
+                      className="text-lg py-2 font-medium flex gap-3"
+                    >
+                      {bankInfo.meta.tokenLogoUri && (
+                        <Image
+                          src={bankInfo.meta.tokenLogoUri}
+                          alt={bankInfo.meta.tokenName}
+                          width={24}
+                          height={24}
+                          className="rounded-full"
+                        />
+                      )}
+                      <span>{bankInfo.meta.tokenSymbol}</span>
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+                <CommandGroup heading="All tokens">
+                  {extendedBankInfos.slice(3, 6).map((bankInfo, index) => (
+                    <CommandItem
+                      key={index}
+                      value={bankInfo?.address?.toString().toLowerCase()}
+                      onSelect={(currentValue) => {
+                        setCurrentToken(
+                          extendedBankInfos.find(
+                            (bankInfo) => bankInfo.address.toString().toLowerCase() === currentValue
+                          ) ?? null
+                        );
+                      }}
+                      className="text-lg py-2 font-medium flex gap-3"
+                    >
+                      {bankInfo.meta.tokenLogoUri && (
+                        <Image
+                          src={bankInfo.meta.tokenLogoUri}
+                          alt={bankInfo.meta.tokenName}
+                          width={24}
+                          height={24}
+                          className="rounded-full"
+                        />
+                      )}
+                      <span>{bankInfo.meta.tokenSymbol}</span>
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </Command>
             </PopoverContent>
           </Popover>
-          <input
+          <Input
             type="number"
-            value={0}
-            className="bg-transparent w-full text-right"
-            style={{ WebkitAppearance: "none" }}
+            ref={amountRef}
+            placeholder="0"
+            className="bg-transparent w-full text-right outline-none focus-visible:outline-none focus-visible:ring-0 border-none text-3xl font-medium"
           />
         </div>
         <Button className="w-full py-6">Select token and amount</Button>
