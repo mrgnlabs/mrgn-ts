4583bf96bc3ba0ed8ed968c0f9412a50745e6d8e
Adam Chambers <adammchambers@gmail.com>
2023-12-04 17:34:59 -0500

    feat: action box tokens abstracted and bank filtering implemented

--- apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBox.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBox.tsx
@@ -1,27 +1,19 @@
 import React from "react";
 
-import Image from "next/image";
-
-import { cn } from "~/utils";
 import { usdFormatter } from "@mrgnlabs/mrgn-common";
-import { useMrgnlendStore, useUiStore } from "~/store";
+import { useUiStore } from "~/store";
 
 import { MrgnLabeledSwitch } from "~/components/common/MrgnLabeledSwitch";
+import { ActionBoxTokens } from "~/components/common/ActionBox/ActionBoxTokens";
 
-import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "~/components/ui/command";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
-import { IconChevronDown } from "~/components/ui/icons";
 
 import { LendingModes } from "~/types";
-
 import type { ExtendedBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
 
 export const ActionBox = () => {
-  const [extendedBankInfos] = useMrgnlendStore((state) => [state.extendedBankInfos]);
   const [lendingMode, setLendingMode] = useUiStore((state) => [state.lendingMode, state.setLendingMode]);
-  const [isTokenPopoverOpen, setIsTokenPopoverOpen] = React.useState(false);
   const [currentToken, setCurrentToken] = React.useState<ExtendedBankInfo | null>(null);
   const [amount, setAmount] = React.useState<number | null>(null);
 
@@ -43,93 +35,7 @@ export const ActionBox = () => {
       <div className="p-6 bg-background-gray text-white w-full max-w-[480px] rounded-xl">
         <p className="text-lg mb-3">You {lendingMode === LendingModes.LEND ? "supply" : "borrow"}</p>
         <div className="bg-background text-3xl rounded-lg flex justify-between items-center p-4 font-medium mb-5">
-          <Popover open={isTokenPopoverOpen} onOpenChange={(open) => setIsTokenPopoverOpen(open)}>
-            <PopoverTrigger asChild>
-              <Button
-                className={cn(
-                  "bg-background-gray text-white text-xl p-6 pr-5 gap-2.5 transition-colors hover:bg-background-gray-light",
-                  isTokenPopoverOpen && "bg-background-gray-light"
-                )}
-              >
-                {currentToken && (
-                  <>
-                    <Image
-                      src={currentToken.meta.tokenLogoUri!}
-                      alt={currentToken.meta.tokenName}
-                      width={28}
-                      height={28}
-                      className="rounded-full"
-                    />
-                    <span>{currentToken.meta.tokenSymbol}</span>
-                  </>
-                )}
-                {!currentToken && <>Select token</>}
-                <IconChevronDown className="shrink-0 ml-2" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="p-2 w-[300px] max-h-96" align="start" side="bottom" sideOffset={10}>
-              <Command>
-                <CommandInput placeholder="Search token..." className="h-9 text-lg" />
-                <CommandEmpty>No framework found.</CommandEmpty>
-                <CommandGroup heading="In your wallet">
-                  {extendedBankInfos.slice(0, 3).map((bankInfo, index) => (
-                    <CommandItem
-                      key={index}
-                      value={bankInfo?.address?.toString().toLowerCase()}
-                      onSelect={(currentValue) => {
-                        setCurrentToken(
-                          extendedBankInfos.find(
-                            (bankInfo) => bankInfo.address.toString().toLowerCase() === currentValue
-                          ) ?? null
-                        );
-
-                        setIsTokenPopoverOpen(false);
-                      }}
-                      className="text-lg py-2 font-medium flex gap-3"
-                    >
-                      {bankInfo.meta.tokenLogoUri && (
-                        <Image
-                          src={bankInfo.meta.tokenLogoUri}
-                          alt={bankInfo.meta.tokenName}
-                          width={24}
-                          height={24}
-                          className="rounded-full"
-                        />
-                      )}
-                      <span>{bankInfo.meta.tokenSymbol}</span>
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-                <CommandGroup heading="All tokens">
-                  {extendedBankInfos.slice(3, 6).map((bankInfo, index) => (
-                    <CommandItem
-                      key={index}
-                      value={bankInfo?.address?.toString().toLowerCase()}
-                      onSelect={(currentValue) => {
-                        setCurrentToken(
-                          extendedBankInfos.find(
-                            (bankInfo) => bankInfo.address.toString().toLowerCase() === currentValue
-                          ) ?? null
-                        );
-                      }}
-                      className="text-lg py-2 font-medium flex gap-3"
-                    >
-                      {bankInfo.meta.tokenLogoUri && (
-                        <Image
-                          src={bankInfo.meta.tokenLogoUri}
-                          alt={bankInfo.meta.tokenName}
-                          width={24}
-                          height={24}
-                          className="rounded-full"
-                        />
-                      )}
-                      <span>{bankInfo.meta.tokenSymbol}</span>
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </Command>
-            </PopoverContent>
-          </Popover>
+          <ActionBoxTokens currentToken={currentToken} setCurrentToken={setCurrentToken} />
           <Input
             type="number"
             value={amount!}
