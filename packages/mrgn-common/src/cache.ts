6efa876d30a51bf4b58f7d15e6786696abdbb1a8
Jakob Povšič <jakob@margin.services>
2023-12-07 18:30:38 +0100

    fix: rename token metadata url

--- packages/mrgn-common/src/cache.ts
+++ packages/mrgn-common/src/cache.ts
@@ -49,7 +49,7 @@ function parseTokenMetadatas(tokenMetadataListRaw: TokenMetadataListRaw): {
 async function loadTokenMetadatas(): Promise<{
   [symbol: string]: TokenMetadata;
 }> {
-  const response = await fetch(`https://storage.googleapis.com/mrgn-public/mrgn-token-metadata-cache.json`, {
+  const response = await fetch(`https://storage.googleapis.com/mrgn-public/token-metadata.json`, {
     headers: {
       Accept: "application/json",
     },
