1961e8750c3dee94adadcc7d6227f20735398b58
Adam Chambers <adammchambers@gmail.com>
2023-12-13 13:46:41 +0000

    fix: clean up portfolio on small screens

--- apps/marginfi-v2-ui/src/components/common/Portfolio/UserStats/UserStats.tsx
+++ apps/marginfi-v2-ui/src/components/common/Portfolio/UserStats/UserStats.tsx
@@ -9,7 +9,7 @@ interface props {
 
 export const UserStats: FC<props> = ({ supplied, borrowed, netValue, interest }) => {
   return (
-    <div className="flex justify-between flex-wrap my-5">
+    <div className="flex justify-between flex-wrap mt-5 mb-10 gap-y-4">
       <Stat label="Supplied" value={supplied} />
       <Stat label="Borrowed" value={borrowed} />
       <Stat label="Net value" value={netValue} />
@@ -19,7 +19,7 @@ export const UserStats: FC<props> = ({ supplied, borrowed, netValue, interest })
 };
 
 const Stat = ({ label, value }: { label: string; value: string }) => (
-  <dl className="w-40 sm:w-60 ">
+  <dl className="w-1/2 md:w-auto">
     <dt className="text-sm">{label}</dt>
     <dd className="text-xl font-medium text-white">{value}</dd>
   </dl>
