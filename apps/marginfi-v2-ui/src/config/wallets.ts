17dd1a43f66be829310a883941587dd7be77e611
Adam Chambers <adammchambers@gmail.com>
2023-12-19 10:04:36 -0500

    feat: add wallet connect adapter

--- apps/marginfi-v2-ui/src/config/wallets.ts
+++ apps/marginfi-v2-ui/src/config/wallets.ts
@@ -4,6 +4,7 @@ import {
   GlowWalletAdapter,
   BackpackWalletAdapter,
   TrustWalletAdapter,
+  WalletConnectWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import {
   createDefaultAddressSelector,
@@ -11,6 +12,7 @@ import {
   createDefaultWalletNotFoundHandler,
   SolanaMobileWalletAdapter,
 } from "@solana-mobile/wallet-adapter-mobile";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 export const WALLET_ADAPTERS = [
   new SolanaMobileWalletAdapter({
@@ -29,4 +31,17 @@ export const WALLET_ADAPTERS = [
   new BackpackWalletAdapter(),
   new GlowWalletAdapter(),
   new TrustWalletAdapter(),
+  new WalletConnectWalletAdapter({
+    network: WalletAdapterNetwork.Mainnet,
+    options: {
+      relayUrl: "wss://relay.walletconnect.com",
+      projectId: "b60e76594a7010d8ab8744a3d6b53a9a",
+      metadata: {
+        name: "marginfi",
+        description: "marginfi",
+        url: "https://app.marginfi.com",
+        icons: ["https://app.marginfi.com/mrgn_logo_512.png"],
+      },
+    },
+  }),
 ];
