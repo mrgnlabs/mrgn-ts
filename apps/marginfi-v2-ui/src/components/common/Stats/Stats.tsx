02012461a8c6c0ff0e1ff87448a5a243eaa00aeb
Kobe Leenders <leenderskobe@gmail.com>
2023-12-20 14:16:38 +0100

    fix: centered stats

--- apps/marginfi-v2-ui/src/components/common/Stats/Stats.tsx
+++ apps/marginfi-v2-ui/src/components/common/Stats/Stats.tsx
@@ -29,12 +29,15 @@ export const Stats = () => {
   return (
     <ul className="flex text-muted-foreground gap-4 justify-center mt-10 mb-6 text-base md:gap-8 md:text-base">
       {statsList.map((stat, index) => (
-        <li key={index} className={cn(index !== statsList.length - 1 && "border-r-2 border-white/20 pr-8")}>
-          <dl className="space-y-1 md:space-y-2">
-            <dt>{stat.label}</dt>
-            <dd className="font-bold md:text-2xl text-white">{stat.value}</dd>
-          </dl>
-        </li>
+        <>
+          <li key={index} >
+            <dl className="space-y-1 md:space-y-2">
+              <dt>{stat.label}</dt>
+              <dd className="font-bold md:text-2xl text-white">{stat.value}</dd>
+            </dl>
+          </li>
+          {index !== statsList.length - 1 && <li className={"border-r-2 border-white/20"} />}
+        </>
       ))}
     </ul>
   );
