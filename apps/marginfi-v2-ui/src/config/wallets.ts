4d6935c02bda0b26af757b0ad4069e418cda80c9
Adam Chambers <adammchambers@gmail.com>
2023-12-26 10:12:27 -0500

    chore: upgrade wallet adapter, add wc config

--- apps/marginfi-v2-ui/src/config/wallets.ts
+++ apps/marginfi-v2-ui/src/config/wallets.ts
@@ -1,9 +1,8 @@
 import {
   PhantomWalletAdapter,
   SolflareWalletAdapter,
-  GlowWalletAdapter,
-  BackpackWalletAdapter,
   TrustWalletAdapter,
+  WalletConnectWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import {
   createDefaultAddressSelector,
@@ -11,6 +10,7 @@ import {
   createDefaultWalletNotFoundHandler,
   SolanaMobileWalletAdapter,
 } from "@solana-mobile/wallet-adapter-mobile";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 export const WALLET_ADAPTERS = [
   new SolanaMobileWalletAdapter({
@@ -26,7 +26,18 @@ export const WALLET_ADAPTERS = [
   }),
   new PhantomWalletAdapter(),
   new SolflareWalletAdapter(),
-  new BackpackWalletAdapter(),
-  new GlowWalletAdapter(),
   new TrustWalletAdapter(),
+  new WalletConnectWalletAdapter({
+    network: WalletAdapterNetwork.Mainnet,
+    options: {
+      relayUrl: "wss://relay.walletconnect.com",
+      projectId: "b60e76594a7010d8ab8744a3d6b53a9a",
+      metadata: {
+        name: "marginfi",
+        description: "marginfi",
+        url: "https://app.marginfi.com",
+        icons: ["https://app.marginfi.com/mrgn_logo_512.png"],
+      },
+    },
+  }),
 ];
