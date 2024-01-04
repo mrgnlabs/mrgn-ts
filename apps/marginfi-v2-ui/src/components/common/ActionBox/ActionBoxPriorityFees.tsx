092480403eb68477a7990c2bceddf893f67315a5
Adam Chambers <adammchambers@gmail.com>
2024-01-04 10:58:00 -0500

    chore: abstract actionbox priorityfees sub component

---
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxPriorityFees.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+import { useUiStore } from "~/store";
+import { cn } from "~/utils";
+
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { IconInfoCircle } from "~/components/ui/icons";
+
+type ActionBoxPriorityFeesProps = {
+  setIsPriorityFeesMode: (value: boolean) => void;
+};
+
+export const ActionBoxPriorityFees = ({ setIsPriorityFeesMode }: ActionBoxPriorityFeesProps) => {
+  const [priorityFee, setPriorityFee] = useUiStore((state) => [state.priorityFee, state.setPriorityFee]);
+
+  const priorityFeeRef = React.useRef<HTMLInputElement>(null);
+  const [isCustomPriorityFeeMode, setIsCustomPriorityFeeMode] = React.useState<boolean>(false);
+  const [customPriorityFee, setCustomPriorityFee] = React.useState<number | null>(null);
+
+  return (
+    <>
+      <h2 className="text-lg font-normal mb-2 flex items-center gap-2">
+        Set transaction priority <IconInfoCircle size={16} />
+      </h2>
+      <ul className="grid grid-cols-3 gap-3 mb-6">
+        <li>
+          <Button
+            className={cn(
+              "flex flex-col gap-0.5 h-auto w-full font-light border border-transparent bg-background/50 transition-colors hover:bg-background-gray-hover",
+              priorityFee === 0.0 && customPriorityFee === null && "bg-background-gray-hover border-chartreuse"
+            )}
+            variant="secondary"
+            onClick={() => {
+              setPriorityFee(0);
+              setCustomPriorityFee(null);
+              setIsCustomPriorityFeeMode(false);
+            }}
+          >
+            Normal <strong className="font-medium">0 SOL</strong>
+          </Button>
+        </li>
+        <li>
+          <Button
+            className={cn(
+              "flex flex-col gap-0.5 h-auto w-full font-light border border-transparent bg-background/50 transition-colors hover:bg-background-gray-hover",
+              priorityFee === 0.00005 && customPriorityFee === null && "bg-background-gray-hover border-chartreuse"
+            )}
+            variant="secondary"
+            onClick={() => {
+              setPriorityFee(0.00005);
+              setCustomPriorityFee(null);
+              setIsCustomPriorityFeeMode(false);
+            }}
+          >
+            High <strong className="font-medium">0.00005 SOL</strong>
+          </Button>
+        </li>
+        <li>
+          <Button
+            className={cn(
+              "flex flex-col gap-0.5 h-auto w-full font-light border border-transparent bg-background/50 transition-colors hover:bg-background-gray-hover",
+              priorityFee === 0.005 && customPriorityFee === null && "bg-background-gray-hover border-chartreuse"
+            )}
+            variant="secondary"
+            onClick={() => {
+              setPriorityFee(0.005);
+              setCustomPriorityFee(null);
+              setIsCustomPriorityFeeMode(false);
+            }}
+          >
+            Turbo <strong className="font-medium">0.005 SOL</strong>
+          </Button>
+        </li>
+      </ul>
+      <h2 className="font-normal mb-2">or set manually</h2>
+      <div className="relative mb-6">
+        <Input
+          ref={priorityFeeRef}
+          type="number"
+          className={cn(
+            "h-auto bg-background/50 py-3 px-4 border border-transparent text-white transition-colors focus-visible:ring-0",
+            priorityFee !== 0.005 && priorityFee !== 0.00005 && priorityFee !== 0 && "border-chartreuse"
+          )}
+          value={customPriorityFee && !isCustomPriorityFeeMode ? customPriorityFee?.toString() : undefined}
+          min={0}
+          placeholder={
+            priorityFee !== 0.005 && priorityFee !== 0.00005 && priorityFee !== 0 ? priorityFee.toString() : "0"
+          }
+          onFocus={() => setIsCustomPriorityFeeMode(true)}
+          onChange={() => setCustomPriorityFee(parseFloat(priorityFeeRef.current?.value || "0"))}
+        />
+        <span className="absolute inset-y-0 right-3 text-sm flex items-center">SOL</span>
+      </div>
+      <Button
+        onClick={() => {
+          if (customPriorityFee) {
+            setPriorityFee(customPriorityFee);
+          }
+          setIsPriorityFeesMode(false);
+        }}
+        className="w-full py-6"
+      >
+        Save Settings
+      </Button>
+    </>
+  );
+};
--- apps/marginfi-v2-ui/src/components/common/ActionBox/index.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/index.tsx
@@ -1,3 +1,7 @@
+export * from "./ActionBox.utils";
 export * from "./ActionBox";
+export * from "./ActionBoxActions";
 export * from "./ActionBoxDialog";
+export * from "./ActionBoxPreview";
+export * from "./ActionBoxPriorityFees";
 export * from "./ActionBoxTokens";
