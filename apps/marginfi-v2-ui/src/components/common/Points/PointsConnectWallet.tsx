f748be4dcbc8fa5f83fc00ec9b792a242e1d185b
Adam Chambers <adammchambers@gmail.com>
2024-01-18 14:26:35 -0500

    chore: points overview responsive cleanup

--- apps/marginfi-v2-ui/src/components/common/Points/PointsConnectWallet.tsx
+++ apps/marginfi-v2-ui/src/components/common/Points/PointsConnectWallet.tsx
@@ -4,7 +4,7 @@ import { WalletButton } from "~/components/common/Wallet";
 
 export const PointsConnectWallet = () => {
   return (
-    <div className="max-w-[800px] p-4 mx-auto w-full bg-background-gray-dark rounded-xl">
+    <div className="max-w-[800px] p-4 mx-auto w-full bg-background-gray-dark rounded-xl mt-8 md:mt-0">
       <div className="w-full flex flex-col gap-4 justify-evenly items-center p-2 text-base text-white font-aeonik font-[400] rounded-xl text-center">
         <h2 className="text-2xl font-medium">Sign in to access your points</h2>
         <WalletButton />
