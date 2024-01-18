325b92fd4f82a55f7b32049b373d957dcefb32ef
Adam Chambers <adammchambers@gmail.com>
2024-01-18 13:51:26 -0500

    feat: modify popover / dialog bg

--- apps/marginfi-v2-ui/src/components/ui/dialog.tsx
+++ apps/marginfi-v2-ui/src/components/ui/dialog.tsx
@@ -19,7 +19,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      "fixed inset-0 h-screen z-50 bg-background/60 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 h-screen z-50 bg-background/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props}
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "outline-none fixed h-full md:max-h-screen border border-border/50 overflow-auto md:h-auto top-0 left-0 md:left-[50%] md:top-[50%] z-50 flex flex-col justify-center md:grid w-full md:max-w-xl md:translate-x-[-50%] md:translate-y-[-50%] gap-4 bg-background-gray-dark p-8 md:p-12 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
+        "outline-none fixed h-full md:max-h-screen border border-border/50 overflow-auto md:h-auto top-0 left-0 md:left-[50%] md:top-[50%] z-50 flex flex-col justify-center md:grid w-full md:max-w-xl md:translate-x-[-50%] md:translate-y-[-50%] gap-4 bg-popover p-8 md:p-12 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
         className
       )}
       onOpenAutoFocus={(e) => e.preventDefault()}
