8a6fc1928cfb1e84c577d4fd73b136a1723dd341
Adam Chambers <adammchambers@gmail.com>
2024-01-18 14:28:42 -0500

    feat: reduce mobile header size

--- apps/marginfi-v2-ui/src/components/common/PageHeader/PageHeader.tsx
+++ apps/marginfi-v2-ui/src/components/common/PageHeader/PageHeader.tsx
@@ -18,7 +18,7 @@ export const PageHeader: FC<PageHeaderProps> = ({ children }) => {
       <Head>
         <link rel="preload" as="image" href="/WaveBG3.png" type="image/png" />
       </Head>
-      <div className="flex w-full h-[90px] sm:h-[60px] justify-center items-center border-y bg-[url('/WaveBG3.png')]">
+      <div className="flex w-full h-[74px] sm:h-[60px] justify-center items-center border-y bg-[url('/WaveBG3.png')]">
         <div className="w-full xl:w-4/5 px-4 xl:max-w-7xl flex flex-row justify-between items-center border-solid font-aeonik font-normal text-2xl sm:text-3xl">
           <div className="flex gap-4 justify-center items-center">
             <Mobile>
