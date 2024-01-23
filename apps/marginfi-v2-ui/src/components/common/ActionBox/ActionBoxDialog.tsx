c45bac80b479a45f2fd018e05d230a6fae3ae9a6
Adam Chambers <adammchambers@gmail.com>
2024-01-23 18:56:54 -0500

    feat: card / dialog variant updates, implemented on eco / mint

--- apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxDialog.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxDialog.tsx
@@ -26,7 +26,7 @@ export const ActionBoxDialog = ({
   return (
     <Dialog open={isDialogOpen} onOpenChange={(open) => setIsDialogOpen(open)}>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="md:flex md:max-w-[520px] md:py-3 md:px-5 p-0 sm:rounded-2xl bg-transparent">
+      <DialogContent className="md:flex md:max-w-[520px] md:py-3 md:px-5 p-0 sm:rounded-2xl bg-transparent border-none">
         <ActionBox
           isDialog={true}
           handleCloseDialog={() => setIsDialogOpen(false)}
