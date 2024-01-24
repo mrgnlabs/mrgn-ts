507b4cc06381e0336e3b6cb43dbf52727a874e71
Adam Chambers <adammchambers@gmail.com>
2024-01-24 16:08:21 -0500

    feat: wallet UI updates, accordion with tokens and notifications placeholder

--- apps/marginfi-v2-ui/src/components/ui/icons.tsx
+++ apps/marginfi-v2-ui/src/components/ui/icons.tsx
@@ -34,6 +34,8 @@ import {
   IconConfetti,
   IconTrophyFilled,
   IconStar,
+  IconBell,
+  IconLink,
 } from "@tabler/icons-react";
 import { cn } from "~/utils/themeUtils";
 
@@ -576,6 +578,8 @@ export {
   IconConfetti,
   IconTrophyFilled,
   IconStar,
+  IconBell,
+  IconLink,
 
   // customed icons
   IconBrandGoogle,
