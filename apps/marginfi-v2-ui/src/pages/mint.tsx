51266c432200c11c7052f6ad0003fa28668ed458
Adam Chambers <adammchambers@gmail.com>
2024-01-23 19:43:21 -0500

    feat: convertkit api integration

--- apps/marginfi-v2-ui/src/pages/mint.tsx
+++ apps/marginfi-v2-ui/src/pages/mint.tsx
@@ -2,18 +2,16 @@ import React from "react";
 
 import Link from "next/link";
 
+import { useMrgnlendStore } from "~/store";
+
+import { PageHeader } from "~/components/common/PageHeader";
+
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Button } from "~/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "~/components/ui/dialog";
 import { IconYBX, IconLST, IconCheck, IconExternalLink } from "~/components/ui/icons";
 import { Input } from "~/components/ui/input";
+import { Loader } from "~/components/ui/loader";
 
 const integrations = [
   {
@@ -77,8 +75,10 @@ enum MintPageState {
 }
 
 export default function MintPage() {
+  const [initialized] = useMrgnlendStore((state) => [state.initialized]);
   const [mintPageState, setMintPageState] = React.useState<MintPageState>(MintPageState.DEFAULT);
   const [ybxDialogOpen, setYBXDialogOpen] = React.useState(false);
+  const emailInputRef = React.useRef<HTMLInputElement>(null);
 
   const cards = React.useMemo(
     () => [
@@ -105,101 +105,130 @@ export default function MintPage() {
     []
   );
 
-  const signUp = React.useCallback(() => {
-    console.log("sign up");
-  }, []);
+  const signUp = React.useCallback(async () => {
+    if (!emailInputRef.current) {
+      return;
+    }
+
+    const res = await fetch(
+      `https://api.convertkit.com/v3/forms/${process.env.NEXT_PUBLIC_CONVERT_KIT_YBX_FORM_UID}/subscribe`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          api_key: process.env.NEXT_PUBLIC_CONVERT_KIT_API_KEY,
+          email: emailInputRef.current.value,
+        }),
+      }
+    );
+
+    if (!res.ok) {
+      setMintPageState(MintPageState.ERROR);
+      return;
+    }
+
+    setMintPageState(MintPageState.SUCCESS);
+  }, [emailInputRef]);
 
   return (
     <>
-      <div className="w-full max-w-7xl mx-auto px-4 md:px-8 space-y-20 pt-20 pb-28">
-        <div className="w-full max-w-3xl mx-auto space-y-20">
-          <h1 className="text-3xl font-medium text-center leading-normal">
-            Crypto&apos;s highest yielding, decentralised stablecoin Backed by Solana&apos;s MEV-boosted, highest
-            yielding LST
-          </h1>
+      <PageHeader showDesktopTitle={false}>Mint</PageHeader>
+      <div className="w-full max-w-7xl mx-auto px-4 md:px-8 space-y-20 pt-16 lg:pt-20 pb-28">
+        {!initialized && <Loader label="Loading YBX / LST..." className="mt-8" />}
+        {initialized && (
+          <>
+            <div className="w-full max-w-3xl mx-auto space-y-20">
+              <h1 className="text-3xl font-medium text-center leading-normal">
+                Crypto&apos;s highest yielding, decentralised stablecoin Backed by Solana&apos;s MEV-boosted, highest
+                yielding LST
+              </h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 md:gap-11">
-            {cards.map((item) => (
-              <Card variant="secondary">
-                <CardHeader>
-                  <CardTitle className="flex items-center gap-2 text-3xl">
-                    <item.icon />
-                    {item.title}
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <p>{item.description}</p>
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 md:gap-11">
+                {cards.map((item) => (
+                  <Card variant="secondary">
+                    <CardHeader>
+                      <CardTitle className="flex items-center gap-2 text-3xl">
+                        <item.icon />
+                        {item.title}
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <p>{item.description}</p>
 
-                  <p className="text-lg font-semibold my-6">{item.price}</p>
+                      <p className="text-lg font-semibold my-6">{item.price}</p>
 
-                  <ul className="space-y-2.5 mb-4">
-                    {item.features.map((feature) => (
-                      <li className="flex items-center gap-1">
-                        <IconCheck className="text-success" />
-                        {feature}
-                      </li>
-                    ))}
-                  </ul>
+                      <ul className="space-y-2.5 mb-4">
+                        {item.features.map((feature) => (
+                          <li className="flex items-center gap-1">
+                            <IconCheck className="text-success" />
+                            {feature}
+                          </li>
+                        ))}
+                      </ul>
 
-                  <p className="text-right text-sm">{item.footer}</p>
+                      <p className="text-right text-sm">{item.footer}</p>
 
-                  <Button
-                    variant="secondary"
-                    size="lg"
-                    className="mt-4"
-                    onClick={() => {
-                      if (item.action) {
-                        item.action();
-                      }
-                    }}
-                  >
-                    Mint {item.title}
-                  </Button>
-                </CardContent>
-              </Card>
-            ))}
-          </div>
-        </div>
+                      <Button
+                        variant="secondary"
+                        size="lg"
+                        className="mt-4"
+                        onClick={() => {
+                          if (item.action) {
+                            item.action();
+                          }
+                        }}
+                      >
+                        Mint {item.title}
+                      </Button>
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+            </div>
 
-        <div className="w-full bg-background p-10 xl:px-16 text-center rounded-xl border border-border">
-          <h2 className="text-3xl font-medium mb-6">Integrations</h2>
-          <p>40+ dAPPs where you can use YBX and LST</p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-10">
-            {integrations.map((item, i) => (
-              <Card key={i} variant="gradient">
-                <CardHeader>
-                  <CardTitle className="flex items-center justify-center text-xl">
-                    <div className="flex items-center">
-                      <img src={item.altIcon} className="w-8 h-8 rounded-full" />
-                      <item.icon className="-translate-x-3.5" size={32} />
-                    </div>
-                    {item.title}
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <ul className="space-y-2">
-                    {Object.entries(item.info).map(([key, value], j) => (
-                      <li className="flex items-center justify-between gap-1" key={j}>
-                        <span className="text-muted-foreground">
-                          {key.substring(0, 1).toUpperCase() + key.substring(1)}
-                        </span>{" "}
-                        {value}
-                      </li>
-                    ))}
-                  </ul>
+            <div className="w-full bg-background/75 py-12 px-10 xl:px-16 text-center rounded-xl border border-border">
+              <h2 className="text-3xl font-medium mb-6">Integrations</h2>
+              <p>40+ dAPPs where you can use YBX and LST</p>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-10">
+                {integrations.map((item, i) => (
+                  <Card key={i} variant="gradient">
+                    <CardHeader>
+                      <CardTitle className="flex items-center justify-center text-xl">
+                        <div className="flex items-center">
+                          <img src={item.altIcon} className="w-8 h-8 rounded-full" />
+                          <item.icon className="-translate-x-3.5" size={32} />
+                        </div>
+                        {item.title}
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <ul className="space-y-2">
+                        {Object.entries(item.info).map(([key, value], j) => (
+                          <li className="flex items-center justify-between gap-1" key={j}>
+                            <span className="text-muted-foreground">
+                              {key.substring(0, 1).toUpperCase() + key.substring(1)}
+                            </span>{" "}
+                            {value}
+                          </li>
+                        ))}
+                      </ul>
 
-                  <Link href={item.link} target="_blank" rel="noreferrer" className="w-full">
-                    <Button variant="default" size="lg" className="mt-4 w-full">
-                      {item.action} <IconExternalLink size={20} />
-                    </Button>
-                  </Link>
+                      <Link href={item.link} target="_blank" rel="noreferrer" className="w-full">
+                        <Button variant="default" size="lg" className="mt-4 w-full">
+                          {item.action} <IconExternalLink size={20} />
+                        </Button>
+                      </Link>
 
-                  <p className="text-muted-foreground text-sm mt-4">{item.platform}</p>
-                </CardContent>
-              </Card>
-            ))}
-          </div>
-        </div>
+                      <p className="text-muted-foreground text-sm mt-4">{item.platform}</p>
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <Dialog open={ybxDialogOpen} onOpenChange={(open) => setYBXDialogOpen(open)}>
         <DialogContent>
@@ -207,18 +236,38 @@ export default function MintPage() {
             <DialogTitle>Get Notified</DialogTitle>
             <DialogDescription>Sign up to stay up to date with YBX</DialogDescription>
           </DialogHeader>
-          <form
-            className="w-full px-8"
-            onSubmit={(e) => {
-              e.preventDefault();
-              signUp();
-            }}
-          >
-            <div className="flex items-center w-full gap-2">
-              <Input type="email" placeholder="Email" className="w-full" required />
-              <Button type="submit">Sign Up</Button>
+
+          {mintPageState === MintPageState.SUCCESS && (
+            <div className="flex flex-col items-center gap-4">
+              <p className="flex items-center justify-center gap-2">
+                <IconCheck size={18} className="text-success" /> You are signed up!
+              </p>
+              <Button variant="outline" onClick={() => setYBXDialogOpen(false)}>
+                Back to Mint
+              </Button>
             </div>
-          </form>
+          )}
+
+          {(mintPageState === MintPageState.DEFAULT || mintPageState === MintPageState.ERROR) && (
+            <>
+              <form
+                className="w-full px-8"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  signUp();
+                }}
+              >
+                <div className="flex items-center w-full gap-2">
+                  <Input ref={emailInputRef} type="email" placeholder="Email" className="w-full" required />
+                  <Button type="submit">Sign Up</Button>
+                </div>
+              </form>
+
+              {mintPageState === MintPageState.ERROR && (
+                <p className="text-destructive-foreground text-sm text-center">Error signing up, please try again.</p>
+              )}
+            </>
+          )}
         </DialogContent>
       </Dialog>
     </>
