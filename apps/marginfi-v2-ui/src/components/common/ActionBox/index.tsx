cdc04608ebe0749cbc2b191712e5fecc27ec56ba
Kobe Leenders <leenderskobe@gmail.com>
2024-01-25 16:57:39 +0100

    feat: actionbox restructure

--- apps/marginfi-v2-ui/src/components/common/ActionBox/index.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/index.tsx
@@ -2,6 +2,6 @@ export * from "./ActionBox.utils";
 export * from "./ActionBox";
 export * from "./ActionBoxActions";
 export * from "./ActionBoxDialog";
-export * from "./ActionBoxPreview";
 export * from "./ActionBoxPriorityFees";
 export * from "./ActionBoxTokens";
+export * from "./ActionBoxLendWrapper";
