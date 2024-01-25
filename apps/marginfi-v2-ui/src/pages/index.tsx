6bcc95181728cfc08a659fccf02b9ed8d127cc87
Kobe Leenders <leenderskobe@gmail.com>
2024-01-25 17:02:24 +0100

    fix: add lending wrapper to mobile

--- apps/marginfi-v2-ui/src/pages/index.tsx
+++ apps/marginfi-v2-ui/src/pages/index.tsx
@@ -93,7 +93,7 @@ export default function HomePage() {
               />
             )}
             <Stats />
-            <ActionBox />
+            <ActionBoxLendWrapper />
           </>
         )}
       </Mobile>
