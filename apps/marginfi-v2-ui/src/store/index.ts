e507432a9444553c18c60e97d6872aecfcc103e6
Kobe Leenders <leenderskobe@gmail.com>
2024-01-29 22:50:27 +0100

    wip: lst actionbox

--- apps/marginfi-v2-ui/src/store/index.ts
+++ apps/marginfi-v2-ui/src/store/index.ts
@@ -6,9 +6,11 @@ import {
 } from "@mrgnlabs/marginfi-v2-ui-state";
 import { UseBoundStore, StoreApi } from "zustand";
 import { UiState, createUiStore, SORT_OPTIONS_MAP } from "./uiStore";
+import { LstState, createLstStore } from "./lstStore";
 
 const useUiStore: UseBoundStore<StoreApi<UiState>> = createUiStore();
 const useMrgnlendStore: UseBoundStore<StoreApi<MrgnlendState>> = createPersistentMrgnlendStore();
 const useUserProfileStore: UseBoundStore<StoreApi<UserProfileState>> = createUserProfileStore();
+export const useLstStore: UseBoundStore<StoreApi<LstState>> = createLstStore();
 
 export { useUiStore, useMrgnlendStore, useUserProfileStore, SORT_OPTIONS_MAP };
