88a8b2b4769654859a594036d664e9aab0b68ada
Adam Chambers <adammchambers@gmail.com>
2024-01-30 17:23:08 -0500

    feat: add interval check to send notifications every 24 hours

--- apps/notifications/src/app/webhook/route.ts
+++ apps/notifications/src/app/webhook/route.ts
@@ -1,6 +1,15 @@
 import * as admin from "firebase-admin";
 import { Resend } from "resend";
 
+type UserData = {
+  email: string;
+  wallet_address: string;
+  account_health: boolean;
+  ybx_updates: boolean;
+  updated_at: admin.firestore.Timestamp;
+  last_notification?: admin.firestore.Timestamp;
+};
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
@@ -11,29 +20,46 @@ if (!admin.apps.length) {
   });
 }
 
-export async function GET(request: Request) {
+export async function GET(request: Request): Promise<Response> {
   const { searchParams } = new URL(request.url);
   const wallet = searchParams.get("wallet");
+  const healthParam = searchParams.get("health");
 
-  if (!wallet) {
-    return Response.json({ error: "Wallet address required" }, { status: 404 });
+  if (!wallet || !healthParam) {
+    return new Response(JSON.stringify({ error: "Wallet address and account health required" }), { status: 404 });
+  }
+
+  const health = parseInt(healthParam);
+
+  if (isNaN(health) || health > 25) {
+    return new Response(JSON.stringify({ error: "Notification not required" }), { status: 404 });
   }
 
   const db = admin.firestore();
-  const user = await db.collection("notification_settings").doc(wallet).get();
+  const userDocRef = db.collection("notification_settings").doc(wallet);
+  const user = await userDocRef.get();
 
   if (!user.exists) {
-    return Response.json({ error: "User not found" }, { status: 404 });
+    return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
   }
 
-  const userData = user.data();
+  const userData = user.data() as UserData;
 
   if (!userData) {
-    return Response.json({ error: "User data not found" }, { status: 404 });
+    return new Response(JSON.stringify({ error: "User data not found" }), { status: 404 });
   }
 
   if (!userData.account_health) {
-    return Response.json({ error: "User has account health notifications turned off" }, { status: 404 });
+    return new Response(JSON.stringify({ error: "User has account health notifications turned off" }), { status: 404 });
+  }
+
+  const notificationInterval = 60 * 1000; // For testing, set to 1 minute. For production, set to 24 hours.
+
+  const now = new Date().getTime();
+  const lastNotificationTime =
+    userData.last_notification instanceof admin.firestore.Timestamp ? userData.last_notification.toDate().getTime() : 0;
+  if (now - lastNotificationTime < notificationInterval) {
+    return new Response(JSON.stringify({ error: "Notification already sent within the interval" }), { status: 429 });
   }
 
   const resend = new Resend(process.env.RESEND_API_KEY);
@@ -41,13 +67,20 @@ export async function GET(request: Request) {
   const { data, error } = await resend.emails.send({
     from: "onboarding@resend.dev",
     to: "engineering@mrgn.group",
-    subject: "Hello World",
-    html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+    subject: "Account Health Notification",
+    html: `
+      <p>Your account health is at ${health}%, if it reaches 0 you are at risk of liquidation.</p>
+      <p><a href="https://app.marginfi.com">Login to marginfi</a>
+    `,
   });
 
   if (error) {
-    return Response.json({ error: error.message }, { status: 500 });
+    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
   }
 
-  return Response.json({ data });
+  await userDocRef.update({
+    last_notification: admin.firestore.FieldValue.serverTimestamp(),
+  });
+
+  return new Response(JSON.stringify({ data }));
 }
