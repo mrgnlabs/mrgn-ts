0c14ccf30bd439dbd6d56907c779249935a3a61a
Adam Chambers <adammchambers@gmail.com>
2024-02-07 22:58:49 -0500

    feat: basic responsive header / footer

--- apps/marginfi-landing-page-v2/src/components/footer/footer.tsx
+++ apps/marginfi-landing-page-v2/src/components/footer/footer.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+import { cn } from "~/lib/utils";
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "~/components/ui/accordion";
 import { IconMrgn } from "~/components/ui/icons";
 
 type NavItems = {
@@ -65,8 +68,14 @@ const navItems: NavItems = {
 
 export const Footer = () => {
   return (
-    <footer className="w-full py-8 px-6 flex justify-between items-start gap-4 border-t border-border bg-background">
-      <div className="space-y-4">
+    <footer
+      className={cn(
+        "w-full py-10 px-6 flex flex-col justify-between items-center gap-12 border-t border-border bg-background",
+        "md:gap-16 md:py-12",
+        "lg:flex-row lg:items-start lg:gap-8 lg:py-10"
+      )}
+    >
+      <div className="flex flex-col gap-4 items-center lg:items-start">
         <div className="flex items-center gap-4 text-3xl">
           <IconMrgn size={42} />
           marginfi
@@ -75,7 +84,8 @@ export const Footer = () => {
           {new Date().getFullYear()} &copy; Margin Labs INC. All rights reserved.
         </small>
       </div>
-      <div className="flex gap-24 ml-auto text-sm">
+
+      <div className="hidden gap-32 text-sm md:flex lg:ml-auto">
         {Object.keys(navItems).map((key, index) => (
           <div key={index} className="w-full space-y-4 min-w-fit">
             <h4 className=" text-base">{key}</h4>
@@ -89,6 +99,27 @@ export const Footer = () => {
               ))}
             </ul>
           </div>
+        ))}
+      </div>
+
+      <div className="md:hidden w-full">
+        {Object.keys(navItems).map((key, index) => (
+          <Accordion key={index} type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="hover:no-underline">{key}</AccordionTrigger>
+              <AccordionContent>
+                <ul className="space-y-2 w-full">
+                  {navItems[key].map((item, index) => (
+                    <li key={index}>
+                      <Link href={item.href} className="text-muted-foreground transition-colors hover:text-primary">
+                        {item.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         ))}
       </div>
     </footer>
