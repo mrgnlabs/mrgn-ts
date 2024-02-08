16734e6c31642bc7aaa1de552cb9576248ac4347
Adam Chambers <adammchambers@gmail.com>
2024-02-08 09:16:13 -0500

    feat: pass all content in as props, prep for cms

--- apps/marginfi-landing-page-v2/src/components/gradient-border-box/gradient-border-box.tsx
+++ apps/marginfi-landing-page-v2/src/components/gradient-border-box/gradient-border-box.tsx
@@ -1,19 +1,49 @@
+import { Button } from "~/components/ui/button";
+import { IconSolana, IconSparkles } from "~/components/ui/icons";
+
 import "./gradient-border-box.css";
+import Link from "next/link";
 
 type GradientBorderBoxProps = {
-  icon?: JSX.Element;
-  title: string;
+  icon?: string;
+  heading: string;
   description?: string;
-  action?: JSX.Element;
+  action?: {
+    href: string;
+    text: string;
+  };
 };
 
-export const GradientBorderBox = ({ icon, title, description, action }: GradientBorderBoxProps) => {
+export const GradientBorderBox = ({ icon, heading, description, action }: GradientBorderBoxProps) => {
   return (
     <div className="gradient-border-box py-10 px-16 w-full h-full">
-      {icon && <div className="mb-4 flex justify-center">{icon}</div>}
-      <h2 className="text-xl font-medium">{title}</h2>
+      {icon && <Icon name={icon} />}
+      <h2 className="text-xl font-medium">{heading}</h2>
       {description && <p className="text-sm text-muted-foreground mt-3">{description}</p>}
-      {action && <div className="mt-8">{action}</div>}
+      {action && (
+        <div className="mt-8">
+          <Button>
+            <Link href={action.href}></Link>
+            {action.text}
+          </Button>
+        </div>
+      )}
     </div>
   );
+};
+
+type IconProps = {
+  name: string;
+};
+
+export const Icon = ({ name }: IconProps) => {
+  let icon = <></>;
+  switch (name) {
+    case "solana":
+      icon = <IconSolana />;
+    case "sparkles":
+      icon = <IconSparkles />;
+  }
+
+  return <div className="mb-4 flex justify-center">{icon}</div>;
 };
