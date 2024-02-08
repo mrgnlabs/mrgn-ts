c5990887a21e46f21a6f41d57a8e68c37f1af583
Adam Chambers <adammchambers@gmail.com>
2024-02-07 23:12:18 -0500

    feat: responsive hero styles

--- apps/marginfi-landing-page-v2/src/components/hero/hero.tsx
+++ apps/marginfi-landing-page-v2/src/components/hero/hero.tsx
@@ -1,7 +1,9 @@
 export const Hero = () => {
   return (
-    <div className="w-full max-w-6xl mx-auto py-8 space-y-8 text-center">
-      <h1 className="text-6xl">Dolore aute cillum cupidatat velit aliquip tempor eu irure dolor culpa dolor.</h1>
+    <div className="w-full max-w-6xl mx-auto py-8 px-6 space-y-8 text-center">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl">
+        Dolore aute cillum cupidatat velit aliquip tempor eu irure dolor culpa dolor.
+      </h1>
       <h2 className="w-full max-w-3xl mx-auto text-lg text-muted-foreground">
         Reprehenderit sit aliquip mollit aliquip Lorem amet ullamco ad minim qui. Eu culpa commodo Lorem labore.
         Pariatur fugiat anim proident deserunt anim irure.
