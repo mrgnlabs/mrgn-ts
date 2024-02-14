b25e1d2f192427f646fb377953c6ded503ea9aea
Adam Chambers <adammchambers@gmail.com>
2024-02-14 01:40:51 -0500

    feat: update action complete to support LST minting

--- apps/marginfi-v2-ui/src/components/common/ActionComplete/ActionComplete.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionComplete/ActionComplete.tsx
@@ -3,26 +3,30 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import JSBI from "jsbi";
 import Confetti from "react-confetti";
 import { useWindowSize } from "@uidotdev/usehooks";
-import { numeralFormatter, shortenAddress } from "@mrgnlabs/mrgn-common";
+import { PublicKey } from "@solana/web3.js";
+import { ActionType, ActiveBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
+import { percentFormatterDyn, shortenAddress } from "@mrgnlabs/mrgn-common";
 
 import { cn } from "~/utils";
-import { useUiStore } from "~/store";
+import { useUiStore, useLstStore, useMrgnlendStore } from "~/store";
 import { useAssetItemData } from "~/hooks/useAssetItemData";
 import { useIsMobile } from "~/hooks/useIsMobile";
 
 import { Dialog, DialogContent } from "~/components/ui/dialog";
-import { IconConfetti, IconExternalLink } from "~/components/ui/icons";
+import { IconConfetti, IconExternalLink, IconArrowDown, IconArrowUp } from "~/components/ui/icons";
 import { Button } from "~/components/ui/button";
-import { ActionType } from "@mrgnlabs/marginfi-v2-ui-state";
 
 export const ActionComplete = () => {
+  const [extendedBankInfos] = useMrgnlendStore((state) => [state.extendedBankInfos]);
   const [isActionComplete, setIsActionComplete, previousTxn] = useUiStore((state) => [
     state.isActionComplete,
     state.setIsActionComplete,
     state.previousTxn,
   ]);
+  const [lstData] = useLstStore((state) => [state.lstData]);
   const { rateAP } = useAssetItemData({
     bank: previousTxn?.bank!,
     isInLendingMode: previousTxn?.type === ActionType.Deposit,
@@ -30,9 +34,18 @@ export const ActionComplete = () => {
   const { width, height } = useWindowSize();
   const isMobile = useIsMobile();
 
+  const lstBank = React.useMemo(() => {
+    const filtered = extendedBankInfos.filter((bank) =>
+      bank.address.equals(new PublicKey("DMoqjmsuoru986HgfjqrKEvPv8YBufvBGADHUonkadC5"))
+    );
+    return filtered.length > 0 ? filtered[0] : null;
+  }, [extendedBankInfos]);
+
   const actionTextColor = React.useMemo(() => {
-    if (previousTxn?.type === ActionType.Deposit || previousTxn?.type === ActionType.Withdraw) return "text-success";
-    if (previousTxn?.type === ActionType.Borrow || previousTxn?.type === ActionType.Repay) return "text-warning";
+    const successTypes = [ActionType.Deposit, ActionType.Withdraw, ActionType.MintLST];
+    const warningTypes = [ActionType.Borrow, ActionType.Repay];
+    if (successTypes.includes(previousTxn?.type!)) return "text-success";
+    if (warningTypes.includes(previousTxn?.type!)) return "text-warning";
     return "";
   }, [previousTxn?.type]);
 
@@ -53,46 +66,103 @@ export const ActionComplete = () => {
             <header className="space-y-4 text-center flex flex-col items-center justify-center">
               <IconConfetti size={48} />
               <h2 className="font-medium text-xl">
-                {previousTxn?.type === ActionType.Deposit ? "Deposit" : previousTxn?.type} Completed!
+                {previousTxn?.type === ActionType.Deposit && "Deposit Completed!"}
+                {previousTxn?.type === ActionType.MintLST && "LST Minted!"}
+                {previousTxn?.type !== ActionType.Deposit &&
+                  previousTxn?.type !== ActionType.MintLST &&
+                  previousTxn?.type + " Completed!"}
               </h2>
             </header>
-            <div className="flex flex-col items-center gap-2 border-b border-border pb-10">
-              <div className="flex items-center justify-center gap-2">
-                <h3 className="text-4xl font-medium">
-                  {previousTxn?.amount} {previousTxn?.bank.meta.tokenSymbol}
-                </h3>
-                <Image
-                  className="rounded-full w-9 h-9"
-                  src={previousTxn?.bank.meta.tokenLogoUri || ""}
-                  alt={(previousTxn?.bank.meta.tokenSymbol || "Token") + "  logo"}
-                  width={36}
-                  height={36}
-                />
-              </div>
-            </div>
-            <dl className="grid grid-cols-2 w-full text-muted-foreground gap-x-8 gap-y-2">
-              {previousTxn?.bank.position && (
-                <>
-                  <dt>Total {previousTxn?.bank.meta.tokenSymbol} Deposits</dt>
+
+            {!previousTxn.lstQuote && (
+              <>
+                <div className="flex flex-col items-center gap-2 border-b border-border pb-10">
+                  <div className="flex items-center justify-center gap-2">
+                    <h3 className="text-4xl font-medium">
+                      {previousTxn?.amount} {previousTxn?.bank.meta.tokenSymbol}
+                    </h3>
+                    <Image
+                      className="rounded-full w-9 h-9"
+                      src={previousTxn?.bank.meta.tokenLogoUri || ""}
+                      alt={(previousTxn?.bank.meta.tokenSymbol || "Token") + "  logo"}
+                      width={36}
+                      height={36}
+                    />
+                  </div>
+                </div>
+                <dl className="grid grid-cols-2 w-full text-muted-foreground gap-x-8 gap-y-2">
+                  {previousTxn?.bank.position && (
+                    <>
+                      <dt>Total {previousTxn?.bank.meta.tokenSymbol} Deposits</dt>
+                      <dd className="text-right">
+                        {previousTxn?.bank.position.amount} {previousTxn?.bank.meta.tokenSymbol}
+                      </dd>
+                    </>
+                  )}
+                  <dt>APY</dt>
+                  <dd className={cn("text-right", actionTextColor)}>{rateAP}</dd>
+                  <dt>Transaction</dt>
                   <dd className="text-right">
-                    {previousTxn?.bank.position.amount} {previousTxn?.bank.meta.tokenSymbol}
+                    <Link
+                      href={`https://solscan.io/tx/${previousTxn?.txn}`}
+                      className="flex items-center justify-end gap-1.5 text-chartreuse text-sm"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {shortenAddress(previousTxn?.txn || "")}{" "}
+                      <IconExternalLink size={15} className="-translate-y-[1px]" />
+                    </Link>
                   </dd>
-                </>
-              )}
-              <dt>APY</dt>
-              <dd className={cn("text-right", actionTextColor)}>{rateAP}</dd>
-              <dt>Transaction</dt>
-              <dd className="text-right">
-                <Link
-                  href={`https://solscan.io/tx/${previousTxn?.txn}`}
-                  className="flex items-center justify-end gap-1.5 text-chartreuse text-sm"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {shortenAddress(previousTxn?.txn || "")} <IconExternalLink size={15} className="-translate-y-[1px]" />
-                </Link>
-              </dd>
-            </dl>
+                </dl>
+              </>
+            )}
+
+            {previousTxn.lstQuote && lstData && lstBank && (
+              <>
+                <div className="flex flex-col items-center gap-2 border-b border-border pb-10">
+                  <div className="flex items-center justify-center gap-2">
+                    <h3 className="text-4xl font-medium">
+                      {JSBI.toNumber(previousTxn.lstQuote.quoteResponse.outAmount) / 1e9} {lstBank.meta.tokenSymbol}
+                    </h3>
+                    <Image
+                      className="rounded-full w-9 h-9"
+                      src={lstBank.meta.tokenLogoUri || ""}
+                      alt={(lstBank.meta.tokenSymbol || "Token") + "  logo"}
+                      width={36}
+                      height={36}
+                    />
+                  </div>
+                </div>
+                <dl className="grid grid-cols-2 w-full text-muted-foreground gap-x-8 gap-y-2">
+                  <dt>Staked</dt>
+                  <dd className="text-right">
+                    {previousTxn.amount} {previousTxn.bank.meta.tokenSymbol}
+                  </dd>
+                  {lstBank && (
+                    <>
+                      <dt>Total {lstBank.meta.tokenSymbol}</dt>
+                      <dd className="text-right">
+                        {lstBank.userInfo.tokenAccount.balance} {lstBank.meta.tokenSymbol}
+                      </dd>
+                    </>
+                  )}
+                  <dt>APY</dt>
+                  <dd className="text-right text-success">{percentFormatterDyn.format(lstData.projectedApy)}</dd>
+                  <dt>Transaction</dt>
+                  <dd className="text-right">
+                    <Link
+                      href={`https://solscan.io/tx/${previousTxn?.txn}`}
+                      className="flex items-center justify-end gap-1.5 text-chartreuse text-sm"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {shortenAddress(previousTxn?.txn || "")}{" "}
+                      <IconExternalLink size={15} className="-translate-y-[1px]" />
+                    </Link>
+                  </dd>
+                </dl>
+              </>
+            )}
             <Button className="w-full mx-auto" onClick={() => setIsActionComplete(false)}>
               Done
             </Button>
