ef0d86e4934c349b44755d8f37afcacac23d0536
Adam Chambers <adammchambers@gmail.com>
2024-02-16 12:39:49 -0500

    feat: migration home page ui

--- apps/marginfi-v2-ui/src/pages/migrate/index.tsx
+++ apps/marginfi-v2-ui/src/pages/migrate/index.tsx
@@ -2,26 +2,45 @@ import Link from "next/link";
 
 import { PageHeader } from "~/components/common/PageHeader";
 import { Button } from "~/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "~/components/ui/card";
 
 export default function MigrateAccountPage() {
   return (
     <>
       <PageHeader>Migrate Account</PageHeader>
-      <div className="flex flex-col items-center max-w-md mx-auto py-8 space-y-8">
-        <header className="text-center space-y-4">
+      <div className="flex flex-col items-center w-full max-w-3xl mx-auto py-8 space-y-12">
+        <header className="text-center space-y-4 max-w-md mx-auto">
           <h1 className="text-3xl font-medium">Migrate your marginfi account</h1>
           <p>
-            There are multiple accounts associated with your wallet address. Please ensure the correct account is
-            selected before migrating.
+            Fugiat exercitation excepteur eiusmod in in velit laborum irure duis cillum aute ullamco reprehenderit
+            minim.
           </p>
         </header>
-        <div className="flex items-center justify-center gap-4">
-          <Link href="/migrate/account">
-            <Button>Migrate Account</Button>
-          </Link>
-          <Link href="/migrate/points">
-            <Button>Migrate Points</Button>
-          </Link>
+        <div className="w-full flex flex-col items-center justify-center gap-8 px-6 md:flex-row">
+          <Card variant="secondary" className="w-full">
+            <CardHeader>
+              <CardTitle>Migrate Account</CardTitle>
+              <CardDescription>Migrate your account to a new authority.</CardDescription>
+            </CardHeader>
+            <CardContent></CardContent>
+            <CardFooter>
+              <Link href="/migrate/account">
+                <Button variant="outline">Start account migration</Button>
+              </Link>
+            </CardFooter>
+          </Card>
+          <Card variant="secondary" className="w-full">
+            <CardHeader>
+              <CardTitle>Migrate Points</CardTitle>
+              <CardDescription>Migrate your points from one wallet to another.</CardDescription>
+            </CardHeader>
+            <CardContent></CardContent>
+            <CardFooter>
+              <Link href="/migrate/points">
+                <Button variant="outline">Start points migration</Button>
+              </Link>
+            </CardFooter>
+          </Card>
         </div>
       </div>
     </>
