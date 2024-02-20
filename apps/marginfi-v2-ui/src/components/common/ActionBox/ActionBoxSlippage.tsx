136a37e50f98d0fb52f4ffe3c7863c0926069d8b
Kobe Leenders <leenderskobe@gmail.com>
2024-02-20 19:47:48 +0100

    feat: slippage feature & lst improvements

--- apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxSlippage.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxSlippage.tsx
@@ -1,44 +1,56 @@
 import React from "react";
 
 import { ActionType } from "@mrgnlabs/marginfi-v2-ui-state";
+import { useForm } from "react-hook-form";
 
-import { useUiStore } from "~/store";
 import { cn } from "~/utils";
 
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/components/ui/tooltip";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
-import { IconInfoCircle, IconArrowLeft } from "~/components/ui/icons";
+import { IconArrowLeft } from "~/components/ui/icons";
+import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
+import { Label } from "~/components/ui/label";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "~/components/ui/form";
 
 type ActionBoxSlippageProps = {
   mode: ActionType;
-  setIsPriorityFeesMode: (value: boolean) => void;
+  slippageBps: number;
+  setSlippageBps: (value: number) => void;
 };
 
 const DEFAULT_SLIPPAGE_BPS = 100;
 
-const priorityFeeOptions = [
+const slippageOptions = [
   {
     label: "Low",
-    value: 100,
+    value: 0.5,
   },
   {
     label: "Normal",
-    value: 500,
+    value: 1,
   },
   {
     label: "High",
-    value: 1000,
+    value: 5,
   },
 ];
 
-export const ActionBoxSlippage = ({ mode, setIsPriorityFeesMode }: ActionBoxSlippageProps) => {
-  const [priorityFee, setPriorityFee] = useUiStore((state) => [state.priorityFee, state.setPriorityFee]);
-  const [selectedSlippage, setSelectedSlippage] = React.useState<number | null>(DEFAULT_SLIPPAGE_BPS);
+interface SlippageForm {
+  slippageBps: number;
+}
 
-  const slippageRef = React.useRef<HTMLInputElement>(null);
-  const [isCustomPriorityFeeMode, setIsCustomPriorityFeeMode] = React.useState<boolean>(false);
-  const [customSlippage, setCustomSlippage] = React.useState<number | null>(null);
+export const ActionBoxSlippage = ({ mode, slippageBps, setSlippageBps }: ActionBoxSlippageProps) => {
+  const form = useForm<SlippageForm>({
+    defaultValues: {
+      slippageBps: slippageBps,
+    },
+  });
+  const formWatch = form.watch();
+
+  const isCustomSlippage = React.useMemo(
+    () => (slippageOptions.find((value) => value.value === formWatch.slippageBps) ? false : true),
+    [formWatch.slippageBps]
+  );
 
   const modeLabel = React.useMemo(() => {
     let label = "";
@@ -52,83 +64,83 @@ export const ActionBoxSlippage = ({ mode, setIsPriorityFeesMode }: ActionBoxSlip
     return label;
   }, [mode]);
 
+  function onSubmit(data: SlippageForm) {
+    setSlippageBps(data.slippageBps);
+  }
+
   return (
-    <div className="space-y-6">
-      <button className="flex items-center gap-1.5 text-sm" onClick={() => setIsPriorityFeesMode(false)}>
-        <IconArrowLeft size={18} /> Back {modeLabel}
-      </button>
-      <h2 className="text-lg font-normal mb-2 flex items-center gap-2">
-        Set transaction priority{" "}
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <IconInfoCircle size={16} />
-            </TooltipTrigger>
-            <TooltipContent>
-              <div className="space-y-2">
-                <p>Priority fees are paid to the Solana network.</p>
-                <p>This additional fee helps boost how a transaction is prioritized.</p>
-              </div>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </h2>
-      <ul className="grid grid-cols-3 gap-3 mb-6">
-        {priorityFeeOptions.map((option) => (
-          <li key={option.value}>
-            <Button
-              className={cn(
-                "flex flex-col gap-0.5 h-auto w-full font-light border border-transparent bg-background/50 transition-colors hover:bg-background-gray-hover",
-                selectedSlippage === option.value &&
-                  customSlippage === null &&
-                  "bg-background-gray-hover border-chartreuse"
-              )}
-              variant="secondary"
-              onClick={() => {
-                setSelectedSlippage(option.value);
-                setCustomSlippage(null);
-                setIsCustomPriorityFeeMode(false);
-              }}
-            >
-              {option.label} <strong className="font-medium">{option.value} SOL</strong>
-            </Button>
-          </li>
-        ))}
-      </ul>
-      <h2 className="font-normal mb-2">or set manually</h2>
-      <div className="relative mb-6">
-        <Input
-          ref={slippageRef}
-          type="number"
-          className={cn(
-            "h-auto bg-background/50 py-3 px-4 border border-transparent text-white transition-colors focus-visible:ring-0",
-            selectedSlippage !== 100 && selectedSlippage !== 500 && selectedSlippage !== 1000 && "border-chartreuse"
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <button className="flex items-center gap-1.5 text-sm" onClick={() => setSlippageBps(slippageBps)}>
+          <IconArrowLeft size={18} /> Back {modeLabel}
+        </button>
+        <h2 className="text-lg font-normal mb-2 flex items-center gap-2">Set transaction slippage </h2>
+        <FormField
+          control={form.control}
+          name="slippageBps"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormControl>
+                <RadioGroup
+                  onValueChange={(value) => field.onChange(Number(value))}
+                  defaultValue={field.value.toString()}
+                  className="flex justify-between"
+                >
+                  {slippageOptions.map((option) => (
+                    <div
+                      className={cn(
+                        "w-full font-light border border-transparent rounded p-3 bg-background/50 transition-colors hover:bg-background-gray-hover",
+                        field.value === option.value && "bg-background-gray-hover border-chartreuse"
+                      )}
+                    >
+                      <RadioGroupItem value={option.value.toString()} id={option.value.toString()} className="hidden" />
+                      <Label
+                        className={"flex flex-col gap-2 h-auto w-full text-center"}
+                        htmlFor={option.value.toString()}
+                      >
+                        {" "}
+                        {option.label} <strong className="font-medium">{option.value} %</strong>
+                      </Label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
           )}
-          value={customSlippage && !isCustomPriorityFeeMode ? customSlippage?.toString() : undefined}
-          min={0}
-          placeholder={
-            selectedSlippage !== 100 && selectedSlippage !== 500 && selectedSlippage !== 1000
-              ? priorityFee.toString()
-              : "0"
-          }
-          onFocus={() => setIsCustomPriorityFeeMode(true)}
-          onChange={() => setCustomSlippage(parseFloat(slippageRef.current?.value || "0"))}
         />
-        <span className="absolute inset-y-0 right-3 text-sm flex items-center">SOL</span>
-      </div>
-      <Button
-        onClick={() => {
-          if (customSlippage) {
-            setPriorityFee(customSlippage);
-          } else if (selectedSlippage !== null) {
-            setPriorityFee(selectedSlippage);
-          }
-          setIsPriorityFeesMode(false);
-        }}
-        className="w-full py-6"
-      >
-        Save Settings
-      </Button>
-    </div>
+        <h2 className="font-normal mb-2">or set manually</h2>
+
+        <FormField
+          control={form.control}
+          name="slippageBps"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative mb-6">
+                  <Input
+                    type="number"
+                    min={0}
+                    value={isCustomSlippage ? field.value : undefined}
+                    placeholder={isCustomSlippage ? field.value.toString() : "0"}
+                    onChange={(e) => field.onChange(e)}
+                    className={cn(
+                      "h-auto bg-background/50 py-3 px-4 border border-transparent text-white transition-colors focus-visible:ring-0",
+                      isCustomSlippage && "border-chartreuse"
+                    )}
+                  />
+                  <span className="absolute inset-y-0 right-3 text-sm flex items-center">%</span>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="w-full py-6">
+          Save Settings
+        </Button>
+      </form>
+    </Form>
   );
 };
