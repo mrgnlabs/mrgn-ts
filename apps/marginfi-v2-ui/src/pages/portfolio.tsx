2941faa8281618a2e3d52fa4093b38e8b131affb
chambaz <adammchambers@gmail.com>
2024-02-29 13:10:05 -0500

    feat: add actioncomplete modal to portoflio page

--- apps/marginfi-v2-ui/src/pages/portfolio.tsx
+++ apps/marginfi-v2-ui/src/pages/portfolio.tsx
@@ -2,19 +2,21 @@ import React from "react";
 
 import Link from "next/link";
 
-import { useMrgnlendStore, useUserProfileStore } from "~/store";
+import { useMrgnlendStore, useUserProfileStore, useUiStore } from "~/store";
 import { useWalletContext } from "~/hooks/useWalletContext";
 
 import { PageHeader } from "~/components/common/PageHeader";
 import { PointsConnectWallet, PointsOverview } from "~/components/common/Points";
 import { EmissionsBanner } from "~/components/mobile/EmissionsBanner";
 import { Portfolio } from "~/components/common/Portfolio";
+import { ActionComplete } from "~/components/common/ActionComplete";
 import { Loader } from "~/components/ui/loader";
 
 export default function PortfolioPage() {
   const { connected } = useWalletContext();
   const [initialized] = useMrgnlendStore((state) => [state.initialized]);
   const [userPointsData] = useUserProfileStore((state) => [state.userPointsData]);
+  const [previousTxn] = useUiStore((state) => [state.previousTxn]);
 
   return (
     <>
@@ -38,6 +40,7 @@ export default function PortfolioPage() {
           </>
         )}
       </div>
+      {initialized && previousTxn && <ActionComplete />}
     </>
   );
 }
