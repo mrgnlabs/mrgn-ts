21e1dcef1074f60ee94a2f220e1c8dac0f44bc7e
chambaz <adammchambers@gmail.com>
2024-03-04 16:56:36 -0500

    feat: make all fields optional in marginfi-api upsert

--- apps/marginfi-api/src/app/notifications/route.ts
+++ apps/marginfi-api/src/app/notifications/route.ts
@@ -1,18 +1,24 @@
 import db from "~/lib/pg";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const wallet = searchParams.get("wallet_address");
   const apiKey = request.headers.get("X-API-KEY");
 
   if (!apiKey || apiKey !== process.env.API_KEY) {
-    return new Response(JSON.stringify({ error: "Invalid or missing API key" }), {
-      status: 401,
-      headers: { "Content-Type": "application/json" },
-    });
+    console.error("Invalid or missing API key");
+    return Response.json(
+      { error: "Invalid or missing API key" },
+      {
+        status: 401,
+      }
+    );
   }
 
   if (!wallet) {
+    console.error("Missing wallet address parameter");
     return Response.json(
       { error: "Missing wallet address parameter" },
       {
@@ -26,6 +32,7 @@ export async function GET(request: Request) {
     const { rows } = await db.query(queryText, [wallet]);
 
     if (rows.length === 0) {
+      console.error("Notification settings not found");
       return Response.json(
         { error: "Notification settings not found" },
         {
@@ -53,25 +60,40 @@ export async function POST(request: Request) {
   const apiKey = request.headers.get("X-API-KEY");
 
   if (!apiKey || apiKey !== process.env.API_KEY) {
-    return new Response(JSON.stringify({ error: "Invalid or missing API key" }), {
-      status: 401,
-      headers: { "Content-Type": "application/json" },
-    });
+    return Response.json({ error: "Invalid or missing API key" }, { status: 401 });
   }
 
-  if (!data.wallet_address || !data.email) {
-    return Response.json({ error: "Missing required fields" }, { status: 400 });
+  if (!data.wallet_address) {
+    return Response.json({ error: "Missing wallet address parameter" }, { status: 400 });
   }
+
+  // Start constructing the query dynamically
+  let fields: string[] = ["wallet_address"];
+  let values: string[] = [data.wallet_address];
+  let params: string[] = ["$1"];
+  let updateSets: string[] = [];
+  let counter: number = 2;
+
+  // Dynamically add fields that are present
+  ["email", "account_health", "ybx_updates", "last_notification"].forEach((field) => {
+    if (data.hasOwnProperty(field)) {
+      fields.push(field);
+      values.push(data[field]);
+      params.push(`$${counter}`);
+      updateSets.push(`${field} = EXCLUDED.${field}`);
+      counter++;
+    }
+  });
+
+  const queryText = `
+    INSERT INTO notification_settings (${fields.join(", ")})
+    VALUES (${params.join(", ")})
+    ON CONFLICT (wallet_address)
+    DO UPDATE SET ${updateSets.join(", ")};
+  `;
 
   try {
-    const queryText = `
-      INSERT INTO notification_settings (wallet_address, email, account_health, ybx_updates, updated_at)
-      VALUES ($1, $2, $3, $4, NOW())
-      ON CONFLICT (wallet_address)
-      DO UPDATE SET email = EXCLUDED.email, account_health = EXCLUDED.account_health, ybx_updates = EXCLUDED.ybx_updates, updated_at = NOW();
-    `;
-
-    await db.query(queryText, [data.wallet_address, data.email, data.account_health, data.ybx_updates]);
+    await db.query(queryText, values);
     return Response.json({ success: true }, { status: 200 });
   } catch (error) {
     console.error("Database error:", error);
