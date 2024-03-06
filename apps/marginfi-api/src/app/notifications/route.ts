9b6f2f847ff7fbd4c7f39781012dabd012157cae
chambaz <adammchambers@gmail.com>
2024-03-06 16:16:43 -0500

    chore: remove old pg env vars / clean up

--- apps/marginfi-api/src/app/notifications/route.ts
+++ apps/marginfi-api/src/app/notifications/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 import db from "~/lib/pg";
 
 export const dynamic = "force-dynamic";
@@ -7,9 +9,9 @@ export async function GET(request: Request) {
   const wallet = searchParams.get("wallet_address");
   const apiKey = request.headers.get("X-API-KEY");
 
-  if (!apiKey || apiKey !== process.env.API_KEY) {
+  if (!apiKey || apiKey !== process.env.MARGINFI_API_KEY) {
     console.error("Invalid or missing API key");
-    return Response.json(
+    return NextResponse.json(
       { error: "Invalid or missing API key" },
       {
         status: 401,
@@ -19,7 +21,7 @@ export async function GET(request: Request) {
 
   if (!wallet) {
     console.error("Missing wallet address parameter");
-    return Response.json(
+    return NextResponse.json(
       { error: "Missing wallet address parameter" },
       {
         status: 400,
@@ -33,7 +35,7 @@ export async function GET(request: Request) {
 
     if (rows.length === 0) {
       console.error("Notification settings not found");
-      return Response.json(
+      return NextResponse.json(
         { error: "Notification settings not found" },
         {
           status: 404,
@@ -41,12 +43,12 @@ export async function GET(request: Request) {
       );
     }
 
-    return Response.json(rows[0], {
+    return NextResponse.json(rows[0], {
       status: 200,
     });
   } catch (error) {
     console.error("Database error:", error);
-    return Response.json(
+    return NextResponse.json(
       { error: "Internal server error" },
       {
         status: 500,
@@ -59,12 +61,12 @@ export async function POST(request: Request) {
   const data = await request.json();
   const apiKey = request.headers.get("X-API-KEY");
 
-  if (!apiKey || apiKey !== process.env.API_KEY) {
-    return Response.json({ error: "Invalid or missing API key" }, { status: 401 });
+  if (!apiKey || apiKey !== process.env.MARGINFI_API_KEY) {
+    return NextResponse.json({ error: "Invalid or missing API key" }, { status: 401 });
   }
 
   if (!data.wallet_address) {
-    return Response.json({ error: "Missing wallet address parameter" }, { status: 400 });
+    return NextResponse.json({ error: "Missing wallet address parameter" }, { status: 400 });
   }
 
   // Start constructing the query dynamically
@@ -94,9 +96,9 @@ export async function POST(request: Request) {
 
   try {
     await db.query(queryText, values);
-    return Response.json({ success: true }, { status: 200 });
+    return NextResponse.json({ success: true }, { status: 200 });
   } catch (error) {
     console.error("Database error:", error);
-    return Response.json({ error: "Internal server error" }, { status: 500 });
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
 }
