3ac6b430e24d27aabec4f0bd9d55a7f2de885c06
chambaz <adammchambers@gmail.com>
2024-03-07 13:16:10 -0500

    chore: inline styles

--- apps/marginfi-notifications/src/lib/emailTemplates.ts
+++ apps/marginfi-notifications/src/lib/emailTemplates.ts
@@ -10,350 +10,121 @@ export function accountHealthEmail(health: number): string {
   <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Simple Transactional Email</title>
     <style media="all" type="text/css">
-      /* -------------------------------------
-    GLOBAL RESETS
-------------------------------------- */
+@media all {
+  .btn-primary table td:hover {
+    background-color: #E3E3E3 !important;
+  }
 
-      body {
-        font-family: Helvetica, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        font-size: 16px;
-        line-height: 1.3;
-        -ms-text-size-adjust: 100%;
-        -webkit-text-size-adjust: 100%;
-      }
+  .btn-primary a:hover {
+    background-color: #E3E3E3 !important;
+    border-color: #E3E3E3 !important;
+  }
+}
+@media only screen and (max-width: 640px) {
+  .main p,
+.main td,
+.main span {
+    font-size: 16px !important;
+  }
 
-      table {
-        border-collapse: separate;
-        mso-table-lspace: 0pt;
-        mso-table-rspace: 0pt;
-        width: 100%;
-      }
+  .wrapper {
+    padding: 16px !important;
+  }
 
-      table td {
-        font-family: Helvetica, sans-serif;
-        font-size: 16px;
-        vertical-align: top;
-      }
-      /* -------------------------------------
-    BODY & CONTAINER
-------------------------------------- */
+  .content {
+    padding: 0 !important;
+  }
 
-      body {
-        background-color: #111313;
-        margin: 0;
-        padding: 0;
-      }
+  .container {
+    padding: 0 !important;
+    padding-top: 8px !important;
+    width: 100% !important;
+  }
 
-      .body {
-        background-color: #111313;
-        width: 100%;
-      }
+  .main {
+    border-left-width: 0 !important;
+    border-radius: 0 !important;
+    border-right-width: 0 !important;
+  }
 
-      .container {
-        margin: 0 auto !important;
-        max-width: 600px;
-        padding: 0;
-        padding-top: 24px;
-        width: 600px;
-      }
+  .btn table {
+    max-width: 100% !important;
+    width: 100% !important;
+  }
 
-      .content {
-        box-sizing: border-box;
-        display: block;
-        margin: 0 auto;
-        max-width: 600px;
-        padding: 0;
-      }
-      /* -------------------------------------
-    HEADER, FOOTER, MAIN
-------------------------------------- */
+  .btn a {
+    font-size: 16px !important;
+    max-width: 100% !important;
+    width: 100% !important;
+  }
+}
+@media all {
+  .ExternalClass {
+    width: 100%;
+  }
 
-      .main {
-        background: #191d20;
-        border: 1px solid #141719;
-        border-radius: 16px;
-        color: #9f9fa7;
-        text-align: center;
-        width: 100%;
-      }
+  .ExternalClass,
+.ExternalClass p,
+.ExternalClass span,
+.ExternalClass font,
+.ExternalClass td,
+.ExternalClass div {
+    line-height: 100%;
+  }
 
-      .wrapper {
-        box-sizing: border-box;
-        padding: 36px 96px;
-      }
+  .apple-link a {
+    color: inherit !important;
+    font-family: inherit !important;
+    font-size: inherit !important;
+    font-weight: inherit !important;
+    line-height: inherit !important;
+    text-decoration: none !important;
+  }
 
-      .header-image {
-        margin-bottom: 20px;
-        width: 64px;
-      }
-
-      .footer {
-        clear: both;
-        padding-top: 24px;
-        text-align: center;
-        width: 100%;
-      }
-
-      .footer td,
-      .footer p,
-      .footer span,
-      .footer a {
-        color: #9a9ea6;
-        font-size: 16px;
-        text-align: center;
-      }
-      /* -------------------------------------
-    TYPOGRAPHY
-------------------------------------- */
-
-      p {
-        font-family: Helvetica, sans-serif;
-        font-size: 16px;
-        font-weight: normal;
-        margin: 0;
-        margin-bottom: 16px;
-      }
-
-      a {
-        color: #0867ec;
-        text-decoration: underline;
-      }
-
-      h1 {
-        color: #ffffff;
-        font-family: Helvetica, sans-serif;
-        font-size: 24px;
-        font-weight: 600;
-        line-height: 1.2;
-        margin: 0;
-        margin-bottom: 20px;
-      }
-
-      /* -------------------------------------
-    BUTTONS
-------------------------------------- */
-
-      .btn {
-        box-sizing: border-box;
-        min-width: 100% !important;
-        width: 100%;
-        text-align: center;
-        margin: 30px 0 10px;
-      }
-
-      .btn > tbody > tr > td {
-        padding-bottom: 16px;
-      }
-
-      .btn table {
-        width: auto;
-        margin: 0 auto;
-      }
-
-      .btn table td {
-        background-color: #ffffff;
-        border-radius: 4px;
-        text-align: center;
-      }
-
-      .btn a {
-        background-color: #ffffff;
-        border: solid 2px #0867ec;
-        border-radius: 4px;
-        box-sizing: border-box;
-        color: #0867ec;
-        cursor: pointer;
-        display: inline-block;
-        font-size: 16px;
-        font-weight: bold;
-        margin: 0;
-        padding: 12px 24px;
-        text-decoration: none;
-      }
-
-      .btn-primary table td {
-        background-color: #ffffff;
-      }
-
-      .btn-primary a {
-        background-color: #ffffff;
-        border-color: #ffffff;
-        color: #141719;
-      }
-
-      @media all {
-        .btn-primary table td:hover {
-          background-color: #E3E3E3 !important;
-        }
-        .btn-primary a:hover {
-          background-color: #E3E3E3 !important;
-          border-color: #E3E3E3 !important;
-        }
-      }
-
-      /* -------------------------------------
-    OTHER STYLES THAT MIGHT BE USEFUL
-------------------------------------- */
-
-      .last {
-        margin-bottom: 0;
-      }
-
-      .first {
-        margin-top: 0;
-      }
-
-      .align-center {
-        text-align: center;
-      }
-
-      .align-right {
-        text-align: right;
-      }
-
-      .align-left {
-        text-align: left;
-      }
-
-      .text-link {
-        color: #0867ec !important;
-        text-decoration: underline !important;
-      }
-
-      .clear {
-        clear: both;
-      }
-
-      .mt0 {
-        margin-top: 0;
-      }
-
-      .mb0 {
-        margin-bottom: 0;
-      }
-
-      .preheader {
-        color: transparent;
-        display: none;
-        height: 0;
-        max-height: 0;
-        max-width: 0;
-        opacity: 0;
-        overflow: hidden;
-        mso-hide: all;
-        visibility: hidden;
-        width: 0;
-      }
-
-      .powered-by a {
-        text-decoration: none;
-      }
-
-      /* -------------------------------------
-    RESPONSIVE AND MOBILE FRIENDLY STYLES
-------------------------------------- */
-
-      @media only screen and (max-width: 640px) {
-        .main p,
-        .main td,
-        .main span {
-          font-size: 16px !important;
-        }
-        .wrapper {
-          padding: 16px !important;
-        }
-        .content {
-          padding: 0 !important;
-        }
-        .container {
-          padding: 0 !important;
-          padding-top: 8px !important;
-          width: 100% !important;
-        }
-        .main {
-          border-left-width: 0 !important;
-          border-radius: 0 !important;
-          border-right-width: 0 !important;
-        }
-        .btn table {
-          max-width: 100% !important;
-          width: 100% !important;
-        }
-        .btn a {
-          font-size: 16px !important;
-          max-width: 100% !important;
-          width: 100% !important;
-        }
-      }
-      /* -------------------------------------
-    PRESERVE THESE STYLES IN THE HEAD
-------------------------------------- */
-
-      @media all {
-        .ExternalClass {
-          width: 100%;
-        }
-        .ExternalClass,
-        .ExternalClass p,
-        .ExternalClass span,
-        .ExternalClass font,
-        .ExternalClass td,
-        .ExternalClass div {
-          line-height: 100%;
-        }
-        .apple-link a {
-          color: inherit !important;
-          font-family: inherit !important;
-          font-size: inherit !important;
-          font-weight: inherit !important;
-          line-height: inherit !important;
-          text-decoration: none !important;
-        }
-        #MessageViewBody a {
-          color: inherit;
-          text-decoration: none;
-          font-size: inherit;
-          font-family: inherit;
-          font-weight: inherit;
-          line-height: inherit;
-        }
-      }
-    </style>
+  #MessageViewBody a {
+    color: inherit;
+    text-decoration: none;
+    font-size: inherit;
+    font-family: inherit;
+    font-weight: inherit;
+    line-height: inherit;
+  }
+}
+</style>
   </head>
-  <body>
-    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
+  <body style="font-family: Helvetica, sans-serif; -webkit-font-smoothing: antialiased; font-size: 16px; line-height: 1.3; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; background-color: #111313; margin: 0; padding: 0;">
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #111313; width: 100%;" width="100%" bgcolor="#111313">
       <tr>
-        <td>&nbsp;</td>
-        <td class="container">
-          <div class="content">
+        <td style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top;" valign="top">&nbsp;</td>
+        <td class="container" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; max-width: 600px; padding: 0; padding-top: 48px; padding-bottom: 48px; width: 600px; margin: 0 auto;" width="600" valign="top">
+          <div class="content" style="box-sizing: border-box; display: block; margin: 0 auto; max-width: 600px; padding: 0;">
             <!-- START CENTERED WHITE CONTAINER -->
-            <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main">
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background: #191d20; border: 1px solid #141719; border-radius: 16px; color: #9f9fa7; text-align: center; width: 100%;" width="100%" align="center">
               <!-- START MAIN CONTENT AREA -->
               <tr>
-                <td class="wrapper">
-                  <img class="header-image" src="https://marginfi-v2-ui-git-staging-mrgn.vercel.app/mrgn-white.png" />
-                  <h1 class="title"><span style="padding-right: 5px;">⚠</span>️ Account Health Alert</h1>
-                  <p>
-                    Your account health is currently at <strong style="color: #eeeeee;">${health}%</strong>.<br /> If it reaches 0, you are at risk of
+                <td class="wrapper" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; box-sizing: border-box; padding: 36px 96px;" valign="top">
+                  <img class="header-image" src="https://marginfi-v2-ui-git-feature-notifications-mrgn.vercel.app/mrgn-white.png" style="margin-bottom: 20px; width: 64px;" width="64">
+                  <h1 class="title" style="color: #ffffff; font-family: Helvetica, sans-serif; font-size: 24px; font-weight: 600; line-height: 1.2; margin: 0; margin-bottom: 20px;"><span style="padding-right: 5px;">⚠</span>️ Account Health Alert</h1>
+                  <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">
+                    Your account health is currently at <strong style="color: #eeeeee;">${health}%</strong>.<br> If it reaches 0, you are at risk of
                     liquidation.
                   </p>
-                  <p>
+                  <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">
                     To avoid liquidation, consider adding collateral or closing positions to improve your account
                     health.
                   </p>
-                  <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; box-sizing: border-box; width: 100%; text-align: center; margin: 30px 0 10px; min-width: 100%;" width="100%" align="center">
                     <tbody>
                       <tr>
-                        <td align="left">
-                          <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                        <td align="left" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; padding-bottom: 16px;" valign="top">
+                          <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto; margin: 0 auto;">
                             <tbody>
                               <tr>
-                                <td align="center" style="text-align: center;"><a href="href="https://app.marginfi.com"" target="_blank">login to marginfi</a></td>
+                                <td align="center" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; border-radius: 4px; background-color: #ffffff; text-align: center;" valign="top" bgcolor="#ffffff"><a href="href=" https:="" app.marginfi.com""="" target="_blank" style="border: solid 2px #0867ec; border-radius: 4px; box-sizing: border-box; cursor: pointer; display: inline-block; font-size: 16px; font-weight: bold; margin: 0; padding: 12px 24px; text-decoration: none; background-color: #ffffff; border-color: #ffffff; color: #141719;">login to marginfi</a></td>
                               </tr>
                             </tbody>
                           </table>
@@ -370,11 +141,10 @@ export function accountHealthEmail(health: number): string {
             <!-- END CENTERED WHITE CONTAINER -->
           </div>
         </td>
-        <td>&nbsp;</td>
+        <td style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top;" valign="top">&nbsp;</td>
       </tr>
     </table>
   </body>
 </html>
-
   `;
 }
