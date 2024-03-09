63c19c528eb385acbef96c114d550b0b8ebc96ea
chambaz <adammchambers@gmail.com>
2024-03-09 15:38:14 -0500

    fix: staking card button colors

--- apps/marginfi-v2-ui/src/components/common/Staking/StakingCard/SettingsModal.tsx
+++ apps/marginfi-v2-ui/src/components/common/Staking/StakingCard/SettingsModal.tsx
@@ -34,7 +34,7 @@ export const SettingsModal: FC<SettingsModalProps> = ({
       aria-describedby="parent-modal-description"
       className="border-none"
     >
-      <div className="mx-auto mt-48 rounded-xl bg-[#1C2023] w-[400px] max-w-[90%]  p-4">
+      <div className="mx-auto mt-48 rounded-xl bg-[#1C2023] w-[400px] max-w-[90%]  p-4 focus-visible:outline-none">
         <div className="flex flex-row justify-between mb-3">
           <Typography className="font-aeonik font-[700] text-2xl inline">Swap Settings</Typography>
           <div className="cursor-pointer" onClick={handleClose}>
@@ -49,15 +49,17 @@ export const SettingsModal: FC<SettingsModalProps> = ({
             return (
               <a
                 key={idx}
-                className={`relative cursor-pointer flex-1 text-secondary py-3 hover:bg-gray-700 ${
-                  isHighlighted ? "bg-[#DCE85D88] hover:bg-[#DCE85D88]" : "bg-[#1B1B1E]"
+                className={`relative cursor-pointer flex-1 text-primary py-3 hover:bg-gray-700 ${
+                  isHighlighted ? "bg-chartreuse hover:bg-chartreuse" : "bg-[#1B1B1E]"
                 }`}
                 onClick={() => {
                   setLocalSlippage(slippage);
                 }}
               >
                 <div className="h-full w-full leading-none flex justify-center items-center">
-                  <Typography className={`text-secondary mt-[2px]`}>{displayText}</Typography>
+                  <Typography className={`mt-[2px] ${isHighlighted ? "text-accent" : "text-muted-foreground"}`}>
+                    {displayText}
+                  </Typography>
                 </div>
               </a>
             );
