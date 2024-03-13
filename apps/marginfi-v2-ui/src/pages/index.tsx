771940b8c7503d5b87ebd47a4a2844f743527b80
chambaz <adammchambers@gmail.com>
2024-03-12 20:46:50 -0400

    feat: announcements / new bank component with responsive swiper (wip)

--- apps/marginfi-v2-ui/src/pages/index.tsx
+++ apps/marginfi-v2-ui/src/pages/index.tsx
@@ -12,9 +12,10 @@ import { UserMode } from "~/types";
 
 import { Banner } from "~/components/desktop/Banner";
 import { PageHeader } from "~/components/common/PageHeader";
-import { ActionBox, ActionBoxLendWrapper } from "~/components/common/ActionBox";
+import { ActionBoxLendWrapper } from "~/components/common/ActionBox";
 import { Stats } from "~/components/common/Stats";
 import { ActionComplete } from "~/components/common/ActionComplete";
+import { Announcements } from "~/components/common/Announcements";
 
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger } from "~/components/ui/select";
 import { OverlaySpinner } from "~/components/ui/overlay-spinner";
@@ -33,6 +34,7 @@ export default function HomePage() {
     setIsRefreshingStore,
     marginfiAccounts,
     selectedAccount,
+    extendedBankInfos,
   ] = useMrgnlendStore((state) => [
     state.fetchMrgnlendState,
     state.initialized,
@@ -40,6 +42,7 @@ export default function HomePage() {
     state.setIsRefreshingStore,
     state.marginfiAccounts,
     state.selectedAccount,
+    state.extendedBankInfos,
   ]);
 
   return (
@@ -68,6 +71,21 @@ export default function HomePage() {
                 />
               )}
               <Stats />
+              <Announcements
+                items={[
+                  extendedBankInfos[0],
+                  extendedBankInfos[1],
+                  {
+                    text: "Announcement 1",
+                    image: "https://storage.googleapis.com/mrgn-public/mrgn-token-icons/%24WIF.png",
+                    onClick: () => console.log("test"),
+                  },
+                  {
+                    text: "Announcement 2",
+                    image: "https://storage.googleapis.com/mrgn-public/mrgn-token-icons/%24WIF.png",
+                  },
+                ]}
+              />
               {userMode === UserMode.LITE && <ActionBoxLendWrapper />}
             </div>
             <div className="pt-[16px] pb-[64px] px-4 w-full xl:w-4/5 xl:max-w-7xl mt-8 gap-4">
@@ -93,6 +111,21 @@ export default function HomePage() {
               />
             )}
             <Stats />
+            <Announcements
+              items={[
+                extendedBankInfos[0],
+                extendedBankInfos[1],
+                {
+                  text: "Announcement 1",
+                  image: "https://storage.googleapis.com/mrgn-public/mrgn-token-icons/%24WIF.png",
+                  onClick: () => console.log("test"),
+                },
+                {
+                  text: "Announcement 2",
+                  image: "https://storage.googleapis.com/mrgn-public/mrgn-token-icons/%24WIF.png",
+                },
+              ]}
+            />
             <ActionBoxLendWrapper />
           </>
         )}
