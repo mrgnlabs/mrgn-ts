30aabb3341b640cb4fef6fc315a2dd7ae476d66d
chambaz <adammchambers@gmail.com>
2024-03-15 14:34:49 -0400

    feat: actionbox modal false on mobile

--- apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxDialog.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxDialog.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import { ActionType } from "@mrgnlabs/marginfi-v2-ui-state";
 import { PublicKey } from "@solana/web3.js";
 
+import { useIsMobile } from "~/hooks/useIsMobile";
 import { ActionBox } from "~/components/common/ActionBox";
-import { Dialog, DialogTrigger, DialogContent } from "~/components/ui/dialog";
+import { Dialog, DialogTrigger, DialogOverlay, DialogContent } from "~/components/ui/dialog";
 
 type ActionBoxDialogProps = {
   requestedAction?: ActionType;
@@ -20,13 +21,15 @@ export const ActionBoxDialog = ({
   isActionBoxTriggered = false,
 }: ActionBoxDialogProps) => {
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+  const isMobile = useIsMobile();
 
   React.useEffect(() => {
     setIsDialogOpen(isActionBoxTriggered);
   }, [setIsDialogOpen, isActionBoxTriggered]);
 
   return (
-    <Dialog open={isDialogOpen} onOpenChange={(open) => setIsDialogOpen(open)}>
+    <Dialog open={isDialogOpen} modal={!isMobile} onOpenChange={(open) => setIsDialogOpen(open)}>
+      {isMobile && isDialogOpen && <div className="fixed inset-0 h-screen z-50 bg-background md:bg-background/80" />}
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="md:flex md:max-w-[520px] md:py-3 md:px-5 p-0 sm:rounded-2xl bg-transparent border-none">
         <div className="p-4">
