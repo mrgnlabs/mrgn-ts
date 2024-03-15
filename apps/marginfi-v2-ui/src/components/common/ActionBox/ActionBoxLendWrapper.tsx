fa6987f59acee9b7076afd0246096b72026732f7
chambaz <adammchambers@gmail.com>
2024-03-15 12:36:23 -0400

    feat: remove home page tagline, update wallet grays

--- apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxLendWrapper.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxLendWrapper.tsx
@@ -5,9 +5,6 @@ import { ActionBox } from "~/components/common/ActionBox";
 export const ActionBoxLendWrapper = () => {
   return (
     <div className="p-4 space-y-4 w-full">
-      <div className="text-center w-full">
-        <p className="text-muted-foreground">Supply. Earn interest. Borrow. Repeat.</p>
-      </div>
       <ActionBox showLendingHeader={true} />
     </div>
   );
