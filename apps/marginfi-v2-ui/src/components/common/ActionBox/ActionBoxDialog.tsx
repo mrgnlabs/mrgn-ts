66969ce4fbb1eb8dcde39b4ca608a058021c27be
Kobe <leenderskobe@gmail.com>
2024-03-15 20:33:29 +0100

    feat: mobile dialog page

--- apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxDialog.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/ActionBoxDialog.tsx
@@ -5,7 +5,10 @@ import { PublicKey } from "@solana/web3.js";
 
 import { useIsMobile } from "~/hooks/useIsMobile";
 import { ActionBox } from "~/components/common/ActionBox";
-import { Dialog, DialogTrigger, DialogOverlay, DialogContent } from "~/components/ui/dialog";
+import { Dialog, DialogTrigger, DialogOverlay, DialogContent, DialogClose } from "~/components/ui/dialog";
+import { Desktop, Mobile } from "~/mediaQueries";
+import { IconArrowLeft } from "~/components/ui/icons";
+import { useMrgnlendStore } from "~/store";
 
 type ActionBoxDialogProps = {
   requestedAction?: ActionType;
@@ -22,25 +25,61 @@ export const ActionBoxDialog = ({
 }: ActionBoxDialogProps) => {
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
   const isMobile = useIsMobile();
+  const [extendedBankInfos] = useMrgnlendStore((state) => [state.extendedBankInfos]);
 
   React.useEffect(() => {
     setIsDialogOpen(isActionBoxTriggered);
   }, [setIsDialogOpen, isActionBoxTriggered]);
 
+  const selectedBank = React.useMemo(
+    () =>
+      requestedToken
+        ? extendedBankInfos.find((bank) => bank?.address?.equals && bank?.address?.equals(requestedToken)) ?? null
+        : null,
+    [extendedBankInfos, requestedToken]
+  );
+
   return (
     <Dialog open={isDialogOpen} modal={!isMobile} onOpenChange={(open) => setIsDialogOpen(open)}>
-      {isMobile && isDialogOpen && <div className="fixed inset-0 h-screen z-50 bg-background md:bg-background/80" />}
-      <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="md:flex md:max-w-[520px] md:py-3 md:px-5 p-0 sm:rounded-2xl bg-transparent border-none">
-        <div className="p-4">
-          <ActionBox
-            isDialog={true}
-            handleCloseDialog={() => setIsDialogOpen(false)}
-            requestedAction={requestedAction}
-            requestedToken={requestedToken}
-          />
-        </div>
-      </DialogContent>
+      <Mobile>
+        {isDialogOpen && <div className="fixed inset-0 h-screen z-50 bg-background md:bg-background/80" />}
+        <DialogTrigger asChild>{children}</DialogTrigger>
+        <DialogContent
+          hideClose={true}
+          className="mt-24 justify-start flex md:max-w-[520px] md:py-3 md:px-5 p-0 sm:rounded-2xl bg-transparent border-none"
+        >
+          <div>
+            <div
+              className="flex gap-2 items-center capitalize pl-2 cursor-pointer hover:underline"
+              onClick={() => setIsDialogOpen(false)}
+            >
+              <IconArrowLeft /> {`${requestedAction} ${selectedBank?.meta.tokenSymbol}`}
+            </div>
+            <div className="p-4">
+              <ActionBox
+                isDialog={true}
+                handleCloseDialog={() => setIsDialogOpen(false)}
+                requestedAction={requestedAction}
+                requestedToken={requestedToken}
+              />
+            </div>
+          </div>
+        </DialogContent>
+      </Mobile>
+
+      <Desktop>
+        <DialogTrigger asChild>{children}</DialogTrigger>
+        <DialogContent className="md:flex md:max-w-[520px] md:py-3 md:px-5 p-0 sm:rounded-2xl bg-transparent border-none">
+          <div className="p-4">
+            <ActionBox
+              isDialog={true}
+              handleCloseDialog={() => setIsDialogOpen(false)}
+              requestedAction={requestedAction}
+              requestedToken={requestedToken}
+            />
+          </div>
+        </DialogContent>
+      </Desktop>
     </Dialog>
   );
 };
