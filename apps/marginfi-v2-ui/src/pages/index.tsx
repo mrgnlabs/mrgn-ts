832638e9f90c6b7f101c49cb44590670fd8470f3
chambaz <adammchambers@gmail.com>
2024-03-21 11:47:52 -0400

    feat: remove lite / pro mode

--- apps/marginfi-v2-ui/src/pages/index.tsx
+++ apps/marginfi-v2-ui/src/pages/index.tsx
@@ -97,7 +97,7 @@ export default function HomePage() {
                 />
               )}
               <Announcements items={annoucements} />
-              {userMode === UserMode.LITE && <ActionBoxLendWrapper />}
+              <ActionBoxLendWrapper />
             </div>
             <div className="pt-[16px] pb-[64px] px-4 w-full xl:w-4/5 xl:max-w-7xl mt-8 gap-4">
               <AssetsList />
