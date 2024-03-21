5dc09ce96376a1e8e56e9b29949931713657fb3a
chambaz <adammchambers@gmail.com>
2024-03-21 15:01:15 -0400

    chore: remove lite / pro mode

--- apps/marginfi-v2-ui/src/components/common/AssetList/AssetListFilters.tsx
+++ apps/marginfi-v2-ui/src/components/common/AssetList/AssetListFilters.tsx
@@ -11,14 +11,12 @@ import { Label } from "~/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
 import { IconFilter, IconSortAscending, IconSortDescending } from "~/components/ui/icons";
 
-import { LendingModes, PoolTypes, SortType, sortDirection, SortAssetOption, UserMode } from "~/types";
+import { LendingModes, PoolTypes, SortType, sortDirection, SortAssetOption } from "~/types";
 
 export const AssetListFilters = () => {
   const { connected } = useWalletContext();
   const isMobile = useIsMobile();
   const [
-    userMode,
-    setUserMode,
     lendingMode,
     setLendingMode,
     poolFilter,
@@ -29,8 +27,6 @@ export const AssetListFilters = () => {
     sortOption,
     setSortOption,
   ] = useUiStore((state) => [
-    state.userMode,
-    state.setUserMode,
     state.lendingMode,
     state.setLendingMode,
     state.poolFilter,
@@ -62,91 +58,87 @@ export const AssetListFilters = () => {
             </ToggleGroupItem>
           </ToggleGroup>
         </div>
-        {(userMode === UserMode.PRO || isMobile) && (
-          <div
-            className={cn("flex items-center gap-2 text-sm", !connected && "opacity-50")}
-            onClick={(e) => {
-              e.stopPropagation();
-              if (connected) return;
-              setIsWalletAuthDialogOpen(true);
+        <div
+          className={cn("flex items-center gap-2 text-sm", !connected && "opacity-50")}
+          onClick={(e) => {
+            e.stopPropagation();
+            if (connected) return;
+            setIsWalletAuthDialogOpen(true);
+          }}
+        >
+          <Switch
+            id="filter-positions"
+            checked={isFilteredUserPositions}
+            onCheckedChange={() => {
+              if (!connected) return;
+              setIsFilteredUserPositions(!isFilteredUserPositions);
+              setPoolFilter(PoolTypes.ALL);
             }}
+          />
+          <Label
+            htmlFor="filter-positions"
+            className={cn(
+              "transition-colors text-muted-foreground cursor-pointer hover:text-white",
+              isFilteredUserPositions && "text-white"
+            )}
           >
-            <Switch
-              id="filter-positions"
-              checked={isFilteredUserPositions}
-              onCheckedChange={() => {
-                if (!connected) return;
-                setIsFilteredUserPositions(!isFilteredUserPositions);
-                setPoolFilter(PoolTypes.ALL);
+            Filter my positions
+          </Label>
+        </div>
+        <div className="flex flex-col md:flex-row md:items-center gap-3">
+          <div className="space-y-2 w-full md:w-auto">
+            <Select
+              value={poolFilter}
+              disabled={isFilteredUserPositions}
+              onValueChange={(value) => {
+                setPoolFilter(value as PoolTypes);
               }}
-            />
-            <Label
-              htmlFor="filter-positions"
-              className={cn(
-                "transition-colors text-muted-foreground cursor-pointer hover:text-white",
-                isFilteredUserPositions && "text-white"
-              )}
             >
-              Filter my positions
-            </Label>
+              <SelectTrigger className="md:w-[180px]">
+                <div className="flex items-center gap-2">
+                  <IconFilter size={18} />
+                  <SelectValue defaultValue="allpools" placeholder="Select pools" />
+                </div>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All pools</SelectItem>
+                <SelectItem value="isolated">Isolated pools</SelectItem>
+                <SelectItem value="stable">Stablecoins</SelectItem>
+                <SelectItem value="lst">SOL / LST</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
-        )}
-        {(userMode === UserMode.PRO || isMobile) && (
-          <div className="flex flex-col md:flex-row md:items-center gap-3">
-            <div className="space-y-2 w-full md:w-auto">
-              <Select
-                value={poolFilter}
-                disabled={isFilteredUserPositions}
-                onValueChange={(value) => {
-                  setPoolFilter(value as PoolTypes);
-                }}
-              >
-                <SelectTrigger className="md:w-[180px]">
-                  <div className="flex items-center gap-2">
-                    <IconFilter size={18} />
-                    <SelectValue defaultValue="allpools" placeholder="Select pools" />
-                  </div>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="all">All pools</SelectItem>
-                  <SelectItem value="isolated">Isolated pools</SelectItem>
-                  <SelectItem value="stable">Stablecoins</SelectItem>
-                  <SelectItem value="lst">SOL / LST</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="space-y-2 w-full md:w-auto">
-              <Select
-                value={sortOption.value}
-                disabled={isFilteredUserPositions}
-                onValueChange={(value) => setSortOption(SORT_OPTIONS_MAP[value as SortType])}
-              >
-                <SelectTrigger className="md:w-[220px]">
-                  <div className="flex items-center gap-2">
-                    {sortOption.direction === sortDirection.ASC ? (
-                      <IconSortAscending size={18} />
-                    ) : (
-                      <IconSortDescending size={18} />
-                    )}
-                    <SelectValue placeholder="Order by" />
-                  </div>
-                </SelectTrigger>
-                <SelectContent>
-                  {Object.values(SORT_OPTIONS_MAP).map((option) => {
-                    const opt = option as SortAssetOption;
-                    return (
-                      <SelectItem key={opt.value} value={opt.value}>
-                        <div className="flex items-center gap-2">
-                          {lendingMode === LendingModes.LEND || !opt.borrowLabel ? opt.label : opt.borrowLabel}
-                        </div>
-                      </SelectItem>
-                    );
-                  })}
-                </SelectContent>
-              </Select>
-            </div>
+          <div className="space-y-2 w-full md:w-auto">
+            <Select
+              value={sortOption.value}
+              disabled={isFilteredUserPositions}
+              onValueChange={(value) => setSortOption(SORT_OPTIONS_MAP[value as SortType])}
+            >
+              <SelectTrigger className="md:w-[220px]">
+                <div className="flex items-center gap-2">
+                  {sortOption.direction === sortDirection.ASC ? (
+                    <IconSortAscending size={18} />
+                  ) : (
+                    <IconSortDescending size={18} />
+                  )}
+                  <SelectValue placeholder="Order by" />
+                </div>
+              </SelectTrigger>
+              <SelectContent>
+                {Object.values(SORT_OPTIONS_MAP).map((option) => {
+                  const opt = option as SortAssetOption;
+                  return (
+                    <SelectItem key={opt.value} value={opt.value}>
+                      <div className="flex items-center gap-2">
+                        {lendingMode === LendingModes.LEND || !opt.borrowLabel ? opt.label : opt.borrowLabel}
+                      </div>
+                    </SelectItem>
+                  );
+                })}
+              </SelectContent>
+            </Select>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
