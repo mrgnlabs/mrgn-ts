c0eafb38c3d5c3ce3f68c2e8a686de739b8c3b08
chambaz <adammchambers@gmail.com>
2024-03-26 21:34:02 -0400

    feat: wallet v2 token list view / receive copy address button (wip)

--- apps/marginfi-v2-ui/src/components/ui/icons.tsx
+++ apps/marginfi-v2-ui/src/components/ui/icons.tsx
@@ -53,6 +53,7 @@ import {
   IconHelp,
   IconBook,
   IconLogout,
+  IconRefresh,
 } from "@tabler/icons-react";
 import { cn } from "~/utils/themeUtils";
 
@@ -826,6 +827,7 @@ export {
   IconHelp,
   IconBook,
   IconLogout,
+  IconRefresh,
 
   // customed icons
   IconBrandGoogle,
