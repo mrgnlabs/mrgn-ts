209ff96eac0e8d6584a784f0bc8a6e459f04da16
Kobe <leenderskobe@gmail.com>
2024-03-29 16:52:05 +0100

    feat: portfolio page updates

--- apps/marginfi-v2-ui/src/pages/portfolio.tsx
+++ apps/marginfi-v2-ui/src/pages/portfolio.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 
-import Link from "next/link";
+import { useMrgnlendStore, useUiStore } from "~/store";
 
-import { useMrgnlendStore, useUserProfileStore, useUiStore } from "~/store";
-import { useWalletContext } from "~/hooks/useWalletContext";
-
-import { PointsConnectWallet, PointsOverview } from "~/components/common/Points";
-import { EmissionsBanner } from "~/components/mobile/EmissionsBanner";
-import { Portfolio } from "~/components/common/Portfolio";
+import { LendingPortfolio } from "~/components/common/Portfolio";
 import { ActionComplete } from "~/components/common/ActionComplete";
 import { Loader } from "~/components/ui/loader";
-import { Button } from "~/components/ui/button";
-import { IconTrophy } from "~/components/ui/icons";
+import { PortfolioHeader } from "~/components/common/Portfolio/PortfolioHeader";
 
 export default function PortfolioPage() {
-  const { connected } = useWalletContext();
   const [initialized] = useMrgnlendStore((state) => [state.initialized]);
-  const [userPointsData] = useUserProfileStore((state) => [state.userPointsData]);
   const [previousTxn] = useUiStore((state) => [state.previousTxn]);
 
   return (
@@ -25,8 +17,9 @@ export default function PortfolioPage() {
       <div className="flex flex-col max-w-7xl mx-auto w-full h-full justify-start items-center px-4 gap-4 mb-20">
         {initialized && (
           <>
-            <EmissionsBanner />
-            <Portfolio />
+            <PortfolioHeader />
+            <LendingPortfolio />
+            {/* <Portfolio /> */}
           </>
         )}
       </div>
