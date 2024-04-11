01fac090b2402de99a48dfd1f9c2b0c3b3898b15
chambaz <adammchambers@gmail.com>
2024-04-11 15:12:39 -0400

    feat: increase footer icon list gap

--- apps/marginfi-v2-ui/src/components/desktop/Footer/Footer.tsx
+++ apps/marginfi-v2-ui/src/components/desktop/Footer/Footer.tsx
@@ -47,7 +47,7 @@ export const Footer = () => {
   return (
     <footer className="bg-background-gray text-muted-foreground fixed bottom-0 w-full px-4 py-2">
       <nav>
-        <ul className="flex items-center gap-3 justify-end">
+        <ul className="flex items-center gap-3.5 justify-end">
           {footerLinks.map((link, index) => (
             <li key={index}>
               <Link
