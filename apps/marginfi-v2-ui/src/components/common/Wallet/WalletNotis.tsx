10dcbe57dec8bb8e50b7fff4715189ea025d15d7
chambaz <adammchambers@gmail.com>
2024-04-12 06:41:18 -0400

    feat: wallet notis view improvements

--- apps/marginfi-v2-ui/src/components/common/Wallet/WalletNotis.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/WalletNotis.tsx
@@ -5,14 +5,12 @@ import Image from "next/image";
 
 import { useWalletContext } from "~/hooks/useWalletContext";
 import { useConvertkit } from "~/hooks/useConvertkit";
-import { cn } from "~/utils";
+import { signUpYbx } from "~/utils/mintUtils";
 
 import { Button } from "~/components/ui/button";
-import { Checkbox } from "~/components/ui/checkbox";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from "~/components/ui/tooltip";
-import { IconAlertTriangle, IconCheck, IconInfoCircle, IconLoader } from "~/components/ui/icons";
+import { IconAlertTriangle, IconCheck, IconLoader } from "~/components/ui/icons";
 
 enum WalletNotisState {
   DEFAULT = "default",
@@ -21,13 +19,8 @@ enum WalletNotisState {
 }
 
 export const WalletNotis = () => {
-  const { walletAddress } = useWalletContext();
-  const { addSubscriber } = useConvertkit();
-  const [email, setEmail] = React.useState("");
-  const [notificationSettings, setNotificationSettings] = React.useState({
-    health: true,
-    ybx: true,
-  });
+  const [email, setEmail] = React.useState(localStorage.getItem("mrgnYbxNotifications") || "");
+  const emailInputRef = React.useRef<HTMLInputElement>(null);
   const [errorMsg, setErrorMsg] = React.useState<string | null>(null);
   const [walletNotisState, setWalletNotisState] = React.useState<WalletNotisState>(WalletNotisState.DEFAULT);
 
@@ -35,116 +28,43 @@ export const WalletNotis = () => {
     return walletNotisState === WalletNotisState.UPDATING || !email;
   }, [walletNotisState, email]);
 
-  const updateNotificationSettings = React.useCallback(async () => {
-    if (!email) {
-      return;
-    }
-
+  const onSubscribe = React.useCallback(async () => {
+    if (!emailInputRef.current) return;
     setWalletNotisState(WalletNotisState.UPDATING);
+    try {
+      await signUpYbx(emailInputRef, "notifications");
+      setWalletNotisState(WalletNotisState.SUCCESS);
 
-    if (notificationSettings.ybx) {
-      const res = await addSubscriber(process.env.NEXT_PUBLIC_CONVERT_KIT_YBX_NOTIFICATIONS_FORM_UID!, email);
-
-      if (res.error) {
-        setErrorMsg(res.error);
-        setWalletNotisState(WalletNotisState.DEFAULT);
-        return;
-      }
-    }
-
-    const apiUrl = window.location.origin + "/api/user/notifications";
-
-    // create url with query params for notification settings
-    const url = new URL(apiUrl);
-    url.searchParams.append("walletAddress", walletAddress.toBase58());
-    url.searchParams.append("email", email);
-    url.searchParams.append("accountHealth", notificationSettings.health ? "true" : "false");
-    url.searchParams.append("productUpdates", notificationSettings.ybx ? "true" : "false");
-
-    const res = await fetch(url.toString(), {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        walletAddress: walletAddress.toBase58(),
-        email,
-        accountHealth: notificationSettings.health,
-        productUpdates: notificationSettings.ybx,
-      }),
-    });
-
-    if (!res.ok) {
-      setErrorMsg("There was an error updating notifications. Please try again.");
+      localStorage.setItem("mrgnYbxNotifications", emailInputRef.current?.value);
+    } catch (error) {
       setWalletNotisState(WalletNotisState.DEFAULT);
-      return;
+      setErrorMsg("There was an error signing up for notifications. Please try again.");
     }
-
-    setErrorMsg(null);
-    setWalletNotisState(WalletNotisState.SUCCESS);
-
-    setTimeout(() => {
-      setWalletNotisState(WalletNotisState.DEFAULT);
-    }, 2000);
-  }, [email, notificationSettings, walletAddress, addSubscriber]);
-
-  const fetchUsersNotificationSettings = React.useCallback(async () => {
-    const apiUrl = window.location.origin + "/api/user/notifications";
-    const url = new URL(apiUrl);
-    url.searchParams.append("walletAddress", walletAddress.toBase58());
-
-    const res = await fetch(url.toString(), {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!res.ok) {
-      setErrorMsg("There was an error fetching your notification settings. Please try again.");
-      return;
-    }
-
-    const { data } = await res.json();
-
-    setEmail(data.email);
-    setNotificationSettings({
-      health: data.account_health,
-      ybx: data.product_updates,
-    });
-  }, [walletAddress]);
-
-  React.useEffect(() => {
-    // fetchUsersNotificationSettings();
-  }, [walletAddress]);
+  }, [emailInputRef]);
 
   return (
     <form
       className="space-y-4"
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        updateNotificationSettings();
+        onSubscribe();
       }}
     >
-      <p className="flex flex-col gap-2 mb-6 pb-4 border-b border-background-gray-hover text-base font-normal">
-        Set up Telegram notifications with{" "}
-        <Link href="#" className="flex items-center gap-1 group">
-          <Image src="/heimdall.jpg" alt="HeimdallWatchBot Logo" width={28} height={28} className="rounded-full" />{" "}
-          HiemdallWatchBot
+      <p className="flex flex-col gap-2 mb-6 pb-4 border-b border-background-gray-hover">
+        <span className="text-sm font-medium">Set up Telegram notifications with</span>{" "}
+        <Link href="#">
+          <Button variant="ghost" className="px-2 py-2.5 gap-1.5">
+            <Image src="/heimdall.jpg" alt="HeimdallWatchBot Logo" width={28} height={28} className="rounded-full" />{" "}
+            HiemdallWatchBot
+          </Button>
         </Link>
       </p>
       <div className="space-y-2">
-        <Label htmlFor="email" className="flex items-center gap-1.5">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger className="flex items-center gap-1.5">
-                <IconInfoCircle size={16} /> Configure email notifications
-              </TooltipTrigger>
-              <TooltipContent>Click to copy</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+        <Label htmlFor="email" className="flex items-center gap-1.5 text-sm font-medium">
+          Sign up for YBX alerts
         </Label>
         <Input
+          ref={emailInputRef}
           id="email"
           type="email"
           placeholder="example@example.com"
@@ -154,49 +74,6 @@ export const WalletNotis = () => {
           }}
         />
       </div>
-
-      <ul className="space-y-3">
-        <li className="flex items-center gap-1.5">
-          <Checkbox
-            checked={notificationSettings.health}
-            id="health"
-            className={cn(
-              !notificationSettings.health && "border-muted-foreground transition-colors hover:border-primary"
-            )}
-            onCheckedChange={(checked) =>
-              setNotificationSettings({ ...notificationSettings, health: checked as boolean })
-            }
-          />{" "}
-          <Label
-            htmlFor="health"
-            className={cn(
-              "text-primary",
-              !notificationSettings.health && "text-muted-foreground transition-colors hover:text-primary"
-            )}
-          >
-            Account heath / liquidation risk
-          </Label>
-        </li>
-        <li className="flex items-center gap-1.5">
-          <Checkbox
-            checked={notificationSettings.ybx}
-            id="ybx"
-            className={cn(
-              !notificationSettings.ybx && "border-muted-foreground transition-colors hover:border-primary"
-            )}
-            onCheckedChange={(checked) => setNotificationSettings({ ...notificationSettings, ybx: checked as boolean })}
-          />{" "}
-          <Label
-            htmlFor="ybx"
-            className={cn(
-              "text-primary",
-              !notificationSettings.ybx && "text-muted-foreground transition-colors hover:text-primary"
-            )}
-          >
-            YBX launch notifications
-          </Label>
-        </li>
-      </ul>
 
       {errorMsg && (
         <div className="flex items-start gap-2 bg-destructive text-destructive-foreground px-4 py-2 rounded-lg my-4">
