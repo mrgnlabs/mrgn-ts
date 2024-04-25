cfd1a5dc9ddd94a017df14852983f07a08be808c
chambaz <adammchambers@gmail.com>
2024-04-25 16:30:50 -0400

    feat: add close button to announcement dialog

--- apps/marginfi-v2-ui/src/components/common/Announcements/AnnouncementsDialog.tsx
+++ apps/marginfi-v2-ui/src/components/common/Announcements/AnnouncementsDialog.tsx
@@ -18,7 +18,12 @@ import Link from "next/link";
 const announcementLabel = "rlb-notice" as const;
 
 export const AnnouncementsDialog = () => {
-  const [isOpen, setIsOpen] = React.useState(true);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const closeDialog = React.useCallback(() => {
+    localStorage.setItem("mrgnAnnouncementPopup", announcementLabel);
+    setIsOpen(false);
+  }, []);
 
   React.useEffect(() => {
     // only show announcement popup if tutorial has been acknowledged
@@ -38,8 +43,7 @@ export const AnnouncementsDialog = () => {
       open={isOpen}
       onOpenChange={(open) => {
         if (!open) {
-          localStorage.setItem("mrgnAnnouncementPopup", announcementLabel);
-          setIsOpen(false);
+          closeDialog();
         } else {
           setIsOpen(true);
         }
@@ -55,7 +59,12 @@ export const AnnouncementsDialog = () => {
         <div className="flex items-center justify-center text-center">
           <p>
             RLB is being discontinued,{" "}
-            <Link href="#" className="border-b border-primary/75 transition-colors hover:border-transparent">
+            <Link
+              href="https://x.com/rollbitcom/status/1783420690714292461"
+              target="_blank"
+              rel="noreferrer"
+              className="border-b border-primary/75 transition-colors hover:border-transparent"
+            >
               read more here
             </Link>
             .
@@ -63,7 +72,9 @@ export const AnnouncementsDialog = () => {
           </p>
         </div>
         <DialogFooter className="flex items-center sm:justify-center mt-4">
-          <Button variant="secondary">Close</Button>
+          <Button variant="secondary" onClick={closeDialog}>
+            Close
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
