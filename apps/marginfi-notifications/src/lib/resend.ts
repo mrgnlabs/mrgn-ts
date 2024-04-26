23c4fe6f9123911c0bced0eeb3faa8e6bdf6fa3e
Kobe <leenderskobe@gmail.com>
2024-04-26 11:25:33 +0200

    wip: notification rework

--- apps/marginfi-notifications/src/lib/resend.ts
+++ apps/marginfi-notifications/src/lib/resend.ts
@@ -4,17 +4,18 @@ import { env_config } from "../config"; // Adjust the path as necessary
 import { accountHealthEmail } from "./emailTemplates"; // Assuming emailTemplates.ts is already created
 
 export async function sendEmailNotification(email: string, health: number): Promise<{ data?: any; error?: any }> {
-  const resend = new Resend(env_config.RESEND_API_KEY);
+  //const resend = new Resend(env_config.RESEND_API_KEY);
 
   try {
     const emailHtml = accountHealthEmail(health); // Utilize the email template function
-    const { data } = await resend.emails.send({
-      from: "Marginfi Alerts <marginfi@email.marginfi.com>",
-      to: email,
-      bcc: "engineering@mrgn.group",
-      subject: "Marginfi Account Health Alert",
-      html: emailHtml,
-    });
+    const data = undefined;
+    //await resend.emails.send({
+    //   from: "Marginfi Alerts <marginfi@email.marginfi.com>",
+    //   to: email,
+    //   bcc: "engineering@mrgn.group",
+    //   subject: "Marginfi Account Health Alert",
+    //   html: emailHtml,
+    // });
 
     return { data };
   } catch (error) {
