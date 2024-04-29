afb1e88e5be5607b64bd1513c631d8884e4839ef
Kobe <leenderskobe@gmail.com>
2024-04-29 23:51:01 +0200

    wip: actionbox bugfixes

--- apps/marginfi-v2-ui/src/components/common/ActionBox/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import { ExtendedBankInfo, Emissions } from "@mrgnlabs/marginfi-v2-ui-state";
-import { useMemo } from "react";
-import { aprToApy, nativeToUi, percentFormatter } from "@mrgnlabs/mrgn-common";
-import { MarginRequirementType } from "@mrgnlabs/marginfi-client-v2";
-
-export function useAssetItemData({ bank, isInLendingMode }: { bank: ExtendedBankInfo; isInLendingMode: boolean }) {}
