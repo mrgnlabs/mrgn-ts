5aa821c83e92d2e081b0466749a29a9dfc302c66
chambaz <adammchambers@gmail.com>
2024-05-05 16:18:23 +0100

    feat: stats component (wip)

--- apps/marginfi-landing-page-v2/src/app/page.tsx
+++ apps/marginfi-landing-page-v2/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "~/components/hero";
+import { Stats } from "~/components/stats";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Stats />
     </main>
   );
 }
