4c03b6b117a00ad154dba00beb409375106a9173
chambaz <adammchambers@gmail.com>
2024-05-05 17:09:41 +0100

    feat: adjust grids

--- apps/marginfi-landing-page-v2/src/app/page.tsx
+++ apps/marginfi-landing-page-v2/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "~/components/hero";
 import { Stats } from "~/components/stats";
+import { Products } from "~/components/products";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Stats />
+      <Products />
     </main>
   );
 }
