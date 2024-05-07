66d4156a3e8b04b6bccd4cb6909abcbd661b7c89
chambaz <adammchambers@gmail.com>
2024-05-07 09:52:46 -0400

    feat: ecosystem cards

--- apps/marginfi-landing-page-v2/next.config.mjs
+++ apps/marginfi-landing-page-v2/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        port: "",
+        pathname: "/profile_images/**/*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
