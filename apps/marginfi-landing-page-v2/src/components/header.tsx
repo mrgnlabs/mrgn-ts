66b616a2e57038f203618817265330573af13483
chambaz <adammchambers@gmail.com>
2024-05-06 21:35:10 -0400

    feat: launch button active state

--- apps/marginfi-landing-page-v2/src/components/header.tsx
+++ apps/marginfi-landing-page-v2/src/components/header.tsx
@@ -4,17 +4,43 @@ import React from "react";
 
 import Link from "next/link";
 
-import { IconChevronDown } from "@tabler/icons-react";
+import { IconChevronDown, IconBuildingBank, IconBox, IconExternalLink, IconArrowsLeftRight } from "@tabler/icons-react";
 import { motion, useScroll, useTransform } from "framer-motion";
-import { useWindowSize } from "@uidotdev/usehooks";
+import { useWindowSize, useDebounce } from "@uidotdev/usehooks";
+
+import { cn } from "~/lib/utils";
 
 import { Logo } from "~/components/ui/logo";
 import { Button } from "~/components/ui/button";
 import { ScrollTo } from "~/components/ui/scroll-to";
+import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
+
+const CONTENT = {
+  navLinks: [
+    { label: "Products", to: "products" },
+    { label: "Ecosystem", to: "ecosystem" },
+  ],
+  launchLinks: [
+    { label: "Lend", icon: IconBuildingBank, href: "https://app.marginfi.com" },
+    { label: "Mint", icon: IconBox, href: "https://app.marginfi.com/mint" },
+    { label: "Swap", icon: IconArrowsLeftRight, href: "https://app.marginfi.com/swap" },
+    { label: "Launch", icon: IconExternalLink, href: "https://app.marginfi.com/" },
+  ],
+};
 
 export const Header = () => {
+  const [launchPopoverOpen, setLaunchPopoverOpen] = React.useState(true);
+  const debouncedLaunchPopoverOpen = useDebounce(launchPopoverOpen, 200);
   const { scrollY } = useScroll();
   const { height } = useWindowSize();
+
+  const handleMouseEnter = () => {
+    setLaunchPopoverOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setLaunchPopoverOpen(false);
+  };
 
   const headerBackgroundColor = useTransform(
     scrollY,
@@ -33,16 +59,64 @@ export const Header = () => {
 
       <nav className="ml-auto">
         <ul className="flex items-center gap-12">
+          {CONTENT.navLinks.map((link) => (
+            <li key={link.label}>
+              <ScrollTo to={link.to}>
+                <Button variant="ghost">{link.label}</Button>
+              </ScrollTo>
+            </li>
+          ))}
           <li>
-            <ScrollTo to="products">Products</ScrollTo>
-          </li>
-          <li>
-            <ScrollTo to="ecosystem">Ecosystem</ScrollTo>
-          </li>
-          <li>
-            <Button variant="chartreuse">
-              Launch App <IconChevronDown size={18} className="ml-1 -mr-1" />
-            </Button>
+            <Popover open={debouncedLaunchPopoverOpen} onOpenChange={setLaunchPopoverOpen}>
+              <PopoverTrigger asChild onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <div className="bg-gradient-to-r from-mrgn-gold to-mrgn-chartreuse rounded-md p-[1px]">
+                  <Button
+                    variant="chartreuse"
+                    className={cn(debouncedLaunchPopoverOpen && "bg-background text-mrgn-chartreuse")}
+                  >
+                    <span
+                      className={cn(
+                        debouncedLaunchPopoverOpen &&
+                          "bg-gradient-to-r from-mrgn-gold to-mrgn-chartreuse text-transparent bg-clip-text"
+                      )}
+                    >
+                      Launch App
+                    </span>{" "}
+                    <IconChevronDown
+                      size={18}
+                      className={cn(
+                        "ml-1 -mr-1 origin-center transition-transform",
+                        debouncedLaunchPopoverOpen && "rotate-180"
+                      )}
+                    />
+                  </Button>
+                </div>
+              </PopoverTrigger>
+              <PopoverContent
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                className="rounded-lg px-0 py-3 bg-secondary"
+                style={{
+                  width: "var(--popover-width)",
+                }}
+                sideOffset={10}
+              >
+                <nav className="w-full">
+                  <ul>
+                    {CONTENT.launchLinks.map((link) => (
+                      <li key={link.label}>
+                        <Link
+                          className="flex items-center gap-2 py-2 px-6 transition-colors hover:text-mrgn-chartreuse"
+                          href={link.href}
+                        >
+                          <link.icon size={18} /> {link.label}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </nav>
+              </PopoverContent>
+            </Popover>
           </li>
         </ul>
       </nav>
