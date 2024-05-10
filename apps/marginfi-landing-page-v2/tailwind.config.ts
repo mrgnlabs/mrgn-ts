0dcec41294b4689b2b619e47530da87b0fc6ec6f
chambaz <adammchambers@gmail.com>
2024-05-10 11:29:06 -0400

    feat: add xs screen, adjust hero for small screens

--- apps/marginfi-landing-page-v2/tailwind.config.ts
+++ apps/marginfi-landing-page-v2/tailwind.config.ts
@@ -15,6 +15,14 @@ const config = {
         "2xl": "1440px",
       },
     },
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+    },
     extend: {
       fontFamily: {
         sans: ["Aeonik Pro", "Helvetica Neue", "sans-serif"],
