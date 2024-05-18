b3d3483306b15ba70661bcfb437c24fd245c3734
chambaz <adammchambers@gmail.com>
2024-05-17 22:44:02 -0400

    feat: validate grant form

--- apps/marginfi-landing-page-v2/src/components/callout.tsx
+++ apps/marginfi-landing-page-v2/src/components/callout.tsx
@@ -4,12 +4,16 @@ import React from "react";
 
 import { IconArrowRight } from "@tabler/icons-react";
 import { motion, useInView } from "framer-motion";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import { Button } from "~/components/ui/button";
 import { Logo } from "~/components/ui/logo";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Textarea } from "~/components/ui/textarea";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
 import {
   Dialog,
   DialogContent,
@@ -34,14 +38,32 @@ const CONTENT = {
   },
 };
 
+const formSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  projectName: z.string(),
+  projectLink: z.string().url(),
+  projectDesc: z.string(),
+});
+
 export const Callout = () => {
   const targetRef = React.useRef(null);
   const isInView = useInView(targetRef);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  // TODO: Post to ConvertKit
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
 
   const fadeVariants = {
     hidden: { opacity: 0, y: 10, transition: { duration: 0.5 } },
     visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
   };
+
   return (
     <motion.div
       ref={targetRef}
@@ -70,49 +92,97 @@ export const Callout = () => {
               </DialogTitle>
               <DialogDescription>Create a grant proposal and build something iconic with marginfi.</DialogDescription>
             </DialogHeader>
-            <div className="space-y-2 mt-4">
-              <div className="grid md:grid-cols-2 md:gap-8">
-                <div className="space-y-4 py-4">
-                  <div className="space-y-1">
-                    <Label htmlFor="name">Your Name</Label>
-                    <Input id="name" className="col-span-3" placeholder="Your name" />
+            <Form {...form}>
+              <form className="space-y-2 mt-4" onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="grid md:grid-cols-2 md:gap-8">
+                  <div className="space-y-4 py-4">
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem className="space-y-1">
+                          <FormLabel htmlFor="name">Your Name</FormLabel>
+                          <FormControl>
+                            <Input id="name" className="col-span-3" placeholder="Your name" {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="projectName"
+                      render={({ field }) => (
+                        <FormItem className="space-y-1">
+                          <FormLabel htmlFor="projectName">Project Name</FormLabel>
+                          <FormControl>
+                            <Input id="projectName" className="col-span-3" placeholder="Project Name" {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
                   </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="projectName">Project Name</Label>
-                    <Input id="projectName" className="col-span-3" placeholder="Project name" />
-                  </div>
-                </div>
-                <div className="space-y-4 py-4">
-                  <div className="space-y-1">
-                    <Label htmlFor="email">Your Email</Label>
-                    <Input id="email" type="email" className="col-span-3" placeholder="example@example.com" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="projectUrl">Project Link</Label>
-                    <Input
-                      id="projectUrl"
-                      type="url"
-                      className="col-span-3"
-                      placeholder="https://www.yourwebsite.com/"
+                  <div className="space-y-4 py-4">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem className="space-y-1">
+                          <FormLabel htmlFor="email">Your Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              id="email"
+                              type="email"
+                              className="col-span-3"
+                              placeholder="example@example.com"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="projectLink"
+                      render={({ field }) => (
+                        <FormItem className="space-y-1">
+                          <FormLabel htmlFor="projectLink">Project Link</FormLabel>
+                          <FormControl>
+                            <Input
+                              id="projectLink"
+                              type="url"
+                              className="col-span-3"
+                              placeholder="https://www.yourwebsite.com/"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
                     />
                   </div>
                 </div>
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="projectDesc" className="mt-2">
-                  Project Description
-                </Label>
-                <Textarea
-                  id="projectDesc"
-                  className="col-span-3"
-                  rows={4}
-                  placeholder="Tell us about your project..."
+                <FormField
+                  control={form.control}
+                  name="projectDesc"
+                  render={({ field }) => (
+                    <FormItem className="space-y-1">
+                      <FormLabel htmlFor="projectDesc">Project Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          id="projectDesc"
+                          className="col-span-3"
+                          rows={4}
+                          placeholder="Tell us about your project..."
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
                 />
-              </div>
-            </div>
-            <DialogFooter className="sm:justify-center mt-4">
-              <Button type="submit">Submit Project</Button>
-            </DialogFooter>
+                <div className="flex justify-center pt-4">
+                  <Button type="submit">Submit Project</Button>
+                </div>
+              </form>
+            </Form>
           </DialogContent>
         </Dialog>
       </div>
