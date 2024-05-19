88873243816eae318c96d46b5dee5d4b7e12ad2e
chambaz <adammchambers@gmail.com>
2024-05-19 00:43:53 -0400

    feat: add icons to header buttons

--- apps/marginfi-trading-ui/src/components/header.tsx
+++ apps/marginfi-trading-ui/src/components/header.tsx
@@ -1,32 +1,59 @@
+"use client";
+
+import React from "react";
+
 import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import { IconTrendingUp, IconCoins, IconChartLine, IconChartPie } from "@tabler/icons-react";
+
+import { cn } from "~/lib/utils";
 
 import { Button } from "~/components/ui/button";
 import { Logo } from "~/components/ui/logo";
 
+const navItems = [
+  { label: "trending", icon: <IconTrendingUp />, href: "/pools?sort=trending" },
+  { label: "pools", icon: <IconCoins />, href: "/pools" },
+  { label: "trade", icon: <IconChartLine />, href: "/trade" },
+  { label: "portfolio", icon: <IconChartPie />, href: "/portfolio" },
+];
+
 export const Header = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const fullPath = React.useMemo(() => {
+    const search = new URLSearchParams(searchParams);
+    const searchStr = search.toString();
+    return `${pathname}${searchStr ? `?${searchStr}` : ""}`;
+  }, [pathname, searchParams]);
+
+  console.log(fullPath);
   return (
     <header className="flex items-center justify-between gap-8 p-4 lg:py-6 xl:px-8">
-      <Logo size={32} />
+      <Link href="/">
+        <Logo size={32} />
+      </Link>
       <nav className="ml-auto hidden lg:block">
-        <ul className="flex items-center gap-8">
-          <li>
-            <Link href="/">
-              <Button variant="ghost">trending</Button>
-            </Link>
-          </li>
-          <li>
-            <Link href="/trade">
-              <Button variant="ghost">trade</Button>
-            </Link>
-          </li>
-          <li>
-            <Link href="/portfolio">
-              <Button variant="ghost">portfolio</Button>
-            </Link>
-          </li>
+        <ul className="flex items-center gap-6">
+          {navItems.map((item) => (
+            <li key={item.label}>
+              <Link href={item.href}>
+                <Button
+                  variant="ghost"
+                  className={cn("text-muted-foreground", item.href === fullPath && "bg-accent text-primary")}
+                >
+                  {React.cloneElement(item.icon, { size: 18 })}
+                  {item.label}
+                </Button>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
-      <Button>Sign In</Button>
+      <Button variant="secondary" className="gap-3 px-6">
+        <Logo size={16} wordmark={false} /> Sign In
+      </Button>
     </header>
   );
 };
