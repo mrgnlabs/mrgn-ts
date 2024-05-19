2169216aa4ff4be072aaec5f1a90c9c1ccda6a4f
chambaz <adammchambers@gmail.com>
2024-05-19 00:22:17 -0400

    feat: placeholder home / trending page / layout tweaks

--- apps/marginfi-trading-ui/src/app/page.tsx
+++ apps/marginfi-trading-ui/src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function HomePage() {
-  return <div>Home page / Trending tokens</div>;
+  return (
+    <div className="container pt-20">
+      <h1 className="text-5xl font-medium bg-gradient-to-r from-mrgn-gold to-mrgn-chartreuse leading-none inline-block text-transparent bg-clip-text sm:text-[3.5rem] md:text-7xl lg:leading-[1.15] lg:w-2/3">
+        Launch and trade permissionless pools
+        <br className="hidden xl:block" /> on marginfi
+      </h1>
+      <div className="mt-20">
+        <h2>Trending grid here...</h2>
+      </div>
+    </div>
+  );
 }
