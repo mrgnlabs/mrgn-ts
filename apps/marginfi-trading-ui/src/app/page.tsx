038d3af6cfeba20b0b5b38528161ff9fc4ac5f51
chambaz <adammchambers@gmail.com>
2024-05-19 00:11:04 -0400

    chore: set up layouts / routes with header nav component

--- apps/marginfi-trading-ui/src/app/page.tsx
+++ apps/marginfi-trading-ui/src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "~/components/ui/button";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
-      <h1 className="text-4xl font-bold">Welcome to MarginFi Trading UI</h1>
-      <Button>Click me</Button>
-    </main>
-  );
+export default function HomePage() {
+  return <div>Home page / Trending tokens</div>;
 }
---
+++ apps/marginfi-trading-ui/src/app/trade/page.tsx
@@ -0,0 +1,3 @@
+export default function TradePage() {
+  return <div>Trade</div>;
+}
