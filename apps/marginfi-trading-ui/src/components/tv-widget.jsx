a46074290f64a471eb0508ea07ccb27b25ca2ea1
chambaz <adammchambers@gmail.com>
2024-05-21 13:42:41 -0400

    feat: trading view widget component

---
+++ apps/marginfi-trading-ui/src/components/tv-widget.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React, { useEffect, useRef, memo } from "react";
+
+function TradingViewWidget() {
+  const container = useRef();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+        {
+          "autosize": true,
+          "symbol": "CRYPTO:SOLUSD",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "dark",
+          "style": "1",
+          "locale": "en",
+          "allow_symbol_change": false,
+          "save_image": false,
+          "calendar": false,
+          "support_host": "https://www.tradingview.com"
+        }`;
+    container.current.appendChild(script);
+
+    return () => {
+      container.current.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
+      <div
+        className="tradingview-widget-container__widget"
+        style={{ height: "calc(100% - 32px)", width: "100%" }}
+      ></div>
+    </div>
+  );
+}
+
+export default memo(TradingViewWidget);
