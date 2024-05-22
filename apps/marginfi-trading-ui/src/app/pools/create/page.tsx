d2bdff485fe71d1dd481b19698039ac86377a131
chambaz <adammchambers@gmail.com>
2024-05-22 11:30:54 -0400

    feat: create pool form (wip)

---
+++ apps/marginfi-trading-ui/src/app/pools/create/page.tsx
@@ -0,0 +1,15 @@
+import { CreatePool } from "~/components/create-pool";
+
+export default function Component() {
+  return (
+    <div className="pt-8">
+      <div className="bg-background rounded-xl p-8 max-w-3xl mx-auto space-y-8">
+        <div className="text-center space-y-2 max-w-md mx-auto">
+          <h2 className="text-3xl font-medium">Create a Pool</h2>
+          <p className="text-muted-foreground">Id nisi dolor est aute exercitation in fugiat et fugiat aliqua.</p>
+        </div>
+        <CreatePool />
+      </div>
+    </div>
+  );
+}
