2008e68980f85d03db2a9dc266619bcba4f8c60a
Kobe <leenderskobe@gmail.com>
2024-05-28 14:06:45 +0300

    feat: wip onramp & onboarding desktop layout

--- apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Onboarding/OnboardingMain.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Onboarding/OnboardingMain.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { AuthScreenProps, cn } from "~/utils";
+import { useIsMobile } from "~/hooks/useIsMobile";
 
 import { DialogContent } from "~/components/ui/dialog";
 import { Button } from "~/components/ui/button";
@@ -9,46 +10,58 @@ import { OnboardHeader } from "../sharedComponents";
 interface props extends AuthScreenProps {}
 
 export const OnboardingMain = ({ update }: props) => {
+  const isMobile = useIsMobile();
+
   return (
-    <DialogContent className={cn("md:block overflow-hidden p-4 pt-8 md:pt-4 justify-start md:max-w-xl")}>
+    <DialogContent className="md:block overflow-hidden p-4 pt-8 md:pt-4 justify-start md:max-w-xl lg:max-w-6xl">
       <OnboardHeader
         title={"Welcome to marginfi"}
         description={"Sign in to get started with marginfi. If you're new to crypto, let us guide you the process."}
       />
 
-      <div className="w-full space-y-6 mt-2">
-        <div className="relative bg-muted text-muted-foreground transition-all duration-300 w-full p-6 pt-5 rounded-lg overflow-hidden">
-          <div>
-            <h2 className="font-semibold text-2xl text-white">I&apos;m new to crypto</h2>
-            <p className="mt-2 text-sm sm:text-base">Help get up and running</p>
-          </div>
-
-          <div className="mt-4">
-            <Button variant="default" onClick={() => update("ONBOARD_SOCIAL")}>
+      <div className="flex lg:flex-row flex-col gap-4 mt-10">
+        <div
+          className="relative bg-muted hover:bg-muted-highlight text-muted-foreground transition-all duration-300 w-full p-6 pt-5 rounded-lg overflow-hidden cursor-pointer lg:cursor-normal"
+          onClick={() => isMobile && update("ONBOARD_SOCIAL")}
+        >
+          <div className="flex flex-col gap-3">
+            <h2 className="font-semibold text-2xl text-white leading-none">I&apos;m new to crypto</h2>
+            <p className="text-sm leading-none sm:text-base">
+              Sign up with email or social and we'll guide through the process of buying, swapping, and earning yield
+              with crypto.
+            </p>
+            <Button className="w-fit hidden lg:block" onClick={() => update("ONBOARD_SOCIAL")}>
               Get started
             </Button>
           </div>
         </div>
         <div
-          className="relative bg-muted text-muted-foreground transition-all duration-300 w-full p-6 pt-5 rounded-lg overflow-hidden cursor-pointer"
-          onClick={() => update("ONBOARD_SOL")}
+          className="relative bg-muted hover:bg-muted-highlight text-muted-foreground transition-all duration-300 w-full p-6 pt-5 rounded-lg overflow-hidden cursor-pointer"
+          onClick={() => isMobile && update("ONBOARD_SOL")}
         >
-          <div>
-            <h2 className="font-semibold text-2xl text-white">I&apos;m a Solana user</h2>
-            <p className="mt-2 text-[15px] sm:text-base">
-              Sign in with email or socials to use the marginfi PWA, or connect your wallet
+          <div className="flex flex-col gap-3">
+            <h2 className="font-semibold text-2xl text-white leading-none">I&apos;m a Solana user</h2>
+            <p className="text-sm leading-none sm:text-base">
+              Sign in with email or socials to use the marginfi mobile web app, or connect your existing Solana wallet.
             </p>
+            <Button className="w-fit hidden lg:block" onClick={() => isMobile && update("ONBOARD_SOL")}>
+              Sign in
+            </Button>
           </div>
         </div>
         <div
-          className="relative bg-muted text-muted-foreground transition-all duration-300 w-full p-6 pt-5 rounded-lg overflow-hidden cursor-pointer"
-          onClick={() => update("ONBOARD_ETH")}
+          className="relative bg-muted hover:bg-muted-highlight text-muted-foreground transition-all duration-300 w-full p-6 pt-5 rounded-lg overflow-hidden cursor-pointer"
+          onClick={() => isMobile && update("ONBOARD_ETH")}
         >
-          <div>
-            <h2 className="font-semibold text-2xl text-white">I&apos;m an Ethereum user</h2>
-            <p className="mt-2 text-[15px] sm:text-base">
-              Sign in with email or socials and bridge your funds, or connect your eth wallet
+          <div className="flex flex-col gap-3">
+            <h2 className="font-semibold text-2xl text-white leading-none">I&apos;m an Ethereum user</h2>
+            <p className="text-sm leading-none sm:text-base">
+              Sign in with email, social, or connect your wallet. Bridge your funds, swap tokens, and start earning with
+              Solana defi.
             </p>
+            <Button className="w-fit hidden lg:block" onClick={() => update("ONBOARD_ETH")}>
+              Sign in
+            </Button>
           </div>
         </div>
       </div>
--- apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Onboarding/screens/Onramp.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Onboarding/screens/Onramp.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import {
+  Asset,
+  AuthenticationStrategy,
+  Environment,
+  EventKind,
+  MesoEvent,
+  Network,
+  SignedMessageResult,
+  inlineTransfer,
+} from "@meso-network/meso-js";
 
 import { OnrampScreenProps } from "~/utils";
 
@@ -7,9 +17,77 @@ import { ScreenWrapper, WalletSeperator } from "../../sharedComponents";
 interface props extends OnrampScreenProps {}
 
 export const Onramp = ({ onNext }: props) => {
+  const divRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (divRef.current) launchMeso;
+  }, []);
+
+  const launchMeso = React.useCallback(() => {
+    const transfer = inlineTransfer({
+      container: "#outlet", // A valid CSS selector for an element on your page
+
+      partnerId: "marginfi", // Your unique Meso partner ID
+      environment: Environment.SANDBOX, // SANDBOX | PRODUCTION
+      sourceAmount: "100", // The amount (in USD) the user will spend
+      destinationAsset: Asset.SOL, // The asset the user will receive in the transfer. For on-ramping, this will be a token such as `ETH` or `SOL`.
+      network: Network.SOLANA_MAINNET, // The network to use for the transfer
+      walletAddress: "D16QZnFLFgCtVwWsBH4FNXFXhYuW4QQ7qYu5brqmtXy2", // The user's wallet address obtained at runtime by your application
+
+      sourceAsset: "USD",
+      authenticationStrategy: AuthenticationStrategy.BYPASS_WALLET_VERIFICATION,
+
+      // A callback to handle events throughout the integration lifecycle
+      onEvent({ kind, payload }: MesoEvent) {
+        switch (kind) {
+          // The iframe/window is ready
+          case EventKind.READY:
+            break;
+
+          // The transfer has been approved and will go through, however funds have not yet moved.
+          case EventKind.TRANSFER_APPROVED:
+          // The transfer has been finalized and the assets have been transferred.
+          case EventKind.TRANSFER_COMPLETE:
+            console.log(payload.transfer);
+            break;
+
+          // There was an issue with the provided configuration
+          case EventKind.CONFIGURATION_ERROR:
+            console.error(payload.error.message);
+            break;
+
+          // The `network` provided in the configuration is not supported
+          case EventKind.UNSUPPORTED_NETWORK_ERROR:
+            console.error(payload.error.message);
+            break;
+
+          // The `destinationAsset` provided in the configuration is not supported
+          case EventKind.UNSUPPORTED_ASSET_ERROR:
+            console.error(payload.error.message);
+            break;
+
+          // A general error has occurred
+          case EventKind.ERROR:
+            console.error(payload.error.message);
+            break;
+
+          case EventKind.CLOSE:
+            console.log("Meso experience closed.");
+        }
+      },
+
+      // A callback to handle having the user verify their wallet ownership by signing a message
+      onSignMessageRequest: function (message: string): Promise<SignedMessageResult> {
+        throw new Error("Function not implemented.");
+      },
+    });
+  }, []);
+
   return (
     <ScreenWrapper>
       <div className="mx-auto">Mesa onramp coming soon!</div>
+      <div id="outlet"></div>
+      <button id="buyCrypto">Buy Crypto</button>
       <WalletSeperator description="skip for now" onClick={() => onNext()} />
     </ScreenWrapper>
   );
