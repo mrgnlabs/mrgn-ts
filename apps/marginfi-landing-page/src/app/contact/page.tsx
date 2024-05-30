48cd593427b251dec80039eaefd66a7d049f99ca
chambaz <adammchambers@gmail.com>
2024-05-30 11:12:37 -0400

    feat: new contact page / component set up

---
+++ apps/marginfi-landing-page/src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+import { Contact } from "~/components/contact";
+
+export default function ContactPage() {
+  return (
+    <div className="container pt-32">
+      <div className="space-y-6 text-center max-w-2xl mx-auto text-muted-foreground text-lg leading-relaxed">
+        <h1 className="text-5xl font-medium text-primary">Contact Us</h1>
+        <p>
+          Id consectetur cillum id veniam. Ad labore cillum nisi sit consequat cillum elit sint veniam Lorem incididunt
+          nulla in velit. Qui tempor sit ut velit ea culpa eu cillum.
+        </p>
+      </div>
+      <Contact />
+    </div>
+  );
+}
--- apps/marginfi-landing-page/src/components/contact.tsx
+++ apps/marginfi-landing-page/src/components/contact.tsx
@@ -2,7 +2,7 @@ import { getVideoUrl } from "~/lib/utils";
 
 export const Contact = () => {
   return (
-    <div className="relative z-10 container flex items-center gap-20 py-16 lg:py-24 xl:py-32">
+    <div className="relative z-10 max-w-5xl mx-auto w-full flex items-center gap-20 py-16 lg:py-24 xl:py-32">
       <video autoPlay muted>
         <source src={getVideoUrl("contact")} type="video/mp4" />
       </video>
