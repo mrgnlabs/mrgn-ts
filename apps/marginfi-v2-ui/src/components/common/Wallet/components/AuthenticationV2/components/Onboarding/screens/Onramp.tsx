4c809766b161efe32a1af4ce4b2423df43a2f8bc
Kobe <leenderskobe@gmail.com>
2024-05-31 00:59:22 +0300

    wip: onramp improvements

--- apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Onboarding/screens/Onramp.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Onboarding/screens/Onramp.tsx
@@ -10,51 +10,137 @@ import {
   inlineTransfer,
 } from "@meso-network/meso-js";
 
-import { OnrampScreenProps } from "~/utils";
+import { OnrampScreenProps, cn } from "~/utils";
 
 import { ScreenWrapper, WalletSeperator } from "../../sharedComponents";
 import { useWalletContext } from "~/hooks/useWalletContext";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 
 interface props extends OnrampScreenProps {}
 
 export const Onramp = ({ onNext }: props) => {
   const { wallet } = useWalletContext();
-  const divRef = React.useRef(null);
+  const divRef = React.useRef<HTMLDivElement>(null);
+  const [amountRaw, setAmountRaw] = React.useState<string>("");
+  const [isCustomMode, setIsCustomMode] = React.useState<boolean>(false);
+  const [showMeso, setIsShowMeso] = React.useState<boolean>(false);
 
-  React.useEffect(() => {
-    if (divRef.current && !(divRef.current as any)?.innerHTML) {
-      console.log({ divRef });
-      launchMeso();
+  const numberFormater = React.useMemo(() => new Intl.NumberFormat("en-US", { maximumFractionDigits: 10 }), []);
+
+  const amount = React.useMemo(() => {
+    const strippedAmount = amountRaw.replace(/,/g, "");
+    return isNaN(Number.parseFloat(strippedAmount)) ? 0 : Number.parseFloat(strippedAmount);
+  }, [amountRaw]);
+
+  const error = React.useMemo(() => {
+    if (amount > 0 && amount < 25) return "Please enter an amount above 25.";
+  }, [amount]);
+
+  const removeMeso = React.useCallback(() => {
+    if (divRef.current && divRef.current.innerHTML) {
+      divRef.current.innerHTML = "";
+      setIsShowMeso(false);
     }
-  }, [divRef]);
+  }, [divRef, setIsShowMeso]);
 
-  const launchMeso = React.useCallback(() => {
-    const transfer = inlineTransfer({
-      container: "#outlet",
-      partnerId: "marginfi",
-      environment: Environment.SANDBOX,
-      sourceAmount: "100",
-      sourceAsset: "USD",
-      authenticationStrategy: AuthenticationStrategy.BYPASS_WALLET_VERIFICATION,
-      destinationAsset: Asset.SOL,
-      network: Network.SOLANA_MAINNET,
-      walletAddress: wallet.publicKey.toBase58(),
+  const initializeMeso = React.useCallback(() => {
+    if (divRef.current && !(divRef.current as any)?.innerHTML) {
+      const transfer = inlineTransfer({
+        container: "#outlet",
+        partnerId: "marginfi",
+        environment: Environment.SANDBOX,
+        sourceAmount: amount.toString() as any,
+        sourceAsset: "USD",
+        authenticationStrategy: AuthenticationStrategy.BYPASS_WALLET_VERIFICATION,
+        destinationAsset: Asset.SOL,
+        network: Network.SOLANA_MAINNET,
+        walletAddress: wallet.publicKey.toBase58(),
 
-      // A callback to handle events throughout the integration lifecycle
-      onEvent({ kind, payload }: MesoEvent) {
-        console.log(kind, payload);
-      },
+        // A callback to handle events throughout the integration lifecycle
+        onEvent({ kind, payload }: MesoEvent) {
+          console.log(kind, payload);
+        },
 
-      // A callback to handle having the user verify their wallet ownership by signing a message
-      async onSignMessageRequest(message: string) {
-        return "";
-      },
-    });
-  }, [wallet]);
+        // A callback to handle having the user verify their wallet ownership by signing a message
+        async onSignMessageRequest(message: string) {
+          return "";
+        },
+      });
+      setIsShowMeso(true);
+    }
+  }, [divRef, wallet, amount, setIsShowMeso]);
+
+  const amountOptions = [
+    {
+      label: "$25",
+      value: 25,
+    },
+    {
+      label: "$50",
+      value: 50,
+    },
+    // {
+    //   label: "$75",
+    //   value: 75,
+    // },
+    {
+      label: "$100",
+      value: 100,
+    },
+  ];
 
   return (
     <ScreenWrapper>
-      <div id="outlet" ref={divRef} className="h-[350px]"></div>
+      <div className="py-4 space-y-4">
+        {!showMeso && (
+          <>
+            <p className="text-muted-foreground">Select how much USD to onramp</p>
+            <div className="flex justify-between items-center w-full">
+              <ul className="grid grid-cols-4 gap-2">
+                {amountOptions.map((option, idx) => (
+                  <li key={idx}>
+                    <Button
+                      className={cn(
+                        "gap-0.5 h-auto w-full font-light border border-transparent bg-background/50 transition-colors hover:bg-background-gray-hover",
+                        amount === option.value && "bg-background-gray-hover border-chartreuse"
+                      )}
+                      variant="secondary"
+                      onClick={() => {
+                        setAmountRaw(numberFormater.format(option.value));
+                        setIsCustomMode(false);
+                      }}
+                    >
+                      {option.label}
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+              <Input
+                type="text"
+                inputMode="decimal"
+                value={amount}
+                onChange={(e) => setAmountRaw(e.target.value)}
+                onFocus={() => setIsCustomMode(true)}
+                onBlur={() => setIsCustomMode(false)}
+                placeholder="0"
+                className={cn(
+                  "h-aut max-w-[120px] bg-background/50 py-3 px-4 border border-muted-foreground text-white transition-colors focus-visible:ring-0",
+                  !amountOptions.find((value) => value.value === amount) && "border-chartreuse"
+                )}
+              />
+            </div>
+            {error && <div className="text-destructive-foreground text-sm">{error}</div>}
+            <Button disabled={!!error || amount <= 0} onClick={() => initializeMeso()}>
+              Buy crypto
+            </Button>
+          </>
+        )}
+        <div className={cn(showMeso ? "block" : "hidden", "relative")}>
+          <div id="outlet" ref={divRef}></div>
+        </div>
+      </div>
+
       <WalletSeperator description="skip for now" onClick={() => onNext()} />
     </ScreenWrapper>
   );
