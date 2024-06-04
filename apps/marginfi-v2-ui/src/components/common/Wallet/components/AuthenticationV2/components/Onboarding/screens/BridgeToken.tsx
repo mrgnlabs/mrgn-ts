2f5b0f2e9c87d8405b05a2dde2ef42c903b69e3b
chambaz <adammchambers@gmail.com>
2024-06-04 09:06:35 -0400

    feat: improved debridge widget styling

--- apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Onboarding/screens/BridgeToken.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Onboarding/screens/BridgeToken.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { OnrampScreenProps } from "~/utils";
+import { useIsMobile } from "~/hooks/useIsMobile";
 
 import { ScreenWrapper, WalletSeperator } from "../../sharedComponents";
 
@@ -10,6 +11,7 @@ export const BridgeToken: React.FC<props> = ({ onNext }: props) => {
   const divRef = React.useRef<HTMLDivElement>(null);
   const [isMounted, setIsMounted] = React.useState(false);
   const [widget, setWidget] = React.useState<any>();
+  const isMobile = useIsMobile();
 
   React.useEffect(() => {
     setIsMounted(true);
@@ -41,12 +43,12 @@ export const BridgeToken: React.FC<props> = ({ onNext }: props) => {
       element: "debridgeWidget",
       title: "",
       description: "",
-      width: "460",
-      height: "450",
-      r: null,
+      width: isMobile ? "430" : "460",
+      height: "500",
+      r: 16890,
       supportedChains:
-        '{"inputChains":{"1":"all","10":"all","56":"all","137":"all","8453":"all","42161":"all","43114":"all","59144":"all","7565164":"all","245022934":"all"},"outputChains":{"7565164":[""]}}',
-      inputChain: 1,
+        '{"inputChains":{"1":"all","10":"all","56":"all","137":"all","8453":"all","42161":"all","43114":"all","59144":"all","7565164":"all","245022934":"all"},"outputChains":{"7565164":"all"}}',
+      inputChain: 56,
       outputChain: 7565164,
       inputCurrency: "",
       outputCurrency: "",
@@ -60,9 +62,9 @@ export const BridgeToken: React.FC<props> = ({ onNext }: props) => {
       mode: "deswap",
       isEnableCalldata: false,
       styles:
-        "eyJhcHBCYWNrZ3JvdW5kIjoiIzEyMTQxNiIsImFwcEFjY2VudEJnIjoiIzEyMTQxNiIsImNoYXJ0QmciOiIjMTIxNDE2IiwiYmFkZ2UiOiIjMTIxNDE2IiwiYm9yZGVyUmFkaXVzIjo4LCJ0b29sdGlwQmciOiIjMDAwMDAwIiwiZm9ybUNvbnRyb2xCZyI6IiMxZDIxMjUiLCJwcmltYXJ5IjoiI2ZmZmZmZiIsInNlY29uZGFyeSI6IiMwYTBiMGIiLCJpY29uQ29sb3IiOiIjZmZmZmZmIiwiZm9udENvbG9yQWNjZW50IjoiIzM2NDE0OCIsImZvbnRGYW1pbHkiOiIiLCJwcmltYXJ5QnRuQmciOiIjZmZmZmZmIiwic2Vjb25kYXJ5QnRuQmciOiIjMjIyNjJhIiwibGlnaHRCdG5CZyI6IiJ9",
+        "eyJhcHBCYWNrZ3JvdW5kIjoiIzExMTQxNiIsImFwcEFjY2VudEJnIjoiIzExMTQxNiIsImNoYXJ0QmciOiIjMWMyMTI1IiwiYmFkZ2UiOiIjMWQyMTI1IiwiYm9yZGVyUmFkaXVzIjo4LCJ0b29sdGlwQmciOiIjMDAwMDAwIiwiZm9ybUNvbnRyb2xCZyI6IiMxODFkMjAiLCJkcm9wZG93bkJnIjoiIzE4MWQyMCIsInByaW1hcnkiOiIjZmZmZmZmIiwic2Vjb25kYXJ5IjoiIzIyMjYyYSIsImljb25Db2xvciI6IiNmZmZmZmYiLCJmb250Q29sb3JBY2NlbnQiOiIjMzY0MTQ4IiwiZm9udEZhbWlseSI6IiIsInByaW1hcnlCdG5CZyI6IiNmZmZmZmYiLCJzZWNvbmRhcnlCdG5CZyI6IiMyMjI2MmEiLCJsaWdodEJ0bkJnIjoiIn0=",
       theme: "dark",
-      isHideLogo: true,
+      isHideLogo: false,
       logo: "",
     });
 
