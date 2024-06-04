257a0765f0d5191c0748501c1f819c54ee432f51
chambaz <adammchambers@gmail.com>
2024-06-04 11:10:29 -0400

    feat: improved in-app sign in screen

--- apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Returning/InAppSignIn.tsx
+++ apps/marginfi-v2-ui/src/components/common/Wallet/components/AuthenticationV2/components/Returning/InAppSignIn.tsx
@@ -17,20 +17,20 @@ export const InAppSignIn = ({ isLoading, select, update, onClose }: props) => {
   const inAppWallet = React.useMemo(() => {
     if (browser === "Phantom") {
       return {
-        icon: <IconPhantomWallet />,
-        description: "Connect with Phantom",
+        icon: <IconPhantomWallet size={24} />,
+        description: "Sign in with Phantom",
         connect: () => select("Phantom" as any),
       };
     } else if (browser === "Backpack") {
       return {
-        icon: <IconBackpackWallet />,
-        description: "Connect with Backpack",
+        icon: <IconBackpackWallet size={24} />,
+        description: "Sign in with Backpack",
         connect: () => select("Backpack" as any),
       };
     } else {
       return {
-        icon: <IconPhantomWallet />,
-        description: "Connect with Phantom",
+        icon: <IconPhantomWallet size={24} />,
+        description: "Sign in with Phantom",
         connect: () => select("Phantom" as any),
       };
     }
@@ -51,12 +51,8 @@ export const InAppSignIn = ({ isLoading, select, update, onClose }: props) => {
     <div className="pt-6 font-normal">
       <OnboardHeader title={"Sign in to marginfi"} description={"Earn yield, permissionlessly."} />
       <ScreenWrapper>
-        <div className="flex flex-col items-center justify-center gap-3 text-xl text-primary">
-          <span className="rounded-full bg-background-gray-light p-2">{inAppWallet.icon}</span>
-          {inAppWallet.description}
-        </div>
-        <Button disabled={isLoading} onClick={inAppWallet.connect}>
-          {isLoading ? <IconLoader /> : "Sign in"}
+        <Button size="lg" className="mt-2 text-lg font-medium h-12" disabled={isLoading} onClick={inAppWallet.connect}>
+          {isLoading ? <IconLoader /> : inAppWallet.icon} {inAppWallet.description}
         </Button>
         <WalletSeperator description={"more sign in options"} onClick={() => update("RETURNING_USER")} />
       </ScreenWrapper>
