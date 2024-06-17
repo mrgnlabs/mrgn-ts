d4108da25fa4785b3a30aada4489a52e9757533d
chambaz <adammchambers@gmail.com>
2024-06-17 07:19:04 -0400

    feat: successfully creating lending pool

--- packages/marginfi-client-v2/examples/create-banks.ts
+++ packages/marginfi-client-v2/examples/create-banks.ts
@@ -16,36 +16,36 @@ import { BigNumber } from "bignumber.js";
 
 const marginfiGroupPk = new PublicKey("J9VZnaMGTELGCPsqMxk8aoyEGYcVzhorj48HvtDdEtc8");
 
-const bankMint = new PublicKey("LSTxxxnJzKDFSLr4dUkPcmCf5VyryEqzPLz5j4bpxFp");
+const bankMint = new PublicKey("mb1eu7TzEc71KxDpsmsKoucSSuuoGLv1drys1oP2jh6");
 const bank: BankConfigOpt = {
-  assetWeightInit: new BigNumber(0.649999976158142),
-  assetWeightMaint: new BigNumber(0.649999976158142),
+  assetWeightInit: new BigNumber(0),
+  assetWeightMaint: new BigNumber(0),
 
-  liabilityWeightInit: new BigNumber(0.649999976158142),
-  liabilityWeightMaint: new BigNumber(0.649999976158142),
+  liabilityWeightInit: new BigNumber(2.5),
+  liabilityWeightMaint: new BigNumber(1.5),
 
-  depositLimit: new BigNumber(0.649999976158142),
-  borrowLimit: new BigNumber(0.649999976158142),
+  depositLimit: new BigNumber(1000),
+  borrowLimit: new BigNumber(10),
   riskTier: RiskTier.Collateral,
 
-  totalAssetValueInitLimit: new BigNumber(0.649999976158142),
+  totalAssetValueInitLimit: new BigNumber(0),
   interestRateConfig: {
     // Curve Params
-    optimalUtilizationRate: new BigNumber(0.649999976158142),
-    plateauInterestRate: new BigNumber(0.649999976158142),
-    maxInterestRate: new BigNumber(0.649999976158142),
+    optimalUtilizationRate: new BigNumber(0.8),
+    plateauInterestRate: new BigNumber(0.1),
+    maxInterestRate: new BigNumber(3),
 
     // Fees
-    insuranceFeeFixedApr: new BigNumber(0.649999976158142),
-    insuranceIrFee: new BigNumber(0.649999976158142),
-    protocolFixedFeeApr: new BigNumber(0.649999976158142),
-    protocolIrFee: new BigNumber(0.649999976158142),
+    insuranceFeeFixedApr: new BigNumber(0),
+    insuranceIrFee: new BigNumber(0),
+    protocolFixedFeeApr: new BigNumber(0.01),
+    protocolIrFee: new BigNumber(0.05),
   },
   operationalState: OperationalState.Operational,
 
   oracle: {
     setup: OracleSetup.PythEma,
-    keys: [new PublicKey("2H6gWKxJuoFjBS4REqNm4XRa7uVFf9n9yKEowpwh7LML")],
+    keys: [new PublicKey("H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG")],
   },
 };
 
@@ -62,7 +62,9 @@ async function main() {
   console.log("Creating banks in group:", client.groupAddress.toBase58());
   console.log("Creating banks with authority:", client.wallet.publicKey.toBase58());
 
-  const result = await client.createLendingPool(bankMint, bank);
+  const result = await client.createLendingPool(bankMint, bank, {
+    dryRun: false,
+  });
   console.log(result);
 }
 
