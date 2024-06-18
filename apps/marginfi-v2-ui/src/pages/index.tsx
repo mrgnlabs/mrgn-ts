cd84efc7b4ee236ee3ca58ae198153754f75c38a
Nathan Galindo <nathan.zebedee@gmail.com>
2024-06-18 11:42:42 -0500

    remove unused code

--- apps/marginfi-v2-ui/src/pages/index.tsx
+++ apps/marginfi-v2-ui/src/pages/index.tsx
@@ -39,8 +39,6 @@ export default function HomePage() {
   ]);
 
   const annoucements = React.useMemo(() => {
-    const drift = extendedBankInfos.find((bank) => bank.meta.tokenSymbol === "DRIFT");
-    const tnsr = extendedBankInfos.find((bank) => bank.meta.tokenSymbol === "TNSR");
     const mother = extendedBankInfos.find((bank) => bank.meta.tokenSymbol === "MOTHER");
     const hSOL = extendedBankInfos.find((bank) => bank.meta.tokenSymbol === "hSOL");
 
