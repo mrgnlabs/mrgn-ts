6af1de5c1e46c94cb8d7ffb53ac1a9e35e00c620
chambaz <adammchambers@gmail.com>
2024-06-18 21:22:16 -0400

    feat: fetch marginfiClient on pool detail page

--- apps/marginfi-v2-trading/src/components/common/Pool/BankCard.tsx
+++ apps/marginfi-v2-trading/src/components/common/Pool/BankCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Image from "next/image";
 
-import { ActionType, ActiveBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
+import { ActionType, ExtendedBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
 import { numeralFormatter, usdFormatter } from "@mrgnlabs/mrgn-common";
 
 import { cn, getTokenImageURL } from "~/utils";
@@ -13,25 +13,25 @@ import { IconAlertTriangle } from "~/components/ui/icons";
 import { Button } from "~/components/ui/button";
 
 type BankCardProps = {
-  bank: ActiveBankInfo;
+  bank: ExtendedBankInfo;
 };
 
 export const BankCard = ({ bank }: BankCardProps) => {
   const { rateAP } = useAssetItemData({ bank, isInLendingMode: true });
 
-  const isUserPositionPoorHealth = React.useMemo(() => {
-    if (!bank || !bank?.position?.liquidationPrice) {
-      return false;
-    }
+  // const isUserPositionPoorHealth = React.useMemo(() => {
+  //   if (!bank || !bank?.position?.liquidationPrice) {
+  //     return false;
+  //   }
 
-    const alertRange = 0.05;
+  //   const alertRange = 0.05;
 
-    if (bank.position.isLending) {
-      return bank.info.state.price < bank.position.liquidationPrice + bank.position.liquidationPrice * alertRange;
-    } else {
-      return bank.info.state.price > bank.position.liquidationPrice - bank.position.liquidationPrice * alertRange;
-    }
-  }, [bank]);
+  //   if (bank.position.isLending) {
+  //     return bank.info.state.price < bank.position.liquidationPrice + bank.position.liquidationPrice * alertRange;
+  //   } else {
+  //     return bank.info.state.price > bank.position.liquidationPrice - bank.position.liquidationPrice * alertRange;
+  //   }
+  // }, [bank]);
   return (
     <div className="bg-background-gray p-4 rounded-lg space-y-4 flex flex-col justify-between">
       <div className="flex justify-between items-center w-full gap-2">
@@ -50,14 +50,14 @@ export const BankCard = ({ bank }: BankCardProps) => {
             <dd className="text-sm font-normal text-success">{rateAP.concat(...[" ", "APY"])}</dd>
           </dl>
         </div>
-        {bank.position && (
+        {/* {bank.position && (
           <div className="font-medium text-lg mr-2">
             {bank.position.amount < 0.01 ? "< $0.01" : numeralFormatter(bank.position.amount)}
             {" " + bank.meta.tokenSymbol}
           </div>
-        )}
+        )} */}
       </div>
-      {bank.position && (
+      {/* {bank.position && (
         <div className="bg-background/60 py-3 px-4 rounded-lg text-sm">
           <dl className="grid grid-cols-2 gap-y-0.5">
             <dt className="text-muted-foreground">USD value</dt>
@@ -68,12 +68,12 @@ export const BankCard = ({ bank }: BankCardProps) => {
             <dd className="text-right text-white">{usdFormatter.format(bank.info.state.price)}</dd>
           </dl>
         </div>
-      )}
-      {!bank.position && (
-        <div className="bg-background/60 py-6 px-4 rounded-lg text-sm">
-          <p className="text-muted-foreground">No current position.</p>
-        </div>
-      )}
+      )} */}
+      {/* {!bank.position && ( */}
+      <div className="bg-background/60 py-6 px-4 rounded-lg text-sm">
+        <p className="text-muted-foreground">No current position.</p>
+      </div>
+      {/* )} */}
       <ActionBoxDialog requestedAction={ActionType.Deposit} requestedBank={bank}>
         <div className="flex w-full gap-4 mt-auto">
           <Button className="flex-1 h-12" variant="outline">
