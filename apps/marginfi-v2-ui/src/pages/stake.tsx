89bf0cccd6f54bcd983bef75ff7cf702018d7076
Nathan Galindo <nathan.zebedee@gmail.com>
2024-06-23 16:21:01 -0500

    delete stake.tsx, rename mint.tsx, switch 'mint' copy to 'stake'

--- apps/marginfi-v2-ui/src/pages/stake.tsx
+++ apps/marginfi-v2-ui/src/pages/stake.tsx
@@ -1,48 +1,75 @@
 import React from "react";
 
-import { useRouter } from "next/router";
-
+import Link from "next/link";
 import { JupiterProvider } from "@jup-ag/react-hook";
 
-import { ActionType, createJupiterStore } from "@mrgnlabs/marginfi-v2-ui-state";
+import { groupedNumberFormatterDyn, numeralFormatter, usdFormatter } from "@mrgnlabs/mrgn-common";
 
-import { Desktop } from "~/mediaQueries";
-import { usePrevious, Features, isActive } from "~/utils";
-import { useLstStore } from "~/store";
 import { useConnection } from "~/hooks/useConnection";
 import { useWalletContext } from "~/hooks/useWalletContext";
+import { useLstStore, useMrgnlendStore, useUiStore } from "~/store";
+import { LST_MINT } from "~/store/lstStore";
 
-import { OverlaySpinner } from "~/components/ui/overlay-spinner";
+import { ActionComplete } from "~/components/common/ActionComplete";
+import { IconYBX, IconLST, IconSol, IconUsd } from "~/components/ui/icons";
 import { Loader } from "~/components/ui/loader";
-import { ActionBox } from "~/components/common/ActionBox";
+import { YbxDialogNotifications } from "~/components/common/Mint/YbxDialogNotifications";
+import {
+  BankIntegrationCard,
+  IntegrationCard,
+  IntegrationCardSkeleton,
+  MintCardWrapper,
+  YbxDialogPartner,
+} from "~/components/common/Mint";
+import {
+  IntegrationsData,
+  MintCardProps,
+  MintOverview,
+  MintPageState,
+  clampedNumeralFormatter,
+  fetchMintOverview,
+} from "~/utils";
+import { PageHeading } from "~/components/common/PageHeading";
 
-export const useJupiterStore = createJupiterStore();
-
-export default function StakePage() {
-  const { wallet, walletAddress } = useWalletContext();
+export default function MintPage() {
   const { connection } = useConnection();
-  const [mounted, setMounted] = React.useState(false);
+  const { wallet } = useWalletContext();
+  const [mintPageState, setMintPageState] = React.useState<MintPageState>(MintPageState.DEFAULT);
+  const [ybxNotificationsDialogOpen, setYbxNotificationsDialogOpen] = React.useState(false);
+  const [ybxPartnerDialogOpen, setYbxPartnerDialogOpen] = React.useState(false);
+  const [lstDialogOpen, setLSTDialogOpen] = React.useState(false);
+  const [integrations, setIntegrations] = React.useState<IntegrationsData[]>([]);
+  const [lstOverview, setLstOverview] = React.useState<MintOverview>();
+
   const debounceId = React.useRef<NodeJS.Timeout | null>(null);
 
-  const router = useRouter();
+  const [previousTxn] = useUiStore((state) => [state.previousTxn]);
+
+  const [fetchLstState, initialized, setIsRefreshingStore] = useLstStore((state) => [
+    state.fetchLstState,
+    state.initialized,
+    state.setIsRefreshingStore,
+  ]);
+
+  const [extendedBankInfos] = useMrgnlendStore((state) => [state.extendedBankInfos]);
+
+  const lstBank = React.useMemo(
+    () => extendedBankInfos.filter((bank) => bank.info.state.mint.equals(LST_MINT)),
+    [extendedBankInfos]
+  );
 
   React.useEffect(() => {
-    if (router.pathname.startsWith("/stake") && !isActive(Features.STAKE)) {
-      router.push("/");
-    } else {
-      setMounted(true);
-    }
-  }, [router]);
+    fetchVolumeData();
+  }, []);
 
-  const [initialized, isRefreshingStore, fetchLstState, setIsRefreshingStore, userDataFetched, resetUserData] =
-    useLstStore((state) => [
-      state.initialized,
-      state.isRefreshingStore,
-      state.fetchLstState,
-      state.setIsRefreshingStore,
-      state.userDataFetched,
-      state.resetUserData,
-    ]);
+  const fetchVolumeData = async () => {
+    try {
+      const overview = await fetchMintOverview(LST_MINT.toString());
+      setLstOverview(overview);
+    } catch (error) {
+      console.error("Something went wrong fetching LST volume");
+    }
+  };
 
   React.useEffect(() => {
     const fetchData = () => {
@@ -74,83 +101,144 @@ export default function StakePage() {
       }
     };
   }, [wallet]); // eslint-disable-line react-hooks/exhaustive-deps
-  // ^ crucial to omit both `connection` and `fetchMrgnlendState` from the dependency array
-  // TODO: fix...
+
+  const cards = React.useMemo(
+    () => [
+      {
+        title: "LST",
+        label: "protected SOL",
+        labelIcon: IconSol,
+        icon: IconLST,
+        description: "Accrues value against SOL",
+        features: ["Solana's highest yielding LST", "Pay no fees/commission", "Capture MEV rewards"],
+        volume: lstOverview?.volumeUsd ? `${groupedNumberFormatterDyn.format(lstOverview?.volume)} LST` : "-",
+        volumeUsd: lstOverview?.volumeUsd ? usdFormatter.format(lstOverview?.volumeUsd) : "-",
+        tvl: lstOverview?.tvl ? clampedNumeralFormatter(lstOverview?.tvl) : "-",
+        action: () => setLSTDialogOpen(true),
+      } as MintCardProps,
+      {
+        title: "YBX",
+        label: "protected USD",
+        labelIcon: IconUsd,
+        icon: IconYBX,
+        description: "Accrues value against USD",
+        features: [`Solana's decentralized stable-asset`, "Capture staking yield", "Capture MEV rewards"],
+        volume: `YBX`,
+        volumeUsd: ``,
+        tvl: "",
+        action: () => {
+          setYbxNotificationsDialogOpen(true);
+        },
+      } as MintCardProps,
+    ],
+    [lstOverview]
+  );
 
   React.useEffect(() => {
-    setIsRefreshingStore(true);
-    fetchLstState({ connection, wallet }).catch(console.error);
-    const id = setInterval(() => {
-      setIsRefreshingStore(true);
-      fetchLstState().catch(console.error);
-    }, 30_000);
-    return () => clearInterval(id);
-  }, [wallet]); // eslint-disable-line react-hooks/exhaustive-deps
-  // ^ crucial to omit both `connection` and `fetchMrgnlendState` from the dependency array
-  // TODO: fix...
+    const fetchIntegrations = async () => {
+      try {
+        const res = await fetch(`/api/birdeye/markets?token=` + LST_MINT);
+        if (!res.ok) {
+          // throw new Error("Failed to fetch integrations");
+          return;
+        }
+        const data = await res.json();
+        setIntegrations(data);
+      } catch (error) {
+        console.error("Failed to fetch integrations data:", error);
+        // Handle error or set some error state here
+      }
+    };
 
-  const prevWalletAddress = usePrevious(walletAddress);
-  React.useEffect(() => {
-    if (!prevWalletAddress && walletAddress) {
-      resetUserData();
-    }
-  }, [walletAddress, prevWalletAddress, resetUserData]);
-
-  React.useEffect(() => {
-    if (!walletAddress && userDataFetched) {
-      resetUserData();
-    }
-  }, [walletAddress, userDataFetched, resetUserData]);
-
-  if (!mounted) return null;
+    fetchIntegrations();
+  }, []);
 
   return (
     <>
       <JupiterProvider connection={connection} wrapUnwrapSOL={false} platformFeeAndAccounts={undefined}>
-        <StakingContent isInitialized={initialized} />
-        <Desktop>
-          <OverlaySpinner fetching={!initialized || isRefreshingStore} />
-        </Desktop>
+        <div className="w-full max-w-8xl mx-auto px-4 md:px-8 space-y-20 pb-28">
+          {!initialized && <Loader label="Loading YBX / LST..." className="mt-8" />}
+          {initialized && (
+            <>
+              <div className="w-full max-w-4xl mx-auto px-4 md:px-0">
+                <PageHeading
+                  heading={
+                    <>
+                      <h1 className="leading-normal">Inflation protected</h1>
+                      <div className="text-3xl leading-normal flex items-center gap-2 pb-2 justify-center">
+                        <IconSol size={32} />
+                        <p>SOL</p>
+                        <p className="mx-2">and</p>
+                        <IconUsd size={32} />
+                        <p>USD</p>
+                      </div>
+                    </>
+                  }
+                  body={
+                    <p>
+                      The two most important assets on Solana are SOL and USD.
+                      <br className="hidden lg:block" /> Capture inflation automatically with LST and YBX.
+                    </p>
+                  }
+                  links={[]}
+                />
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 md:gap-11 mt-8">
+                  {cards.map((item, idx) => (
+                    <MintCardWrapper mintCard={item} key={idx} />
+                  ))}
+                </div>
+              </div>
+
+              <div className="w-full py-4 px-4 md:px-10 xl:px-16 text-center">
+                <h2 className="text-3xl font-medium mb-3">Integrations</h2>
+                <p className="text-muted-foreground">
+                  Ready to integrate YBX?{" "}
+                  <button
+                    className="border-b border-primary text-primary transition-colors hover:text-chartreuse hover:border-chartreuse"
+                    onClick={() => setYbxPartnerDialogOpen(true)}
+                  >
+                    Become a launch partner.
+                  </button>
+                </p>
+                <div className="flex items-center justify-center flex-wrap gap-8 mt-10 w-full">
+                  {lstBank?.length > 0 ? (
+                    <>
+                      <BankIntegrationCard bank={lstBank[0]} isInLendingMode={true} />
+                    </>
+                  ) : (
+                    <IntegrationCardSkeleton />
+                  )}
+                  {integrations?.length > 0
+                    ? integrations.map((item, i) => <IntegrationCard integrationsData={item} key={i} />)
+                    : [...new Array(5)].map((_, index) => <IntegrationCardSkeleton key={index} />)}
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+        <YbxDialogNotifications
+          onClose={() => setYbxNotificationsDialogOpen(false)}
+          mintPageState={mintPageState}
+          onHandleChangeMintPage={(state) => setMintPageState(state)}
+          open={ybxNotificationsDialogOpen}
+          onOpenChange={(open) => {
+            setMintPageState(MintPageState.DEFAULT);
+            setYbxNotificationsDialogOpen(open);
+          }}
+        />
+        <YbxDialogPartner
+          onClose={() => setYbxPartnerDialogOpen(false)}
+          mintPageState={mintPageState}
+          onHandleChangeMintPage={(state) => setMintPageState(state)}
+          open={ybxPartnerDialogOpen}
+          onOpenChange={(open) => {
+            setMintPageState(MintPageState.DEFAULT);
+            setYbxPartnerDialogOpen(open);
+          }}
+        />
       </JupiterProvider>
+      {initialized && previousTxn && <ActionComplete />}
     </>
   );
 }
-
-const StakingContent = ({ isInitialized }: { isInitialized: boolean }) => (
-  <div className="flex flex-col max-w-[640px] h-full w-full justify-center items-center pb-32 lg:pb-16 px-4">
-    {!isInitialized && <Loader label="Loading mrgnstake..." className="mt-8" />}
-    {isInitialized && (
-      <div className="space-y-6 text-center mb-4">
-        <h1 className="font-bold text-3xl">LST — mrgn&apos;s Liquid Staking Token.</h1>
-        <p>The highest natural yield available from any LST on Solana. By a lot.</p>
-        <div className="text-chartreuse space-y-2 font-bold">
-          <p>LST is the highest natural yielding LST</p>
-          <p>LST is powered by Jito&apos;s MEV-boosted client</p>
-          <p>
-            LST only stakes to 0% commission{" "}
-            <a
-              href="https://stakewiz.com/validator/mrgn2vsZ5EJ8YEfAMNPXmRux7th9cNfBasQ1JJvVwPn"
-              target="_blank"
-              rel="noreferrer"
-              className="border-b border-chartreuse/50 transition-colors hover:border-transparent"
-            >
-              mrgn validators
-            </a>
-          </p>
-        </div>
-      </div>
-    )}
-    {isInitialized && (
-      <>
-        <div className="max-w-[480px] w-full space-y-4">
-          <ActionBox requestedAction={ActionType.MintLST} />
-        </div>
-        <p className="text-white/75 mt-8 text-center">
-          Using mrgn&apos;s sophisticated validator set, you pay no fees, earn more yield, and get more utility out of
-          your staked SOL than anywhere else. Maximum liquidity with Sanctum, maximum MEV rewards with Jito, maximum
-          utility with marginfi, maximum flexibility with Solana DeFi.
-        </p>
-      </>
-    )}
-  </div>
-);
