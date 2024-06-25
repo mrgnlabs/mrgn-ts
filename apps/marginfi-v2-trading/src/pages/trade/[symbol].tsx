97778a66c7cfdfa46b030bd5c10b6c52b1396a6a
chambaz <adammchambers@gmail.com>
2024-06-25 07:12:53 -0400

    chore: add ActionComplete to each main page

--- apps/marginfi-v2-trading/src/pages/trade/[symbol].tsx
+++ apps/marginfi-v2-trading/src/pages/trade/[symbol].tsx
@@ -1,15 +1,14 @@
 import React from "react";
 
-import Error from "next/error";
 import { useRouter } from "next/router";
 
-import { ActiveBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
 import { PublicKey } from "@solana/web3.js";
 
-import { useTradeStore } from "~/store";
+import { useTradeStore, useUiStore } from "~/store";
 import { useConnection } from "~/hooks/useConnection";
 import { useWalletContext } from "~/hooks/useWalletContext";
 
+import { ActionComplete } from "~/components/common/ActionComplete";
 import { TVWidget } from "~/components/common/TVWidget";
 import { TradingBox } from "~/components/common/TradingBox";
 import { PositionList } from "~/components/common/Portfolio";
@@ -25,6 +24,7 @@ export default function TradeSymbolPage() {
     state.setActiveBank,
     state.marginfiClient,
   ]);
+  const [previousTxn] = useUiStore((state) => [state.previousTxn]);
 
   React.useEffect(() => {
     if (!router.query.symbol || !wallet || !connection || activeGroup) return;
@@ -33,23 +33,26 @@ export default function TradeSymbolPage() {
   }, [router.query.symbol, wallet, connection, activeGroup]);
 
   return (
-    <div className="w-full max-w-8xl mx-auto px-4 md:px-8 pb-28 z-10">
-      {(!initialized || !activeGroup) && <Loader label="Loading mrgntrade..." className="mt-8" />}
-      {initialized && activeGroup && activeGroup.token && (
-        <div className="flex flex-col items-start gap-8 pb-16 w-full">
-          <div className="grid grid-cols-12 gap-4 w-full h-full lg:gap-8">
-            <div className="col-span-9 space-y-8 h-[60vh]">
-              <TVWidget symbol={activeGroup.token.info.rawBank.mint.toString()} />
-            </div>
-            <aside className="col-span-3">
-              <TradingBox activeBank={activeGroup.token} />
-            </aside>
-            <div className="col-span-12 space-y-8">
-              <PositionList />
+    <>
+      <div className="w-full max-w-8xl mx-auto px-4 md:px-8 pb-28 z-10">
+        {(!initialized || !activeGroup) && <Loader label="Loading mrgntrade..." className="mt-8" />}
+        {initialized && activeGroup && activeGroup.token && (
+          <div className="flex flex-col items-start gap-8 pb-16 w-full">
+            <div className="grid grid-cols-12 gap-4 w-full h-full lg:gap-8">
+              <div className="col-span-9 space-y-8 h-[60vh]">
+                <TVWidget symbol={activeGroup.token.info.rawBank.mint.toString()} />
+              </div>
+              <aside className="col-span-3">
+                <TradingBox activeBank={activeGroup.token} />
+              </aside>
+              <div className="col-span-12 space-y-8">
+                <PositionList />
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+      {previousTxn && <ActionComplete />}
+    </>
   );
 }
