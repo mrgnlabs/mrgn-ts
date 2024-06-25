a38b2938eae7b0d3428aab1693c403257427b938
Nathan Galindo <nathan.zebedee@gmail.com>
2024-06-25 09:14:35 -0500

    init

--- apps/marginfi-v2-ui/src/components/mobile/MobileNavbar/MobileNavbar.tsx
+++ apps/marginfi-v2-ui/src/components/mobile/MobileNavbar/MobileNavbar.tsx
@@ -27,7 +27,7 @@ export const mobileLinks: NavLinkInfo[] = [
     Icon: IconBuildingBank,
   },
   {
-    href: "/mint",
+    href: "/stake",
     alt: "mint icon",
     label: "mint",
     Icon: IconBox,
