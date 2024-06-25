50e7a188bd8434f9ec0cf094b9e2335c36f16899
chambaz <adammchambers@gmail.com>
2024-06-25 07:12:53 -0400

    chore: add ActionComplete to each main page

--- apps/marginfi-v2-trading/src/pages/index.tsx
+++ apps/marginfi-v2-trading/src/pages/index.tsx
@@ -2,13 +2,12 @@ import React from "react";
 
 import Link from "next/link";
 
-import { useTradeStore } from "~/store";
-import { useWalletContext } from "~/hooks/useWalletContext";
-import { useConnection } from "~/hooks/useConnection";
+import { useTradeStore, useUiStore } from "~/store";
 
 import { PageHeading } from "~/components/common/PageHeading";
 import { PoolCard } from "~/components/common/Pool/PoolCard";
 import { CreatePoolDialog } from "~/components/common/Pool/CreatePoolDialog";
+import { ActionComplete } from "~/components/common/ActionComplete";
 import { IconSearch, IconSortDescending, IconFilter, IconPlus } from "~/components/ui/icons";
 import { Popover, PopoverTrigger, PopoverContent } from "~/components/ui/popover";
 import { Loader } from "~/components/ui/loader";
@@ -16,71 +15,74 @@ import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 
 export default function HomePage() {
-  const { wallet } = useWalletContext();
-  const { connection } = useConnection();
-  const [initialized, fetchTradeState, banks, resetActiveGroup] = useTradeStore((state) => [
+  const [initialized, banks, resetActiveGroup] = useTradeStore((state) => [
     state.initialized,
-    state.fetchTradeState,
     state.banks,
     state.resetActiveGroup,
   ]);
+
+  const [previousTxn] = useUiStore((state) => [state.previousTxn]);
 
   React.useEffect(() => {
     resetActiveGroup();
   }, []);
 
   return (
-    <div className="w-full max-w-8xl mx-auto px-4 md:px-8 pb-28">
-      {!initialized && <Loader label="Loading mrgntrade..." className="mt-8" />}
-      {initialized && (
-        <>
-          <div className="w-full max-w-4xl mx-auto px-4 md:px-0">
-            <PageHeading
-              heading={<h1>mrgntrade</h1>}
-              body={<p>Create permissionless pools, provide liquidity, and trade with mrgntrade.</p>}
-              links={[]}
-              button={<CreatePoolDialog />}
-            />
-          </div>
+    <>
+      <div className="w-full max-w-8xl mx-auto px-4 md:px-8 pb-28">
+        {!initialized && <Loader label="Loading mrgntrade..." className="mt-8" />}
+        {initialized && (
+          <>
+            <div className="w-full max-w-4xl mx-auto px-4 md:px-0">
+              <PageHeading
+                heading={<h1>mrgntrade</h1>}
+                body={<p>Create permissionless pools, provide liquidity, and trade with mrgntrade.</p>}
+                links={[]}
+                button={<CreatePoolDialog />}
+              />
+            </div>
 
-          <div className="w-full space-y-8 px-4 lg:px-8 pt-8 pb-16">
-            <div className="flex items-center gap-4">
-              <div className="relative w-full">
-                <IconSearch size={18} className="absolute inset-y-0 left-4 h-full" />
-                <Input placeholder="Search token names / symbols" className="py-2 pr-3 pl-12 h-10 rounded-lg" />
+            <div className="w-full space-y-8 px-4 lg:px-8 pt-8 pb-16">
+              <div className="flex items-center gap-4">
+                <div className="relative w-full">
+                  <IconSearch size={18} className="absolute inset-y-0 left-4 h-full" />
+                  <Input placeholder="Search token names / symbols" className="py-2 pr-3 pl-12 h-10 rounded-lg" />
+                </div>
+                <div className="flex items-center gap-4 ml-auto">
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button variant="outline" size="lg" className="py-2 h-10">
+                        <IconSortDescending size={20} /> Sort
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80"></PopoverContent>
+                  </Popover>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button variant="outline" size="lg" className="py-2 h-10">
+                        <IconFilter size={20} /> Filter
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80"></PopoverContent>
+                  </Popover>
+                </div>
               </div>
-              <div className="flex items-center gap-4 ml-auto">
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button variant="outline" size="lg" className="py-2 h-10">
-                      <IconSortDescending size={20} /> Sort
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-80"></PopoverContent>
-                </Popover>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button variant="outline" size="lg" className="py-2 h-10">
-                      <IconFilter size={20} /> Filter
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-80"></PopoverContent>
-                </Popover>
+              <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                {banks.length > 0 &&
+                  banks
+                    .sort(
+                      (a, b) =>
+                        b.info.oraclePrice.priceRealtime.price.toNumber() -
+                        a.info.oraclePrice.priceRealtime.price.toNumber()
+                    )
+                    .map((bank, i) => <PoolCard key={i} bank={bank} />)}
               </div>
             </div>
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
-              {banks.length > 0 &&
-                banks
-                  .sort(
-                    (a, b) =>
-                      b.info.oraclePrice.priceRealtime.price.toNumber() -
-                      a.info.oraclePrice.priceRealtime.price.toNumber()
-                  )
-                  .map((bank, i) => <PoolCard key={i} bank={bank} />)}
-            </div>
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+
+      {initialized && previousTxn && <ActionComplete />}
+    </>
   );
 }
