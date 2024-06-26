9eb8b1e4515d7e12005a6f1e14dd2bc7c155edaa
chambaz <adammchambers@gmail.com>
2024-06-25 20:33:17 -0400

    feat: restyle pool listing hero

--- apps/marginfi-v2-trading/src/components/common/PageHeading/PageHeading.tsx
+++ apps/marginfi-v2-trading/src/components/common/PageHeading/PageHeading.tsx
@@ -1,22 +1,30 @@
 import Link from "next/link";
 
+import { cn } from "~/utils";
+
 interface PageHeadingProps {
-  heading: JSX.Element;
+  heading: string;
   body?: JSX.Element;
-  links: {
+  links?: {
     href: string;
     icon: JSX.Element;
   }[];
   button?: JSX.Element;
+  size?: "md" | "lg";
 }
 
-export const PageHeading = ({ heading, body, links, button }: PageHeadingProps) => {
+export const PageHeading = ({ heading, body, links, button, size = "md" }: PageHeadingProps) => {
   return (
-    <div className="text-muted-foreground text-base md:text-lg text-center px-2 pt-4 pb-12 md:pt-0 md:px-0 space-y-3">
-      <h1 className="text-4xl font-medium text-primary">{heading}</h1>
+    <div
+      className={cn(
+        "text-muted-foreground text-base md:text-lg text-center px-2 pt-4 pb-10 md:pt-0 md:px-0 space-y-3",
+        size === "lg" && "space-y-6"
+      )}
+    >
+      <h1 className={cn("text-4xl font-medium text-primary", size === "lg" && "text-6xl")}>{heading}</h1>
       <div className="max-w-2xl w-full mx-auto">{body}</div>
 
-      {links.length > 0 && (
+      {links && links.length > 0 && (
         <ul className="flex items-center gap-4 justify-center pt-2">
           {links.map((link, idx) => (
             <li key={idx}>
