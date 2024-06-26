3958e518a84a68d2e58ef059e98825736fdc98a5
chambaz <adammchambers@gmail.com>
2024-06-25 20:07:08 -0400

    feat: search api

---
+++ apps/marginfi-v2-trading/src/config/trade.ts
@@ -0,0 +1,3 @@
+export const TRADE_GROUPS_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json";
+export const TOKEN_METADATA_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-metadata-cache.json";
+export const BANK_METADATA_MAP = "https://storage.googleapis.com/mrgn-public/mfi-bank-metadata-cache.json";
