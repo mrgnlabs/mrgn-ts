3e3ca7e8d5d709193e895ef89c6e1a69329d1a15
man0s <95379755+losman0s@users.noreply.github.com>
2024-06-28 14:08:32 +0800

    fix: rebase

--- apps/marginfi-v2-ui/src/components/common/ActionBox/components/ActionBoxPreview/Components/LoopPreview/LoopPreview.utils.tsx
+++ apps/marginfi-v2-ui/src/components/common/ActionBox/components/ActionBoxPreview/Components/LoopPreview/LoopPreview.utils.tsx
@@ -128,7 +128,7 @@ export async function simulateLooping({ marginfiClient, account, bank, loopingTx
     ]);
     if (!mfiAccountData || !bankData) throw new Error("Failed to simulate looping");
     const previewBanks = marginfiClient.banks;
-    previewBanks.set(bank.address.toBase58(), Bank.fromBuffer(bank.address, bankData));
+    previewBanks.set(bank.address.toBase58(), Bank.fromBuffer(bank.address, bankData, marginfiClient.program.idl));
     const previewClient = new MarginfiClient(
       marginfiClient.config,
       marginfiClient.program,
@@ -141,7 +141,8 @@ export async function simulateLooping({ marginfiClient, account, bank, loopingTx
     const previewMarginfiAccount = MarginfiAccountWrapper.fromAccountDataRaw(
       account.address,
       previewClient,
-      mfiAccountData
+      mfiAccountData,
+      marginfiClient.program.idl
     );
 
     return (simulationResult = {
