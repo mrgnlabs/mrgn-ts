9bf33c88ebbf81a55b4e16944e696e5039cca74b
chambaz <adammchambers@gmail.com>
2024-07-07 16:37:13 -0400

    feat: load more style pagination

--- apps/marginfi-v2-trading/src/config/trade.ts
+++ apps/marginfi-v2-trading/src/config/trade.ts
@@ -1,3 +1,4 @@
 export const TRADE_GROUPS_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json";
 export const TOKEN_METADATA_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-metadata-cache.json";
 export const BANK_METADATA_MAP = "https://storage.googleapis.com/mrgn-public/mfi-bank-metadata-cache.json";
+export const POOLS_PER_PAGE = 12;
