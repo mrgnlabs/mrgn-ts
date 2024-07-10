03d63e546611f33ac8669efb24397f9b5bebafde
chambaz <adammchambers@gmail.com>
2024-07-10 09:26:10 -0400

    feat: mobile header / nav

--- apps/marginfi-v2-trading/src/components/mobile/MobileNavbar/MobileNavbar.tsx
+++ apps/marginfi-v2-trading/src/components/mobile/MobileNavbar/MobileNavbar.tsx
@@ -2,26 +2,18 @@ import React from "react";
 
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { IconCoins, IconTrendingUp, IconChartPie, IconPlus } from "@tabler/icons-react";
 
-import { useMrgnlendStore, useUiStore, useUserProfileStore } from "~/store";
+import { useUiStore } from "~/store";
 import { useFirebaseAccount } from "~/hooks/useFirebaseAccount";
-import { useWalletContext } from "~/hooks/useWalletContext";
 import { useOs } from "~/hooks/useOs";
 import { cn } from "~/utils/themeUtils";
-import { Button } from "~/components/ui/button";
-
-import {
-  IconCoins,
-  IconProps,
-  IconTrendingUp,
-  IconChartPie,
-} from "~/components/ui/icons";
 
 export interface NavLinkInfo {
   href: string;
   alt: string;
   label: string;
-  Icon: (props: IconProps) => JSX.Element;
+  Icon: React.ElementType;
 }
 
 export const mobileLinks: NavLinkInfo[] = [
@@ -44,15 +36,19 @@ export const mobileLinks: NavLinkInfo[] = [
     label: "portfolio",
     Icon: IconChartPie,
   },
+  {
+    href: "/portfolio",
+    alt: "portfolio icon",
+    label: "create pool",
+    Icon: IconPlus,
+  },
 ];
 
 const MobileNavbar = () => {
-  const { asPath, isReady } = useRouter();
   useFirebaseAccount();
 
   const router = useRouter();
-  const [isMenuModalOpen, setIsMenuModalOpen, isActionBoxInputFocussed] = useUiStore((state) => [
-    state.isMenuDrawerOpen,
+  const [setIsMenuModalOpen, isActionBoxInputFocussed] = useUiStore((state) => [
     state.setIsMenuDrawerOpen,
     state.isActionBoxInputFocussed,
   ]);
@@ -68,8 +64,8 @@ const MobileNavbar = () => {
 
   return (
     <footer>
-      <nav className="fixed w-full bottom-0 z-50 bg-[#0F1111]">
-        <div className="h-full w-full text-xs font-normal text-[#868E95] z-50 flex justify-around relative lg:gap-8">
+      <nav className="fixed w-full bottom-0 z-50 bg-background border">
+        <div className="h-full w-full text-xs font-normal z-50 flex justify-around relative lg:gap-8">
           {mobileLinks.map((linkInfo, index) => {
             const isActive = activeLink === `link${index}`;
             return (
@@ -80,18 +76,13 @@ const MobileNavbar = () => {
                 className={cn(
                   "w-1/4 py-2.5 flex flex-col gap-1 items-center border-t border-border",
                   isIOS && isPWA && "pb-8",
-                  isActive ? "text-chartreuse" : "text-[#999]"
+                  isActive ? "text-primary" : "text-muted-foreground"
                 )}
               >
-                <Button
-                  variant="ghost"
-                  className={cn("text-muted-foreground", asPath === linkInfo.href && "bg-accent text-primary")}
-                >
-                  <linkInfo.Icon />
-                  {linkInfo.label}
-                </Button>
+                <linkInfo.Icon />
+                {linkInfo.label}
               </Link>
-            )
+            );
           })}
         </div>
       </nav>
