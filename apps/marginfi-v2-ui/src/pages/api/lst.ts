7675746e67d3e29696f3ed746ee9d0e78b0d972e
chambaz <adammchambers@gmail.com>
2024-07-10 18:58:36 -0400

    fix: moving apy functions into api route (temp)

--- apps/marginfi-v2-ui/src/pages/api/lst.ts
+++ apps/marginfi-v2-ui/src/pages/api/lst.ts
@@ -1,6 +1,49 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import NodeCache from "node-cache";
-import { fetchAndParsePricesCsv } from "~/utils";
+import { Readable } from "stream";
+import { parse } from "csv-parse";
+
+type PriceRecord = {
+  timestamp: number;
+  epoch: number;
+  price: number;
+};
+
+const parsePriceRecordsFromCSV = async (csv: Readable): Promise<PriceRecord[]> => {
+  const csvParser = parse({ delimiter: ",", columns: true });
+  const records: PriceRecord[] = [];
+  for await (const row of csv.pipe(csvParser)) {
+    const { timestamp, epoch, price } = row;
+    if (!timestamp || !epoch || !price) {
+      throw new Error('Columns "timestamp", "epoch", "price" must be present!');
+    }
+    const record = {
+      timestamp: Math.round(new Date(timestamp).getTime() / 1e3),
+      epoch: Number(epoch),
+      price: Number(price),
+    };
+    if (isNaN(record.timestamp)) {
+      throw new Error("Timestamp must be a... timestamp!");
+    }
+    if (isNaN(record.epoch)) {
+      throw new Error("Epoch must be a number!");
+    }
+    if (isNaN(record.price)) {
+      throw new Error("Price must be a number!");
+    }
+
+    records.push(record);
+  }
+  return records;
+};
+
+const fetchAndParsePricesCsv = async (url: string) => {
+  const csvResponse = await fetch(url);
+  const csvContents = await csvResponse.text();
+  const prices = await parsePriceRecordsFromCSV(Readable.from([csvContents]));
+
+  return prices;
+};
 
 const myCache = new NodeCache({ stdTTL: 600 }); // Cache for 1 hour
 const SOLANA_COMPASS_BASE_URL = "https://raw.githubusercontent.com/glitchful-dev/sol-stake-pool-apy/master/db/";
