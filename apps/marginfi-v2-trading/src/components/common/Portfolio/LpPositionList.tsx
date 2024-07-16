6b51bc3171ad3fffc332934e1af7183720f55081
chambaz <adammchambers@gmail.com>
2024-07-16 15:17:06 -0400

    feat: refactor lp position list to work with portfolio rather than active group

--- apps/marginfi-v2-trading/src/components/common/Portfolio/LpPositionList.tsx
+++ apps/marginfi-v2-trading/src/components/common/Portfolio/LpPositionList.tsx
@@ -18,16 +18,13 @@ import { Badge } from "~/components/ui/badge";
 import { ActiveGroup } from "~/store/tradeStore";
 
 export const LpPositionList = () => {
-  const [marginfiClient, selectedAccount, activeGroup, banks, collateralBanks, marginfiAccounts] = useTradeStore(
-    (state) => [
-      state.marginfiClient,
-      state.selectedAccount,
-      state.activeGroup,
-      state.banks,
-      state.collateralBanks,
-      state.marginfiAccounts,
-    ]
-  );
+  const [marginfiClient, marginfiAccounts, activeGroup, banks, collateralBanks] = useTradeStore((state) => [
+    state.marginfiClient,
+    state.marginfiAccounts,
+    state.activeGroup,
+    state.banks,
+    state.collateralBanks,
+  ]);
 
   const portfolio = React.useMemo(() => {
     const filteredBanks = banks.filter((bank) => bank.isActive) as ActiveBankInfo[];
@@ -42,7 +39,7 @@ export const LpPositionList = () => {
     return tokenValue + usdcValue;
   }, [activeGroup]);
 
-  if (!activeGroup) {
+  if (!portfolio) {
     return <></>;
   }
 
@@ -59,60 +56,61 @@ export const LpPositionList = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {portfolio.length === 0 ? (
-            <TableRow>
-              <TableCell colSpan={7}>
-                <p className="text-sm text-muted-foreground pt-2">No positions found</p>
-              </TableCell>
-            </TableRow>
-          ) : (
-            <></>
-          )}
-          <TableRow className="even:bg-white/50 hover:even:bg-white/50">
-            <TableCell>
-              <Link
-                href={`/pools/${activeGroup.token.address.toBase58()}`}
-                className="flex items-center gap-3 transition-colors hover:text-mrgn-chartreuse"
-              >
-                <div className="flex ">
-                  <Image
-                    src={getTokenImageURL(activeGroup.token.meta.tokenSymbol)}
-                    width={24}
-                    height={24}
-                    alt={activeGroup.token.meta.tokenSymbol}
-                    className="rounded-full shrink-0"
-                  />
-                  <Image
-                    src={getTokenImageURL(activeGroup.usdc.meta.tokenSymbol)}
-                    width={24}
-                    height={24}
-                    alt={activeGroup.usdc.meta.tokenSymbol}
-                    className="rounded-full shrink-0 ml-[-10px]"
-                  />
-                </div>{" "}
-                {`${activeGroup.token.meta.tokenSymbol}/${activeGroup.usdc.meta.tokenSymbol} `}
-              </Link>
-            </TableCell>
-            <TableCell>
-              {activeGroup.token.isActive
-                ? activeGroup.token.position.amount < 0.01
-                  ? "0.01"
-                  : numeralFormatter(activeGroup.token.position.amount)
-                : 0}
-            </TableCell>
-            <TableCell>
-              {activeGroup.usdc.isActive
-                ? activeGroup.usdc.position.amount < 0.01
-                  ? "0.01"
-                  : numeralFormatter(activeGroup.usdc.position.amount)
-                : 0}
-            </TableCell>
-            <TableCell>{usdFormatter.format(totalUsdValue)}</TableCell>
+          {portfolio.map((bank, i) => {
+            const collateralBank = collateralBanks[bank.address.toBase58()];
+            const marginfiAccount = marginfiAccounts ? marginfiAccounts[bank.info.rawBank.group.toBase58()] : undefined;
+            const borrowBank = bank.position.isLending ? collateralBank : bank;
+            const isBorrowing = borrowBank.isActive && !borrowBank.position.isLending;
+            const activeGroup: ActiveGroup = {
+              usdc: collateralBank,
+              token: bank,
+            };
 
-            <TableCell className="text-right">
-              {selectedAccount && <LpActionButtons marginfiAccount={selectedAccount} activeGroup={activeGroup} />}
-            </TableCell>
-          </TableRow>
+            if (!collateralBank) return;
+            return (
+              <TableRow key={i} className="even:bg-white/50 hover:even:bg-white/50">
+                <TableCell>
+                  <Link
+                    href={`/pools/${bank.address.toBase58()}`}
+                    className="flex items-center gap-3 transition-colors hover:text-mrgn-chartreuse"
+                  >
+                    <div className="flex">
+                      <Image
+                        src={getTokenImageURL(bank.meta.tokenSymbol)}
+                        width={24}
+                        height={24}
+                        alt={bank.meta.tokenSymbol}
+                        className="rounded-full shrink-0 z-20"
+                      />
+                      <Image
+                        src={getTokenImageURL(collateralBank.meta.tokenSymbol)}
+                        width={24}
+                        height={24}
+                        alt={collateralBank.meta.tokenSymbol}
+                        className="rounded-full shrink-0 ml-[-12px] z-10"
+                      />
+                    </div>{" "}
+                    {`${bank.meta.tokenSymbol}/${collateralBank.meta.tokenSymbol} `}
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  {bank.isActive ? (bank.position.amount < 0.01 ? "0.01" : numeralFormatter(bank.position.amount)) : 0}
+                </TableCell>
+                <TableCell>
+                  {collateralBank.isActive
+                    ? collateralBank.position.amount < 0.01
+                      ? "0.01"
+                      : numeralFormatter(collateralBank.position.amount)
+                    : 0}
+                </TableCell>
+                <TableCell>{usdFormatter.format(totalUsdValue)}</TableCell>
+
+                <TableCell className="text-right">
+                  {marginfiAccounts && <LpActionButtons marginfiAccount={marginfiAccount} activeGroup={activeGroup} />}
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </div>
