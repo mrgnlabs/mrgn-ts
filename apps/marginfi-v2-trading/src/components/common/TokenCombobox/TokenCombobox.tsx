4d4bb95adbf9aa6e7a3dbbc98cbb34b7ef5438d6
Kobe <leenderskobe@gmail.com>
2024-07-16 12:42:46 +0200

    feat: tradebox dialog & drawer to match ActionBox

--- apps/marginfi-v2-trading/src/components/common/TokenCombobox/TokenCombobox.tsx
+++ apps/marginfi-v2-trading/src/components/common/TokenCombobox/TokenCombobox.tsx
@@ -14,6 +14,9 @@ import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, Command
 import { Button } from "~/components/ui/button";
 
 import type { TokenData } from "~/types";
+import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import { Desktop, Mobile } from "~/mediaQueries";
+import { Drawer, DrawerContent, DrawerTrigger } from "~/components/ui/drawer";
 
 type TokenComboboxProps = {
   selected: ExtendedBankInfo | null;
@@ -31,60 +34,120 @@ export const TokenCombobox = ({ selected, setSelected }: TokenComboboxProps) => 
   }, [extendedBankInfos]);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button variant="secondary" size="lg" className="w-full justify-start pr-4 pl-3 py-3">
-          {selected !== null ? (
-            <>
-              <Image
-                src={getTokenImageURL(selected.meta.tokenSymbol)}
-                width={24}
-                height={24}
-                alt={`Pool ${selected}`}
-                className="rounded-full"
-              />{" "}
-              {selected.meta.tokenSymbol}
-            </>
-          ) : (
-            <>Select pool</>
-          )}
-          <IconChevronDown size={18} className="ml-auto" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="p-0">
-        <Command>
-          <CommandInput placeholder="Select pool..." />
-          <CommandList className="max-h-[390px]">
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup>
-              {banks.map((bank, index) => (
-                <CommandItem
-                  key={index}
-                  className="gap-3 py-2 cursor-pointer rounded-md aria-selected:text-primary"
-                  value={bank.meta.tokenSymbol}
-                  onSelect={(value) => {
-                    const selBank = extendedBankInfos.find((bank) => bank.meta.tokenSymbol.toLowerCase() === value);
-                    if (!selBank) return;
-                    setSelected(selBank);
-                    setOpen(false);
-                  }}
-                >
-                  <Image
-                    src={getTokenImageURL(bank.meta.tokenSymbol)}
-                    width={32}
-                    height={32}
-                    alt={bank.meta.tokenName}
-                    className="rounded-full"
-                  />
-                  <span>{bank.meta.tokenSymbol}</span>
-                  <TokenTrending bank={bank} />
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <>
+      <Desktop>
+        <Dialog open={open} onOpenChange={(open) => setOpen(open)}>
+          <DialogTrigger asChild>
+            <div>
+              <TokenTrigger selected={selected} />
+            </div>
+          </DialogTrigger>
+          <DialogContent className="p-4 bg-background m-0" hideClose={true} hidePadding={true} size="sm" position="top">
+            <div className="h-[500px] relative overflow-auto">
+              <Command>
+                <CommandInput placeholder="Select pool..." />
+                <CommandList className="max-h-[390px]">
+                  <CommandEmpty>No results found.</CommandEmpty>
+                  <CommandGroup>
+                    {banks.map((bank, index) => (
+                      <CommandItem
+                        key={index}
+                        className="gap-3 py-2 cursor-pointer rounded-md aria-selected:text-primary"
+                        value={bank.meta.tokenSymbol}
+                        onSelect={(value) => {
+                          const selBank = extendedBankInfos.find(
+                            (bank) => bank.meta.tokenSymbol.toLowerCase() === value
+                          );
+                          if (!selBank) return;
+                          setSelected(selBank);
+                          setOpen(false);
+                        }}
+                      >
+                        <Image
+                          src={getTokenImageURL(bank.meta.tokenSymbol)}
+                          width={32}
+                          height={32}
+                          alt={bank.meta.tokenName}
+                          className="rounded-full"
+                        />
+                        <span>{bank.meta.tokenSymbol}</span>
+                        <TokenTrending bank={bank} />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </Desktop>
+      <Mobile>
+        <Drawer open={open} onOpenChange={(open) => setOpen(open)}>
+          <DrawerTrigger asChild>
+            <div>
+              <TokenTrigger selected={selected} />
+            </div>
+          </DrawerTrigger>
+          <DrawerContent className="h-full z-[55] mt-0" hideTopTrigger={true}>
+            <Command>
+              <CommandInput placeholder="Select pool..." />
+              <CommandList className="max-h-[390px]">
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup>
+                  {banks.map((bank, index) => (
+                    <CommandItem
+                      key={index}
+                      className="gap-3 py-2 cursor-pointer rounded-md aria-selected:text-primary"
+                      value={bank.meta.tokenSymbol}
+                      onSelect={(value) => {
+                        const selBank = extendedBankInfos.find((bank) => bank.meta.tokenSymbol.toLowerCase() === value);
+                        if (!selBank) return;
+                        setSelected(selBank);
+                        setOpen(false);
+                      }}
+                    >
+                      <Image
+                        src={getTokenImageURL(bank.meta.tokenSymbol)}
+                        width={32}
+                        height={32}
+                        alt={bank.meta.tokenName}
+                        className="rounded-full"
+                      />
+                      <span>{bank.meta.tokenSymbol}</span>
+                      <TokenTrending bank={bank} />
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </DrawerContent>
+        </Drawer>
+      </Mobile>
+    </>
+  );
+};
+
+const TokenTrigger = ({ selected }: { selected: ExtendedBankInfo | null }) => {
+  return (
+    <Button variant="secondary" size="lg" className="min-w-[125px] relative w-full justify-start pr-4 pl-3 py-3">
+      {selected !== null ? (
+        <>
+          <Image
+            src={getTokenImageURL(selected.meta.tokenSymbol)}
+            width={24}
+            height={24}
+            alt={`Pool ${selected}`}
+            className="rounded-full"
+          />{" "}
+          {selected.meta.tokenSymbol}
+        </>
+      ) : (
+        <>Select pool</>
+      )}
+      <div>
+        <IconChevronDown size={18} className="ml-auto" />
+      </div>
+    </Button>
   );
 };
 
