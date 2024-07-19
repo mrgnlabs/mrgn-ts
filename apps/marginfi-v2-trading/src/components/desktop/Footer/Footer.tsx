ebbb9f9b074984f433530b66d2ff1da4c46218dd
chambaz <adammchambers@gmail.com>
2024-07-18 22:50:16 -0400

    feat: footer links / mrgn animation

--- apps/marginfi-v2-trading/src/components/desktop/Footer/Footer.tsx
+++ apps/marginfi-v2-trading/src/components/desktop/Footer/Footer.tsx
@@ -22,32 +22,20 @@ const footerLinks = [
     icon: <IconBrandDiscordFilled />,
   },
   {
-    href: "https://mrgn.substack.com/",
-    icon: <IconBrandSubstack />,
-  },
-  {
     href: "https://github.com/mrgnlabs",
     icon: <IconBrandGithubFilled />,
   },
   {
-    href: "https://mrgn.grafana.net/public-dashboards/a2700f1bbca64aeaa5582a90dbaeb276?orgId=1&refresh=1m",
-    icon: <IconChartHistogram />,
-  },
-  {
     href: "https://docs.marginfi.com/",
     icon: <IconBook />,
-  },
-  {
-    href: "https://marginfi.canny.io/mrgnlend",
-    icon: <IconHelp />,
   },
 ];
 
 export const Footer = () => {
   return (
     <footer className="bg-background fixed bottom-0 w-full flex items-center justify-between px-4 py-2 z-30">
-      <p className="text-sm">
-        <span className="inline-block translate-y-[1px] mr-0.5">◼️</span> built by{" "}
+      <p className="text-xs text-primary/80">
+        <span className="inline-block translate-y-[1px] mr-0.5 animate-pulsate">◼️</span> built by{" "}
         <Link href="https://www.marginfi.com" target="_blank">
           mrgn
         </Link>
