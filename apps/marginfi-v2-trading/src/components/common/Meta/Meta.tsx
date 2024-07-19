8f5349629f4bd5134f9093320f43f6855483a035
chambaz <adammchambers@gmail.com>
2024-07-19 15:59:47 -0400

    feat: update metadata / prod domain

--- apps/marginfi-v2-trading/src/components/common/Meta/Meta.tsx
+++ apps/marginfi-v2-trading/src/components/common/Meta/Meta.tsx
@@ -32,12 +32,9 @@ export const Meta = ({ path, activeGroup }: MrgnProps) => {
         </>
       )}
       <meta property="og:title" content={title} />
-      <meta property="og:url" content="https://marginfi-v2-trading-mrgn.vercel.app/" />
+      <meta property="og:url" content="https://www.thearena.trade/" />
       <meta property="og:type" content="website" />
-      <meta
-        property="og:image"
-        content="https://marginfi-v2-trading-mrgn.vercel.app/marginfi-arena-cover-1200x630.jpg"
-      />
+      <meta property="og:image" content="https://www.thearena.trade/marginfi-arena-cover-1200x630.jpg" />
       <meta property="og:image:alt" content="marginfi" />
       <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:width" content="1200" />
@@ -48,14 +45,10 @@ export const Meta = ({ path, activeGroup }: MrgnProps) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@marginfi" />
       <meta name="twitter:creator" content="@marginfi" />
-      <meta
-        name="twitter:image"
-        content="https://marginfi-v2-trading-mrgn.vercel.app/marginfi-arena-cover-1200x600.jpg"
-      />
+      <meta name="twitter:image" content="https://www.thearena.trade/marginfi-arena-cover-1200x600.jpg" />
       <meta name="robots" content="index,follow" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
-      <link rel="manifest" href="/manifest.json" />
     </Head>
   );
 };
