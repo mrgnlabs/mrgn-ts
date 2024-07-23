4468c33d6080e1f953f5513c4e075c6027fec2f3
chambaz <adammchambers@gmail.com>
2024-07-23 16:39:21 +0300

    fix: hide active pool count if only LP

--- apps/marginfi-v2-trading/src/pages/portfolio.tsx
+++ apps/marginfi-v2-trading/src/pages/portfolio.tsx
@@ -7,7 +7,7 @@ import { ActiveBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
 import { groupedNumberFormatterDyn, usdFormatter } from "@mrgnlabs/mrgn-common";
 
 import { useTradeStore, useUiStore } from "~/store";
-import { getTokenImageURL } from "~/utils";
+import { getTokenImageURL, cn } from "~/utils";
 
 import { PageHeading } from "~/components/common/PageHeading";
 import { PositionCard, LpPositionList } from "~/components/common/Portfolio";
@@ -85,38 +85,43 @@ export default function PortfolioPage() {
               </p>
             ) : (
               <div className="max-w-6xl mx-auto space-y-12">
-                <div className="grid grid-cols-2 gap-8 w-full md:grid-cols-3">
+                <div
+                  className={cn(
+                    "grid grid-cols-2 gap-8 w-full",
+                    portfolioCombined ? "md:grid-cols-3" : "md:grid-col-2"
+                  )}
+                >
                   <StatBlock label="Total long (USD)" value={usdFormatter.format(totalLong)} />
                   <StatBlock label="Total short (USD)" value={usdFormatter.format(totalShort)} />
-                  <div className="col-span-2 md:col-span-1">
-                    <StatBlock
-                      label="Active pools"
-                      value={
-                        portfolioCombined && portfolio && portfolio.long.length > 0 && portfolio.short.length ? (
+                  {portfolioCombined && portfolio && portfolio.long.length > 0 && portfolio.short.length && (
+                    <div className="col-span-2 md:col-span-1">
+                      <StatBlock
+                        label="Active pools"
+                        value={
                           <div className="flex items-center gap-4">
                             {groupedNumberFormatterDyn.format(portfolio.long.length + portfolio.short.length)}
                             <ul className="flex items-center -space-x-2">
                               {portfolioCombined.slice(0, 5).map((bank, index) => (
-                                <Image
-                                  src={getTokenImageURL(bank.meta.tokenSymbol)}
-                                  alt={bank.meta.tokenSymbol}
-                                  width={24}
-                                  height={24}
-                                  key={index}
-                                  className="rounded-full ring-1 ring-primary"
-                                />
+                                <li className="rounded-full bg-white">
+                                  <Image
+                                    src={getTokenImageURL(bank.meta.tokenSymbol)}
+                                    alt={bank.meta.tokenSymbol}
+                                    width={24}
+                                    height={24}
+                                    key={index}
+                                    className="rounded-full ring-1 ring-primary"
+                                  />
+                                </li>
                               ))}
                             </ul>
                             {portfolioCombined?.length - 5 > 0 && (
                               <p className="text-sm text-muted-foreground">+{portfolioCombined?.length - 5} more</p>
                             )}
                           </div>
-                        ) : (
-                          <span>0</span>
-                        )
-                      }
-                    />
-                  </div>
+                        }
+                      />
+                    </div>
+                  )}
                 </div>
                 <div className="grid grid-cols-1 gap-12 w-full md:grid-cols-2">
                   <div className="space-y-6">
