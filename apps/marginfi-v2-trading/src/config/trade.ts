aab5f507006fe272eca243237c0071525fc9ae12
chambaz <adammchambers@gmail.com>
2024-07-24 18:13:32 +0300

    fix: force clear caches

--- apps/marginfi-v2-trading/src/config/trade.ts
+++ apps/marginfi-v2-trading/src/config/trade.ts
@@ -1,7 +1,6 @@
-export const TRADE_GROUPS_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json?invalidatecache=72424";
+export const TRADE_GROUPS_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json?invalidatecache=1813";
 export const TOKEN_METADATA_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-token-metadata-cache.json?invalidatecache=72424";
+  "https://storage.googleapis.com/mrgn-public/mfi-trade-token-metadata-cache.json?invalidatecache=1813";
 export const BANK_METADATA_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-bank-metadata-cache.json?invalidatecache=72424";
+  "https://storage.googleapis.com/mrgn-public/mfi-trade-bank-metadata-cache.json?invalidatecache=1813";
 export const POOLS_PER_PAGE = 12;
