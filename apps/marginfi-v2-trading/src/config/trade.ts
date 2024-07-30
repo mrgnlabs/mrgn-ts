7c96542e6318484735d31e63a57d6be36b2df73a
chambaz <adammchambers@gmail.com>
2024-07-30 11:03:13 -0400

    chore: invalidate trade caches

--- apps/marginfi-v2-trading/src/config/trade.ts
+++ apps/marginfi-v2-trading/src/config/trade.ts
@@ -1,6 +1,6 @@
-export const TRADE_GROUPS_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json?invalidatecache=1926";
+export const TRADE_GROUPS_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json?invalidatecache=1103";
 export const TOKEN_METADATA_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-token-metadata-cache.json?invalidatecache=1926";
+  "https://storage.googleapis.com/mrgn-public/mfi-trade-token-metadata-cache.json?invalidatecache=1103";
 export const BANK_METADATA_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-bank-metadata-cache.json?invalidatecache=1926";
+  "https://storage.googleapis.com/mrgn-public/mfi-trade-bank-metadata-cache.json?invalidatecache=1103";
 export const POOLS_PER_PAGE = 12;
