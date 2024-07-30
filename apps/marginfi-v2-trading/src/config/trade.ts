d87aa158b8fe2339b76d177b22308c909d2a3774
chambaz <adammchambers@gmail.com>
2024-07-30 09:45:45 -0400

    chore: revert groups url change

--- apps/marginfi-v2-trading/src/config/trade.ts
+++ apps/marginfi-v2-trading/src/config/trade.ts
@@ -1,5 +1,4 @@
-export const TRADE_GROUPS_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-groups-stage.json?invalidatecache=1926";
+export const TRADE_GROUPS_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json?invalidatecache=1926";
 export const TOKEN_METADATA_MAP =
   "https://storage.googleapis.com/mrgn-public/mfi-trade-token-metadata-cache.json?invalidatecache=1926";
 export const BANK_METADATA_MAP =
