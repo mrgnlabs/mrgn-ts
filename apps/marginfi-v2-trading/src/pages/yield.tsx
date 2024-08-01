1f17db3b8c00cd47440d2243cdcad2b2af44e575
chambaz <adammchambers@gmail.com>
2024-07-31 23:02:45 -0400

    feat: basic yield table (wip)

--- apps/marginfi-v2-trading/src/pages/yield.tsx
+++ apps/marginfi-v2-trading/src/pages/yield.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-import Image from "next/image";
-import Link from "next/link";
+import { aprToApy, numeralFormatter, percentFormatter, usdFormatter } from "@mrgnlabs/mrgn-common";
 
-import { ActiveBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
-import { groupedNumberFormatterDyn, usdFormatter } from "@mrgnlabs/mrgn-common";
+import { IconSortDescending } from "@tabler/icons-react";
 
-import { useTradeStore, useUiStore } from "~/store";
-import { getTokenImageURL, cn } from "~/utils";
+import { useTradeStore } from "~/store";
 
 import { PageHeading } from "~/components/common/PageHeading";
 import { Loader } from "~/components/ui/loader";
+import { Select, SelectTrigger, SelectContent, SelectValue, SelectItem } from "~/components/ui/select";
+import { Input } from "~/components/ui/input";
+import { IconMrgn } from "~/components/ui/icons";
+import Image from "next/image";
+import { getTokenImageURL } from "~/utils";
 
 export default function PortfolioPage() {
   const [initialized, banks, allBanks, collateralBanks, resetActiveGroup] = useTradeStore((state) => [
@@ -33,6 +35,90 @@ export default function PortfolioPage() {
                 body={<p>Nulla veniam tempor duis duis exercitation et ipsum ea consectetur elit mollit.</p>}
                 links={[]}
               />
+            </div>
+
+            <div className="flex items-center gap-4">
+              <div className="w-full">
+                <Input placeholder="Search pools" />
+              </div>
+              <div>
+                <Select>
+                  <SelectTrigger className="w-[190px] justify-start gap-2">
+                    <IconSortDescending size={16} />
+                    <SelectValue placeholder="Filter" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="all">APY</SelectItem>
+                    <SelectItem value="active">Total Deposits</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="w-full">
+              <div className="grid grid-cols-6 gap-4">
+                <div>Pool</div>
+                <div>Total Deposits</div>
+                <div>Total Deposits (USD)</div>
+                <div>APY</div>
+                <div>Created by</div>
+                <div />
+                <div />
+              </div>
+              <div>
+                {banks.map((bank) => {
+                  const collateralBank = collateralBanks[bank.address.toBase58()];
+                  return (
+                    <>
+                      <div className="grid grid-cols-6 gap-4 py-2" key={bank.address.toBase58()}>
+                        <div className="flex items-center gap-2">
+                          <Image
+                            src={getTokenImageURL(bank.info.state.mint.toBase58())}
+                            alt={bank.meta.tokenSymbol}
+                            width={24}
+                            height={24}
+                            className="rounded-full"
+                          />
+                          {bank.meta.tokenSymbol}
+                        </div>
+                        <div>{numeralFormatter(bank.info.state.totalDeposits)}</div>
+                        <div>
+                          {usdFormatter.format(
+                            bank.info.state.totalDeposits * bank.info.oraclePrice.priceRealtime.price.toNumber()
+                          )}
+                        </div>
+                        <div>{percentFormatter.format(aprToApy(bank.info.state.lendingRate))}</div>
+                        <div>
+                          <IconMrgn size={24} />
+                        </div>
+                      </div>
+                      <div className="grid grid-cols-6 gap-4 py-2" key={bank.address.toBase58()}>
+                        <div className="flex items-center gap-2">
+                          <Image
+                            src={getTokenImageURL(collateralBank.info.state.mint.toBase58())}
+                            alt={collateralBank.meta.tokenSymbol}
+                            width={24}
+                            height={24}
+                            className="rounded-full"
+                          />
+                          {collateralBank.meta.tokenSymbol}
+                        </div>
+                        <div>{numeralFormatter(collateralBank.info.state.totalDeposits)}</div>
+                        <div>
+                          {usdFormatter.format(
+                            collateralBank.info.state.totalDeposits *
+                              collateralBank.info.oraclePrice.priceRealtime.price.toNumber()
+                          )}
+                        </div>
+                        <div>{percentFormatter.format(aprToApy(collateralBank.info.state.lendingRate))}</div>
+                        <div>
+                          <IconMrgn size={24} />
+                        </div>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
             </div>
           </div>
         )}
