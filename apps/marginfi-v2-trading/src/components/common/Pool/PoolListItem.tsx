2f2c539f4559079acd530796894cc16a7952e40a
chambaz <adammchambers@gmail.com>
2024-08-01 19:26:30 -0400

    feat: pool list item component

--- apps/marginfi-v2-trading/src/components/common/Pool/PoolListItem.tsx
+++ apps/marginfi-v2-trading/src/components/common/Pool/PoolListItem.tsx
@@ -4,20 +4,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { ExtendedBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
-import { tokenPriceFormatter, percentFormatter, usdFormatter } from "@mrgnlabs/mrgn-common";
+import { tokenPriceFormatter, percentFormatter, usdFormatter, numeralFormatter } from "@mrgnlabs/mrgn-common";
 
 import { useTradeStore } from "~/store";
 import { getTokenImageURL, cn } from "~/utils";
 
 import { Button } from "~/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/components/ui/tooltip";
 
 import type { TokenData } from "~/types";
 
 type PoolListItemProps = {
   bank: ExtendedBankInfo;
+  last?: boolean;
 };
 
-export const PoolListItem = ({ bank }: PoolListItemProps) => {
+export const PoolListItem = ({ bank, last }: PoolListItemProps) => {
   const [collateralBanks] = useTradeStore((state) => [state.collateralBanks]);
   const [tokenData, setTokenData] = React.useState<TokenData | null>(null);
 
@@ -58,7 +60,7 @@ export const PoolListItem = ({ bank }: PoolListItemProps) => {
   }, [bank]);
 
   return (
-    <div className="grid grid-cols-8 py-2">
+    <div className={cn("grid grid-cols-7 py-2 w-full items-center", !last && "border-b pb-3 mb-2")}>
       <div className="flex items-center gap-2">
         <Image
           src={getTokenImageURL(bank.info.state.mint.toBase58())}
@@ -79,7 +81,7 @@ export const PoolListItem = ({ bank }: PoolListItemProps) => {
         )}
       </div>
       <div>
-        {tokenData && usdFormatter.format(tokenData.volume24h)}{" "}
+        {tokenData && `$${numeralFormatter(tokenData.volume24h)}`}{" "}
         {tokenData?.volumeChange24h && (
           <span className={cn("text-xs ml-2", tokenData.volumeChange24h > 0 ? "text-mrgn-success" : "text-mrgn-error")}>
             {tokenData.volumeChange24h > 0 && "+"}
@@ -87,14 +89,38 @@ export const PoolListItem = ({ bank }: PoolListItemProps) => {
           </span>
         )}
       </div>
+      <div>{tokenData && `$${numeralFormatter(tokenData.marketcap)}`}</div>
       <div>{usdFormatter.format(totalLiquidity)}</div>
-      <div></div>
-      <div className="flex items-center gap-2">
-        <Link href={`/trade/${bank.address.toBase58()}?side=long`}>
-          <Button>Long</Button>
+      <div className="pl-5">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="https://x.com/marginfi" target="_blank">
+                <Image
+                  src="https://pbs.twimg.com/profile_images/1791110026456633344/VGViq-CJ_400x400.jpg"
+                  width={20}
+                  height={20}
+                  alt="marginfi"
+                  className="rounded-full"
+                />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Pool created by marginfi</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+      <div className="flex items-center gap-2 justify-end">
+        <Link href={`/trade/${bank.address.toBase58()}?side=long`} className="w-full">
+          <Button variant="long" className="w-full">
+            Long
+          </Button>
         </Link>
-        <Link href={`/trade/${bank.address.toBase58()}?side=short`}>
-          <Button>Short</Button>
+        <Link href={`/trade/${bank.address.toBase58()}?side=short`} className="w-full">
+          <Button variant="short" className="w-full">
+            Short
+          </Button>
         </Link>
       </div>
     </div>
