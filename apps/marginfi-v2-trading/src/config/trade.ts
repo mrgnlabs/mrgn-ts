d0e6183cadeb745fafd785fbd1abf1960ac713e5
chambaz <adammchambers@gmail.com>
2024-08-09 09:59:36 -0400

    chore: switch back to prod cache

--- apps/marginfi-v2-trading/src/config/trade.ts
+++ apps/marginfi-v2-trading/src/config/trade.ts
@@ -2,5 +2,5 @@ export const TRADE_GROUPS_MAP = "https://storage.googleapis.com/mrgn-public/mfi-
 export const TOKEN_METADATA_MAP =
   "https://storage.googleapis.com/mrgn-public/mfi-trade-token-metadata-cache.json?invalidatecache=1103";
 export const BANK_METADATA_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-bank-metadata-cache-test.json?invalidatecache=1103";
+  "https://storage.googleapis.com/mrgn-public/mfi-trade-bank-metadata-cache.json?invalidatecache=1103";
 export const POOLS_PER_PAGE = 12;
