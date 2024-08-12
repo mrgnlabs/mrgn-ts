0d6a7fc019ee3a805cb47ad6f9822cd3e73337ea
chambaz <adammchambers@gmail.com>
2024-08-12 13:42:05 -0400

    feat: add env vars for json caches

--- apps/marginfi-v2-trading/src/config/trade.ts
+++ apps/marginfi-v2-trading/src/config/trade.ts
@@ -1,6 +1,8 @@
-export const TRADE_GROUPS_MAP = "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json?invalidatecache=1103";
+export const TRADE_GROUPS_MAP =
+  process.env.NEXT_PUBLIC_GROUPS_MAP || "https://storage.googleapis.com/mrgn-public/mfi-trade-groups.json";
 export const TOKEN_METADATA_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-token-metadata-cache.json?invalidatecache=1103";
+  process.env.NEXT_PUBLIC_TOKENS_MAP ||
+  "https://storage.googleapis.com/mrgn-public/mfi-trade-token-metadata-cache.json";
 export const BANK_METADATA_MAP =
-  "https://storage.googleapis.com/mrgn-public/mfi-trade-bank-metadata-cache.json?invalidatecache=1103";
+  process.env.NEXT_PUBLIC_BANKS_MAP || "https://storage.googleapis.com/mrgn-public/mfi-trade-bank-metadata-cache.json";
 export const POOLS_PER_PAGE = 12;
