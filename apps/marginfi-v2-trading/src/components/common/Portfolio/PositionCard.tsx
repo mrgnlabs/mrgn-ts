23523fe4b161bd990dd0d8105febb62fb946cd2d
chambaz <adammchambers@gmail.com>
2024-08-12 22:23:39 -0400

    feat: add health factor to position card

--- apps/marginfi-v2-trading/src/components/common/Portfolio/PositionCard.tsx
+++ apps/marginfi-v2-trading/src/components/common/Portfolio/PositionCard.tsx
@@ -3,24 +3,42 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { IconInfoCircle } from "@tabler/icons-react";
 import { numeralFormatter, tokenPriceFormatter, percentFormatter, usdFormatter } from "@mrgnlabs/mrgn-common";
 
 import { getTokenImageURL, cn } from "~/utils";
-import { useTradeStore } from "~/store";
+import { useGroupBanks, useGroupPosition } from "~/hooks/arenaHooks";
 
 import { PositionActionButtons } from "~/components/common/Portfolio";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/components/ui/tooltip";
 
 import type { GroupData } from "~/store/tradeStore";
-import { useGroupBanks, useGroupPosition } from "~/hooks/arenaHooks";
 
 type PositionCardProps = {
   groupData: GroupData;
-  isLong: boolean;
 };
 
-export const PositionCard = ({ groupData, isLong }: PositionCardProps) => {
-  const { borrowBank, depositBank } = useGroupBanks({ group: groupData });
-  const { positionSizeUsd, positionSizeToken, totalUsdValue, leverage } = useGroupPosition({ group: groupData });
+export const PositionCard = ({ groupData }: PositionCardProps) => {
+  const { borrowBank } = useGroupBanks({ group: groupData });
+  const { positionSizeUsd, totalUsdValue, leverage } = useGroupPosition({ group: groupData });
+
+  const healthColor = React.useMemo(() => {
+    if (groupData.accountSummary.healthFactor) {
+      let color: string;
+
+      if (groupData.accountSummary.healthFactor >= 0.5) {
+        color = "#75BA80";
+      } else if (groupData.accountSummary.healthFactor >= 0.25) {
+        color = "#B8B45F";
+      } else {
+        color = "#CF6F6F";
+      }
+
+      return color;
+    } else {
+      return "#fff";
+    }
+  }, [groupData]);
 
   if (!groupData.pool.token.isActive) return null;
 
@@ -64,16 +82,6 @@ export const PositionCard = ({ groupData, isLong }: PositionCardProps) => {
             {groupData.pool.token.info.oraclePrice.priceRealtime.price.toNumber() > 0.00001
               ? tokenPriceFormatter(groupData.pool.token.info.oraclePrice.priceRealtime.price.toNumber())
               : `$${groupData.pool.token.info.oraclePrice.priceRealtime.price.toNumber().toExponential(2)}`}
-            {groupData.pool.token.tokenData && (
-              <span
-                className={cn(
-                  "ml-1",
-                  groupData.pool.token.tokenData.priceChange24hr > 0 ? "text-mrgn-success" : "text-mrgn-error"
-                )}
-              >
-                {percentFormatter.format(groupData.pool.token.tokenData.priceChange24hr / 100)}
-              </span>
-            )}
           </dd>
           {groupData.pool.token.position.liquidationPrice && (
             <>
@@ -83,6 +91,30 @@ export const PositionCard = ({ groupData, isLong }: PositionCardProps) => {
               </dd>
             </>
           )}
+          <dt className="flex items-center gap-0.5">
+            Health Factor{" "}
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <IconInfoCircle size={14} />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>
+                    Health factors indicate how well-collateralized your account is. A value below 0% exposes you to
+                    liquidation.
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </dt>
+          <dd
+            className="text-right"
+            style={{
+              color: healthColor,
+            }}
+          >
+            {percentFormatter.format(groupData.accountSummary.healthFactor)}
+          </dd>
         </dl>
       </div>
       <div className="flex items-center justify-between gap-4">
