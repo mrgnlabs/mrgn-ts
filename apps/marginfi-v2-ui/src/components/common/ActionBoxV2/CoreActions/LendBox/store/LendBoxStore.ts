98fcbc24998e34004a36edef850393a6503f195a
Kobe <leenderskobe@gmail.com>
2024-08-20 17:35:47 +0200

    feat: changed simulation store logic

--- apps/marginfi-v2-ui/src/components/common/ActionBoxV2/CoreActions/LendBox/store/LendBoxStore.ts
+++ apps/marginfi-v2-ui/src/components/common/ActionBoxV2/CoreActions/LendBox/store/LendBoxStore.ts
@@ -2,15 +2,15 @@ import { create, StateCreator } from "zustand";
 
 import { ActionType, ExtendedBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
 import { ActionMethod } from "@mrgnlabs/mrgn-utils";
-import { MarginfiAccountWrapper } from "@mrgnlabs/marginfi-client-v2";
+import { MarginfiAccountWrapper, SimulationResult } from "@mrgnlabs/marginfi-client-v2";
 
-import { ActionSummary } from "../utils";
+import { ActionSummary, getSimulationResult } from "../utils";
 
 interface LendBoxState {
   // State
   amountRaw: string;
   lendMode: ActionType;
-  actionSummary: ActionSummary | null;
+  simulationResult: SimulationResult | null;
   selectedBank: ExtendedBankInfo | null;
   selectedAccount: MarginfiAccountWrapper | null;
   errorMessage: ActionMethod | null;
@@ -33,7 +33,7 @@ function createLendBoxStore() {
 
 const initialState = {
   amountRaw: "",
-  actionSummary: null,
+  simulationResult: null,
   errorMessage: null,
   lendMode: ActionType.Deposit,
   selectedBank: null,
@@ -81,18 +81,33 @@ const stateCreator: StateCreator<LendBoxState, [], []> = (set, get) => ({
     if (needRefresh) set({ ...initialState, lendMode: requestedAction, selectedBank: requestedBank });
   },
 
-  setAmountRaw(amountRaw, maxAmount) {
+  async setAmountRaw(amountRaw, maxAmount) {
     if (!maxAmount) {
       set({ amountRaw });
     } else {
       const strippedAmount = amountRaw.replace(/,/g, "");
-      const amount = isNaN(Number.parseFloat(strippedAmount)) ? 0 : Number.parseFloat(strippedAmount);
+      let amount = isNaN(Number.parseFloat(strippedAmount)) ? 0 : Number.parseFloat(strippedAmount);
       const numberFormatter = new Intl.NumberFormat("en-US", { maximumFractionDigits: 10 });
 
       if (amount && amount > maxAmount) {
-        set({ amountRaw: numberFormatter.format(maxAmount) });
+        amount = maxAmount;
+      }
+
+      set({ amountRaw: numberFormatter.format(amount) });
+
+      const { lendMode, selectedAccount, selectedBank } = get();
+
+      if (selectedAccount && selectedBank && amount !== 0) {
+        const simulationResult = await getSimulationResult({
+          actionMode: lendMode,
+          account: selectedAccount,
+          bank: selectedBank,
+          amount: amount,
+        });
+
+        set({ simulationResult: simulationResult.simulationResult });
       } else {
-        set({ amountRaw: numberFormatter.format(amount) });
+        set({ simulationResult: null });
       }
     }
   },
