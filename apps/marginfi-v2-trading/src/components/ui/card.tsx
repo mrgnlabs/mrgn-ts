1a01a5e8f26173d5e1b9e7958d230d6384496bf3
chambaz <adammchambers@gmail.com>
2024-08-20 23:29:21 -0400

    feat: mobile card padding

--- apps/marginfi-v2-trading/src/components/ui/card.tsx
+++ apps/marginfi-v2-trading/src/components/ui/card.tsx
@@ -34,7 +34,7 @@ Card.displayName = "Card";
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />
+    <div ref={ref} className={cn("flex flex-col space-y-1.5 p-4 md:p-6", className)} {...props} />
   )
 );
 CardHeader.displayName = "CardHeader";
@@ -52,12 +52,16 @@ const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttribu
 CardDescription.displayName = "CardDescription";
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn("p-6 pt-0 font-normal", className)} {...props} />
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("p-4 pt-0 font-normal md:p-6", className)} {...props} />
+  )
 );
 CardContent.displayName = "CardContent";
 
 const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn("flex items-center p-6 pt-0", className)} {...props} />
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("flex items-center p-4 pt-0 md:p-6", className)} {...props} />
+  )
 );
 CardFooter.displayName = "CardFooter";
 
