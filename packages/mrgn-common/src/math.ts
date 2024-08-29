3b72776f5872a6665e3989fd4bf76b0cb0bbd872
man0s <95379755+losman0s@users.noreply.github.com>
2024-08-29 21:36:25 +0800

    feat: make price api endpoint faster

--- packages/mrgn-common/src/math.ts
+++ packages/mrgn-common/src/math.ts
@@ -6,4 +6,23 @@ function ceil(value: number, decimals: number): number {
   return Math.ceil(value * 10 ** decimals) / 10 ** decimals;
 }
 
-export { ceil, floor };
+function median(values: number[]): number {
+
+  if (values.length === 0) {
+    throw new Error('Input array is empty');
+  }
+
+  // Sorting values, preventing original array
+  // from being mutated.
+  values = [...values].sort((a, b) => a - b);
+
+  const half = Math.floor(values.length / 2);
+
+  return (values.length % 2
+    ? values[half]
+    : (values[half - 1] + values[half]) / 2
+  );
+
+}
+
+export { ceil, floor, median };
