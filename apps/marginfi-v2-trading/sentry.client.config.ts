574de3559108127dbfbb0be8030aaee171c3656f
chambaz <adammchambers@gmail.com>
2024-09-11 09:35:43 -0400

    feat: configure sentry

--- apps/marginfi-v2-trading/sentry.client.config.ts
+++ apps/marginfi-v2-trading/sentry.client.config.ts
@@ -3,3 +3,13 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c2875efdbe35bc4db77f24a3339a38c1@o1279738.ingest.us.sentry.io/4507934431248384",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
