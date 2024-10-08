b7ba0a521312f2236cd94e67bd9f42c276cb2401
borcherd <borcherd@me.com>
2024-10-08 09:19:32 +0800

    feat: loading + state improvements

--- apps/marginfi-v2-ui/src/components/common/Portfolio/PortfolioHeader.tsx
+++ apps/marginfi-v2-ui/src/components/common/Portfolio/PortfolioHeader.tsx
@@ -1,23 +1,12 @@
-import React from "react";
-
-import { useWallet } from "~/components/wallet-v2/hooks/use-wallet.hook";
-
-import { WalletButton } from "~/components/wallet-v2";
 import { PageHeading } from "~/components/common/PageHeading";
 
-interface PortfolioHeaderProps {
-  netValue?: string;
-  points?: string;
-}
-
-export const PortfolioHeader = ({ netValue, points }: PortfolioHeaderProps) => {
-  const { connected } = useWallet();
+export const PortfolioHeader = () => {
   return (
     <PageHeading
       heading="Portfolio"
       body={
         <div className="space-y-6">
-          <p>Manage your marginfi positions.</p> {!connected && <WalletButton />}
+          <p>Manage your marginfi positions.</p>
         </div>
       }
     />
