7c23557012b7349e4a72fb6fdc1b28b276063b8f
Kobe <leenderskobe@gmail.com>
2024-10-09 10:43:11 +0200

    fix: crossbar changing timestamp of oracle

--- apps/marginfi-v2-trading/src/pages/api/oracle/price.ts
+++ apps/marginfi-v2-trading/src/pages/api/oracle/price.ts
@@ -31,6 +31,10 @@ interface OracleData {
   maxAge: number;
 }
 
+interface OracleDataWithTimestamp extends OracleData {
+  timestamp: BigNumber;
+}
+
 interface PriceWithConfidenceString {
   price: string;
   confidence: string;
@@ -94,7 +98,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const oracleAis = await chunkedGetRawMultipleAccountInfoOrdered(connection, [
       ...requestedOraclesData.map((oracleData) => oracleData.oracleKey),
     ]);
-    let swbPullOraclesStale: { data: OracleData; feedHash: string }[] = [];
+    let swbPullOraclesStale: { data: OracleDataWithTimestamp; feedHash: string }[] = [];
     for (const index in requestedOraclesData) {
       const oracleData = requestedOraclesData[index];
       const priceDataRaw = oracleAis[index];
@@ -107,7 +111,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       // If on-chain data is recent enough, use it even for SwitchboardPull oracles
       if (oracleData.oracleSetup === OracleSetup.SwitchboardPull && isStale) {
         swbPullOraclesStale.push({
-          data: oracleData,
+          data: { ...oracleData, timestamp: oraclePrice.timestamp },
           feedHash: Buffer.from(decodeSwitchboardPullFeedData(priceDataRaw.data).feed_hash).toString("hex"),
         });
         continue;
@@ -122,15 +126,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const crossbarPrices = await fetchCrossbarPrices(feedHashes);
 
       for (const {
-        data: { oracleKey },
+        data: { oracleKey, timestamp },
         feedHash,
       } of swbPullOraclesStale) {
         const crossbarPrice = crossbarPrices.get(feedHash);
         if (!crossbarPrice) {
           throw new Error(`Crossbar didn't return data for ${feedHash}`);
         }
+        const updatedOraclePrice = { ...crossbarPrice, timestamp } as OraclePrice;
 
-        updatedOraclePrices.set(oracleKey, crossbarPrice);
+        updatedOraclePrices.set(oracleKey, updatedOraclePrice);
       }
     }
 
