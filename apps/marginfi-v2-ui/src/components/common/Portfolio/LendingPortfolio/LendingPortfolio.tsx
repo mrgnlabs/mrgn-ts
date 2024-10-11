c2b545d8c02bab82836a86f6b0e9fcd0975602f6
Borcherd van Brakell <borcherd@me.com>
2024-10-11 11:42:50 +0800

    chore: fix merge conflicts

--- apps/marginfi-v2-ui/src/components/common/Portfolio/LendingPortfolio/LendingPortfolio.tsx
+++ apps/marginfi-v2-ui/src/components/common/Portfolio/LendingPortfolio/LendingPortfolio.tsx
@@ -7,7 +7,7 @@ import { IconInfoCircle } from "@tabler/icons-react";
 import { numeralFormatter } from "@mrgnlabs/mrgn-common";
 import { usdFormatter, usdFormatterDyn } from "@mrgnlabs/mrgn-common";
 import { ActiveBankInfo, ActionType } from "@mrgnlabs/marginfi-v2-ui-state";
-import { LendingModes } from "@mrgnlabs/mrgn-utils";
+import { borrow, LendingModes } from "@mrgnlabs/mrgn-utils";
 
 import { useMrgnlendStore, useUiStore, useUserProfileStore } from "~/store";
 
@@ -139,125 +139,125 @@ export const LendingPortfolio = () => {
         </p>
       );
     }
+  }
 
-    return (
-      <div className="p-4 md:p-6 rounded-xl space-y-3 w-full mb-10  bg-background-gray-dark">
-        <h2 className="font-medium text-xl">Lend/borrow</h2>
-        <div className="text-muted-foreground">
-          <dl className="flex justify-between items-center gap-2">
-            <dt className="flex items-center gap-1.5 text-sm">
-              Lend/borrow health factor
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <IconInfoCircle size={16} />
-                  </TooltipTrigger>
-                  <TooltipContent side="right">
-                    <div className="flex flex-col gap-2 pb-2">
-                      <p>
-                        Health factor is based off <b>price biased</b> and <b>weighted</b> asset and liability values.
-                      </p>
-                      <div className="font-medium">
-                        When your account health reaches 0% or below, you are exposed to liquidation.
-                      </div>
-                      <p>The formula is:</p>
-                      <p className="text-sm italic text-center">{"(assets - liabilities) / (assets)"}</p>
-                      <p>Your math is:</p>
-                      <p className="text-sm italic text-center">{`(${usdFormatter.format(
-                        accountSummary.lendingAmountWithBiasAndWeighted
-                      )} - ${usdFormatter.format(
-                        accountSummary.borrowingAmountWithBiasAndWeighted
-                      )}) / (${usdFormatter.format(accountSummary.lendingAmountWithBiasAndWeighted)})`}</p>
+  return (
+    <div className="p-4 md:p-6 rounded-xl space-y-3 w-full mb-10  bg-background-gray-dark">
+      <h2 className="font-medium text-xl">Lend/borrow</h2>
+      <div className="text-muted-foreground">
+        <dl className="flex justify-between items-center gap-2">
+          <dt className="flex items-center gap-1.5 text-sm">
+            Lend/borrow health factor
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <IconInfoCircle size={16} />
+                </TooltipTrigger>
+                <TooltipContent side="right">
+                  <div className="flex flex-col gap-2 pb-2">
+                    <p>
+                      Health factor is based off <b>price biased</b> and <b>weighted</b> asset and liability values.
+                    </p>
+                    <div className="font-medium">
+                      When your account health reaches 0% or below, you are exposed to liquidation.
                     </div>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </dt>
-            <dd className="text-xl md:text-2xl font-medium" style={{ color: healthColor }}>
-              {numeralFormatter(accountSummary.healthFactor * 100)}%
-            </dd>
-          </dl>
-          <div className="h-2 bg-background-gray-light rounded-full">
-            <div
-              className="h-2 rounded-full"
-              style={{
-                backgroundColor: healthColor,
-                width: `${accountSummary.healthFactor * 100}%`,
-              }}
-            />
-          </div>
-          <PortfolioUserStats
-            supplied={accountSupplied}
-            borrowed={accountBorrowed}
-            netValue={accountNetValue}
-            points={numeralFormatter(userPointsData.totalPoints)}
+                    <p>The formula is:</p>
+                    <p className="text-sm italic text-center">{"(assets - liabilities) / (assets)"}</p>
+                    <p>Your math is:</p>
+                    <p className="text-sm italic text-center">{`(${usdFormatter.format(
+                      accountSummary.lendingAmountWithBiasAndWeighted
+                    )} - ${usdFormatter.format(
+                      accountSummary.borrowingAmountWithBiasAndWeighted
+                    )}) / (${usdFormatter.format(accountSummary.lendingAmountWithBiasAndWeighted)})`}</p>
+                  </div>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </dt>
+          <dd className="text-xl md:text-2xl font-medium" style={{ color: healthColor }}>
+            {numeralFormatter(accountSummary.healthFactor * 100)}%
+          </dd>
+        </dl>
+        <div className="h-2 bg-background-gray-light rounded-full">
+          <div
+            className="h-2 rounded-full"
+            style={{
+              backgroundColor: healthColor,
+              width: `${accountSummary.healthFactor * 100}%`,
+            }}
           />
         </div>
-        <div className="flex flex-col md:flex-row justify-between flex-wrap gap-8 md:gap-20">
-          <div className="flex flex-col flex-1 gap-4 md:min-w-[340px]">
-            <dl className="flex justify-between items-center gap-2 ">
-              <dt className="text-xl font-medium">Supplied</dt>
-              <dt className="text-muted-foreground">{accountSupplied}</dt>
-            </dl>
-            {isStoreInitialized ? (
-              lendingBanks.length > 0 ? (
-                <div className="flex flex-col gap-4">
-                  {lendingBanks.map((bank) => (
-                    <PortfolioAssetCard
-                      key={bank.meta.tokenSymbol}
-                      bank={bank}
-                      isInLendingMode={true}
-                      isBorrower={borrowingBanks.length > 0}
-                    />
-                  ))}
-                </div>
-              ) : (
-                <div color="#868E95" className="font-aeonik font-[300] text-sm flex gap-1">
-                  No lending positions found.
-                </div>
-              )
+        <PortfolioUserStats
+          supplied={accountSupplied}
+          borrowed={accountBorrowed}
+          netValue={accountNetValue}
+          points={numeralFormatter(userPointsData.totalPoints)}
+        />
+      </div>
+      <div className="flex flex-col md:flex-row justify-between flex-wrap gap-8 md:gap-20">
+        <div className="flex flex-col flex-1 gap-4 md:min-w-[340px]">
+          <dl className="flex justify-between items-center gap-2 ">
+            <dt className="text-xl font-medium">Supplied</dt>
+            <dt className="text-muted-foreground">{accountSupplied}</dt>
+          </dl>
+          {isStoreInitialized ? (
+            lendingBanks.length > 0 ? (
+              <div className="flex flex-col gap-4">
+                {lendingBanks.map((bank) => (
+                  <PortfolioAssetCard
+                    key={bank.meta.tokenSymbol}
+                    bank={bank}
+                    isInLendingMode={true}
+                    isBorrower={borrowingBanks.length > 0}
+                  />
+                ))}
+              </div>
             ) : (
-              <PortfolioAssetCardSkeleton />
-            )}
-          </div>
-          <div className="flex flex-col flex-1 gap-4 md:min-w-[340px]">
-            <dl className="flex justify-between items-center gap-2">
-              <dt className="text-xl font-medium">Borrowed</dt>
-              <dt className="text-muted-foreground">{accountBorrowed}</dt>
-            </dl>
-            {isStoreInitialized ? (
-              borrowingBanks.length > 0 ? (
-                <div className="flex flex-col gap-4">
-                  {borrowingBanks.map((bank) => (
-                    <PortfolioAssetCard
-                      key={bank.meta.tokenSymbol}
-                      bank={bank}
-                      isInLendingMode={false}
-                      isBorrower={borrowingBanks.length > 0}
-                    />
-                  ))}
-                </div>
-              ) : (
-                <div color="#868E95" className="font-aeonik font-[300] text-sm flex gap-1">
-                  No borrow positions found.{" "}
-                  <button
-                    className="border-b border-primary/50 transition-colors hover:border-primary"
-                    onClick={() => {
-                      setLendingMode(LendingModes.BORROW);
-                      router.push("/");
-                    }}
-                  >
-                    Search the pools
-                  </button>{" "}
-                  and open a new borrow.
-                </div>
-              )
+              <div color="#868E95" className="font-aeonik font-[300] text-sm flex gap-1">
+                No lending positions found.
+              </div>
+            )
+          ) : (
+            <PortfolioAssetCardSkeleton />
+          )}
+        </div>
+        <div className="flex flex-col flex-1 gap-4 md:min-w-[340px]">
+          <dl className="flex justify-between items-center gap-2">
+            <dt className="text-xl font-medium">Borrowed</dt>
+            <dt className="text-muted-foreground">{accountBorrowed}</dt>
+          </dl>
+          {isStoreInitialized ? (
+            borrowingBanks.length > 0 ? (
+              <div className="flex flex-col gap-4">
+                {borrowingBanks.map((bank) => (
+                  <PortfolioAssetCard
+                    key={bank.meta.tokenSymbol}
+                    bank={bank}
+                    isInLendingMode={false}
+                    isBorrower={borrowingBanks.length > 0}
+                  />
+                ))}
+              </div>
             ) : (
-              <PortfolioAssetCardSkeleton />
-            )}
-          </div>
+              <div color="#868E95" className="font-aeonik font-[300] text-sm flex gap-1">
+                No borrow positions found.{" "}
+                <button
+                  className="border-b border-primary/50 transition-colors hover:border-primary"
+                  onClick={() => {
+                    setLendingMode(LendingModes.BORROW);
+                    router.push("/");
+                  }}
+                >
+                  Search the pools
+                </button>{" "}
+                and open a new borrow.
+              </div>
+            )
+          ) : (
+            <PortfolioAssetCardSkeleton />
+          )}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
