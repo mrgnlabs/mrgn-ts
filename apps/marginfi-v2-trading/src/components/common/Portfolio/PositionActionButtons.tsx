fef54507479985ef7c1b8fabee584dc91e7d99f1
chambaz <adammchambers@gmail.com>
2024-10-17 17:52:02 -0400

    chore: integrate mrgn-ui actionbox for add / reduce (wip)

--- apps/marginfi-v2-trading/src/components/common/Portfolio/PositionActionButtons.tsx
+++ apps/marginfi-v2-trading/src/components/common/Portfolio/PositionActionButtons.tsx
@@ -15,6 +15,7 @@ import { useWallet } from "~/components/wallet-v2/hooks/use-wallet.hook";
 import { calculateClosePositions } from "~/utils";
 
 import { ActionBoxDialog } from "~/components/common/ActionBox";
+import { ActionBox, ActionBoxProvider } from "~/components/action-box-v2";
 import { Button } from "~/components/ui/button";
 import {
   Dialog,
@@ -39,7 +40,7 @@ export const PositionActionButtons = ({
   activeGroup,
 }: PositionActionButtonsProps) => {
   const { connection } = useConnection();
-  const { wallet } = useWallet();
+  const { wallet, connected } = useWallet();
   const [platformFeeBps] = useUiStore((state) => [state.platformFeeBps]);
   const [actionTransaction, setActionTransaction] = React.useState<{
     closeTxn: VersionedTransaction | Transaction;
@@ -50,9 +51,11 @@ export const PositionActionButtons = ({
   const [multiStepToast, setMultiStepToast] = React.useState<MultiStepToastHandle | null>(null);
   const [isClosing, setIsClosing] = React.useState(false);
 
-  const [refreshGroup, setIsRefreshingStore] = useTradeStore((state) => [
+  const [fetchTradeState, refreshGroup, setIsRefreshingStore, nativeSolBalance] = useTradeStore((state) => [
+    state.fetchTradeState,
     state.refreshGroup,
     state.setIsRefreshingStore,
+    state.nativeSolBalance,
   ]);
   const [slippageBps, priorityFee, setIsActionComplete, setPreviousTxn] = useUiStore((state) => [
     state.slippageBps,
@@ -218,176 +221,268 @@ export const PositionActionButtons = ({
   }, [multiStepToast, setActionTransaction, setMultiStepToast]);
 
   return (
-    <div className="flex gap-3 w-full">
-      <ActionBoxDialog
-        requestedBank={depositBanks[0]}
-        requestedAction={ActionType.Deposit}
-        requestedAccount={activeGroup.selectedAccount ?? undefined}
-        activeGroupArg={activeGroup}
-      >
-        <Button
-          variant="outline"
-          size="sm"
-          className="gap-1 min-w-16"
-          onClick={() => {
-            capture("position_add_btn_click", {
-              group: activeGroup?.groupPk?.toBase58(),
-              token: activeGroup.pool.token.meta.tokenSymbol,
-            });
-          }}
-        >
-          <IconPlus size={14} />
-          Add
-        </Button>
-      </ActionBoxDialog>
-      {borrowBank && isBorrowing && (
+    <ActionBoxProvider
+      banks={[activeGroup.pool.token, activeGroup.pool.quoteTokens[0]]}
+      nativeSolBalance={nativeSolBalance}
+      marginfiClient={activeGroup.client}
+      selectedAccount={activeGroup.selectedAccount}
+      connected={connected}
+      accountSummaryArg={activeGroup.accountSummary}
+    >
+      <div className="flex gap-3 w-full">
         <ActionBoxDialog
-          requestedBank={borrowBank}
-          requestedAction={ActionType.Repay}
-          requestedAccount={activeGroup.selectedAccount ?? undefined}
-          activeGroupArg={activeGroup}
-        >
-          <Button
-            variant="outline"
-            size="sm"
-            className="gap-1 min-w-16"
-            onClick={() => {
-              capture("position_reduce_btn_click", {
-                group: activeGroup?.groupPk?.toBase58(),
-                token: activeGroup.pool.token.meta.tokenSymbol,
-              });
-            }}
-          >
-            <IconMinus size={14} />
-            Reduce
-          </Button>
-        </ActionBoxDialog>
-      )}
-      {!isBorrowing && (
-        <ActionBoxDialog
-          activeGroupArg={activeGroup}
           requestedBank={depositBanks[0]}
-          requestedAction={ActionType.Withdraw}
+          requestedAction={ActionType.Deposit}
           requestedAccount={activeGroup.selectedAccount ?? undefined}
-          requestedCollateralBank={depositBanks.length > 1 ? depositBanks[1] : undefined}
+          activeGroupArg={activeGroup}
         >
           <Button
             variant="outline"
             size="sm"
             className="gap-1 min-w-16"
             onClick={() => {
-              capture("position_withdraw_btn_click", {
+              capture("position_add_btn_click", {
                 group: activeGroup?.groupPk?.toBase58(),
                 token: activeGroup.pool.token.meta.tokenSymbol,
               });
             }}
           >
-            <IconMinus size={14} />
-            Withdraw
+            <IconPlus size={14} />
+            Add (old)
           </Button>
         </ActionBoxDialog>
-      )}
-      <Button
-        onClick={() => closeTransaction()}
-        disabled={isClosing}
-        variant="destructive"
-        size="sm"
-        className={cn("gap-1 min-w-16", rightAlignFinalButton && "ml-auto")}
-      >
-        <IconX size={14} />
-        Close
-      </Button>
-
-      <Dialog open={!!actionTransaction} onOpenChange={() => onClose()}>
-        <DialogContent className="space-y-12 w-full">
-          <DialogHeader>
-            <DialogTitle className="flex flex-col items-center gap-2 border-b border-border pb-10">
-              <span className="flex items-center justify-center gap-2">
-                {activeGroup.pool.token && (
-                  <Image
-                    className="rounded-full w-9 h-9"
-                    src={activeGroup.pool.token.meta.tokenLogoUri}
-                    alt={(activeGroup.pool.token?.meta.tokenSymbol || "Token") + "  logo"}
-                    width={36}
-                    height={36}
-                  />
-                )}
-                <span className="text-4xl font-medium">
-                  {`${activeGroup.pool.token.meta.tokenSymbol}/${activeGroup.pool.quoteTokens[0].meta.tokenSymbol}`}
-                </span>
-              </span>
-            </DialogTitle>
-            <DialogDescription className="sr-only">
-              {`${activeGroup.pool.token.meta.tokenSymbol}/${activeGroup.pool.quoteTokens[0].meta.tokenSymbol}`}
-            </DialogDescription>
-          </DialogHeader>
-          <dl className="grid grid-cols-2 w-full text-muted-foreground gap-x-8 gap-y-2">
-            {depositBanks.map((bank) => (
-              <React.Fragment key={bank.meta.tokenSymbol}>
-                <dt>Supplied</dt>
-                <dd className="text-right">
-                  {bank.position.amount} {bank.meta.tokenSymbol}
-                </dd>
-              </React.Fragment>
-            ))}
-
-            {borrowBank && (
-              <>
-                <dt>Borrowed</dt>
-                <dd className="text-right">
-                  {borrowBank.position.amount} {borrowBank.meta.tokenSymbol}
-                </dd>
-              </>
-            )}
-
-            {actionTransaction?.quote?.priceImpactPct && (
-              <>
-                <dt>Price impact</dt>
-                <dd
-                  className={cn(
-                    Number(actionTransaction.quote.priceImpactPct) > 0.05
-                      ? "text-mrgn-error"
-                      : Number(actionTransaction.quote.priceImpactPct) > 0.01
-                      ? "text-alert-foreground"
-                      : "text-mrgn-success",
-                    "text-right"
-                  )}
-                >
-                  {percentFormatter.format(Number(actionTransaction.quote.priceImpactPct))}
-                </dd>
-              </>
-            )}
-
-            {actionTransaction?.quote?.slippageBps && (
-              <>
-                <dt>Slippage</dt>
-                <dd className={cn(actionTransaction.quote.slippageBps > 500 && "text-alert-foreground", "text-right")}>
-                  {percentFormatter.format(Number(actionTransaction.quote.slippageBps) / 10000)}
-                </dd>
-              </>
-            )}
-
-            <dt>Platform fee</dt>
-            {actionTransaction?.quote?.platformFee?.feeBps && (
-              <>
-                <dd className="text-right">
-                  {percentFormatter.format(actionTransaction.quote.platformFee.feeBps / 10000)}
-                </dd>
-              </>
-            )}
-          </dl>
-          <DialogFooter>
+        <ActionBox.Lend
+          isDialog={true}
+          useProvider={true}
+          lendProps={{
+            connected: connected,
+            requestedLendType: ActionType.Deposit,
+            requestedBank: activeGroup.pool.token ?? undefined,
+            captureEvent: () => {
+              capture("position_add_btn_click", {
+                group: activeGroup?.groupPk?.toBase58(),
+                token: activeGroup.pool.token.meta.tokenSymbol,
+              });
+            },
+            onComplete: () => {
+              fetchTradeState({
+                connection,
+                wallet,
+              });
+            },
+          }}
+          dialogProps={{
+            trigger: (
+              <Button
+                variant="outline"
+                size="sm"
+                className="gap-1 min-w-16"
+                onClick={() => {
+                  capture("position_add_btn_click", {
+                    group: activeGroup?.groupPk?.toBase58(),
+                    token: activeGroup.pool.token.meta.tokenSymbol,
+                  });
+                }}
+              >
+                <IconPlus size={14} />
+                Add
+              </Button>
+            ),
+            title: `Supply ${activeGroup.pool.token.meta.tokenSymbol}`,
+          }}
+        />
+        {borrowBank && isBorrowing && (
+          <ActionBoxDialog
+            requestedBank={borrowBank}
+            requestedAction={ActionType.Repay}
+            requestedAccount={activeGroup.selectedAccount ?? undefined}
+            activeGroupArg={activeGroup}
+          >
             <Button
-              variant="destructive"
-              disabled={isLoading}
-              className="w-full mx-auto"
-              onClick={() => processTransaction()}
+              variant="outline"
+              size="sm"
+              className="gap-1 min-w-16"
+              onClick={() => {
+                capture("position_reduce_btn_click", {
+                  group: activeGroup?.groupPk?.toBase58(),
+                  token: activeGroup.pool.token.meta.tokenSymbol,
+                });
+              }}
             >
-              {isLoading ? <IconLoader2 className="animate-spin" /> : "Confirm close position"}
+              <IconMinus size={14} />
+              Reduce (old)
             </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
+          </ActionBoxDialog>
+        )}
+        {borrowBank && isBorrowing && (
+          <ActionBox.Repay
+            useProvider={true}
+            repayProps={{
+              requestedBank: borrowBank,
+              connected: connected,
+              captureEvent: (event, properties) => {
+                capture("position_reduce_btn_click", {
+                  group: activeGroup?.groupPk?.toBase58(),
+                  token: activeGroup.pool.token.meta.tokenSymbol,
+                });
+              },
+              onComplete: () => {
+                fetchTradeState({
+                  connection,
+                  wallet,
+                });
+              },
+            }}
+            isDialog={true}
+            dialogProps={{
+              trigger: (
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="gap-1 min-w-16"
+                  onClick={() => {
+                    capture("position_reduce_btn_click", {
+                      group: activeGroup?.groupPk?.toBase58(),
+                      token: activeGroup.pool.token.meta.tokenSymbol,
+                    });
+                  }}
+                >
+                  <IconMinus size={14} />
+                  Reduce
+                </Button>
+              ),
+              title: `Reduce ${borrowBank?.meta.tokenSymbol}`,
+            }}
+          />
+        )}
+        {!isBorrowing && (
+          <ActionBoxDialog
+            activeGroupArg={activeGroup}
+            requestedBank={depositBanks[0]}
+            requestedAction={ActionType.Withdraw}
+            requestedAccount={activeGroup.selectedAccount ?? undefined}
+            requestedCollateralBank={depositBanks.length > 1 ? depositBanks[1] : undefined}
+          >
+            <Button
+              variant="outline"
+              size="sm"
+              className="gap-1 min-w-16"
+              onClick={() => {
+                capture("position_withdraw_btn_click", {
+                  group: activeGroup?.groupPk?.toBase58(),
+                  token: activeGroup.pool.token.meta.tokenSymbol,
+                });
+              }}
+            >
+              <IconMinus size={14} />
+              Withdraw
+            </Button>
+          </ActionBoxDialog>
+        )}
+        <Button
+          onClick={() => closeTransaction()}
+          disabled={isClosing}
+          variant="destructive"
+          size="sm"
+          className={cn("gap-1 min-w-16", rightAlignFinalButton && "ml-auto")}
+        >
+          <IconX size={14} />
+          Close
+        </Button>
+
+        <Dialog open={!!actionTransaction} onOpenChange={() => onClose()}>
+          <DialogContent className="space-y-12 w-full">
+            <DialogHeader>
+              <DialogTitle className="flex flex-col items-center gap-2 border-b border-border pb-10">
+                <span className="flex items-center justify-center gap-2">
+                  {activeGroup.pool.token && (
+                    <Image
+                      className="rounded-full w-9 h-9"
+                      src={activeGroup.pool.token.meta.tokenLogoUri}
+                      alt={(activeGroup.pool.token?.meta.tokenSymbol || "Token") + "  logo"}
+                      width={36}
+                      height={36}
+                    />
+                  )}
+                  <span className="text-4xl font-medium">
+                    {`${activeGroup.pool.token.meta.tokenSymbol}/${activeGroup.pool.quoteTokens[0].meta.tokenSymbol}`}
+                  </span>
+                </span>
+              </DialogTitle>
+              <DialogDescription className="sr-only">
+                {`${activeGroup.pool.token.meta.tokenSymbol}/${activeGroup.pool.quoteTokens[0].meta.tokenSymbol}`}
+              </DialogDescription>
+            </DialogHeader>
+            <dl className="grid grid-cols-2 w-full text-muted-foreground gap-x-8 gap-y-2">
+              {depositBanks.map((bank) => (
+                <React.Fragment key={bank.meta.tokenSymbol}>
+                  <dt>Supplied</dt>
+                  <dd className="text-right">
+                    {bank.position.amount} {bank.meta.tokenSymbol}
+                  </dd>
+                </React.Fragment>
+              ))}
+
+              {borrowBank && (
+                <>
+                  <dt>Borrowed</dt>
+                  <dd className="text-right">
+                    {borrowBank.position.amount} {borrowBank.meta.tokenSymbol}
+                  </dd>
+                </>
+              )}
+
+              {actionTransaction?.quote?.priceImpactPct && (
+                <>
+                  <dt>Price impact</dt>
+                  <dd
+                    className={cn(
+                      Number(actionTransaction.quote.priceImpactPct) > 0.05
+                        ? "text-mrgn-error"
+                        : Number(actionTransaction.quote.priceImpactPct) > 0.01
+                        ? "text-alert-foreground"
+                        : "text-mrgn-success",
+                      "text-right"
+                    )}
+                  >
+                    {percentFormatter.format(Number(actionTransaction.quote.priceImpactPct))}
+                  </dd>
+                </>
+              )}
+
+              {actionTransaction?.quote?.slippageBps && (
+                <>
+                  <dt>Slippage</dt>
+                  <dd
+                    className={cn(actionTransaction.quote.slippageBps > 500 && "text-alert-foreground", "text-right")}
+                  >
+                    {percentFormatter.format(Number(actionTransaction.quote.slippageBps) / 10000)}
+                  </dd>
+                </>
+              )}
+
+              <dt>Platform fee</dt>
+              {actionTransaction?.quote?.platformFee?.feeBps && (
+                <>
+                  <dd className="text-right">
+                    {percentFormatter.format(actionTransaction.quote.platformFee.feeBps / 10000)}
+                  </dd>
+                </>
+              )}
+            </dl>
+            <DialogFooter>
+              <Button
+                variant="destructive"
+                disabled={isLoading}
+                className="w-full mx-auto"
+                onClick={() => processTransaction()}
+              >
+                {isLoading ? <IconLoader2 className="animate-spin" /> : "Confirm close position"}
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </ActionBoxProvider>
   );
 };
