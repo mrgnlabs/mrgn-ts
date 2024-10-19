4b1d1614b8fa4b136a6e23a7b70c6641a01d7f97
chambaz <adammchambers@gmail.com>
2024-10-19 19:55:19 -0400

    chore: disable sentry defaults

--- apps/marginfi-v2-ui/sentry.edge.config.ts
+++ apps/marginfi-v2-ui/sentry.edge.config.ts
@@ -2,6 +2,10 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://5dd81f7047c001781ac8a520d8355f11@o1279738.ingest.us.sentry.io/4505861944573952",
-  tracesSampleRate: 1,
   debug: false,
+  autoSessionTracking: false,
+  integrations: [],
+  defaultIntegrations: false,
+  enableTracing: false,
+  tracesSampleRate: 0,
 });
