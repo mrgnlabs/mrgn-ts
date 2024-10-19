e2927a5a110d71700b049fedf0e2ac10d8aebbfd
chambaz <adammchambers@gmail.com>
2024-10-19 19:55:19 -0400

    chore: disable sentry defaults

--- apps/marginfi-v2-trading/sentry.server.config.ts
+++ apps/marginfi-v2-trading/sentry.server.config.ts
@@ -6,10 +6,10 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://c2875efdbe35bc4db77f24a3339a38c1@o1279738.ingest.us.sentry.io/4507934431248384",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  autoSessionTracking: false,
+  integrations: [],
+  defaultIntegrations: false,
+  enableTracing: false,
+  tracesSampleRate: 0,
 });
