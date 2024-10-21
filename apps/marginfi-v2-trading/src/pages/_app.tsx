3be46bd9ca6e836838002b67a5f205e91c2bd19b
chambaz <adammchambers@gmail.com>
2024-10-21 10:36:14 -0400

    chore: add hotjar

--- apps/marginfi-v2-trading/src/pages/_app.tsx
+++ apps/marginfi-v2-trading/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import App, { AppContext, AppInitialProps, AppProps } from "next/app";
+import Script from "next/script";
 import { useRouter } from "next/router";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -88,6 +89,19 @@ export default function MrgnApp({ Component, pageProps, path, bank }: AppProps &
         <>
           <GoogleAnalytics gaId="G-T5B2WRLKL9" />
           <GoogleTagManager gtmId="GTM-WFBC4RZ7" />
+          <Script
+            id="hotjar-script"
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5178229,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          />
           <SpeedInsights />
         </>
       )}
--- apps/marginfi-v2-ui/src/pages/_app.tsx
+++ apps/marginfi-v2-ui/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-
 import App, { AppContext, AppInitialProps, AppProps } from "next/app";
+import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import dynamic from "next/dynamic";
@@ -170,6 +170,19 @@ export default function MrgnApp({ Component, pageProps, path }: AppProps & MrgnA
         <>
           <GoogleAnalytics gaId="G-0ZTQRWVG02" />
           <GoogleTagManager gtmId="GTM-KJJ3CR6Q" />
+          <Script
+            id="hotjar-script"
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5178229,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          />
           <SpeedInsights />
         </>
       )}
