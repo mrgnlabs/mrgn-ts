e2a4bb44b47a9002a4c6ef54ae5fb8e184de59d5
Kobe <leenderskobe@gmail.com>
2024-10-28 21:00:04 +0100

    feat: normal, high, mama prio fees

--- packages/mrgn-utils/src/priority.utils.ts
+++ packages/mrgn-utils/src/priority.utils.ts
@@ -1,5 +1,6 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { getRecentPrioritizationFeesByPercentile, PriotitizationFeeLevels } from "./grpf";
+import { TransactionPriorityType } from "./types";
 
 export const DEFAULT_BUNDLE_TIP_LAMPORTS = 0.0001; // 100_000 lamports = 0.0001 SOL
 export const DEFAULT_BUNDLE_TIP_UPPER_BOUND = 0.003; // 1_000_000 lamports = 0.003 SOL
@@ -14,16 +15,6 @@ export interface TipFloorDataResponse {
   ema_landed_tips_50th_percentile: number;
 }
 
-export const handleBundleTip = (tipFloorData: TipFloorDataResponse) => {
-  const { ema_landed_tips_50th_percentile, landed_tips_50th_percentile, landed_tips_25th_percentile } = tipFloorData;
-
-  if (ema_landed_tips_50th_percentile > DEFAULT_BUNDLE_TIP_UPPER_BOUND) {
-    return { isCongested: true, bundleTip: DEFAULT_BUNDLE_TIP_UPPER_BOUND };
-  } else {
-    return { isCongested: false, bundleTip: ema_landed_tips_50th_percentile };
-  }
-};
-
 export const uiToMicroLamports = (ui: number, limitCU: number = 1_400_000) => {
   const priorityFeeMicroLamports = ui * LAMPORTS_PER_SOL * 1_000_000;
   const microLamports = Math.round(priorityFeeMicroLamports / limitCU);
@@ -36,23 +27,7 @@ export const microLamportsToUi = (microLamports: number, limitCU: number = 1_400
   return priorityFeeUi;
 };
 
-export const getRpcPriorityFee = async (connection: Connection) => {
-  const result = await getRecentPrioritizationFeesByPercentile(
-    connection,
-    {
-      lockedWritableAccounts: [], // TODO: investigate this
-      percentile: PriotitizationFeeLevels.LOW,
-      fallback: false,
-    },
-    10
-  );
-
-  const uiResult = result.map((r) => microLamportsToUi(r.prioritizationFee));
-
-  return uiResult[0];
-};
-
-const fetchBundleTip = async () => {
+export const getBundleTip = async (priorityType: TransactionPriorityType) => {
   const response = await fetch("/api/bundles/tip", {
     method: "GET",
     headers: {
@@ -66,7 +41,60 @@ const fetchBundleTip = async () => {
 
   const bundleTipData: TipFloorDataResponse = await response.json();
 
-  const { isCongested, bundleTip } = handleBundleTip(bundleTipData);
+  const { ema_landed_tips_50th_percentile, landed_tips_50th_percentile, landed_tips_75th_percentile } = bundleTipData;
 
-  return bundleTip;
+  let priorityFee = 0;
+
+  if (priorityType === "HIGH") {
+    priorityFee =
+      ema_landed_tips_50th_percentile > landed_tips_50th_percentile
+        ? ema_landed_tips_50th_percentile
+        : landed_tips_50th_percentile;
+  } else if (priorityType === "MAMAS") {
+    priorityFee = landed_tips_75th_percentile;
+  } else {
+    priorityFee =
+      ema_landed_tips_50th_percentile > landed_tips_50th_percentile
+        ? landed_tips_50th_percentile
+        : ema_landed_tips_50th_percentile;
+  }
+
+  return priorityFee;
+};
+
+export const getRpcPriorityFeeMicroLamports = async (connection: Connection, priorityType: TransactionPriorityType) => {
+  const recentPrioritizationFees = await getRecentPrioritizationFeesByPercentile(
+    connection,
+    {
+      lockedWritableAccounts: [], // TODO: investigate this
+      percentile: PriotitizationFeeLevels.HIGH,
+      fallback: false,
+    },
+    20
+  );
+
+  const { min, max, sum } = recentPrioritizationFees.reduce(
+    (acc, current) => {
+      return {
+        min: Math.min(acc.min, current.prioritizationFee),
+        max: Math.max(acc.max, current.prioritizationFee),
+        sum: acc.sum + current.prioritizationFee,
+      };
+    },
+    { min: recentPrioritizationFees[0].prioritizationFee, max: recentPrioritizationFees[0].prioritizationFee, sum: 0 }
+  );
+
+  const average = sum / recentPrioritizationFees.length;
+
+  let priorityFee = 0;
+
+  if (priorityType === "HIGH") {
+    priorityFee = average;
+  } else if (priorityType === "MAMAS") {
+    priorityFee = max;
+  } else {
+    priorityFee = min;
+  }
+
+  return priorityFee;
 };
