4ba9da5e1b7529bee85485db1427fc3826a62b88
chambaz <adammchambers@gmail.com>
2024-11-02 14:04:38 +0000

    chore: remove tracking scripts, now managed through gtm

--- apps/marginfi-landing-page/src/app/layout.tsx
+++ apps/marginfi-landing-page/src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Header } from "~/components/header";
@@ -27,19 +26,6 @@ export default function RootLayout({
           <>
             <GoogleAnalytics gaId="G-TS0DF3DM7G" />
             <GoogleTagManager gtmId="GTM-53CZ2GG8" />
-            <Script
-              id="hotjar-script"
-              dangerouslySetInnerHTML={{
-                __html: `(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5178229,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-              }}
-            />
             <SpeedInsights />
           </>
         )}
