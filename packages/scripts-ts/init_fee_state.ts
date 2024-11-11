1108130e5567a285c1117040748d6814ebe687b1
jgur-psyops <jonathan@mrgn.group>
2024-11-11 11:57:30 -0500

    base58 version of init script

--- packages/scripts-ts/init_fee_state.ts
+++ packages/scripts-ts/init_fee_state.ts
@@ -4,16 +4,26 @@ import marginfiIdl from "../marginfi-client-v2/src/idl/marginfi.json";
 import { bigNumberToWrappedI80F48, WrappedI80F48 } from "@mrgnlabs/mrgn-common";
 import { AnchorProvider, Program } from "@coral-xyz/anchor";
 import { loadKeypairFromFile } from "./utils";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import { Keypair } from "@solana/web3.js";
+
+/**
+ * If true, send the tx. IF false, output the unsigned b58 tx to console.
+ */
+const sendTx = false;
 
 type Config = {
   PROGRAM_ID: string;
   ADMIN_PUBKEY: PublicKey;
   WALLET_PUBKEY: PublicKey;
+  /// Required if using multisig only
+  MULTISIG_PAYER: PublicKey;
 };
 const config: Config = {
   PROGRAM_ID: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
   ADMIN_PUBKEY: new PublicKey("H4QMTHMVbJ3KrB5bz573cBBZKoYSZ2B4mSST1JKzPUrH"),
   WALLET_PUBKEY: new PublicKey("H4QMTHMVbJ3KrB5bz573cBBZKoYSZ2B4mSST1JKzPUrH"),
+  MULTISIG_PAYER: new PublicKey("3HGdGLrnK9DsnHi1mCrUMLGfQHcu6xUrXhMY14GYjqvM"),
 };
 
 type InitGlobalFeeStateArgs = {
@@ -40,6 +50,14 @@ const initGlobalFeeState = (program: Program<Marginfi>, args: InitGlobalFeeState
 };
 
 async function main() {
+  if (sendTx) {
+    await createAndSendTx();
+  } else {
+    await echoBase58Tx();
+  }
+}
+
+async function createAndSendTx() {
   marginfiIdl.address = config.PROGRAM_ID;
   const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
   const wallet = loadKeypairFromFile(process.env.HOME + "/.config/solana/id.json");
@@ -62,6 +80,7 @@ async function main() {
     marginfiIdl as Marginfi,
     provider
   );
+
   const transaction = new Transaction().add(await initGlobalFeeState(program, args));
 
   try {
@@ -70,6 +89,40 @@ async function main() {
   } catch (error) {
     console.error("Transaction failed:", error);
   }
+}
+
+async function echoBase58Tx() {
+  marginfiIdl.address = config.PROGRAM_ID;
+  const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+  const args: InitGlobalFeeStateArgs = {
+    payer: config.MULTISIG_PAYER,
+    admin: config.ADMIN_PUBKEY,
+    wallet: config.WALLET_PUBKEY,
+    bankInitFlatSolFee: 5000,
+    programFeeFixed: bigNumberToWrappedI80F48(0),
+    programFeeRate: bigNumberToWrappedI80F48(0.01),
+  };
+
+  let dummyWallet = Keypair.generate();
+  
+  // @ts-ignore
+  const provider = new AnchorProvider(connection, dummyWallet);
+  const program: Program<Marginfi> = new Program(
+    // @ts-ignore
+    marginfiIdl as Marginfi,
+    provider
+  );
+
+  const transaction = new Transaction().add(await initGlobalFeeState(program, args));
+  transaction.feePayer = config.MULTISIG_PAYER; // Set the fee payer to Squads wallet
+  const { blockhash } = await connection.getLatestBlockhash();
+  transaction.recentBlockhash = blockhash;
+  const serializedTransaction = transaction.serialize({
+    requireAllSignatures: false,
+    verifySignatures: false,
+  });
+  const base58Transaction = bs58.encode(serializedTransaction);
+  console.log("Base58-encoded transaction:", base58Transaction);
 }
 
 main().catch((err) => {
