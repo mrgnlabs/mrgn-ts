e17887f53b0579e87a785b4d0a786f825d6bf079
Kobe <leenderskobe@gmail.com>
2024-11-12 09:49:42 +0100

    fix: implemented fallbacks to oracle prices

--- apps/marginfi-v2-ui/src/pages/api/oracle/price.ts
+++ apps/marginfi-v2-ui/src/pages/api/oracle/price.ts
@@ -18,6 +18,7 @@ import {
   FeedResponse,
 } from "@mrgnlabs/marginfi-client-v2/dist/vendor";
 import { chunkedGetRawMultipleAccountInfoOrdered, median, Wallet } from "@mrgnlabs/mrgn-common";
+import { push } from "@socialgouv/matomo-next";
 import { Connection, PublicKey } from "@solana/web3.js";
 import BigNumber from "bignumber.js";
 import { NextApiRequest, NextApiResponse } from "next";
@@ -88,11 +89,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       Object.entries(feedIdMapRaw).map(([key, value]) => [key, new PublicKey(value)])
     );
 
-    const requestedOraclesData = banksMap.map((b) => ({
-      oracleKey: findOracleKey(BankConfig.fromAccountParsed(b.data.config), feedIdMap).toBase58(),
-      oracleSetup: parseOracleSetup(b.data.config.oracleSetup),
-      maxAge: b.data.config.oracleMaxAge,
-    }));
+    const mintMap = new Map<string, PublicKey>();
+
+    const requestedOraclesData = banksMap.map((b) => {
+      const oracleKey = findOracleKey(BankConfig.fromAccountParsed(b.data.config), feedIdMap).toBase58();
+      mintMap.set(oracleKey, b.data.mint);
+
+      return {
+        oracleKey,
+        oracleSetup: parseOracleSetup(b.data.config.oracleSetup),
+        maxAge: b.data.config.oracleMaxAge,
+      };
+    });
 
     // Fetch on-chain data for all oracles
     const oracleAis = await chunkedGetRawMultipleAccountInfoOrdered(connection, [
@@ -102,14 +110,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     for (const index in requestedOraclesData) {
       const oracleData = requestedOraclesData[index];
       const priceDataRaw = oracleAis[index];
-      const oraclePrice = parsePriceInfo(oracleData.oracleSetup, priceDataRaw.data);
+      let oraclePrice = parsePriceInfo(oracleData.oracleSetup, priceDataRaw.data);
+
+      if (oraclePrice.priceRealtime.price.isNaN()) {
+        oraclePrice.priceRealtime.price = new BigNumber(0);
+      }
 
       const currentTime = Math.round(Date.now() / 1000);
       const oracleTime = oraclePrice.timestamp.toNumber();
       const isStale = currentTime - oracleTime > oracleData.maxAge;
 
       // If on-chain data is recent enough, use it even for SwitchboardPull oracles
-      if (oracleData.oracleSetup === OracleSetup.SwitchboardPull && isStale) {
+      if (oracleData.oracleSetup === OracleSetup.SwitchboardPull && !isStale) {
         swbPullOraclesStale.push({
           data: { ...oracleData, timestamp: oraclePrice.timestamp },
           feedHash: Buffer.from(decodeSwitchboardPullFeedData(priceDataRaw.data).feed_hash).toString("hex"),
@@ -123,7 +135,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (swbPullOraclesStale.length > 0) {
       // Batch-fetch and cache price data from Crossbar for stale SwitchboardPull oracles
       const feedHashes = swbPullOraclesStale.map((value) => value.feedHash);
-      const crossbarPrices = await fetchCrossbarPrices(feedHashes);
+      const crossbarPrices = await handleFetchCrossbarPrices(feedHashes, mintMap);
 
       for (const {
         data: { oracleKey, timestamp },
@@ -133,8 +145,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if (!crossbarPrice) {
           throw new Error(`Crossbar didn't return data for ${feedHash}`);
         }
-        const updatedOraclePrice = { ...crossbarPrice, timestamp } as OraclePrice;
-
+        let updatedOraclePrice = { ...crossbarPrice, timestamp } as OraclePrice;
         updatedOraclePrices.set(oracleKey, updatedOraclePrice);
       }
     }
@@ -149,16 +160,125 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 }
 
-async function fetchCrossbarPrices(feedHashes: string[]): Promise<Map<string, OraclePrice>> {
+async function handleFetchCrossbarPrices(
+  feedHashes: string[],
+  mintMap: Map<string, PublicKey>
+): Promise<Map<string, OraclePrice>> {
+  try {
+    // main crossbar
+    const payload: CrossbarSimulatePayload = [];
+
+    const { payload: mainPayload, brokenFeeds: mainBrokenFeeds } = await fetchCrossbarPrices(
+      feedHashes,
+      SWITCHBOARD_CROSSSBAR_API
+    );
+    payload.push(...mainPayload);
+
+    if (!mainBrokenFeeds.length) {
+      return crossbarPayloadToOraclePricePerFeedHash(payload);
+    }
+
+    if (process.env.SWITCHBOARD_CROSSSBAR_API_FALLBACK) {
+      // fallback crossbar
+      const { payload: fallbackPayload, brokenFeeds: fallbackBrokenFeeds } = await fetchCrossbarPrices(
+        mainBrokenFeeds,
+        process.env.SWITCHBOARD_CROSSSBAR_API_FALLBACK
+      );
+
+      payload.push(...fallbackPayload);
+
+      if (!fallbackBrokenFeeds.length) {
+        return crossbarPayloadToOraclePricePerFeedHash(payload);
+      }
+    }
+
+    // birdeye as last resort
+    const birdeyePayload = await fetchBirdeyePrices(feedHashes, mintMap);
+
+    console.log("birdeyePayload", birdeyePayload);
+
+    payload.push(...birdeyePayload);
+
+    return crossbarPayloadToOraclePricePerFeedHash(payload);
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Couldn't fetch from crossbar");
+  }
+}
+
+interface BirdeyeTokenPrice {
+  value: number;
+  updateUnixTime: number;
+  updateHumanTime: string;
+  priceChange24h: number;
+}
+
+interface BirdeyePriceResponse {
+  success: boolean;
+  data: {
+    [tokenAddress: string]: BirdeyeTokenPrice;
+  };
+}
+
+async function fetchBirdeyePrices(
+  feedHashes: string[],
+  mintMap: Map<string, PublicKey>
+): Promise<CrossbarSimulatePayload> {
+  // TODO: implement
+  try {
+    const tokens = feedHashes.map((feedHash) => mintMap.get(feedHash)!.toBase58());
+
+    const response = await fetch(`/api/birdeye/multiPrice?tokens=${tokens.join(",")}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch oracle prices");
+    }
+
+    const responseBody = (await response.json()) as BirdeyePriceResponse;
+
+    const priceData = responseBody.data;
+
+    const finalPayload: CrossbarSimulatePayload = feedHashes.map((feedHash) => {
+      const tokenAddress = mintMap.get(feedHash)!.toBase58();
+      const price = priceData[tokenAddress];
+      return {
+        feedHash,
+        results: [price.value],
+      };
+    });
+
+    return finalPayload;
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Couldn't fetch from birdeye");
+  }
+}
+
+async function fetchCrossbarPrices(
+  feedHashes: string[],
+  endpoint: string,
+  username?: string,
+  bearer?: string
+): Promise<{ payload: CrossbarSimulatePayload; brokenFeeds: string[] }> {
   const controller = new AbortController();
   const timeoutId = setTimeout(() => {
     controller.abort();
   }, 8000);
 
+  const isAuth = username && bearer;
+
+  const basicAuth = isAuth ? Buffer.from(`${username}:${bearer}`).toString("base64") : undefined;
+
   try {
     const feedHashesString = feedHashes.join(",");
-    const response = await fetch(`${SWITCHBOARD_CROSSSBAR_API}/simulate/${feedHashesString}`, {
+    const response = await fetch(`${endpoint}/simulate/${feedHashesString}`, {
       headers: {
+        Authorization: basicAuth ? `Basic ${basicAuth}` : "",
         Accept: "application/json",
       },
       signal: controller.signal,
@@ -169,12 +289,15 @@ async function fetchCrossbarPrices(feedHashes: string[]): Promise<Map<string, Or
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
+
     const payload = (await response.json()) as CrossbarSimulatePayload;
 
-    return crossbarPayloadToOraclePricePerFeedHash(payload);
+    const brokenFeeds = payload.filter((feed) => feed.results[0] === null).map((feed) => feed.feedHash);
+
+    return { payload, brokenFeeds };
   } catch (error) {
     console.error("Error:", error);
-    throw new Error("Couldn't fetch from crossbar");
+    return { payload: [], brokenFeeds: feedHashes };
   }
 }
 
