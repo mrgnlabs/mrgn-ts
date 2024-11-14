c89fb3563f8582815101bfa0567bc0c4666ea1aa
borcherd <borcherd@me.com>
2024-11-14 16:15:30 +0700

    chore: timeouts & error handling

--- apps/marginfi-v2-ui/src/pages/api/bundles/sendBundle.ts
+++ apps/marginfi-v2-ui/src/pages/api/bundles/sendBundle.ts
@@ -27,15 +27,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const grpcClient = searcherClient(JITO_ENDPOINT);
-    let isLeaderSlot = false;
-    // timeout after 10 seconds and return an error
-    while (!isLeaderSlot) {
-      const next_leader = await grpcClient.getNextScheduledLeader();
-      const num_slots = next_leader.nextLeaderSlot - next_leader.currentSlot;
-      isLeaderSlot = num_slots <= 2;
-      console.log(`next jito leader slot in ${num_slots} slots`);
-      await new Promise((r) => setTimeout(r, 500));
-    }
+
+    await waitForLeaderSlot(grpcClient);
 
     const txs = transactions.map((tx) => VersionedTransaction.deserialize(bs58.decode(tx)));
 
@@ -47,26 +40,53 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     try {
       let whileLoop = true;
+      let timedOut = false;
 
-      // timeout in while loop 15sec
+      setTimeout(() => {
+        timedOut = true;
+      }, 15000);
+
       while (whileLoop) {
+        if (timedOut) {
+          throw new Error("Timeout: Stopped after 15 seconds of trying to get bundle result.");
+        }
+
         const grpcClient = searcherClient(JITO_ENDPOINT);
-        // details: 'bundle contains an already processed transaction',
-        const bundleId = await grpcClient.sendBundle(b);
+
+        let bundleId = "";
+        try {
+          bundleId = await grpcClient.sendBundle(b);
+        } catch (error) {
+          console.error(error);
+          // @ts-ignore
+          if (error.details.includes("already processed transaction")) {
+            console.log("bundle already processed, sending res");
+            res.status(200).json({ bundleId });
+            break;
+          } else {
+            throw error;
+          }
+        }
 
         console.log("bundleId:", bundleId);
 
         try {
           const bundleResult = await getBundleResult(grpcClient);
           res.status(200).json({ bundleId: bundleResult });
-          whileLoop = false;
+          break;
         } catch (error) {
-          // if timeout error continue
-          console.error("error getting bundle result:", error);
+          if (error instanceof BundleError && error.code === "timeout") {
+            console.log("Timeout error in getBundleResult; retrying...");
+          } else {
+            console.error("error getting bundle result:", error);
+            throw error;
+          }
         }
 
         await sleep(500);
       }
+
+      console.log("while ended without sending res");
     } catch (e) {
       console.error("error sending bundle:", e);
       return res.status(500).json({ error: "Error sending bundle" });
@@ -75,6 +95,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     console.error("Error:", error);
     return res.status(500).json({ error: "Error processing transactions" });
   }
+}
+
+async function waitForLeaderSlot(grpcClient: SearcherClient): Promise<void> {
+  let isLeaderSlot = false;
+  let timedOut = false;
+
+  const timeout = setTimeout(() => {
+    timedOut = true;
+  }, 25000);
+
+  while (!isLeaderSlot) {
+    if (timedOut) {
+      clearTimeout(timeout);
+      throw new Error("Timeout: No leader slot found within 25 seconds.");
+    }
+
+    const next_leader = await grpcClient.getNextScheduledLeader();
+    const num_slots = next_leader.nextLeaderSlot - next_leader.currentSlot;
+    isLeaderSlot = num_slots <= 2;
+    console.log(`next jito leader slot in ${num_slots} slots`);
+
+    await new Promise((r) => setTimeout(r, 500));
+  }
+
+  clearTimeout(timeout);
 }
 
 export function getBundleResult(grpcClient: SearcherClient) {
@@ -86,23 +131,23 @@ export function getBundleResult(grpcClient: SearcherClient) {
       if (bundleResult.accepted || bundleResult.finalized || bundleResult.processed) {
         resolve(bundleResult.bundleId);
       } else if (bundleResult.rejected) {
-        reject(new Error("Bundle rejected by the block-engine."));
+        reject(new BundleError("Bundle rejected by the block-engine.", "rejected"));
       } else if (bundleResult.dropped) {
-        reject(new Error("Bundle was accepted but never landed on-chain."));
+        reject(new BundleError("Bundle was accepted but never landed on-chain.", "dropped"));
       } else {
-        reject(new Error("Unknown error sending bundle"));
+        reject(new BundleError("Unknown error sending bundle", "unknown"));
       }
     };
 
     const errorCallback = (error: Error) => {
       if (!error.message.includes("CANCELLED")) {
-        reject(error);
+        reject(new BundleError(error.message, "errorCallback"));
       }
     };
 
     const timeout = setTimeout(() => {
       reset();
-      reject(new Error("Timeout: No bundle result received within 10 seconds."));
+      reject(new BundleError("Timeout: No bundle result received within 10 seconds.", "timeout"));
     }, 3000);
 
     reset = grpcClient.onBundleResult(
@@ -116,4 +161,14 @@ export function getBundleResult(grpcClient: SearcherClient) {
       }
     );
   });
+}
+
+class BundleError extends Error {
+  code: string;
+
+  constructor(message: string, code: string) {
+    super(message);
+    this.code = code;
+    this.name = "BundleError";
+  }
 }
