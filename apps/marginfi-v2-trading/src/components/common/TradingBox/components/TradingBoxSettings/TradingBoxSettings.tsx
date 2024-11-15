13c184063841a1c7c296809a1cfd977c950f2e57
chambaz <adammchambers@gmail.com>
2024-11-15 15:09:56 -0500

    fix: remove tradebox settings view enum, only slippage needed now

--- apps/marginfi-v2-trading/src/components/common/TradingBox/components/TradingBoxSettings/TradingBoxSettings.tsx
+++ apps/marginfi-v2-trading/src/components/common/TradingBox/components/TradingBoxSettings/TradingBoxSettings.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { IconArrowLeft } from "@tabler/icons-react";
 
-import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group";
 import { Slippage } from "./components";
 
 type TradingBoxSettingsProps = {
@@ -11,40 +10,14 @@ type TradingBoxSettingsProps = {
   setSlippageBps: (value: number) => void;
 };
 
-enum SettingsState {
-  Slippage = "slippage",
-  PriorityFee = "priority-fee",
-}
-
 export const TradingBoxSettings = ({ toggleSettings, slippageBps, setSlippageBps }: TradingBoxSettingsProps) => {
-  const [settingsMode, setSettingsMode] = React.useState<SettingsState>(SettingsState.PriorityFee);
-
-  const toggleSettingsMode = (value: SettingsState | "") => {
-    if (value !== "") {
-      setSettingsMode(value);
-    }
-  };
-
   return (
     <div className="space-y-6">
       <button className="flex items-center gap-1.5 text-sm" onClick={() => toggleSettings(false)}>
         <IconArrowLeft size={18} /> Back to trading
       </button>
-      {/* <div className="space-y-3">
-        <ToggleGroup value={settingsMode} onValueChange={toggleSettingsMode} type="single" size="lg" className="gap-4">
-          <ToggleGroupItem value="slippage" className="w-1/2 text-xs border">
-            Slippage
-          </ToggleGroupItem>
-
-          <ToggleGroupItem value="priority-fee" className="w-1/2 text-xs gap-1.5 border">
-            Priority Fee
-          </ToggleGroupItem>
-        </ToggleGroup>
-      </div> */}
       <div>
-        {settingsMode === SettingsState.Slippage && (
-          <Slippage toggleSettings={toggleSettings} slippageBps={slippageBps} setSlippageBps={setSlippageBps} />
-        )}
+        <Slippage toggleSettings={toggleSettings} slippageBps={slippageBps} setSlippageBps={setSlippageBps} />
       </div>
     </div>
   );
