bfce7f252f1a23935ce12c5c57a059782bc75c51
chambaz <adammchambers@gmail.com>
2024-11-22 08:06:55 -0500

    chore: remove timestamp from proxy generation

--- packages/mrgn-utils/src/rpc.utils.ts
+++ packages/mrgn-utils/src/rpc.utils.ts
@@ -1,9 +1,5 @@
 export function generateEndpoint(endpoint: string, rpcProxyKey: string = "") {
-  const now = new Date();
-  const midnight = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1));
-  const timestamp = Math.floor(midnight.getTime() / 1000);
-  const key = `${endpoint}-${timestamp}-${rpcProxyKey}`;
-  const hash = Buffer.from(key, "utf8").toString("base64").replace(/[/+=]/g, "");
-
+  if (!rpcProxyKey) return endpoint;
+  const hash = Buffer.from(rpcProxyKey, "utf8").toString("base64").replace(/[/+=]/g, "");
   return `${endpoint}/${hash}`;
 }
