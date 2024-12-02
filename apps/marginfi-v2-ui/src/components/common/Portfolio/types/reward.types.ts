601cb99742a06f28dcf57c1027e83335af375af9
borcherd <borcherd@me.com>
2024-12-02 12:12:37 +0400

    feat: initial work

--- apps/marginfi-v2-ui/src/components/common/Portfolio/types/reward.types.ts
+++ apps/marginfi-v2-ui/src/components/common/Portfolio/types/reward.types.ts
@@ -1,4 +1,8 @@
 export type rewardsType = {
-  totalReward: number;
-  rewards: { bank: string; amount: string }[];
+  state: "NOT_FETCHED" | "NO_REWARDS" | "REWARDS_FETCHED" | "ERROR";
+  tooltipContent: string;
+  rewards: {
+    totalReward: number;
+    rewards: { bank: string; amount: string }[];
+  };
 };
