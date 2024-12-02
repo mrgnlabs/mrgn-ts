13e9720cf58bb9e8ddbb0a60237ba2877adeeb53
borcherd <borcherd@me.com>
2024-12-02 15:01:39 +0400

    chore: cleanup callback deps

--- packages/mrgn-ui/src/components/wallet-v2/components/wallet-auth-accounts/wallet-auth-accounts.tsx
+++ packages/mrgn-ui/src/components/wallet-v2/components/wallet-auth-accounts/wallet-auth-accounts.tsx
@@ -253,7 +253,7 @@ export const WalletAuthAccounts = ({
     useAuthTxn,
     fetchAccountLabels,
     activateAccount,
-    marginfiAccounts.length,
+    marginfiAccounts,
   ]);
 
   React.useEffect(() => {
