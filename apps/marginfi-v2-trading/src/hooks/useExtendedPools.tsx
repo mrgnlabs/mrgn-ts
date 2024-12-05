064fe6bbea99ac5b80e437b21fdf1b863f8ddad1
k0beleenders <leenderskobe@gmail.com>
2024-12-05 18:25:36 +0100

    feat: store integration yield page & mrgnclient helper functions (wip)

---
+++ apps/marginfi-v2-trading/src/hooks/useExtendedPools.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useTradeStoreV2 } from "~/store";
+import { ArenaBank, ArenaPoolV2, ArenaPoolV2Extended, GroupStatus } from "~/store/tradeStoreV2";
+
+export function useExtendedPools() {
+  const [arenaPools, banksByBankPk] = useTradeStoreV2((state) => [state.arenaPools, state.banksByBankPk]);
+
+  const extendedPools = React.useMemo(
+    () =>
+      Object.values(arenaPools).map((pool) => {
+        const tokenBank = banksByBankPk[pool.tokenBankPk.toBase58()];
+        const quoteBank = banksByBankPk[pool.quoteBankPk.toBase58()];
+
+        return {
+          groupPk: pool.groupPk,
+          status: getPoolPositionStatus(pool, tokenBank, quoteBank),
+          tokenBank: tokenBank,
+          quoteBank: quoteBank,
+        };
+      }) as ArenaPoolV2Extended[],
+    [arenaPools, banksByBankPk]
+  );
+
+  return extendedPools;
+}
+
+function getPoolPositionStatus(pool: ArenaPoolV2, tokenBank: ArenaBank, quoteBank: ArenaBank): GroupStatus {
+  let isLpPosition = true;
+  let hasAnyPosition = false;
+  let isLendingInAny = false;
+  let isLong = false;
+  let isShort = false;
+
+  if (tokenBank.isActive && tokenBank.position) {
+    hasAnyPosition = true;
+    if (tokenBank.position.isLending) {
+      isLendingInAny = true;
+    } else if (tokenBank.position.usdValue > 0) {
+      isShort = true;
+      isLpPosition = false;
+    }
+  }
+
+  if (quoteBank.isActive && quoteBank.position) {
+    hasAnyPosition = true;
+    if (quoteBank.position.isLending) {
+      isLendingInAny = true;
+    } else if (quoteBank.position.usdValue > 0) {
+      if (tokenBank.isActive && tokenBank.position && tokenBank.position.isLending) {
+        isLong = true;
+      }
+      isLpPosition = false;
+    }
+  }
+
+  let status = GroupStatus.EMPTY;
+
+  if (hasAnyPosition) {
+    if (isLpPosition && isLendingInAny) {
+      status = GroupStatus.LP;
+    } else if (isLong) {
+      status = GroupStatus.LONG;
+    } else if (isShort) {
+      status = GroupStatus.SHORT;
+    }
+  }
+
+  return status;
+}
