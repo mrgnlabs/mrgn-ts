6f607eeb0eaa9a6a42ba44e799e2ee11a33a9307
chambaz <adammchambers@gmail.com>
2024-12-07 08:57:42 -0500

    fix: default to dialog version token dropdown on ios pwa

--- packages/mrgn-ui/src/components/action-box-v2/components/action-input/bank-list-command/bank-list-command.tsx
+++ packages/mrgn-ui/src/components/action-box-v2/components/action-input/bank-list-command/bank-list-command.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FocusOn } from "react-focus-on";
 import { IconX } from "@tabler/icons-react";
 import { ExtendedBankInfo } from "@mrgnlabs/marginfi-v2-ui-state";
 
@@ -17,28 +16,26 @@ export const BankListCommand = ({ selectedBank, onSetSearchQuery, onClose, child
   const isMobile = useIsMobile();
 
   return (
-    // Using FocusOn to handle focus of command input but only on desktop
-    <FocusOn onEscapeKey={onClose} onClickOutside={onClose} autoFocus={!isMobile}>
-      <Command
-        className="bg-mfi-action-box-background relative"
-        shouldFilter={false}
-        value={selectedBank?.address?.toString().toLowerCase() ?? ""}
+    <Command
+      className="bg-mfi-action-box-background relative"
+      shouldFilter={false}
+      value={selectedBank?.address?.toString().toLowerCase() ?? ""}
+    >
+      <CommandInput
+        placeholder="Search token..."
+        wrapperClassName="fixed mx-2 lg:mx-0 bg-mfi-action-box-background w-[calc(100%-30px)] px-4 lg:pl-3 border rounded-lg border-border z-40 flex justify-between"
+        className="h-12"
+        autoFocus={true}
+        onValueChange={(value) => onSetSearchQuery(value)}
+      />
+      <button
+        onClick={onClose}
+        className={cn("fixed z-50", isMobile ? "top-9 right-4" : "top-8 right-6")}
+        aria-label="Close"
       >
-        <CommandInput
-          placeholder="Search token..."
-          wrapperClassName="fixed mx-2 lg:mx-0 bg-mfi-action-box-background w-[calc(100%-30px)] px-4 lg:pl-3 border rounded-lg border-border z-40 flex justify-between"
-          className="h-12"
-          onValueChange={(value) => onSetSearchQuery(value)}
-        />
-        <button
-          onClick={onClose}
-          className={cn("fixed z-50", isMobile ? "top-9 right-4" : "top-8 right-6")}
-          aria-label="Close"
-        >
-          <IconX size={18} className="opacity-50" />
-        </button>
-        <CommandList className="overflow-auto mt-[60px]">{children}</CommandList>
-      </Command>
-    </FocusOn>
+        <IconX size={18} className="opacity-50" />
+      </button>
+      <CommandList className="overflow-auto mt-[60px]">{children}</CommandList>
+    </Command>
   );
 };
