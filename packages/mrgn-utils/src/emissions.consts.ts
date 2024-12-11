55997b58a8b8fdf301632ae619c979a4856f6eaa
chambaz <adammchambers@gmail.com>
2024-12-11 13:53:26 -0500

    chore: add usds emissions metadata

--- packages/mrgn-utils/src/emissions.consts.ts
+++ packages/mrgn-utils/src/emissions.consts.ts
@@ -1,9 +1,10 @@
 export const EMISSION_MINT_INFO_MAP = new Map<string, { tokenSymbol: string; tokenLogoUri: string }>([
-  // [
-  //   "bSOL",
-  //   {
-  //     tokenSymbol: "BLZE",
-  //     tokenLogoUri: "/blze.png",
-  //   },
-  // ],
+  [
+    "USDS",
+    {
+      tokenSymbol: "USDS",
+      tokenLogoUri:
+        "https://storage.googleapis.com/mrgn-public/mrgn-token-icons/USDSwr9ApdHk5bvJKMjzff41FfuX8bSxdKcR81vTwcA.png",
+    },
+  ],
 ]);
