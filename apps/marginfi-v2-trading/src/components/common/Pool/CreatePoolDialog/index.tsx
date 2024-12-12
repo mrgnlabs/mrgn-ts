0f21521ab906d6cb55b916bde09245f6c27d9013
Kobe <leenderskobe@gmail.com>
2024-12-12 15:50:56 +0100

    feat: updated permissionless pools flow

--- apps/marginfi-v2-trading/src/components/common/Pool/CreatePoolDialog/index.tsx
+++ apps/marginfi-v2-trading/src/components/common/Pool/CreatePoolDialog/index.tsx
@@ -1,5 +1,5 @@
 export * from "./CreatePoolDialog";
-export * from "./components/CreatePoolMint";
+export * from "./components/CreatePoolToken";
 export * from "./components/CreatePoolForm";
 export * from "./components/CreatePoolSuccess";
 export * from "./types";
