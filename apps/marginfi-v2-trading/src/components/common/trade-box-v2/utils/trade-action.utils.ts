740a3df1fe2519fa0f9ababc18bb23343df44575
borcherd <borcherd@me.com>
2024-12-12 10:32:17 +0100

    feat: tx execution

---
+++ apps/marginfi-v2-trading/src/components/common/trade-box-v2/utils/trade-action.utils.ts
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from "uuid";
+import {
+  ActionMessageType,
+  calculateLoopingParams,
+  CalculateLoopingProps,
+  executeLoopingAction,
+  LoopActionTxns,
+  ExecuteLoopingActionProps,
+  IndividualFlowError,
+} from "@mrgnlabs/mrgn-utils";
+
+import { ExecuteActionsCallbackProps } from "~/components/action-box-v2/types";
+
+interface ExecuteTradeActionsProps extends ExecuteActionsCallbackProps {
+  props: ExecuteLoopingActionProps;
+}
+
+export const handleExecuteTradeAction = async ({
+  props,
+  captureEvent,
+  setIsLoading,
+  setIsComplete,
+  setError,
+}: ExecuteTradeActionsProps) => {
+  try {
+    setIsLoading(true);
+    const attemptUuid = uuidv4();
+    captureEvent(`user_trade_initiate`, {
+      uuid: attemptUuid,
+      tokenSymbol: props.borrowBank.meta.tokenSymbol,
+      tokenName: props.borrowBank.meta.tokenName,
+      amount: props.depositAmount,
+      priorityFee: props.processOpts?.priorityFeeMicro ?? 0,
+    });
+
+    const txnSig = await executeLoopingAction(props);
+
+    setIsLoading(false);
+
+    if (txnSig) {
+      setIsComplete(Array.isArray(txnSig) ? txnSig : [txnSig]);
+      captureEvent(`user_trade`, {
+        uuid: attemptUuid,
+        tokenSymbol: props.borrowBank.meta.tokenSymbol,
+        tokenName: props.borrowBank.meta.tokenName,
+        amount: props.depositAmount,
+        txn: txnSig!,
+        priorityFee: props.processOpts?.priorityFeeMicro ?? 0,
+      });
+    }
+  } catch (error) {
+    setError(error as IndividualFlowError);
+  }
+};
