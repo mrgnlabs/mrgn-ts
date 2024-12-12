98ecbb9a1b25a87f0f385cd65411d74c6388a5c9
borcherd <borcherd@me.com>
2024-12-12 10:11:15 +0100

    feat: various ui fixes & simulation finetuning

--- apps/marginfi-v2-trading/src/components/common/trade-box-v2/components/leverage-slider/leverage-slider.tsx
+++ apps/marginfi-v2-trading/src/components/common/trade-box-v2/components/leverage-slider/leverage-slider.tsx
@@ -1,31 +1,74 @@
-import { Label } from "~/components/ui/label";
+import React from "react";
+
+import { cn } from "@mrgnlabs/mrgn-utils";
 import { Slider } from "~/components/ui/slider";
+import { ArenaBank } from "~/store/tradeStoreV2";
 
-interface LeverageSliderProps {
-  leverage: number;
+type LeverageSliderProps = {
+  selectedBank: ArenaBank | null;
+  selectedSecondaryBank: ArenaBank | null;
+  amountRaw: string;
+  leverageAmount: number;
   maxLeverage: number;
-  setLeverage: (value: number) => void;
-}
+  setLeverageAmount: (amount: number) => void;
+};
 
-export const LeverageSlider = ({ leverage, maxLeverage, setLeverage }: LeverageSliderProps) => {
+export const LeverageSlider = ({
+  selectedBank,
+  selectedSecondaryBank,
+  amountRaw,
+  leverageAmount,
+  maxLeverage,
+  setLeverageAmount,
+}: LeverageSliderProps) => {
+  const bothBanksSelected = React.useMemo(
+    () => Boolean(selectedBank && selectedSecondaryBank),
+    [selectedBank, selectedSecondaryBank]
+  );
+
   return (
-    <div className="space-y-2">
-      <div className="flex justify-between">
-        <Label>Leverage</Label>
-        <span className="text-sm font-medium text-muted-foreground">{leverage.toFixed(2)}x</span>
+    <>
+      <div
+        className={cn(
+          "space-y-6",
+          (!bothBanksSelected || !amountRaw) && "pointer-events-none cursor-default opacity-50"
+        )}
+      >
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <p className="text-sm font-normal text-muted-foreground">Leverage</p>
+          </div>
+          <Slider
+            defaultValue={[1]}
+            max={maxLeverage === 0 ? 1 : maxLeverage}
+            min={1}
+            step={0.01}
+            value={[leverageAmount]}
+            onValueChange={(value) => {
+              if (value[0] > maxLeverage || value[0] <= 1) return;
+              setLeverageAmount(value[0]);
+            }}
+            disabled={!bothBanksSelected || !amountRaw}
+          />
+          <div className="flex items-center justify-between">
+            <p className="text-sm font-normal text-muted-foreground">
+              {leverageAmount > 1 && `${leverageAmount.toFixed(2)}x leverage`}
+            </p>
+            <span className="flex items-center gap-1">
+              <span className="text-muted-foreground text-sm">
+                {maxLeverage.toFixed(2)}x
+                <button
+                  disabled={!!!maxLeverage}
+                  className="ml-1 text-xs cursor-pointer text-chartreuse border-b border-transparent transition hover:border-chartreuse"
+                  onClick={() => setLeverageAmount(Number(maxLeverage))}
+                >
+                  MAX
+                </button>
+              </span>
+            </span>
+          </div>
+        </div>
       </div>
-      <Slider
-        className="w-full"
-        defaultValue={[1]}
-        min={1}
-        step={0.01}
-        max={maxLeverage === 0 ? 1 : maxLeverage}
-        value={[leverage]}
-        onValueChange={(value) => {
-          if (value[0] > maxLeverage) return;
-          setLeverage(value[0]);
-        }}
-      />
-    </div>
+    </>
   );
 };
