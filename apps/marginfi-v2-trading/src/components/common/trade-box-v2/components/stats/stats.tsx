05f8c753ccce407789b353b535ff57954ba9bdec
borcherd <borcherd@me.com>
2024-12-13 17:03:40 +0100

    feat: update stats

--- apps/marginfi-v2-trading/src/components/common/trade-box-v2/components/stats/stats.tsx
+++ apps/marginfi-v2-trading/src/components/common/trade-box-v2/components/stats/stats.tsx
@@ -28,7 +28,18 @@ export const Stats = ({ activePool, accountSummary, simulationResult, actionTxns
       {stats && (
         <dl className={cn("grid grid-cols-2 gap-y-2 pt-6 text-xs")}>
           {stats.map((stat, idx) => (
-            <ActionStatItem key={idx} label={stat.label}>
+            <ActionStatItem
+              key={idx}
+              label={stat.label}
+              classNames={cn(
+                stat.color &&
+                  (stat.color === "SUCCESS"
+                    ? "text-success"
+                    : stat.color === "ALERT"
+                    ? "text-alert-foreground"
+                    : "text-destructive-foreground")
+              )}
+            >
               <stat.value />
             </ActionStatItem>
           ))}
