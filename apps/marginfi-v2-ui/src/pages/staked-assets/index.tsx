1becee580954b408683947c193d46f026be9a2cf
chambaz <adammchambers@gmail.com>
2024-12-13 16:22:34 -0500

    feat: add actionbox and placeholder staked assets page layout

--- apps/marginfi-v2-ui/src/pages/staked-assets/index.tsx
+++ apps/marginfi-v2-ui/src/pages/staked-assets/index.tsx
@@ -1,16 +1,49 @@
 import Link from "next/link";
 
+import { LendingModes } from "@mrgnlabs/mrgn-utils";
+import { ActionType } from "@mrgnlabs/marginfi-v2-ui-state";
+import { ActionBox } from "@mrgnlabs/mrgn-ui";
+
+import { useMrgnlendStore, useUiStore } from "~/store";
+import { useWallet } from "~/components/wallet-v2/hooks/use-wallet.hook";
+
 import { PageHeading } from "~/components/common/PageHeading";
 
 import { Button } from "~/components/ui/button";
 
 export default function StakedAssetsPage() {
+  const { walletContextState, connected } = useWallet();
+  const [lendingMode] = useUiStore((state) => [state.lendingMode]);
+
+  const [fetchMrgnlendState] = useMrgnlendStore((state) => [state.fetchMrgnlendState]);
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center px-4">
       <PageHeading heading="Staked Asset Banks" body={<p>Deposit your native stake and use it as collateral.</p>} />
-      <Button asChild>
-        <Link href="/staked-assets/create">Create Staked Asset Bank</Link>
-      </Button>
+
+      <ActionBox.Lend
+        useProvider={true}
+        lendProps={{
+          requestedLendType: lendingMode === LendingModes.LEND ? ActionType.Deposit : ActionType.Borrow,
+          connected: connected,
+          walletContextState: walletContextState,
+          captureEvent: (event, properties) => {
+            // capture(event, properties);
+          },
+          onComplete: () => {
+            fetchMrgnlendState();
+          },
+        }}
+      />
+
+      <div className="flex flex-col justify-center items-center mt-16 gap-4">
+        <p className="text-sm text-center text-muted-foreground leading-relaxed">
+          Don&apos;t see your native stake?
+          <br className="hiddensm:block" /> Create a new Staked Asset bank for your validator.
+        </p>
+        <Link href="/staked-assets/create">
+          <Button variant="secondary">Create Staked Asset Bank</Button>
+        </Link>
+      </div>
     </div>
   );
 }
