9ff31982bb054a673fe00db530bf392588242cdf
chambaz <adammchambers@gmail.com>
2024-12-13 22:38:43 -0500

    feat: form validation and placeholder create bank function

--- apps/marginfi-v2-ui/src/pages/staked-assets/create.tsx
+++ apps/marginfi-v2-ui/src/pages/staked-assets/create.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useState } from "react";
+import React from "react";
 
 import { useDropzone } from "react-dropzone";
-import { IconPhoto } from "@tabler/icons-react";
+import { IconPhoto, IconLoader2 } from "@tabler/icons-react";
 import { cn, useIsMobile } from "@mrgnlabs/mrgn-utils";
 
 import { PageHeading } from "~/components/common/PageHeading";
@@ -10,15 +10,29 @@ import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
+type CreateStakedAssetForm = {
+  voteAccountKey: string;
+  assetName: string;
+  assetLogo: File | null;
+};
+
 export default function CreateStakedAssetPage() {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [form, setForm] = React.useState<CreateStakedAssetForm>({
+    voteAccountKey: "",
+    assetName: "",
+    assetLogo: null,
+  });
+  const [isLoading, setIsLoading] = React.useState(false);
   const isMobile = useIsMobile();
 
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    if (acceptedFiles.length > 0) {
-      setSelectedFile(acceptedFiles[0]);
-    }
-  }, []);
+  const onDrop = React.useCallback(
+    (acceptedFiles: File[]) => {
+      if (acceptedFiles.length > 0) {
+        setForm({ ...form, assetLogo: acceptedFiles[0] });
+      }
+    },
+    [form]
+  );
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
@@ -28,23 +42,52 @@ export default function CreateStakedAssetPage() {
     maxFiles: 1,
   });
 
+  const createStakedAssetBank = React.useCallback(async () => {
+    setIsLoading(true);
+    console.log(form);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, [form]);
+
   return (
     <div>
       <PageHeading
         heading="Staked Asset Banks"
         body={<p>Create a new staked asset bank and let stakers use their native stake as collateral.</p>}
       />
-      <form className="flex flex-col gap-8 px-4 md:px-0">
+      <form
+        className="flex flex-col gap-8 px-4 md:px-0"
+        onSubmit={(e) => {
+          e.preventDefault();
+          createStakedAssetBank();
+        }}
+      >
         <div className="flex flex-col gap-2">
           <Label htmlFor="name">Vote Account Key</Label>
-          <Input id="name" placeholder="Enter validator vote account public key" />
+          <Input
+            required
+            id="name"
+            placeholder="Enter validator vote account public key"
+            value={form.voteAccountKey}
+            onChange={(e) => setForm({ ...form, voteAccountKey: e.target.value })}
+          />
         </div>
         <div className="flex flex-col gap-2">
           <Label htmlFor="name">Asset Name</Label>
-          <Input id="name" placeholder="Enter asset ticker" />
+          <Input
+            required
+            id="name"
+            placeholder="Enter asset ticker"
+            value={form.assetName}
+            onChange={(e) => setForm({ ...form, assetName: e.target.value })}
+          />
         </div>
         <div className="flex flex-col gap-2">
-          <Label htmlFor="logo">Asset Logo</Label>
+          <Label htmlFor="logo">
+            Asset Logo <span className="text-xs text-muted-foreground">(optional)</span>
+          </Label>
           <div
             className={cn(
               "flex gap-4 items-center cursor-pointer p-4 group rounded-lg transition-colors hover:bg-background-gray",
@@ -63,16 +106,23 @@ export default function CreateStakedAssetPage() {
               <IconPhoto size={24} />
             </div>
             <p className="text-sm text-muted-foreground">
-              {selectedFile
-                ? `File: ${selectedFile.name}`
+              {form.assetLogo
+                ? `File: ${form.assetLogo.name}`
                 : isMobile
                 ? "Tap to select an image"
                 : "Drop an image here or click to select"}
             </p>
           </div>
         </div>
-        <Button disabled={true} type="submit" size="lg">
-          Create Staked Asset Bank
+        <Button disabled={!form.voteAccountKey || !form.assetName || isLoading} type="submit" size="lg">
+          {isLoading ? (
+            <>
+              <IconLoader2 size={16} className="animate-spin" />
+              Creating Staked Asset Bank...
+            </>
+          ) : (
+            "Create Staked Asset Bank"
+          )}
         </Button>
       </form>
     </div>
