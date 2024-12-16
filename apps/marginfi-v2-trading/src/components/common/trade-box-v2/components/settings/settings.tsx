e49fe02fee3fc9158b9d269d415fe11869ff0057
borcherd <borcherd@me.com>
2024-12-16 14:40:28 +0100

    feat: update settings dialog

--- apps/marginfi-v2-trading/src/components/common/trade-box-v2/components/settings/settings.tsx
+++ apps/marginfi-v2-trading/src/components/common/trade-box-v2/components/settings/settings.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { IconArrowLeft } from "@tabler/icons-react";
-
 import { Slippage } from "./components";
 
 type TradingBoxSettingsProps = {
@@ -13,9 +11,6 @@ type TradingBoxSettingsProps = {
 export const TradingBoxSettings = ({ toggleSettings, slippageBps, setSlippageBps }: TradingBoxSettingsProps) => {
   return (
     <div className="space-y-6">
-      <button className="flex items-center gap-1.5 text-sm" onClick={() => toggleSettings(false)}>
-        <IconArrowLeft size={18} /> Back to trading
-      </button>
       <div>
         <Slippage toggleSettings={toggleSettings} slippageBps={slippageBps} setSlippageBps={setSlippageBps} />
       </div>
