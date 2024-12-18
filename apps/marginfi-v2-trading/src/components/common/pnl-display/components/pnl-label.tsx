fe8567044cbee67ecc2acea57251860b79d68b7a
chambaz <adammchambers@gmail.com>
2024-12-18 12:18:10 -0500

    feat: add $ / % toggle to share position dialog

--- apps/marginfi-v2-trading/src/components/common/pnl-display/components/pnl-label.tsx
+++ apps/marginfi-v2-trading/src/components/common/pnl-display/components/pnl-label.tsx
@@ -2,13 +2,16 @@ import { dynamicNumeralFormatter } from "@mrgnlabs/mrgn-common";
 import { cn } from "@mrgnlabs/mrgn-utils";
 
 type PnlLabelProps = {
+  type?: "$" | "%";
   pnl: number;
+  positionSize: number;
   className?: string;
 };
 
-const PnlLabel = ({ pnl, className }: PnlLabelProps) => {
+const PnlLabel = ({ type = "$", pnl, positionSize, className }: PnlLabelProps) => {
   const positionState = pnl > 0 ? "positive" : pnl < 0 ? "negative" : "neutral";
   const pnlSign = positionState === "positive" ? "+" : positionState === "negative" ? "-" : "";
+  const pnlPercentage = (pnl / positionSize) * 100;
   return (
     <span
       className={cn(
@@ -17,10 +20,17 @@ const PnlLabel = ({ pnl, className }: PnlLabelProps) => {
         positionState === "negative" && "text-error"
       )}
     >
-      {`${pnlSign}$${dynamicNumeralFormatter(Math.abs(pnl), {
-        minDisplay: 0.0001,
-        maxDisplay: 100000,
-      })}`}
+      {pnlSign}
+      {type === "$" && (
+        <span>
+          $
+          {dynamicNumeralFormatter(Math.abs(pnl), {
+            minDisplay: 0.0001,
+            maxDisplay: 100000,
+          })}
+        </span>
+      )}
+      {type === "%" && <span>{pnlPercentage.toFixed(2)}%</span>}
     </span>
   );
 };
