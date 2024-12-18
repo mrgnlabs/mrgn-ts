a874f1a6178bc4872b8d3d66dba4da126c8618da
chambaz <adammchambers@gmail.com>
2024-12-18 12:37:25 -0500

    feat: add optional click to toggle pnl label

--- apps/marginfi-v2-trading/src/components/common/pnl-display/components/pnl-label.tsx
+++ apps/marginfi-v2-trading/src/components/common/pnl-display/components/pnl-label.tsx
@@ -1,37 +1,58 @@
+import React from "react";
+
 import { dynamicNumeralFormatter } from "@mrgnlabs/mrgn-common";
 import { cn } from "@mrgnlabs/mrgn-utils";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/components/ui/tooltip";
 
 type PnlLabelProps = {
   type?: "$" | "%";
   pnl: number;
   positionSize: number;
   className?: string;
+  disableClickToChangeType?: boolean;
 };
 
-const PnlLabel = ({ type = "$", pnl, positionSize, className }: PnlLabelProps) => {
+const PnlLabel = ({ type = "$", pnl, positionSize, className, disableClickToChangeType = false }: PnlLabelProps) => {
+  const [currentType, setCurrentType] = React.useState(type);
   const positionState = pnl > 0 ? "positive" : pnl < 0 ? "negative" : "neutral";
   const pnlSign = positionState === "positive" ? "+" : positionState === "negative" ? "-" : "";
   const pnlPercentage = (pnl / positionSize) * 100;
+
   return (
-    <span
-      className={cn(
-        className,
-        positionState === "positive" && "text-success",
-        positionState === "negative" && "text-error"
-      )}
-    >
-      {pnlSign}
-      {type === "$" && (
-        <span>
-          $
-          {dynamicNumeralFormatter(Math.abs(pnl), {
-            minDisplay: 0.0001,
-            maxDisplay: 100000,
-          })}
-        </span>
-      )}
-      {type === "%" && <span>{pnlPercentage.toFixed(2)}%</span>}
-    </span>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <span
+            className={cn(
+              className,
+              !disableClickToChangeType && "cursor-pointer",
+              positionState === "positive" && "text-success",
+              positionState === "negative" && "text-error"
+            )}
+            onClick={() => {
+              if (disableClickToChangeType) return;
+              setCurrentType(currentType === "$" ? "%" : "$");
+            }}
+          >
+            {pnlSign}
+            {currentType === "$" && (
+              <span>
+                $
+                {dynamicNumeralFormatter(Math.abs(pnl), {
+                  minDisplay: 0.0001,
+                  maxDisplay: 100000,
+                })}
+              </span>
+            )}
+            {currentType === "%" && <span>{pnlPercentage.toFixed(2)}%</span>}
+          </span>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Toggle to show {currentType === "$" ? "%" : "$"}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
