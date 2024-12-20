33f03e9cc49f131d16128a42ef2c10bef9b9c599
chambaz <adammchambers@gmail.com>
2024-12-20 12:16:50 -0500

    fix: improved undefined pool checks

--- apps/marginfi-v2-trading/src/hooks/usePositionsData.ts
+++ apps/marginfi-v2-trading/src/hooks/usePositionsData.ts
@@ -2,16 +2,18 @@ import React from "react";
 import { PublicKey } from "@solana/web3.js";
 
 import { useTradeStoreV2 } from "~/store";
+import { ArenaPoolPositions } from "~/types/trade-store.types";
 
 type UsePositionsDataProps = {
-  groupPk: PublicKey;
+  groupPk?: PublicKey;
 };
 
 export function usePositionsData({ groupPk }: UsePositionsDataProps) {
   const [positionsByGroupPk] = useTradeStoreV2((state) => [state.positionsByGroupPk]);
 
   const positions = React.useMemo(() => {
-    return positionsByGroupPk[groupPk.toBase58()];
+    if (!groupPk) return {} as ArenaPoolPositions;
+    return positionsByGroupPk[groupPk.toBase58()] ?? ({} as ArenaPoolPositions);
   }, [positionsByGroupPk, groupPk]);
 
   return positions;
