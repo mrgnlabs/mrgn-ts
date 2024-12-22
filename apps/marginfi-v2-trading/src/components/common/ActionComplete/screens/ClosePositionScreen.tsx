c7390ad6fa8890795d9b9a10f3e7b35f0d616e51
borcherd <borcherd@me.com>
2024-12-22 22:15:40 +0100

    chore: add conditional rendering for api data

--- apps/marginfi-v2-trading/src/components/common/ActionComplete/screens/ClosePositionScreen.tsx
+++ apps/marginfi-v2-trading/src/components/common/ActionComplete/screens/ClosePositionScreen.tsx
@@ -38,18 +38,28 @@ export const ClosePositionScreen = ({ tokenBank, size, leverage, entryPrice, exi
           </div>
           <h3 className="text-2xl font-medium text-center">{tokenBank?.meta.tokenSymbol} position closed</h3>
         </div>
-        <div className="flex items-center justify-center gap-2">
-          <PnlLabel pnl={pnl} positionSize={size} className="text-3xl" />
-          <PnlBadge pnl={pnl} positionSize={size} className="text-sm" />
-        </div>
+        {pnl ? (
+          <div className="flex items-center justify-center gap-2">
+            <PnlLabel pnl={pnl} positionSize={size} className="text-3xl" />
+            <PnlBadge pnl={pnl} positionSize={size} className="text-sm" />
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
       <dl className="grid grid-cols-2 w-full text-muted-foreground gap-x-8 gap-y-2">
         <dt>Size</dt>
         <dd className="text-right">${dynamicNumeralFormatter(size)}</dd>
         <dt>Leverage</dt>
         <dd className="text-right">{leverage}x</dd>
-        <dt>Entry Price</dt>
-        <dd className="text-right">${dynamicNumeralFormatter(entryPrice)}</dd>
+        {entryPrice ? (
+          <>
+            <dt>Entry Price</dt>
+            <dd className="text-right">${dynamicNumeralFormatter(entryPrice)}</dd>
+          </>
+        ) : (
+          <></>
+        )}
         <dt>Exit Price</dt>
         <dd className="text-right">${dynamicNumeralFormatter(exitPrice)}</dd>
         <dt>Transaction</dt>
