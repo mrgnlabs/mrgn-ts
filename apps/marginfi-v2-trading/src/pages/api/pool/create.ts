de836c2f96fa68059187db7f76dcef364c6c3024
Kobe <leenderskobe@gmail.com>
2024-12-24 14:02:49 +0100

    fix: create pool improvements

--- apps/marginfi-v2-trading/src/pages/api/pool/create.ts
+++ apps/marginfi-v2-trading/src/pages/api/pool/create.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(401).json({ error: "Unauthorized" });
     }
 
-    const createPool: { group: string; asset: string; quote: string; lut: string } = req.body;
+    const { base_bank, created_by, featured, group, lookup_tables, quote_banks } = req.body;
 
     const response = await fetch(ARENA_URL, {
       method: "POST",
@@ -17,22 +17,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         Authorization: `${req.headers.authorization}`,
       },
       body: JSON.stringify({
-        base_bank: createPool.asset,
-        created_by: "mfi1dtjy2mJ9J21UoaQ5dsRnbcg4MBU1CTacVyBp1HF",
-        featured: true,
-        group: createPool.group,
-        lookup_tables: [createPool.lut],
-        quote_banks: [createPool.quote],
+        base_bank,
+        created_by,
+        featured,
+        group,
+        lookup_tables,
+        quote_banks,
       }),
     });
-    console.log("response", response);
 
     if (!response.ok) {
       return res.status(response.status).json({ error: "Failed to add pool" });
     }
-
-    const data = await response.json();
-    res.status(200).json(data);
+    res.status(200).json({ message: "Pool added" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
