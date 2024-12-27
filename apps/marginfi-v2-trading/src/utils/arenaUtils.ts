6f2a325891bbe78c954ee6e21b8f7e37c7b7286a
chambaz <adammchambers@gmail.com>
2024-12-27 16:37:32 -0500

    feat: add support for mfi sponsored or permissionless create by icons

--- apps/marginfi-v2-trading/src/utils/arenaUtils.ts
+++ apps/marginfi-v2-trading/src/utils/arenaUtils.ts
@@ -1,5 +1,11 @@
 export type GroupPositionInfo = "LP" | "LONG" | "SHORT" | null;
 
+export const mfiAddresses = [
+  "mfi1dtjy2mJ9J21UoaQ5dsRnbcg4MBU1CTacVyBp1HF",
+  "MFv2hWf31Z9kbCa1snEPYctwafyhdvnV7FZnsebVacA",
+  "9bzQQGkii8DKyZ3WQZt8WTW2zXgNkYuNwMj8agqpd3Lu",
+];
+
 // export function getGroupPositionInfo({ group }: { group: GroupData }): GroupPositionInfo {
 //   const tokenBank = group.pool.token;
 //   const quoteTokens = group.pool.quoteTokens;
