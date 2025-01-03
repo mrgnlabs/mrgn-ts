17639cbb100d6a13213e34175b6e792925c90ead
borcherd <borcherd@me.com>
2025-01-03 13:35:05 +0100

    feat: use custom font

--- apps/marginfi-v2-trading/src/pages/api/share-image/generate.tsx
+++ apps/marginfi-v2-trading/src/pages/api/share-image/generate.tsx
@@ -6,6 +6,20 @@ import { DynamicShareImage } from "~/components/common/dynamic-share-image";
 
 export const runtime = "edge";
 
+async function loadFont(font: string, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}:wght@700&text=${encodeURIComponent(text)}`;
+  const css = await (await fetch(url)).text();
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
+
+  if (resource) {
+    const response = await fetch(resource[1]);
+    if (response.ok) {
+      return await response.arrayBuffer();
+    }
+  }
+
+  throw new Error(`Failed to load font data for ${font}`);
+}
 // Generate link preview image
 // Note that a lot of usual CSS is unsupported, including tailwind.
 export default async function GET(request: NextRequest) {
@@ -30,6 +44,14 @@ export default async function GET(request: NextRequest) {
     {
       width: 720,
       height: 360,
+      fonts: [
+        {
+          name: "Orbitron",
+          data: await loadFont("Orbitron", `${tokenSymbol}/${quoteTokenSymbol}`),
+          weight: 700,
+          style: "normal",
+        },
+      ],
     }
   );
 }
