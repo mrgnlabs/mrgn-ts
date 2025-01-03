1a9a69544c3dab8723c6f8956e31629f487902f4
chambaz <adammchambers@gmail.com>
2025-01-03 12:56:40 -0500

    chore: add susd emissions metadata

--- packages/mrgn-utils/src/emissions.consts.ts
+++ packages/mrgn-utils/src/emissions.consts.ts
@@ -7,4 +7,12 @@ export const EMISSION_MINT_INFO_MAP = new Map<string, { tokenSymbol: string; tok
         "https://storage.googleapis.com/mrgn-public/mrgn-token-icons/USDSwr9ApdHk5bvJKMjzff41FfuX8bSxdKcR81vTwcA.png",
     },
   ],
+  [
+    "sUSD",
+    {
+      tokenSymbol: "sUSD",
+      tokenLogoUri:
+        "https://storage.googleapis.com/mrgn-public/mrgn-token-icons/susdabGDNbhrnCa6ncrYo81u4s9GM8ecK2UwMyZiq4X.png",
+    },
+  ],
 ]);
