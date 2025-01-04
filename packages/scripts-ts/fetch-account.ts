9246cab47893717dfaf6e2818440a80671b77d4b
jgur-psyops <jonathan@mrgn.group>
2025-01-04 13:15:49 -0500

    Fetch account balances with fancy table (debugging sUSD emissions)

---
+++ packages/scripts-ts/fetch-account.ts
@@ -0,0 +1,70 @@
+import {
+  Connection,
+  PublicKey,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { Program, AnchorProvider } from "@coral-xyz/anchor";
+
+import { loadKeypairFromFile } from "./utils";
+import { assertI80F48Approx, assertKeysEqual } from "./softTests";
+import { Marginfi } from "./idl/marginfi-types";
+import marginfiIdl from "./idl/marginfi.json";
+import { wrappedI80F48toBigNumber } from "@mrgnlabs/mrgn-common";
+
+const verbose = true;
+
+type Config = {
+  PROGRAM_ID: string;
+  ACCOUNT: PublicKey;
+};
+
+const config: Config = {
+  PROGRAM_ID: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
+  ACCOUNT: new PublicKey("BXY7J98nbWmmb54cc6QnoX2rh59QdPEz19oSrhpFgnbA"),
+};
+
+async function main() {
+  marginfiIdl.address = config.PROGRAM_ID;
+  const connection = new Connection(
+    "https://api.mainnet-beta.solana.com",
+    "confirmed"
+  );
+  const wallet = loadKeypairFromFile(
+    process.env.HOME + "/.config/solana/id.json"
+  );
+
+  // @ts-ignore
+  const provider = new AnchorProvider(connection, wallet, {
+    preflightCommitment: "confirmed",
+  });
+
+  const program = new Program<Marginfi>(marginfiIdl as Marginfi, provider);
+  let acc = await program.account.marginfiAccount.fetch(config.ACCOUNT);
+  let balances = acc.lendingAccount.balances;
+  let activeBalances = [];
+  for (let i = 0; i < balances.length; i++) {
+    if (balances[i].active === false) continue; // Skip inactive balances
+
+    activeBalances.push({
+        "Bank PK": balances[i].bankPk.toString(),
+        "Asset Tag": balances[i].bankAssetTag,
+        "Liability Shares": formatNumber(wrappedI80F48toBigNumber(balances[i].liabilityShares)),
+        "Asset Shares": formatNumber(wrappedI80F48toBigNumber(balances[i].assetShares)),
+        "Emissions Outstanding": formatNumber(wrappedI80F48toBigNumber(balances[i].emissionsOutstanding)),
+        "Last Update": balances[i].lastUpdate.toString(),
+    });
+}
+
+function formatNumber(num) {
+    const number = parseFloat(num).toFixed(4);
+    return number === "0.0000" ? "-" : number;
+}
+
+// Display the active balances as a formatted table
+console.table(activeBalances);
+}
+
+main().catch((err) => {
+  console.error(err);
+});
