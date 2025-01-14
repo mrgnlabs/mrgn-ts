05dba46beba29751d4f9afec462a529f9c9a1e35
borcherd <borcherd@me.com>
2025-01-14 10:37:00 +0100

    chore: swap-lend page mrgnlend

---
+++ apps/marginfi-v2-ui/src/pages/swap-lend.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { ActionBox as ActionBoxV2 } from "@mrgnlabs/mrgn-ui";
+import { capture } from "@mrgnlabs/mrgn-utils";
+import { useMrgnlendStore } from "~/store";
+
+import { PageHeading } from "~/components/common/PageHeading";
+
+import { Loader } from "~/components/ui/loader";
+import { useWallet } from "~/components/wallet-v2";
+
+export default function SwapLendPage() {
+  const [initialized] = useMrgnlendStore((state) => [state.initialized, state.extendedBankInfos]);
+  const { connected } = useWallet();
+
+  return (
+    <>
+      {!initialized && <Loader label="Loading marginfi..." className="mt-16" />}
+
+      {initialized && (
+        <div className="w-full max-w-7xl mx-auto mb-20 px-5">
+          <PageHeading heading="Swap & Lend" body={<p>Swap and lend your assets to maximize yield.</p>} />
+          <ActionBoxV2.SwapLend
+            useProvider={true}
+            swapLendProps={{
+              connected: connected,
+              requestedDepositBank: undefined,
+              requestedSwapBank: undefined,
+              captureEvent: (event, properties) => {
+                capture(event, properties);
+              },
+            }}
+          />
+        </div>
+      )}
+    </>
+  );
+}
