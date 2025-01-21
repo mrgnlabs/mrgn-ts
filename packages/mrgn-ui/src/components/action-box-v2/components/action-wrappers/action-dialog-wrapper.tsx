3eb403769c020409b6a6cfee0dae07a74866fb8b
borcherd <borcherd@me.com>
2025-01-21 10:50:44 +0100

    feat: update mobile actionbox styling

--- packages/mrgn-ui/src/components/action-box-v2/components/action-wrappers/action-dialog-wrapper.tsx
+++ packages/mrgn-ui/src/components/action-box-v2/components/action-wrappers/action-dialog-wrapper.tsx
@@ -9,7 +9,8 @@ import {
   DialogHeader,
   DialogDescription,
 } from "~/components/ui/dialog";
-import { useIsMobile, usePrevious, cn } from "@mrgnlabs/mrgn-utils";
+import { Drawer, DrawerTrigger, DrawerContent } from "~/components/ui/drawer";
+import { useIsMobile, usePrevious, cn, Mobile, Desktop } from "@mrgnlabs/mrgn-utils";
 import { useActionBoxStore } from "../../store";
 
 export interface ActionDialogProps {
@@ -23,57 +24,48 @@ interface ActionDialogWrapperProps extends ActionDialogProps {
 }
 
 export const ActionDialogWrapper = ({ trigger, children, title, isTriggered = false }: ActionDialogWrapperProps) => {
-  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
   const [isActionComplete] = useActionBoxStore((state) => [state.isActionComplete]);
   const prevIsActionComplete = usePrevious(isActionComplete);
   const isMobile = useIsMobile();
 
   React.useEffect(() => {
     if (!prevIsActionComplete && isActionComplete) {
-      setIsDialogOpen(false);
+      setIsOpen(false);
     }
   }, [prevIsActionComplete, isActionComplete]);
 
   React.useEffect(() => {
-    setIsDialogOpen(isTriggered);
-  }, [setIsDialogOpen, isTriggered]);
+    setIsOpen(isTriggered);
+  }, [setIsOpen, isTriggered]);
 
   return (
-    <Dialog open={isDialogOpen} modal={!isMobile} onOpenChange={(open) => setIsDialogOpen(open)}>
-      <DialogTrigger asChild>{trigger}</DialogTrigger>
-      <DialogContent
-        hideClose={isMobile}
-        className={cn(
-          "shadow-none overflow-visible",
-          isMobile
-            ? "mt-[60px] justify-start flex md:max-w-[520px] md:py-3 md:px-5 p-0 pt-6 sm:rounded-2xl border-none z-50"
-            : "md:flex md:max-w-[520px] md:py-3 md:px-5 p-0 sm:rounded-2xl bg-transparent border-none"
-        )}
-        closeClassName={!isMobile ? "-top-1 -right-1" : undefined}
-      >
-        <DialogHeader className="sr-only">
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{title}</DialogDescription>
-        </DialogHeader>
-        <div>
-          {isMobile && (
-            <div
-              className="flex gap-2 items-center capitalize pl-2 cursor-pointer hover:underline"
-              onClick={() => setIsDialogOpen(false)}
-            >
-              <IconArrowLeft /> {title}
-            </div>
-          )}
-          <div
-            className={cn(
-              "bg-mfi-action-box-background shadow-lg rounded-lg",
-              isMobile ? "p-4 pt-0 h-screen mb-8 mt-5" : "p-1"
-            )}
-          >
+    <>
+      <Mobile>
+        <Drawer open={isOpen} onOpenChange={(open) => setIsOpen(open)}>
+          <DrawerTrigger asChild>{trigger}</DrawerTrigger>
+          <DrawerContent className="rounded-lg bg-mfi-action-box-background shadow-lg justify-start flex  border-none z-50 p-4 pt-0">
             {children}
-          </div>
-        </div>
-      </DialogContent>
-    </Dialog>
+          </DrawerContent>
+        </Drawer>
+      </Mobile>
+      <Desktop>
+        <Dialog open={isOpen} modal onOpenChange={(open) => setIsOpen(open)}>
+          <DialogTrigger asChild>{trigger}</DialogTrigger>
+          <DialogContent
+            className={
+              "shadow-none overflow-visible md:flex md:max-w-[520px] py-3 px-5 sm:rounded-2xl bg-transparent border-none"
+            }
+            closeClassName={"-top-1 -right-1"}
+          >
+            <DialogHeader className="sr-only">
+              <DialogTitle>{title}</DialogTitle>
+              <DialogDescription>{title}</DialogDescription>
+            </DialogHeader>
+            <div className="bg-mfi-action-box-background shadow-lg rounded-lg p-1">{children}</div>
+          </DialogContent>
+        </Dialog>
+      </Desktop>
+    </>
   );
 };
