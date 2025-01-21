795eb4b76a15e748f902d25bf431d05dd0159935
chambaz <adammchambers@gmail.com>
2025-01-21 14:45:52 -0500

    fix: check for banks in extended bank sorting fns

--- packages/marginfi-v2-ui-state/src/store/mrgnlendStore.ts
+++ packages/marginfi-v2-ui-state/src/store/mrgnlendStore.ts
@@ -393,6 +393,8 @@ const stateCreator: StateCreator<MrgnlendState, [], []> = (set, get) => ({
       const sortedExtendedBankMetadatas = extendedBankMetadatas.sort((am, bm) => {
         const a = sortedExtendedBankInfos.find((a) => a.address.equals(am.address))!;
         const b = sortedExtendedBankInfos.find((b) => b.address.equals(bm.address))!;
+
+        if (!a || !b) return 0;
         return b.info.state.totalDeposits * b.info.state.price - a.info.state.totalDeposits * a.info.state.price;
       });
 
