50f27be8c6821bcb4e7242ada4e7af363ec30e93
chambaz <adammchambers@gmail.com>
2025-01-22 10:14:07 -0500

    chore: add examples to borrow script

--- packages/scripts-ts/borrow.ts
+++ packages/scripts-ts/borrow.ts
@@ -1,6 +1,7 @@
 // Run deposit_single_pool first to convert to LST. In production, these will likely be atomic.
 import {
   AccountMeta,
+  ComputeBudgetProgram,
   Connection,
   Keypair,
   PublicKey,
@@ -26,7 +27,7 @@ type Config = {
   GROUP: PublicKey;
   ACCOUNT: PublicKey;
   BANK: PublicKey;
-  STAKE_POOL: PublicKey;
+  STAKE_POOL?: PublicKey;
   MINT: PublicKey;
   /** In native decimals */
   AMOUNT: BN;
@@ -55,23 +56,75 @@ type Config = {
   REMAINING: PublicKey[];
 };
 
-const config: Config = {
-  PROGRAM_ID: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
-  GROUP: new PublicKey("FCPfpHA69EbS8f9KKSreTRkXbzFpunsKuYf5qNmnJjpo"),
-  ACCOUNT: new PublicKey("E3uJyxW232EQAVZ9P9V6CFkxzjqqVdbh8XvUmxtZdGUt"),
-  BANK: new PublicKey("3evdJSa25nsUiZzEUzd92UNa13TPRJrje1dRyiQP5Lhp"),
-  STAKE_POOL: new PublicKey("AvS4oXtxWdrJGCJwDbcZ7DqpSqNQtKjyXnbkDbrSk6Fq"),
-  MINT: new PublicKey("So11111111111111111111111111111111111111112"),
-  AMOUNT: new BN(0.0002 * 10 ** 9), // sol has 9 decimals
-  REMAINING: [
-    new PublicKey("3jt43usVm7qL1N5qPvbzYHWQRxamPCRhri4CxwDrf6aL"),
-    new PublicKey("7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE"),
-    new PublicKey("BADo3D6nMtGnsAaTv3iEes8mMcq92TuFoBWebFe8kzeA"), // lst mint
-    new PublicKey("3e8RuaQMCPASZSMJAskHX6ZfuTtQ3JvoNPFoEvaVRn78"), // lst pool
-    new PublicKey("3evdJSa25nsUiZzEUzd92UNa13TPRJrje1dRyiQP5Lhp"),
-    new PublicKey("7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE"),
-  ],
+const examples = {
+  borrowJupSOLAgainstUSDC: {
+    PROGRAM_ID: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
+    GROUP: new PublicKey("FCPfpHA69EbS8f9KKSreTRkXbzFpunsKuYf5qNmnJjpo"),
+    ACCOUNT: new PublicKey("2GMbwepeyW5xzgm3cQLivdPWLydrFevLy2iBbZab3pd6"),
+    BANK: new PublicKey("EJuhmswifV6wumS28Sfr5W8B18CJ29m1ZNKkhbhbYDCA"),
+    MINT: new PublicKey("jupSoLaHXQiZZTSfEWMTRRgpnyFm8f6sZdosWBjx93v"),
+    AMOUNT: new BN(0.000605243 * 10 ** 9), // jupsol has 9 decimals
+    REMAINING: [
+      new PublicKey("Ek5JSFJFD8QgXM6rPDCzf31XhDp1q3xezaWYSkJWqbqc"), // usdc bank
+      new PublicKey("Dpw1EAVrSB1ibxiDQyTAW6Zip3J4Btk2x4SgApQCeFbX"), // usdc oracle
+      new PublicKey("EJuhmswifV6wumS28Sfr5W8B18CJ29m1ZNKkhbhbYDCA"), // jupsol bank
+      new PublicKey("HX5WM3qzogAfRCjBUWwnniLByMfFrjm1b5yo4KoWGR27"), // jupsol oracle
+    ],
+  },
+  borrowSOLAgainstUSDC: {
+    PROGRAM_ID: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
+    GROUP: new PublicKey("FCPfpHA69EbS8f9KKSreTRkXbzFpunsKuYf5qNmnJjpo"),
+    ACCOUNT: new PublicKey("2GMbwepeyW5xzgm3cQLivdPWLydrFevLy2iBbZab3pd6"),
+    BANK: new PublicKey("3evdJSa25nsUiZzEUzd92UNa13TPRJrje1dRyiQP5Lhp"),
+    MINT: new PublicKey("So11111111111111111111111111111111111111112"),
+    AMOUNT: new BN(0.000815446 * 10 ** 9), // sol has 9 decimals
+    REMAINING: [
+      new PublicKey("Ek5JSFJFD8QgXM6rPDCzf31XhDp1q3xezaWYSkJWqbqc"), // usdc bank
+      new PublicKey("Dpw1EAVrSB1ibxiDQyTAW6Zip3J4Btk2x4SgApQCeFbX"), // usdc oracle
+      new PublicKey("EJuhmswifV6wumS28Sfr5W8B18CJ29m1ZNKkhbhbYDCA"), // jupsol bank
+      new PublicKey("HX5WM3qzogAfRCjBUWwnniLByMfFrjm1b5yo4KoWGR27"), // jupsol oracle
+      new PublicKey("3evdJSa25nsUiZzEUzd92UNa13TPRJrje1dRyiQP5Lhp"), // sol bank
+      new PublicKey("7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE"), // sol oracle
+    ],
+  },
+  borrowUSDCAgainstStakedSOL: {
+    PROGRAM_ID: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
+    GROUP: new PublicKey("FCPfpHA69EbS8f9KKSreTRkXbzFpunsKuYf5qNmnJjpo"),
+    ACCOUNT: new PublicKey("7SBEjeEjhzRvWsrTq7UiNWBLjcYwE1hdbmMK5wUaeVhU"),
+    BANK: new PublicKey("Ek5JSFJFD8QgXM6rPDCzf31XhDp1q3xezaWYSkJWqbqc"),
+    STAKE_POOL: new PublicKey("AvS4oXtxWdrJGCJwDbcZ7DqpSqNQtKjyXnbkDbrSk6Fq"),
+    MINT: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+    AMOUNT: new BN(0.007734 * 10 ** 6), // usdc has 6 decimals
+    REMAINING: [
+      new PublicKey("3jt43usVm7qL1N5qPvbzYHWQRxamPCRhri4CxwDrf6aL"), // staked sol bank
+      new PublicKey("7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE"), // staked sol oracle
+      new PublicKey("BADo3D6nMtGnsAaTv3iEes8mMcq92TuFoBWebFe8kzeA"), // lst mint
+      new PublicKey("AvS4oXtxWdrJGCJwDbcZ7DqpSqNQtKjyXnbkDbrSk6Fq"), // lst pool
+      new PublicKey("Ek5JSFJFD8QgXM6rPDCzf31XhDp1q3xezaWYSkJWqbqc"), // usdc bank
+      new PublicKey("Dpw1EAVrSB1ibxiDQyTAW6Zip3J4Btk2x4SgApQCeFbX"), // usdc oracle
+    ],
+  },
 };
+
+const config: Config = examples.borrowJupSOLAgainstUSDC;
+
+// const config: Config = {
+//   PROGRAM_ID: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
+//   GROUP: new PublicKey("FCPfpHA69EbS8f9KKSreTRkXbzFpunsKuYf5qNmnJjpo"),
+//   ACCOUNT: new PublicKey("E3uJyxW232EQAVZ9P9V6CFkxzjqqVdbh8XvUmxtZdGUt"),
+//   BANK: new PublicKey("3evdJSa25nsUiZzEUzd92UNa13TPRJrje1dRyiQP5Lhp"),
+//   STAKE_POOL: new PublicKey("AvS4oXtxWdrJGCJwDbcZ7DqpSqNQtKjyXnbkDbrSk6Fq"),
+//   MINT: new PublicKey("So11111111111111111111111111111111111111112"),
+//   AMOUNT: new BN(0.0002 * 10 ** 9), // sol has 9 decimals
+//   REMAINING: [
+//     new PublicKey("3jt43usVm7qL1N5qPvbzYHWQRxamPCRhri4CxwDrf6aL"),
+//     new PublicKey("7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE"),
+//     new PublicKey("BADo3D6nMtGnsAaTv3iEes8mMcq92TuFoBWebFe8kzeA"), // lst mint
+//     new PublicKey("3e8RuaQMCPASZSMJAskHX6ZfuTtQ3JvoNPFoEvaVRn78"), // lst pool
+//     new PublicKey("3evdJSa25nsUiZzEUzd92UNa13TPRJrje1dRyiQP5Lhp"),
+//     new PublicKey("7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE"),
+//   ],
+// };
 
 async function main() {
   marginfiIdl.address = config.PROGRAM_ID;
@@ -93,17 +146,19 @@ async function main() {
     provider
   );
 
-  // Equivalent to findPoolMintAddress
-  const [lstMint] = PublicKey.findProgramAddressSync(
-    [Buffer.from("mint"), config.STAKE_POOL.toBuffer()],
-    SINGLE_POOL_PROGRAM_ID
-  );
-  // Equivalent to findPoolStakeAuthorityAddress
-  const [pool] = PublicKey.findProgramAddressSync(
-    [Buffer.from("stake"), config.STAKE_POOL.toBuffer()],
-    SINGLE_POOL_PROGRAM_ID
-  );
-  console.log("mint: " + lstMint + " pool " + pool);
+  if (config.STAKE_POOL) {
+    // Equivalent to findPoolMintAddress
+    const [lstMint] = PublicKey.findProgramAddressSync(
+      [Buffer.from("mint"), config.STAKE_POOL.toBuffer()],
+      SINGLE_POOL_PROGRAM_ID
+    );
+    // Equivalent to findPoolStakeAuthorityAddress
+    const [pool] = PublicKey.findProgramAddressSync(
+      [Buffer.from("stake"), config.STAKE_POOL.toBuffer()],
+      SINGLE_POOL_PROGRAM_ID
+    );
+    console.log("mint: " + lstMint + " pool " + pool);
+  }
 
   const oracleMeta: AccountMeta[] = config.REMAINING.map((pubkey) => ({
     pubkey,
@@ -133,6 +188,9 @@ async function main() {
       .remainingAccounts(oracleMeta)
       .instruction()
   );
+
+  const priorityFee = 100000; // 0.0001 SOL (100,000 lamports)
+  transaction.instructions.unshift(ComputeBudgetProgram.setComputeUnitPrice({ microLamports: priorityFee }));
 
   try {
     const signature = await sendAndConfirmTransaction(connection, transaction, [wallet]);
