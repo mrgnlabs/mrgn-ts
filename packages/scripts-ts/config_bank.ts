5fb47767b450adc2b2db46edf79fe5c01ba16e2f
jgur-psyops <jonathan@mrgn.group>
2025-01-23 16:37:47 -0500

    add oracle meta

---
+++ packages/scripts-ts/config_bank.ts
@@ -0,0 +1,169 @@
+// Runs once per group, before any staked banks can be init.
+import { AccountMeta, Connection, PublicKey, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import { Program, AnchorProvider, Wallet, BN } from "@coral-xyz/anchor";
+import { Marginfi } from "../marginfi-client-v2/src/idl/marginfi-types";
+import marginfiIdl from "../marginfi-client-v2/src/idl/marginfi.json";
+import { I80F48_ONE, loadKeypairFromFile } from "./utils";
+import {
+  bigNumberToWrappedI80F48,
+  TOKEN_PROGRAM_ID,
+  WrappedI80F48,
+  wrappedI80F48toBigNumber,
+} from "@mrgnlabs/mrgn-common";
+import { InterestRateConfigRaw, RiskTierRaw } from "@mrgnlabs/marginfi-client-v2";
+import { assertBNEqual, assertI80F48Approx, assertKeysEqual } from "./softTests";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+
+/**
+ * If true, send the tx. If false, output the unsigned b58 tx to console.
+ */
+const sendTx = false;
+const verbose = true;
+
+type Config = {
+  PROGRAM_ID: string;
+  GROUP_KEY: PublicKey;
+  BANK: PublicKey;
+  SOL_ORACLE: PublicKey;
+  SOL_ORACLE_FEED: PublicKey;
+  ADMIN: PublicKey;
+
+  // Keep default values to use the defaults...
+  MULTISIG_PAYER?: PublicKey; // May be omitted if not using squads
+};
+
+const config: Config = {
+  PROGRAM_ID: "MFv2hWf31Z9kbCa1snEPYctwafyhdvnV7FZnsebVacA",
+  GROUP_KEY: new PublicKey("4qp6Fx6tnZkY5Wropq9wUYgtFxXKwE6viZxFHg3rdAG8"),
+  BANK: new PublicKey("8g5qG6PVygcVSXV1cJnjXaD1yhrDwcWAMQCY2wR9VuAf"),
+  SOL_ORACLE: new PublicKey("H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG"),
+  SOL_ORACLE_FEED: new PublicKey("7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE"),
+  ADMIN: new PublicKey("AZtUUe9GvTFq9kfseu9jxTioSgdSfjgmZfGQBmhVpTj1"),
+
+  MULTISIG_PAYER: new PublicKey("AZtUUe9GvTFq9kfseu9jxTioSgdSfjgmZfGQBmhVpTj1"),
+};
+
+async function main() {
+  marginfiIdl.address = config.PROGRAM_ID;
+  const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+  const wallet = loadKeypairFromFile(process.env.HOME + "/keys/staging-deploy.json");
+
+  // @ts-ignore
+  const provider = new AnchorProvider(connection, wallet, {
+    preflightCommitment: "confirmed",
+  });
+
+  const program = new Program<Marginfi>(marginfiIdl as Marginfi, provider);
+
+  const transaction = new Transaction();
+
+  let bankConfig = defaultBankConfigOptRaw();
+
+  const oracleMeta: AccountMeta = {
+    pubkey: config.SOL_ORACLE_FEED,
+    isSigner: false,
+    isWritable: false,
+  };
+
+  transaction.add(
+    await program.methods
+      .lendingPoolConfigureBank(bankConfig)
+      .accounts({
+        marginfiGroup: config.GROUP_KEY,
+        admin: config.ADMIN,
+        bank: config.BANK,
+      })
+      .remainingAccounts([oracleMeta])
+      .instruction()
+  );
+
+  if (sendTx) {
+    try {
+      const signature = await sendAndConfirmTransaction(connection, transaction, [wallet]);
+      console.log("Transaction signature:", signature);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    }
+  } else {
+    transaction.feePayer = config.MULTISIG_PAYER; // Set the fee payer to Squads wallet
+    const { blockhash } = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = blockhash;
+    const serializedTransaction = transaction.serialize({
+      requireAllSignatures: false,
+      verifySignatures: false,
+    });
+    const base58Transaction = bs58.encode(serializedTransaction);
+    console.log("Base58-encoded transaction:", base58Transaction);
+  }
+}
+
+const ASSET_TAG_DEFAULT = 0;
+const ASSET_TAG_SOL = 1;
+const ASSET_TAG_STAKED = 2;
+
+/**
+ * No change
+ * @returns
+ */
+const defaultBankConfigOptRaw = () => {
+  let bankConfigOpt: BankConfigOptRaw = {
+    assetWeightInit: null,
+    assetWeightMaint: null,
+    liabilityWeightInit: null,
+    liabilityWeightMaint: null,
+    depositLimit: new BN(0),
+    borrowLimit: new BN(0),
+    riskTier: {
+      collateral: undefined,
+    },
+    assetTag: ASSET_TAG_STAKED,
+    totalAssetValueInitLimit: new BN(1),
+    interestRateConfig: null,
+    operationalState: {
+      operational: undefined,
+    },
+    oracle: {
+      setup: { pythPushOracle: {} },
+      keys: [config.SOL_ORACLE],
+    },
+    oracleMaxAge: 100,
+    permissionlessBadDebtSettlement: null,
+    freezeSettings: null,
+  };
+
+  return bankConfigOpt;
+};
+
+type InterestRateConfigRawWithOrigination = InterestRateConfigRaw & {
+  protocolOriginationFee: WrappedI80F48;
+};
+
+type BankConfigOptRaw = {
+  assetWeightInit: WrappedI80F48 | null;
+  assetWeightMaint: WrappedI80F48 | null;
+
+  liabilityWeightInit: WrappedI80F48 | null;
+  liabilityWeightMaint: WrappedI80F48 | null;
+
+  depositLimit: BN | null;
+  borrowLimit: BN | null;
+  riskTier: { collateral: {} } | { isolated: {} } | null;
+  assetTag: number;
+  totalAssetValueInitLimit: BN | null;
+
+  interestRateConfig: InterestRateConfigRawWithOrigination | null;
+  operationalState: { paused: {} } | { operational: {} } | { reduceOnly: {} } | null;
+
+  oracle: {
+    setup: { none: {} } | { pythLegacy: {} } | { switchboardV2: {} } | { pythPushOracle: {} } | { switchboardPull: {} };
+    keys: PublicKey[];
+  } | null;
+
+  oracleMaxAge: number | null;
+  permissionlessBadDebtSettlement: boolean | null;
+  freezeSettings: boolean | null;
+};
+
+main().catch((err) => {
+  console.error(err);
+});
