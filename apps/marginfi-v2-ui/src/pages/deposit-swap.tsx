9e5c13f078dd57249aa829ac0f26c183e90fb6ba
chambaz <adammchambers@gmail.com>
2025-01-25 14:42:54 -0500

    feat: fetch user wallet tokens on deposit swap page

--- apps/marginfi-v2-ui/src/pages/deposit-swap.tsx
+++ apps/marginfi-v2-ui/src/pages/deposit-swap.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+
 import { ActionBox as ActionBoxV2 } from "@mrgnlabs/mrgn-ui";
+import { PublicKey } from "@solana/web3.js";
 import { capture } from "@mrgnlabs/mrgn-utils";
+
 import { useMrgnlendStore } from "~/store";
+import { WalletToken } from "~/types";
 
 import { PageHeading } from "~/components/common/PageHeading";
-
 import { Loader } from "~/components/ui/loader";
 import { useWallet } from "~/components/wallet-v2";
 
@@ -13,7 +16,24 @@ export default function DepositSwapPage() {
     state.initialized,
     state.extendedBankInfosWithoutStakedAssets,
   ]);
-  const { connected } = useWallet();
+  const { connected, wallet } = useWallet();
+  const [walletTokens, setWalletTokens] = React.useState<WalletToken[] | null>(null);
+
+  React.useEffect(() => {
+    const fetchWalletTokens = async (wallet: PublicKey) => {
+      console.log("fetching wallet tokens", wallet.toBase58());
+      const response = await fetch(`/api/user/wallet?wallet=${wallet.toBase58()}`);
+      const data = await response.json();
+      setWalletTokens(data);
+
+      console.log("Users wallet tokens", data);
+    };
+
+    // walletTokens is null on load and set to empty array when fetching
+    if (!connected || !wallet || walletTokens !== null) return;
+    fetchWalletTokens(wallet.publicKey);
+    setWalletTokens([]);
+  }, [connected, wallet, walletTokens]);
 
   return (
     <>
