a9b476ce6e2fc088b2f5ed326ef6ef75fed450d4
chambaz <adammchambers@gmail.com>
2025-02-10 15:57:17 -0500

    feat: rework asset list nav / search bar

--- apps/marginfi-v2-ui/src/components/desktop/AssetList/components/AssetListNav.tsx
+++ apps/marginfi-v2-ui/src/components/desktop/AssetList/components/AssetListNav.tsx
@@ -12,6 +12,7 @@ import { Label } from "~/components/ui/label";
 import { Switch } from "~/components/ui/switch";
 import { Input } from "~/components/ui/input";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/components/ui/tooltip";
+import { Button } from "~/components/ui/button";
 
 const AssetListNav = () => {
   const [poolFilter, setPoolFilter, lendingMode, setLendingMode, assetListSearch, setAssetListSearch] = useUiStore(
@@ -24,6 +25,7 @@ const AssetListNav = () => {
       state.setAssetListSearch,
     ]
   );
+  const [isSearchExpanded, setIsSearchExpanded] = React.useState(false);
 
   const switchComponent = (
     <Switch
@@ -37,9 +39,20 @@ const AssetListNav = () => {
   );
 
   return (
-    <div className="space-y-6 relative">
-      <div className="flex justify-between items-center gap-8 px-2">
-        <div className="flex items-center gap-3">
+    <div className="relative">
+      <div className="flex justify-between items-center bg-background-gray p-2 rounded-lg">
+        <ToggleGroup type="single" value={poolFilter} onValueChange={setPoolFilter} className="shrink-0">
+          <ToggleGroupItem value="global" aria-label="Toggle global">
+            Global
+          </ToggleGroupItem>
+          <ToggleGroupItem value="isolated" aria-label="Toggle isolated">
+            Isolated
+          </ToggleGroupItem>
+          <ToggleGroupItem value="native_stake" aria-label="Toggle staked" className="relative">
+            ✨ Native Stake
+          </ToggleGroupItem>
+        </ToggleGroup>
+        <div className="flex items-center gap-3 ml-10">
           <Label
             className={cn(
               "text-muted-foreground/60 hover:text-foreground/50 cursor-pointer transition-colors",
@@ -77,37 +90,39 @@ const AssetListNav = () => {
             Borrow
           </Label>
         </div>
-      </div>
-      <div className="flex justify-between items-center gap-8 bg-background-gray p-2 rounded-lg">
-        <ToggleGroup type="single" value={poolFilter} onValueChange={setPoolFilter} className="shrink-0">
-          <ToggleGroupItem value="global" aria-label="Toggle global">
-            Global
-          </ToggleGroupItem>
-          <ToggleGroupItem value="isolated" aria-label="Toggle isolated">
-            Isolated
-          </ToggleGroupItem>
-          <ToggleGroupItem value="native_stake" aria-label="Toggle staked" className="relative">
-            ✨ Native Stake
-          </ToggleGroupItem>
-        </ToggleGroup>
-        <div className="relative w-full text-muted-foreground max-w-[320px] ml-auto">
-          <IconSearch size={15} className="absolute top-2.5 left-4" />
-          <Input
-            placeholder="Search assets"
-            className="py-1.5 h-auto px-10 w-full rounded-full border-background-gray-hover transition-colors focus:text-primary/70 focus-visible:ring-primary/50"
-            value={assetListSearch}
-            onChange={(e) => {
-              setAssetListSearch(e.target.value);
-            }}
-          />
-          <IconX
-            size={15}
-            className={cn(
-              "absolute top-2.5 right-3 cursor-pointer opacity-0 transition-opacity hover:opacity-100",
-              assetListSearch.length && "opacity-50"
-            )}
-            onClick={() => setAssetListSearch("")}
-          />
+        <div className={cn("relative text-muted-foreground ml-auto mr-4", isSearchExpanded && "w-full ml-16")}>
+          <Button
+            variant="ghost"
+            size="icon"
+            className={cn("flex rounded-md", isSearchExpanded && "hidden")}
+            onClick={() => setIsSearchExpanded(!isSearchExpanded)}
+          >
+            <IconSearch size={15} />
+          </Button>
+          <div className={cn("hidden", isSearchExpanded && "block")}>
+            <IconSearch size={15} className="absolute top-3 left-4" />
+            <Input
+              placeholder="Search assets"
+              className="py-2 h-auto px-10 w-full rounded-full border-background-gray-hover transition-colors focus:text-primary/70 focus-visible:ring-primary/50"
+              value={assetListSearch}
+              onChange={(e) => {
+                setAssetListSearch(e.target.value);
+              }}
+              onBlur={() => {
+                if (assetListSearch.length === 0) {
+                  setIsSearchExpanded(false);
+                }
+              }}
+            />
+            <IconX
+              size={15}
+              className="absolute top-3 right-3 cursor-pointer"
+              onClick={() => {
+                setAssetListSearch("");
+                setIsSearchExpanded(false);
+              }}
+            />
+          </div>
         </div>
         <Select>
           <SelectTrigger className="md:w-[180px] shrink-0">
