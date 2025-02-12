b136ddbd41c5b16078c347f9510d4c9e84254581
borcherd <borcherd@me.com>
2025-02-12 18:09:34 -0300

    feat: sonnes toast implementation for multisteptoast

--- packages/mrgn-toasts/src/components/toasts/error-toast/error-toast.tsx
+++ packages/mrgn-toasts/src/components/toasts/error-toast/error-toast.tsx
@@ -2,27 +2,20 @@ import { IconX } from "@tabler/icons-react";
 
 interface ErrorToastProps {
   title: string;
-  message: string;
-  description?: string;
+  description: string;
   code?: number;
 }
 
-export function ErrorToast({ title, message, description, code }: ErrorToastProps) {
+export function ErrorToast({ title, description, code }: ErrorToastProps) {
   return (
     <div className="w-full h-full rounded-md z-50 md:min-w-[340px]">
       <h2 className="text-lg mb-5 font-medium">{title}</h2>
-      <div className="space-y-2.5">
-        <div className="text-muted-foreground text-sm">
-          <div className="flex items-start space-x-2">
-            <div className="flex items-center space-x-2 md:w-max">
-              <IconX size={16} className="flex-shrink-0 text-mrgn-error" />
-              <span className="text-error">{message}</span>
-            </div>
-          </div>
+      <div className="flex justify-between items-start">
+        <div className="flex items-center space-x-2">
+          <IconX size={16} className="text-mrgn-error flex-shrink-0" />
+          <span className="text-error text-sm leading-snug">{description}</span>
         </div>
-        {description && <div className="py-3 px-6 text-xs max-w-xs text-muted-foreground">{description}</div>}
-
-        {code && <small className="text-primary/50 absolute top-8 right-3 text-[10px]">Code {code}</small>}
+        {code && <small className="text-primary/50 text-xs pr-2">Code {code}</small>}
       </div>
     </div>
   );
