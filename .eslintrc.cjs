1734f78bd5df7d311f79fba5837a2d29c4bb8b86
Kobe <leenderskobe@gmail.com>
2025-02-19 16:41:02 +0900

    fix: eslint & hoisting

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ["next", "prettier"], // Next.js + Prettier
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  settings: {
+    next: {
+      rootDir: ["apps/marginfi-v2-ui/", "apps/marginfi-v2-trading/"],
+    },
+  },
+};
