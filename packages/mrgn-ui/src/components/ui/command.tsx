6c512c44b0128e22c3fdd4418abecd4f13448f7e
chambaz <adammchambers@gmail.com>
2025-03-05 20:45:30 -0500

    feat: search pools dialog v2 (checkpoint)

--- packages/mrgn-ui/src/components/ui/command.tsx
+++ packages/mrgn-ui/src/components/ui/command.tsx
@@ -49,7 +49,7 @@ const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input> & CommandInputProps
 >(({ className, wrapperClassName = "", ...props }, ref) => (
-  <div className={cn("items-center px-3 flex", wrapperClassName)} cmdk-input-wrapper="">
+  <div className={cn("items-center px-3 flex border rounded-md", wrapperClassName)} cmdk-input-wrapper="">
     <IconSearch size={16} className="mr-2 shrink-0 opacity-50" />
     <CommandPrimitive.Input
       ref={ref}
