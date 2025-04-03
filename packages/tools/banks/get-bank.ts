dee7df22bbc30fc682b40481e954f0ec1654efd9
chambaz <adammchambers@gmail.com>
2025-04-03 17:04:49 -0400

    chore: switch from oracles to birdeye (wip)

--- packages/tools/banks/get-bank.ts
+++ packages/tools/banks/get-bank.ts
@@ -36,7 +36,7 @@ async function main() {
     .parseSync();
 
   const program = getMarginfiProgram(argv.env as Environment);
-  const bankMetadata = await getBankMetadata();
+  const bankMetadata = await getBankMetadata(argv.env as Environment);
 
   let bankPubkey: PublicKey;
   if (argv.address) {
