e8c51200e63a2fa04a614de679837784ed7297f5
chambaz <adammchambers@gmail.com>
2025-04-25 22:01:35 -0400

    feat: interest earned

--- apps/marginfi-v2-ui/src/components/common/Portfolio/components/user-stats/portfolio-user-stats.tsx
+++ apps/marginfi-v2-ui/src/components/common/Portfolio/components/user-stats/portfolio-user-stats.tsx
@@ -1,4 +1,4 @@
-import { IconClockHour4 } from "@tabler/icons-react";
+import { IconInfoCircle } from "@tabler/icons-react";
 
 interface PortfolioUserStatsProps {
   supplied: string;
@@ -14,18 +14,19 @@ export const PortfolioUserStats = ({ supplied, borrowed, netValue }: PortfolioUs
       <Stat label="Borrowed" value={borrowed} />
       <Stat label="Net value" value={netValue} />
       <Stat
-        label="Interest earned"
-        value={
-          <span className="text-xs font-light flex w-full gap-1.5 items-center mt-1">
-            <IconClockHour4 size={14} /> Coming soon
-          </span>
+        label={
+          <div className="flex items-center gap-1">
+            <IconInfoCircle size={16} />
+            Interest earned
+          </div>
         }
+        value={<span className="text-mrgn-success text-right w-full block">+$2.56</span>}
       />
     </div>
   );
 };
 
-const Stat = ({ label, value }: { label: string; value: JSX.Element | string }) => (
+const Stat = ({ label, value }: { label: JSX.Element | string; value: JSX.Element | string }) => (
   <dl className="w-1/2 md:w-auto">
     <dt className="text-sm">{label}</dt>
     <dd className="text-xl font-medium text-white">{value}</dd>
