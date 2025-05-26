588af74abd0728572f280099bcc71af75074f8c2
chambaz <adammchambers@gmail.com>
2025-05-25 21:14:20 -0400

    feat: custom looper icon / updated looper header

--- apps/marginfi-v2-ui/src/pages/looper.tsx
+++ apps/marginfi-v2-ui/src/pages/looper.tsx
@@ -6,7 +6,7 @@ import { useMrgnlendStore } from "~/store";
 import { PageHeading } from "~/components/common/PageHeading";
 import { EmodeExplore } from "~/components/common/emode/components";
 import { Loader } from "~/components/ui/loader";
-import { IconEmode } from "~/components/ui/icons";
+import { IconEmode, IconLooper } from "~/components/ui/icons";
 
 export default function LooperPage() {
   const [initialized, extendedBankInfosWithoutStakedAssets, extendedBankInfos] = useMrgnlendStore((state) => [
@@ -21,23 +21,28 @@ export default function LooperPage() {
       {!initialized && <Loader label="Loading looper..." className="mt-16" />}
 
       {initialized && (
-        <div className="w-full max-w-7xl mx-auto mb-20 px-5">
+        <div className="w-full max-w-7xl mx-auto mb-20 px-2 md:px-5">
           <PageHeading
-            heading="Looper ➰"
+            heading={
+              <div className="flex items-center justify-center gap-2.5">
+                Looper
+                <IconLooper size={52} />
+              </div>
+            }
             body={
-              <div className="flex flex-col items-center">
-                <p>Leverage your deposits to maximize yield.</p>
-                <div className="flex items-center gap-2">
-                  <div className="inline-flex items-center gap-1 text-mfi-emode">
-                    <IconEmode size={16} className="translate-y-px" /> e-mode
-                  </div>
-                  <p>
-                    pairs enable increased leverage,{" "}
-                    <EmodeExplore
-                      trigger={<button className="border-b border-foreground/50">explore e-mode pairings</button>}
-                    />
-                  </p>
-                </div>
+              <div className="flex flex-col items-center -translate-y-1">
+                <p>
+                  Leverage your deposits to maximize yield.
+                  <br />
+                  <IconEmode size={24} className="inline-block" /> e-mode pairs increase leverage.{" "}
+                  <EmodeExplore
+                    trigger={
+                      <button className="border-b border-foreground/50 leading-none transition-colors hover:border-transparent">
+                        explore e-mode pairs
+                      </button>
+                    }
+                  />
+                </p>
               </div>
             }
           />
