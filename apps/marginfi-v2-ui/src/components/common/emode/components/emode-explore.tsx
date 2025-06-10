b098f88ae590db16cb14acc0521943d58e6a0cea
borcherd <borcherd@me.com>
2025-06-10 17:21:25 +0200

    feat: dialog/drawer on emode explore and strats

--- apps/marginfi-v2-ui/src/components/common/emode/components/emode-explore.tsx
+++ apps/marginfi-v2-ui/src/components/common/emode/components/emode-explore.tsx
@@ -16,6 +16,15 @@ import {
 } from "~/components/ui/dialog";
 import Link from "next/link";
 import { EmodeTable } from "./emode-table";
+import { Desktop, Mobile } from "~/mediaQueryUtils";
+import {
+  DrawerContent,
+  DrawerTrigger,
+  Drawer,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerDescription,
+} from "~/components/ui/drawer";
 
 interface EmodeExploreProps {
   trigger?: React.ReactNode;
@@ -32,40 +41,77 @@ const EmodeExplore = ({ trigger, initialBank, emodeTag }: EmodeExploreProps) => 
   );
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>{trigger || defaultTrigger}</DialogTrigger>
-      <DialogContent
-        className="overflow-visible p-2 md:p-6 md:py-8 md:max-w-2xl"
-        closeClassName="md:-top-8 md:-right-8 md:z-50"
-      >
-        <DialogHeader className="space-y-3">
-          <DialogTitle className="text-3xl font-normal flex items-center gap-2">
-            Explore{" "}
-            <div className="flex items-center gap-1">
-              <IconEmodeSimple size={32} />
-              e-mode
-            </div>
-            {emodeTag && <span className="lowercase"> {EmodeTag[emodeTag]}</span>}
-            pairs
-          </DialogTitle>
-          <DialogDescription>
-            View e-mode pairs and boosted weights.
-            <br />
-            For more information{" "}
-            <Link
-              href="https://docs.marginfi.com/emode"
-              target="_blank"
-              rel="noreferrer"
-              className="border-b border-foreground/50 transition-colors hover:border-transparent"
-            >
-              read the documentation
-            </Link>
-            .
-          </DialogDescription>
-        </DialogHeader>
-        <EmodeTable initialBank={initialBank} />
-      </DialogContent>
-    </Dialog>
+    <>
+      <Mobile>
+        <Drawer>
+          <DrawerTrigger asChild>{trigger || defaultTrigger}</DrawerTrigger>
+          <DrawerContent className="overflow-visible p-2 pb-6 md:p-6 md:py-8 md:max-w-2xl">
+            <DrawerHeader className="space-y-3">
+              <DrawerTitle className="text-3xl font-normal flex items-center justify-center gap-2">
+                Explore{" "}
+                <div className="flex items-center gap-1">
+                  <IconEmodeSimple size={32} />
+                  e-mode
+                </div>
+                {emodeTag && <span className="lowercase"> {EmodeTag[emodeTag]}</span>}
+                pairs
+              </DrawerTitle>
+              <DrawerDescription>
+                View e-mode pairs and boosted weights.
+                <br />
+                For more information{" "}
+                <Link
+                  href="https://docs.marginfi.com/emode"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="border-b border-foreground/50 transition-colors hover:border-transparent"
+                >
+                  read the documentation
+                </Link>
+                .
+              </DrawerDescription>
+            </DrawerHeader>
+            <EmodeTable initialBank={initialBank} />
+          </DrawerContent>
+        </Drawer>
+      </Mobile>
+      <Desktop>
+        <Dialog>
+          <DialogTrigger asChild>{trigger || defaultTrigger}</DialogTrigger>
+          <DialogContent
+            className="overflow-visible p-2 md:p-6 md:py-8 md:max-w-2xl"
+            closeClassName="md:-top-8 md:-right-8 md:z-50"
+          >
+            <DialogHeader className="space-y-3">
+              <DialogTitle className="text-3xl font-normal flex items-center gap-2">
+                Explore{" "}
+                <div className="flex items-center gap-1">
+                  <IconEmodeSimple size={32} />
+                  e-mode
+                </div>
+                {emodeTag && <span className="lowercase"> {EmodeTag[emodeTag]}</span>}
+                pairs
+              </DialogTitle>
+              <DialogDescription>
+                View e-mode pairs and boosted weights.
+                <br />
+                For more information{" "}
+                <Link
+                  href="https://docs.marginfi.com/emode"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="border-b border-foreground/50 transition-colors hover:border-transparent"
+                >
+                  read the documentation
+                </Link>
+                .
+              </DialogDescription>
+            </DialogHeader>
+            <EmodeTable initialBank={initialBank} />
+          </DialogContent>
+        </Dialog>
+      </Desktop>
+    </>
   );
 };
 
--- apps/marginfi-v2-ui/src/components/common/emode/components/emode-strategies.tsx
+++ apps/marginfi-v2-ui/src/components/common/emode/components/emode-strategies.tsx
@@ -16,6 +16,15 @@ import {
 import { Button } from "~/components/ui/button";
 import { IconEmodeSimple } from "~/components/ui/icons";
 import { Table, TableBody, TableCell, TableRow } from "~/components/ui/table";
+import { Desktop, Mobile } from "~/mediaQueryUtils";
+import {
+  DrawerContent,
+  DrawerTrigger,
+  Drawer,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerDescription,
+} from "~/components/ui/drawer";
 
 import { getEmodeStrategies } from "~/emode.utils";
 import { ActionBox, useWallet } from "~/components";
@@ -113,83 +122,114 @@ const EmodeStrategies = ({ extendedBankInfos }: EmodeStrategiesProps) => {
     return strategies;
   }, [extendedBankInfos]);
 
+  const strategyTrigger = (
+    <Button variant="secondary" size="sm" className="bg-gradient-to-r to-[#483975] from-[#292E32]">
+      <IconSparkles size={18} /> e-mode strategies
+    </Button>
+  );
+
+  const strategiesTable = (
+    <div className="w-full">
+      <Table>
+        <TableBody>
+          {emodeStrategies.map((strategy) => {
+            return (
+              <TableRow key={strategy.symbol} className="even:bg-accent">
+                <TableCell className="p-3">
+                  <div className="flex items-center gap-2.5">
+                    <Image
+                      src={strategy.icon}
+                      alt={strategy.symbol}
+                      width={32}
+                      height={32}
+                      className="rounded-full"
+                    />
+                    {strategy.symbol}
+                  </div>
+                </TableCell>
+                <TableCell className="p-3">
+                  <p className="text-xs">{strategy.description}</p>
+                </TableCell>
+                <TableCell className="text-right p-3">
+                  <ActionBox.Lend
+                    useProvider={true}
+                    lendProps={{
+                      requestedLendType: strategy.action,
+                      requestedBank: strategy.bank,
+                      walletContextState: walletContextState,
+                      connected: connected,
+                      stakeAccounts: [],
+                      captureEvent: (event, properties) => {
+                        capture(event, properties);
+                      },
+                      onComplete: () => {
+                        fetchMrgnlendState();
+                      },
+                    }}
+                    isDialog={true}
+                    dialogProps={{
+                      trigger: (
+                        <Button size="sm" className="w-full">
+                          {strategy.action}
+                        </Button>
+                      ),
+                      title: `${strategy.action} ${strategy.bank?.meta.tokenSymbol}`,
+                    }}
+                  />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
+
   return emodeStrategies.length > 0 ? (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="secondary" size="sm" className="bg-gradient-to-r to-[#483975] from-[#292E32]">
-          <IconSparkles size={18} /> e-mode strategies
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="text-2xl font-normal flex items-center gap-2">
-            Explore{" "}
-            <div className="flex items-center gap-1">
-              <IconEmodeSimple size={32} />
-              e-mode
-            </div>
-            strategies
-          </DialogTitle>
-          <DialogDescription className="text-sm">
-            There are strategies available to help you maximize your e-mode returns.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="w-full">
-          <Table>
-            <TableBody>
-              {emodeStrategies.map((strategy) => {
-                return (
-                  <TableRow key={strategy.symbol} className="even:bg-accent">
-                    <TableCell className="p-3">
-                      <div className="flex items-center gap-2.5">
-                        <Image
-                          src={strategy.icon}
-                          alt={strategy.symbol}
-                          width={32}
-                          height={32}
-                          className="rounded-full"
-                        />
-                        {strategy.symbol}
-                      </div>
-                    </TableCell>
-                    <TableCell className="p-3">
-                      <p className="text-xs">{strategy.description}</p>
-                    </TableCell>
-                    <TableCell className="text-right p-3">
-                      <ActionBox.Lend
-                        useProvider={true}
-                        lendProps={{
-                          requestedLendType: strategy.action,
-                          requestedBank: strategy.bank,
-                          walletContextState: walletContextState,
-                          connected: connected,
-                          stakeAccounts: [],
-                          captureEvent: (event, properties) => {
-                            capture(event, properties);
-                          },
-                          onComplete: () => {
-                            fetchMrgnlendState();
-                          },
-                        }}
-                        isDialog={true}
-                        dialogProps={{
-                          trigger: (
-                            <Button size="sm" className="w-full">
-                              {strategy.action}
-                            </Button>
-                          ),
-                          title: `${strategy.action} ${strategy.bank?.meta.tokenSymbol}`,
-                        }}
-                      />
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <Mobile>
+        <Drawer>
+          <DrawerTrigger asChild>{strategyTrigger}</DrawerTrigger>
+          <DrawerContent className="overflow-visible p-2 pb-6 md:p-6 md:py-8">
+            <DrawerHeader className="space-y-3">
+              <DrawerTitle className="text-2xl font-normal flex items-center justify-center gap-2">
+                Explore{" "}
+                <div className="flex items-center gap-1">
+                  <IconEmodeSimple size={32} />
+                  e-mode
+                </div>
+                strategies
+              </DrawerTitle>
+              <DrawerDescription>
+                There are strategies available to help you maximize your e-mode returns.
+              </DrawerDescription>
+            </DrawerHeader>
+            {strategiesTable}
+          </DrawerContent>
+        </Drawer>
+      </Mobile>
+      <Desktop>
+        <Dialog>
+          <DialogTrigger asChild>{strategyTrigger}</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-2xl font-normal flex items-center gap-2">
+                Explore{" "}
+                <div className="flex items-center gap-1">
+                  <IconEmodeSimple size={32} />
+                  e-mode
+                </div>
+                strategies
+              </DialogTitle>
+              <DialogDescription className="text-sm">
+                There are strategies available to help you maximize your e-mode returns.
+              </DialogDescription>
+            </DialogHeader>
+            {strategiesTable}
+          </DialogContent>
+        </Dialog>
+      </Desktop>
+    </>
   ) : (
     <></>
   );
